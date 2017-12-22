90844b4b9241f068a910d9b36b654f0eec0512f4
totutotu <tuomo.torppa@gmail.com>
2017-12-22 12:16:21 +0200

    updater

--- src/services/database_updater/oodi_interface.js
+++ src/services/database_updater/oodi_interface.js
@@ -1,14 +1,11 @@
 const axios = require('axios')
 require('dotenv').config()
+const base_url = process.env.OODI_ADDR
 
 axios.defaults.auth = {
   username: 'tktl',
   password: process.env.OODI_PW
 }
-
-
-
-const base_url = process.env.OODI_ADDR
 
 function getStudent(studentNumber) {
   axios.get(base_url + '/students/' + studentNumber)
@@ -23,15 +20,6 @@ function getStudent(studentNumber) {
 
 }
 
-// String getTeacherDetails(String courseCode, String date)
-
-// String queryUrl(String url)
-
-// List < String > getStudentNumbers()
-
-// Student getStudent(String studentNumber)
-
-// returns a list og studyright ids
 function getStudentStudyRights(studentNumber) {
   axios.get(base_url + '/students/' + studentNumber + '/studyrights')
     .then(response => {
@@ -44,15 +32,37 @@ function getStudentStudyRights(studentNumber) {
     })
 
 }
+
+function getStudyRight(studyRightId) {
+  axios.get(base_url + '/studyrights/' + studyRightId)
+    .then(response => {
+      console.log(response)
+      return response
+    })
+    .catch(error => {
+      console.log('error')
+    })
+}
+
+// String getTeacherDetails(String courseCode, String date)
+
+// String queryUrl(String url)
+
+// List < String > getStudentNumbers()
+
+// Student getStudent(String studentNumber)
+
+// returns a list og studyright ids
+
 // 
 // Organization getOrganization(String organizationId)
 // 
 // List < Credit > getStudentCourseCredits(Student student)
 // 
-// StudyRight getStudyRight(String studyRightId)
+
 // 
 // List < String > getStudyRightIdStrings(String data)
 
 module.exports = {
-  getStudentStudyRights, getStudent
+  getStudentStudyRights, getStudent, getStudyRight
 }
