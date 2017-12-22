59c444f12e688680125dabd9459f6c7d5e9599ad
Tiainen <mitiaine@lm7-cs-18.cs.helsinki.fi>
2017-12-22 11:20:30 +0200

    created folder for database updating

---
+++ src/services/database_updater/database_updater.js
@@ -0,0 +1,53 @@
+//const Sequelize = require('sequelize')
+//const { Studyright, Student, Credit, CourseInstance, Course, TagStudent, sequelize } = require('../models')
+//const { formatStudent } = require('../services/students')
+//const Op = Sequelize.Op
+const axios = require('axios')
+
+const url = 'https://oprek4.it.helsinki.fi:30023/students/013737399'
+axios.get(url, {
+  auth: {
+    username: 'tktl',
+    password: process.env.OODI_PW
+  }
+}
+)
+  .then(response => {
+    console.log(response)
+  })
+  .catch(error => {
+    console.log(error)
+  })
+/*
+let minStudentNumber = 1000000
+let maxStudentNumber = 1500000
+
+// add possibility to update specific students ?
+console.log('Running updater on ' + minStudentNumber + '-' + maxStudentNumber)
+
+for (let i = minStudentNumber; i < maxStudentNumber; i++) {
+  let studentNumber = '0' + i + getStudentNumberChecksum(i.toString)
+  console.log('Updating student ' + studentNumber)
+
+  updateStudentInformation(studentNumber)
+}
+
+/**
+ * Calculate checksum of student number.
+ *
+ * @param studentNumber Student number without the checksum (last digit).
+ * @return Checksum digit.
+ 
+function getStudentNumberChecksum(studentNumber) {
+  let checksumNumbers = [7, 3, 1]
+  let checksum = 0
+
+  for (let i = 0; i < studentNumber.length; i++) {
+    // go from end to start
+    let currentNumber = studentNumber.charAt(studentNumber.length - (i + 1))
+    checksum += currentNumber * (checksumNumbers[i % checksumNumbers.length])
+  }
+
+  return (10 - (checksum % 10)) % 10
+}
+*/
---
+++ src/services/database_updater/oodi_interface.js
@@ -0,0 +1,17 @@
+String getTeacherDetails(String courseCode, String date)
+
+String queryUrl(String url)
+
+List<String> getStudentNumbers()
+
+Student getStudent(String studentNumber)
+
+List<StudyRight> getStudentStudyRights(Student student)
+
+Organization getOrganization(String organizationId)
+
+List<Credit> getStudentCourseCredits(Student student)
+
+StudyRight getStudyRight(String studyRightId)
+
+List<String> getStudyRightIdStrings(String data)
