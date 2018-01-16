f9ed86fec70d4e4feb330dd003e491d69e69e76a
eero3 <eero.siren@kolumbus.fi>
2018-01-16 11:36:03 +0200

    ran lintix

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,24 @@
 import {
-    getDepartmentSuccess,
-    findStudents
-} from "../api";
+  getDepartmentSuccess,
+  findStudents
+} from '../api';
 
 export const ADD_ERROR = 'ADD_ERROR';
 export const GET_DEPARTMENT_SUCCESS = 'GET_DEPARTMENT_SUCCESS';
 export const FIND_STUDENTS = 'FIND_STUDENTS';
 
 export const addError = errorJson => ({
-    type: ADD_ERROR,
-    errorJson
+  type: ADD_ERROR,
+  errorJson
 });
 
 export const getDepartmentSuccessAction = date => ({
-    type: GET_DEPARTMENT_SUCCESS,
-    payload: getDepartmentSuccess(date)
+  type: GET_DEPARTMENT_SUCCESS,
+  payload: getDepartmentSuccess(date)
 });
 
 export const findStudentsAction = searchStr => ({
-   type: FIND_STUDENTS,
-   payload: findStudents(searchStr)
+  type: FIND_STUDENTS,
+  payload: findStudents(searchStr)
 });
 
--- src/components/StudentStatistics/index.jsx
+++ src/components/StudentStatistics/index.jsx
@@ -1,43 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {addError, findStudentsAction} from "../../actions";
+import { addError, findStudentsAction } from '../../actions';
 
 class StudentStatistics extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {
 
-        };
-    }
+    };
+  }
 
-    componentDidMount() {
-        /* TODO: test purpose onlys  */
-        const defaultSearch = 'ee';
-        this.props.dispatchFindStudents(defaultSearch)
-            .then(json => this.setState({students: json}),
-                err => this.props.dispatchFindStudents(err));
+  componentDidMount() {
+    /* TODO: test purpose onlys  */
+    const defaultSearch = 'ee';
+    this.props.dispatchFindStudents(defaultSearch)
+      .then(
+        json => this.setState({ students: json }),
+        err => this.props.dispatchFindStudents(err)
+      );
+  }
 
-    }
-
-    render() {
-        return (
-            <div>
-                {JSON.stringify(this.state.students)}
-            </div>
-        );
-    }
-
-
+  render() {
+    return (
+      <div>
+        {JSON.stringify(this.state.students)}
+      </div>
+    );
+  }
 }
 
 
 const mapStateToProps = () => ({});
 
 const mapDispatchToProps = dispatch => ({
-    dispatchFindStudents: searchStr =>
-        dispatch(findStudentsAction(searchStr)),
-    dispatchAddError: err => dispatch(addError(err))
+  dispatchFindStudents: searchStr =>
+    dispatch(findStudentsAction(searchStr)),
+  dispatchAddError: err => dispatch(addError(err))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudentStatistics);
