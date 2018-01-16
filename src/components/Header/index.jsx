799e703750a424196f087d43950a3bceca7d2855
eero3 <eero.siren@kolumbus.fi>
2018-01-16 15:32:13 +0200

    added navigation button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 import styles from './header.css';
 
+import { routes } from '../../constants';
+import NavigationButton from '../NavigationButton';
+
 const Header = () =>
   (
     <header className={styles.header} role="banner">
       <a className={styles.logo} href="/">
-        <h1 className={styles.logoText}>oodikone pt2 header</h1>
+        <h1 className={styles.logoText}>oodikone</h1>
       </a>
+      <div>
+        {
+              Object.keys(routes).map(key =>
+                <NavigationButton key={key} route={routes[key].route} text={routes[key].text} />)
+          }
+      </div>
+
+
     </header>
   );
 
