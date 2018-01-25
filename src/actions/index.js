50023d4ab5cea04b8acf0d1ad2bde60149556930
eero3 <eero.siren@kolumbus.fi>
2018-01-25 15:56:44 +0200

    - added search for courses

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,13 @@
 import {
   getDepartmentSuccess,
-  findStudents
+  findStudents,
+  findCoursesByName
 } from '../api';
 
 export const ADD_ERROR = 'ADD_ERROR';
 export const GET_DEPARTMENT_SUCCESS = 'GET_DEPARTMENT_SUCCESS';
 export const FIND_STUDENTS = 'FIND_STUDENTS';
+export const FIND_COURSES = 'FIND_COURSES';
 
 export const addError = errorJson => ({
   type: ADD_ERROR,
@@ -20,5 +22,10 @@ export const getDepartmentSuccessAction = date => ({
 export const findStudentsAction = searchStr => ({
   type: FIND_STUDENTS,
   payload: findStudents(searchStr)
+});
+
+export const findCoursesAction = searchStr => ({
+  type: FIND_COURSES,
+  payload: findCoursesByName(searchStr)
 });
 
