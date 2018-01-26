2d22e8797387c9f5c65452674b56e8993e208716
eero3 <eero.siren@kolumbus.fi>
2018-01-26 14:03:08 +0200

    - created dynamic search result component

--- src/components/StudentStatistics/index.jsx
+++ src/components/StudentStatistics/index.jsx
@@ -2,45 +2,98 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getTranslate, getActiveLanguage } from 'react-localize-redux';
+import { Search } from 'semantic-ui-react';
 
 import { addError, findStudentsAction } from '../../actions';
 
 import styles from './studentStatistics.css';
+import SearchResultTable from '../SearchResultTable';
+
+const { func } = PropTypes;
+
+const DEFAULT_STATE = {
+  students: [],
+  isLoading: false,
+  searchStr: ''
+};
 
 class StudentStatistics extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.resetComponent = this.resetComponent.bind(this);
+    this.handleResultSelect = this.handleResultSelect.bind(this);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.fetchStudentList = this.fetchStudentList.bind(this);
 
-    };
+    this.state = DEFAULT_STATE;
   }
 
-  componentDidMount() {
-    /* TODO: test purpose onlys  */
-    const defaultSearch = 'ee';
-    this.props.dispatchFindStudents(defaultSearch)
+  resetComponent() {
+    this.setState(DEFAULT_STATE);
+  }
+
+  handleResultSelect(e, value) {
+    console.log(value);
+  }
+
+
+  handleSearchChange(e, { value }) {
+    if (value.length > 0) {
+      this.setState({ searchStr: value });
+      this.fetchStudentList();
+    } else {
+      this.resetComponent();
+    }
+  }
+
+  fetchStudentList() {
+    const { searchStr } = this.state;
+    this.setState({ isLoading: true });
+    this.props.dispatchFindStudents(searchStr)
       .then(
-        json => this.setState({ students: json }),
-        err => this.props.dispatchFindStudents(err)
+        json => this.setState({ students: json.value, isLoading: false }),
+        err => this.props.dispatchAddError(err)
       );
   }
 
   render() {
+    const { isLoading, students, searchStr } = this.state;
+
     const t = this.props.translate;
+    const headers = [
+      t('studentStatistics.studentNumber'),
+      t('studentStatistics.started'),
+      t('studentStatistics.credits')
+    ];
+    const rows = students.map(({ studentNumber, started, credits }) =>
+      ({ studentNumber, started, credits }));
     return (
-      <div className={styles.example}>
+      <div className={styles.container}>
+        <Search
+          className={styles.studentSearch}
+          input={{ fluid: true }}
+          loading={isLoading}
+          onSearchChange={this.handleSearchChange}
+          showNoResults={false}
+          value={searchStr}
+        />
+        <SearchResultTable
+          headers={headers}
+          rows={rows}
+          rowClickFn={this.handleResultSelect}
+        />
         <div>{`Students: ${t('common.example')}`}</div>
-        {JSON.stringify(this.state.students)}
+        {isLoading}
+        {JSON.stringify(students)}
       </div>
     );
   }
 }
 
-const { func } = PropTypes;
-
 StudentStatistics.propTypes = {
   dispatchFindStudents: func.isRequired,
+  dispatchAddError: func.isRequired,
   translate: func.isRequired
 };
 
