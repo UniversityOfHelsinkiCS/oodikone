6e7474709df97a9ce7e2cc8464bc85ebf11225e7
eero3 <eero.siren@kolumbus.fi>
2018-02-01 13:11:13 +0200

    optimized student credit accumulation graph a bit added title to chart editet student styles

--- src/components/CreditAccumulationGraph/index.jsx
+++ src/components/CreditAccumulationGraph/index.jsx
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import { ResponsiveContainer, LineChart, XAxis, YAxis, Legend, Line, Tooltip } from 'recharts';
 import _ from 'lodash';
 import moment from 'moment';
+import { Header, Segment } from 'semantic-ui-react';
 
 import styles from './creditAccumulationGraph.css';
 import { DISPLAY_DATE_FORMAT } from '../../constants';
 import { reformatDate } from '../../common';
+import { red, turquoise } from '../../styles/variables/colors';
 
 const getStudentData = (courses) => {
   let totalCredits = 0;
@@ -27,7 +29,6 @@ const getCombinedChartData = (courses, startDate) => {
 
 
   const studentData = getStudentData(courses);
-
   const dates = {};
   let day = moment(startDate);
   let lastReferenceDay = moment(startDate);
@@ -38,55 +39,61 @@ const getCombinedChartData = (courses, startDate) => {
     if (day.isSame(moment(lastReferenceDay).add(6, 'month'))) {
       referenceCredits += 6;
       lastReferenceDay = day;
+      dates[displayDate] = { displayDate };
     }
 
-    if (Object.keys(dates).length === 0 || day.isSame(lastDate)) {
-      dates[displayDate] = { referenceCredits };
-    } else {
-      dates[displayDate] = { };
+    if (Object.keys(dates).length === 0) {
+      dates[displayDate] = { displayDate, referenceCredits };
     }
-
 
     const index = studentData.findIndex(course => course.date === displayDate);
 
     if (index !== -1) {
       const course = studentData[index];
+      dates[displayDate] = { displayDate };
       Object.assign(dates[course.date], dates[course.date], course);
     }
 
     day = moment(day).add(1, 'day');
   }
+  const finalDate = reformatDate(day, DISPLAY_DATE_FORMAT);
+  dates[finalDate] = { finalDate, referenceCredits };
 
   return [...Object.values(dates)];
 };
 
 const CreditAccumulationGraph = (props) => {
-  const { students } = props;
+  const { students, title } = props;
 
   const firstDate = moment(students[0].started);
 
   const chartData = getCombinedChartData(students[0].courses, firstDate);
+  _.sortBy(chartData, course => moment(course.displayDate));
 
   return (
     <div className={styles.graphContainer}>
-      <ResponsiveContainer height={400}>
-        <LineChart data={chartData}>
-          <XAxis dataKey="displayDate" />
-          <YAxis dataKey="referenceCredits" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="totalCredits" stroke="#435345" connectNulls />
-          <Line type="monotone" dot={false} dataKey="referenceCredits" stroke="#435345" connectNulls />
-        </LineChart>
-      </ResponsiveContainer>
+      <Header attached="top" size="large">{title}</Header>
+      <Segment attached="bottom">
+        <ResponsiveContainer height={400}>
+          <LineChart data={chartData}>
+            <XAxis dataKey="displayDate" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="totalCredits" stroke={red} connectNulls />
+            <Line type="monotone" dot={false} dataKey="referenceCredits" stroke={turquoise} connectNulls />
+          </LineChart>
+        </ResponsiveContainer>
+      </Segment>
     </div>
   );
 };
 
-const { arrayOf, object } = PropTypes;
+const { arrayOf, object, string } = PropTypes;
 
 CreditAccumulationGraph.propTypes = {
-  students: arrayOf(object).isRequired
+  students: arrayOf(object).isRequired,
+  title: string.isRequired
 };
 
 export default CreditAccumulationGraph;
