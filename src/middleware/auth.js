d13b1c50b4d26181f548026dea36f05e4e90b0cc
totutotu <tuomo.torppa@gmail.com>
2018-02-20 10:06:52 +0200

    Removed unnecessary comments

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,18 +1,17 @@
 module.exports.checkAuth = async (req, res, next) => {
   if (!req.session.user_id) { // if a user is not already logged in
-    console.log('no session user id')
     if (!req.headers['shib-session-id']) { //if a user has not just arrived with a valid shibsessionid 
       if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'dev') { //if env isn't test or dev
-        console.log('oh no , nothing found and not even in the right environment')
         res.status(401).end()
       }
     }
     req.session.user_id = req.headers['shib-session-id']
 
-    // do some logging in
+    // do some logging in, fetch person information and admin information
+    // from db and do whatever you feel like doing 
     next()
   } else {
-    console.log('user already logged in')
+    console.log('user already logged')
     next()
   }
 }
