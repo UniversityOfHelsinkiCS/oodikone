6a83d961715cf39d8031e6d916268c9ed5474e6f
Jami Kousa <jakousa@cs.helsinki.fi>
2018-02-27 11:07:41 +0200

    missing route

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,13 @@ const courses = require('./routes/courses')
 const department = require('./routes/department')
 const students = require('./routes/students')
 const tags = require('./routes/tags')
-
-const auth = require('./middleware/auth')
+const population = require('./routes/population')
 
 
 module.exports = (app, url) => {
-  app.use(auth.checkAuth)
   app.use(url, courses)
   app.use(url, department)
   app.use(url, students)
   app.use(url, tags)
+  app.use(url, population)
 }
