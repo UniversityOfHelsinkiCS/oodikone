6aa0291a195f4ac8beada4b15189b463013be7bc
eero3 <eero.siren@kolumbus.fi>
2018-03-02 14:10:14 +0200

    removed test env from auth middleware and cleaned js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,15 +7,11 @@ module.exports.checkAuth = async (req, res, next) => {
   const token = req.headers['x-access-token']
   if (token) {
     await jwt.verify(token, conf.TOKEN_SECRET, (err, decoded) => {
-      if (!err && // If no error
-        (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') || // environment is okay or
-        decoded.userId === req.headers['eduPersonPrincipalName'].split('@')[0]) // userId is okay, then
-      {
+      if (!err && isShibboUser(decoded.userId, req)) {
         req.decodedToken = decoded // everything is good, save to request for use in other routes
         next()
-      } else {
-        res.status(403).end()
       }
+      res.status(403).end()
     })
   } else {
     res.status(403).end()
