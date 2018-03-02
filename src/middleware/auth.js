53219da6ae9201ecb23a8f7864fb77c8e9d447d6
totutotu <tuomo.torppa@gmail.com>
2018-03-02 11:59:49 +0200

    Authentication fixed

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,14 +4,18 @@ const conf = require('../conf-backend')
 module.exports.checkAuth = async (req, res, next) => {
   const token = req.headers['x-access-token']
   if (token) {
-    jwt.verify(token, conf.TOKEN_SECRET, (err, decoded) => {
+    await jwt.verify(token, conf.TOKEN_SECRET, (err, decoded) => {
       if (!err && // If no error
-        (decoded.userId === req.headers['eduPersonPrincipalName'].split('@')[0] || // And userId or
-          process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')) { // environment is okay, then
+        (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') || // environment is okay or
+        decoded.userId === req.headers['eduPersonPrincipalName'].split('@')[0]) // userId is okay, then
+      {
         req.decodedToken = decoded // everything is good, save to request for use in other routes
-        return next()
+        next()
+      } else {
+        res.status(403).end()
       }
     })
+  } else {
+    res.status(403).end()
   }
-  return res.status(403).end()
 }
