5efd3be53491b4e9c4e62df21e53765ac1b02569
eero3 <eero.siren@kolumbus.fi>
2018-03-02 14:05:14 +0200

    fixed unresolved promise & removed test env from auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,17 +1,19 @@
 const jwt = require('jsonwebtoken')
 const conf = require('../conf-backend')
 
+const isShibboUser = (userId, req) => userId === req.headers['eduPersonPrincipalName'].split('@')[0]
+
 module.exports.checkAuth = async (req, res, next) => {
   const token = req.headers['x-access-token']
   if (token) {
     jwt.verify(token, conf.TOKEN_SECRET, (err, decoded) => {
-      if (!err && // If no error
-        (decoded.userId === req.headers['eduPersonPrincipalName'].split('@')[0] || // And userId or
-          process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')) { // environment is okay, then
+      if(!err && (isShibboUser(decoded.userId, req))) {
         req.decodedToken = decoded // everything is good, save to request for use in other routes
         return next()
       }
+      return res.status(403).end()
     })
+      .then(() => {})
   }
   return res.status(403).end()
 }
