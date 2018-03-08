59a60dbc560812540a69a0ca8efe82a48c73f5c9
Jami Kousa <jakousa@cs.helsinki.fi>
2018-03-08 12:24:56 +0200

    new get "departmentSuccess"

--- src/redux/department.js
+++ src/redux/department.js
@@ -8,8 +8,24 @@ export const getDepartmentSuccess = (date) => {
 
 const reducer = (state = [], action) => {
   switch (action.type) {
+    case 'GET_DEPARTMENT_ATTEMPT':
+      return {
+        pending: true,
+        error: state.error,
+        data: state.data
+      };
+    case 'GET_DEPARTMENT_FAILURE':
+      return {
+        pending: false,
+        error: true,
+        data: action.response
+      };
     case 'GET_DEPARTMENT_SUCCESS':
-      return action.response;
+      return {
+        pending: false,
+        error: false,
+        data: action.response
+      };
     default:
       return state;
   }
