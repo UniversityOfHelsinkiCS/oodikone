e0a1564994da6ae0ac53fd2ffe174eec4baf0ca7
totutotu <tuomo.torppa@gmail.com>
2018-03-16 11:23:08 +0200

    switched to using real enabled Units when using populationStatistics

--- src/services/units.js
+++ src/services/units.js
@@ -4,10 +4,16 @@ const all = () => {
   return Unit.findAll()
 }
 
+const findAllEnabled = () => {
+  return Unit.findAll({
+    where: { enabled: true }
+  })
+}
+
 const byId = (id) => {
   return Unit.findById(id)
 }
 
 module.exports = {
-  all, byId
+  all, byId, findAllEnabled
 }
