11fdb519138c8e97d46df92589bc2af345f5b59b
Matti Luukkainen <mluukkai@iki.fi>
2018-03-21 22:03:35 +0200

    logging with winston/papertrail

--- src/services/database_updater/student_list_updater.js
+++ src/services/database_updater/student_list_updater.js
@@ -2,16 +2,29 @@ const oodi = require('./oodi_interface')
 const util = require('./util')
 const { StudentList } = require('../../models')
 
+const winston = require('winston')
+require('winston-papertrail').Papertrail
+
+const winstonPapertrail = new winston.transports.Papertrail({
+  host: process.env.PAPERTRAIL_HOST,
+  port: process.env.PAPERTRAIL_PORT
+})
+
+winstonPapertrail.on('error', function (err) {
+  console.log(err)
+})
+
+const logger = new winston.Logger({
+  transports: [
+    winstonPapertrail,
+    new winston.transports.Console()
+  ]
+})
+
 /*
   run in staging with: 
-
-  docker exec -it staging_backend sh
-
-  STUDENTS_FROM=1112000 STUDENTS_TO=1112100   npm run update_studentnumbers_dev
-
-  docker exec -it -e STUDENTS_FROM=1112000 -e STUDENTS_TO=1112010 staging_backend npm run update_studentnumbers_dev
+  docker exec -it -e STUDENTS_FROM=1112000 -e STUDENTS_TO=1113000 staging_backend npm run update_studentnumbers_dev
 */
-
 
 const arraysEqual = (a1, a2) => {
   return JSON.stringify(a1.sort()) === JSON.stringify(a2.sort())
@@ -33,15 +46,15 @@ async function run () {
 
   const range = maxStudentNumber - minStudentNumber
 
-  console.log('student numbers form', minStudentNumber, 'to', maxStudentNumber, 'total of', range, 'students')
-  console.log('log message every', step, 'student')
+  logger.info('student numbers form '+ minStudentNumber + ' to ' + maxStudentNumber+  ' total of ' + range + ' students')
+  logger.info('log message every '+ step + ' student')
 
   for (let i = minStudentNumber; i < maxStudentNumber; i++) {
     let studentNumber = '0' + i + util.getStudentNumberChecksum(String(i))
 
     if (i % step === 1) {
       const iteration = (1+(i - minStudentNumber) / step).toFixed()
-      console.log(iteration, '/', range / step + 1, timestamp() )
+      logger.info(iteration + '/' + (range / step + 1) + ' ' + timestamp() )
     }
     const student = await oodi.getStudent(studentNumber)
     if (student !== null && student !== undefined) {
@@ -61,9 +74,8 @@ async function run () {
       description: `${minStudentNumber} to ${maxStudentNumber}`
     })
   } else if (!arraysEqual(cached.student_numbers, validStudents)) {
-    console.log('ERROR in student lists. There were', cached.student_numbers.length, 'students, was', cached.description)
-    console.log('found: valid', validStudents.length, 'out of', maxStudentNumber - minStudentNumber)
-    
+    logger.info('ERROR in student lists. There were ' + cached.student_numbers.length + ' students, was '+ cached.description)
+    logger.info('found: valid ' + validStudents.length + ' out of ' + (maxStudentNumber - minStudentNumber))
     cached.student_numbers = validStudents
     cached.description = `${minStudentNumber} to ${maxStudentNumber}`
     await cached.save()
@@ -71,9 +83,8 @@ async function run () {
   }
 
   await cached.save()
-
-  console.log('student list cached')
-  console.log('found: valid', validStudents.length, 'out of', maxStudentNumber - minStudentNumber)
+  logger.info('student list cached')
+  logger.info('found: valid '+ validStudents.length + ' out of '+ (maxStudentNumber - minStudentNumber))
 
   process.exit(0)
 }
