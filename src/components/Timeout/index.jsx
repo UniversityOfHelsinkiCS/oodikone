1c404f4adae0f59f86696a82e40e05c9a1413aa1
Jami Kousa <jakousa@cs.helsinki.fi>
2018-03-27 11:12:09 +0300

    refactors and timeout

---
+++ src/components/Timeout/index.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+const Timeout = Composition => class _Timeout extends Component {
+  componentDidMount() {
+    this.timeouts = {}
+  }
+
+  componentWillUnmount() {
+    this.clearAllTimeouts()
+  }
+
+  setTimeout = (identifier, ...args) => {
+    this.timeouts[identifier] = setTimeout(...args)
+  }
+
+  clearTimeout = (identifier) => {
+    if (this.timeouts[identifier]) {
+      clearTimeout(this.timeouts[identifier])
+    }
+  }
+
+  clearAllTimeouts = () => {
+    Object.keys(this.timeouts)
+      .map(key => this.timeouts[key])
+      .forEach(clearTimeout)
+    this.timeouts = {}
+  }
+
+  render() {
+    const { timeouts, setTimeout, clearTimeout, clearAllTimeouts } = this
+
+    return (
+      <Composition
+        timeouts={timeouts}
+        setTimeout={setTimeout}
+        clearTimeout={clearTimeout}
+        clearAllTimeouts={clearAllTimeouts}
+        {...this.props}
+      />
+    )
+  }
+}
+
+export default Timeout
