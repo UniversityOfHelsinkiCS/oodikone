35912972470172f0c1ce62a9b11f9cd40e9d094f
Jami Kousa <jakousa@cs.helsinki.fi>
2018-03-29 14:16:53 +0300

    user access controls

--- src/components/EnableUsers/index.jsx
+++ src/components/EnableUsers/index.jsx
@@ -1,57 +1,128 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Dropdown, Grid, List } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { func, object, bool, arrayOf } from 'prop-types'
+import { func, shape, string, number, bool, arrayOf } from 'prop-types'
 import { getTranslate, getActiveLanguage } from 'react-localize-redux'
 
-import { getUsers, enableUser } from '../../redux/users'
+import { getUsers, enableUser, addUserUnit, removeUserUnit } from '../../redux/users'
+import { getUnits } from '../../redux/units'
 
 class EnableUsers extends Component {
-  static propTypes = {
-    getUsers: func.isRequired,
-    enableUser: func.isRequired,
-    users: arrayOf(object).isRequired,
-    error: bool.isRequired
-  }
-
   componentDidMount() {
     this.props.getUsers()
+    this.props.getUnits()
   }
 
-  enableUser = (id) => {
+  enableUser = id => () => {
     this.props.enableUser(id)
   }
 
+  handleClick = uid => (e, { value }) => {
+    this.props.addUserUnit(uid, value)
+  }
+
+  removeAccess = (unit, uid) => () => {
+    this.props.removeUserUnit(uid, unit)
+  }
+
+  renderUnitList = (units, user) => {
+    if (!units) return null
+    return (
+      <List divided verticalAlign="middle">
+        {units.map(unit => (
+          <List.Item key={unit.id}>
+            <List.Content floated="right">
+              <Button onClick={this.removeAccess(unit, user)}>Remove</Button>
+            </List.Content>
+            <List.Content>
+              {unit.name}
+            </List.Content>
+          </List.Item>
+        ))}
+
+      </List >
+    )
+  }
+
   render() {
-    return this.props.error ? <h1>Error: {this.props.error} </h1> : (
+    const { users, error, units } = this.props
+    const unitOptions = units.map(unit => ({ key: unit.id, value: unit.id, text: unit.name }))
+    return error ? null : (
       <div>
         <h1>Enable or disable access to OodiKone</h1>
-        {this.props.users ? this.props.users.map(u => (
-          <div key={u.id}>
-            <h3>{u.full_name}, {u.username} - Access: {u.is_enabled ? 'En' : 'Dis' }abled</h3>
-            <Button onClick={() => this.enableUser(u.id)}>Enable/Disable</Button>
-          </div>
-        )) : <h2>No users could be fetched</h2>
-        }
+        {users.map(u => (
+          <Grid key={u.id} divided="vertically">
+            <Grid.Row columns={2}>
+              <Grid.Column>
+                <h3>{u.full_name}, {u.username} - Access to oodikone: {u.is_enabled ? 'En' : 'Dis'}abled</h3>
+                <Dropdown
+                  placeholder="Select unit"
+                  options={unitOptions}
+                  onChange={this.handleClick(u.username)}
+                  fluid
+                  search
+                  selection
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Button onClick={this.enableUser(u.id)}>Enable/Disable</Button>
+                {this.renderUnitList(u.units, u.username)}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        ))}
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ locale, users }) => ({
+EnableUsers.propTypes = {
+  getUsers: func.isRequired,
+  enableUser: func.isRequired,
+  addUserUnit: func.isRequired,
+  removeUserUnit: func.isRequired,
+  getUnits: func.isRequired,
+  units: arrayOf(shape({
+    id: number,
+    name: string
+  })).isRequired,
+  users: arrayOf(shape({
+    id: number,
+    full_name: string,
+    is_enabled: bool,
+    username: string,
+    units: arrayOf(shape({
+      id: number,
+      name: string
+    }))
+  })).isRequired,
+  error: bool.isRequired
+}
+
+const mapStateToProps = ({ locale, users, units }) => ({
   translate: getTranslate(locale),
   currentLanguage: getActiveLanguage(locale).value,
+  units: units.data,
   users: users.data,
   pending: users.pending,
-  error: users.error
+  error: users.error || false
 })
 
 const mapDispatchToProps = dispatch => ({
-  getUsers() {
+  getUsers: () => {
     dispatch(getUsers())
   },
-  enableUser(id) {
+  enableUser: (id) => {
     dispatch(enableUser(id))
+  },
+  addUserUnit: (uid, unit) => {
+    dispatch(addUserUnit(uid, unit))
+  },
+  removeUserUnit: (uid, unit) => {
+    dispatch(removeUserUnit(uid, unit))
+  },
+  getUnits: () => {
+    dispatch(getUnits())
   }
 })
 
