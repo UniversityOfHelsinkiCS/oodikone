12e6709c19fe0687333b21629ef1d08c052ef4e2
Matti Luukkainen <mluukkai@iki.fi>
2018-04-10 13:55:57 +0300

    fix logger

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,10 +1,11 @@
 const winston = require('winston')
+
 require('winston-papertrail').Papertrail
 
 const winstonPapertrail = new winston.transports.Papertrail({
-  host: process.env.PAPERTRAIL_HOST,
-  port: process.env.PAPERTRAIL_PORT,
-  hostname: process.env.PAPERTRAIL_HOSTNAME
+  host: process.env.PAPERTRAIL_HOST ||'localhost',
+  port: process.env.PAPERTRAIL_PORT || 12345,
+  hostname: process.env.PAPERTRAIL_HOSTNAME || 'travis'
 })
 
 winstonPapertrail.on('error', function (err) {
@@ -16,11 +17,26 @@ const transports = {
   console: new winston.transports.Console({ level: 'debug' })
 }
 
-const loggerPapertrail = new winston.Logger({
+let loggerPapertrail = new winston.Logger({
   transports: [
     transports.papertrail, transports.console
   ]
 })
+
+if ( process.env.NODE_ENV === 'test' ) {
+  console.log('logger for tests')
+  loggerPapertrail = new winston.Logger({
+    transports: [
+      transports.console
+    ]
+  })
+} else {
+  loggerPapertrail = new winston.Logger({
+    transports: [
+      transports.papertrail, transports.console
+    ]
+  })
+}
 
 transports.papertrail.level = 'info'
 
