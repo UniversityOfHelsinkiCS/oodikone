880cc958e8f97aa7aa324b421fab9fc3c4c5e937
Jami Kousa <jakousa@cs.helsinki.fi>
2018-04-20 17:34:57 +0300

    morgan & graylog

--- src/app.js
+++ src/app.js
@@ -10,17 +10,12 @@ const app = express()
 app.use(cors({ credentials: true, origin: conf.frontend_addr }))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  console.log(`${req.path} ${JSON.stringify(req.query)} ${new Date()}`)
-  next()
-})
-
 app.get('/ping', async (req, res) => {
   res.json({ data: 'pong' })
 })
 
 const BASE_URL = process.env.NODE_ENV === 'dev' ||
-process.env.NODE_ENV === 'test' ? '/api' : '/'
+  process.env.NODE_ENV === 'test' ? '/api' : '/'
 
 routes(app, BASE_URL)
 
--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,6 @@
+const morgan = require('morgan')
+const logger = require('./util/logger')
+
 const courses = require('./routes/courses')
 const department = require('./routes/department')
 const students = require('./routes/students')
@@ -7,13 +10,28 @@ const users = require('./routes/users')
 
 const auth = require('./middleware/auth')
 
+const accessLogger = morgan((tokens, req, res) => {
+  const fields = ['method', 'url', 'status', 'response-time', 'remote-addr', 'remote-user', 'user-agent', 'referrer']
+  const message = [
+    req.decodedToken.name, ':',
+    tokens['method'](req, res),
+    tokens['url'](req, res),
+    tokens['status'](req, res),
+    '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+  const meta = req.decodedToken
+  fields.forEach(field => meta[field] = tokens[field](req, res))
+  logger.info(message, meta)
+})
+
 module.exports = (app, url) => {
   app.use(url, login)
-  app.use(auth.checkAuth)
+  app.use(auth.checkAuth, accessLogger)
   app.use(url, courses)
   app.use(url, department)
   app.use(url, students)
   app.use(url, population)
-  app.use(url, auth.checkAdminAuth)
+  app.use(auth.checkAdminAuth)
   app.use(url, users)
 }
