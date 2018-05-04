7a6027f20e697013eb49e3049135374a22499644
Matti Luukkainen <mluukkai@iki.fi>
2018-05-04 14:17:52 +0300

    optimized population statistict

--- src/routes/population.js
+++ src/routes/population.js
@@ -3,8 +3,51 @@ const Population = require('../services/populations')
 const User = require('../services/users')
 const Unit = require('../services/units')
 
+router.get('/v2/populationstatistics', async (req, res) => {
+  try {
+    console.log(req.query)
+    if (!req.query.year || !req.query.semester || !req.query.studyRights) {
+      res.status(400).json({ error: 'The query should have a year, semester and study rights defined' })
+      return
+    }
+
+    if (!Array.isArray(req.query.studyRights)) { // studyRights should always be an array
+      req.query.studyRights = [req.query.studyRights]
+    }
+
+    if (!req.decodedToken.admin) {
+      const accesses = await Promise.all(req.query.studyRights.map(async right => {
+        const user = await User.byUsername(req.decodedToken.userId)
+        const units = await User.getUnits(user.id)
+        return units.some(unit => unit.id === right)
+      }))
+
+      if (accesses.some(access => !access)) {
+        res.status(403).json([])
+        return
+      }
+    }
+
+    if (req.query.months==null) {
+      req.query.months = 12
+    }
+    
+    const result = await Population.optimizedStatisticsOf(req.query)
+    if (result.error) {
+      res.status(400).json(result)
+      return
+    }
+
+    console.log(`request completed ${new Date()}`)
+    res.json(result)    
+  } catch (e) {
+    res.status(400).json({ error: e })
+  }
+})
+
 router.get('/populationstatistics', async (req, res) => {
   try {
+    console.log(req.query)
     if (!req.query.year || !req.query.semester || !req.query.studyRights) {
       res.status(400).json({ error: 'The query should have a year, semester and study rights defined' })
       return
