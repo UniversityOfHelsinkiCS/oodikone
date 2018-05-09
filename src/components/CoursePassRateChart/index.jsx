8b483cf4e810762b1665310c18db47eb500aa332
totutotu <tuomo.torppa@gmail.com>
2018-05-09 11:01:13 +0300

    Course Yearly Stats MVP,  for admin users only for now

---
+++ src/components/CoursePassRateChart/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import { Header } from 'semantic-ui-react'
+
+const { array, shape, string } = PropTypes
+
+
+const StackedBarChart = ({ stats }) => {
+  const data = stats.stats.map(year => (
+    { name: year.time, passed: year.passed, failed: year.failed }))
+  return (
+    <div>
+      <Header as="h3">{stats.code}</Header>
+      <BarChart
+        width={600}
+        height={300}
+        data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="passed" stackId="a" fill="#8884d8" />
+        <Bar dataKey="failed" stackId="a" fill="#82ca9d" />
+      </BarChart >
+    </div>
+  )
+}
+
+StackedBarChart.propTypes = {
+  stats: shape({
+    code: string.isRequired,
+    stats: array.isRequired
+  }).isRequired
+}
+
+export default StackedBarChart
