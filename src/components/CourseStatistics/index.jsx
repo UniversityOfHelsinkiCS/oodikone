920022cab039d34aace80759d399e255ab169f41
totutotu <tuomo.torppa@gmail.com>
2018-05-11 14:12:49 +0300

    Refactored isValidYear from popstats to common, contnd yearly course

--- src/components/CourseStatistics/index.jsx
+++ src/components/CourseStatistics/index.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Header } from 'semantic-ui-react'
+import { Form, Button, Header, Checkbox } from 'semantic-ui-react'
+import Datetime from 'react-datetime'
 import Timeout from '../Timeout'
 import CourseSearch from '../CourseSearch'
 import CoursePassRateChart from '../CoursePassRateChart'
 import { getCourseStatistics } from '../../redux/courseStatistics'
+import { isValidYear, isInDateFormat, reformatDate } from '../../common'
 
-import styles from './courseStatistics.css'
+
+import style from './courseStatistics.css'
 import sharedStyles from '../../styles/shared'
 
 const { shape, func, array } = PropTypes
@@ -20,7 +23,8 @@ const INITIAL_YEARS = {
 class CourseStatistics extends Component {
   state = {
     selectedCourse: { name: 'No course selected', code: '' },
-    ...INITIAL_YEARS
+    ...INITIAL_YEARS,
+    validYear: true
   }
 
   handleResultSelect = (e, { result }) => {
@@ -35,13 +39,95 @@ class CourseStatistics extends Component {
     this.props.getCourseStatistics({ code, start, end, separate: true })
   }
 
+  handleYearSelection = (year) => {
+    const validYear = isInDateFormat(year, 'YYYY') && isValidYear(year)
+    if (validYear) {
+      this.setState({
+        validYear,
+        start: reformatDate(year, 'YYYY')
+      })
+    } else {
+      this.setState({ validYear })
+    }
+  }
+
+  renderEnrollmentDateSelector = () => {
+    const { validYear, start, end } = this.state
+
+    return (
+      <Form>
+        <Form.Group key="year" className={style.enrollmentSelectorGroup}>
+          <Form.Field error={!validYear} className={style.yearSelect}>
+            <label>Start year</label>
+            <Datetime
+              className={style.yearSelectInput}
+              control={Datetime}
+              dateFormat="YYYY"
+              timeFormat={false}
+              closeOnSelect
+              value={start}
+              isValidDate={isValidYear}
+              onChange={this.handleYearSelection}
+            />
+          </Form.Field>
+          <Form.Field className={style.yearControl}>
+            <Button.Group basic vertical className={style.yearControlButtonGroup}>
+              <Button
+                icon="plus"
+                className={style.yearControlButton}
+                onClick={this.addYear}
+              />
+              <Button
+                icon="minus"
+                className={style.yearControlButton}
+                onClick={this.subtractYear}
+              />
+            </Button.Group>
+          </Form.Field>
+          <Form.Field error={!validYear} className={style.yearSelect}>
+            <label>End year</label>
+            <Datetime
+              className={style.yearSelectInput}
+              control={Datetime}
+              dateFormat="YYYY"
+              timeFormat={false}
+              closeOnSelect
+              value={end}
+              isValidDate={isValidYear}
+              onChange={this.handleYearSelection}
+            />
+          </Form.Field>
+          <Form.Field className={style.yearControl}>
+            <Button.Group basic vertical className={style.yearControlButtonGroup}>
+              <Button
+                icon="plus"
+                className={style.yearControlButton}
+                onClick={this.addYear}
+              />
+              <Button
+                icon="minus"
+                className={style.yearControlButton}
+                onClick={this.subtractYear}
+              />
+            </Button.Group>
+          </Form.Field>
+          <Form.Field>
+            <Checkbox label="Separate Spring/Fall" />
+            <label>Select semester thing</label>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+    )
+  }
+
   render() {
     const { data } = this.props.courseStatistics
     return (
-      <div className={styles.container}>
+      <div className={style.container}>
         <Header className={sharedStyles.segmentTitle} size="large">
           Course Statistics
         </Header>
+        {this.renderEnrollmentDateSelector()}
         <CourseSearch handleResultSelect={this.handleResultSelect} />
 
         {data.map(course => <CoursePassRateChart key={course.name} stats={course} />)}
