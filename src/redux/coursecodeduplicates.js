34484d9e3df98a4549b55cecac440d431c16393d
totutotu <tuomo.torppa@gmail.com>
2018-05-30 15:53:16 +0300

    Course duplicate code managing UI started

---
+++ src/redux/coursecodeduplicates.js
@@ -0,0 +1,64 @@
+import { callController } from '../apiConnection'
+
+export const getDuplicates = () => {
+  const route = '/courses/duplicatecodes/all'
+  const prefix = 'GET_ALL_DUPLICATES_'
+  return callController(route, prefix)
+}
+
+export const addDuplicate = (code1, code2) => {
+  const route = `/courses/duplicatecodes/${code1}/${code2}`
+  const prefix = 'ADD_DUPLICATE_'
+  const data = { code1, code2 }
+  const method = 'post'
+  return callController(route, prefix, data, method)
+}
+
+const reducer = (state = { data: [] }, action) => {
+  switch (action.type) {
+    case 'GET_ALL_DUPLICATES_ATTEMPT':
+      return {
+        pending: true,
+        selected: state.selected,
+        data: state.data
+      }
+    case 'GET_ALL_DUPLICATES_FAILURE':
+      return {
+        pending: false,
+        error: true,
+        selected: state.selected,
+        data: state.data
+      }
+    case 'GET_ALL_DUPLICATES_SUCCESS':
+      return {
+        pending: false,
+        error: false,
+        selected: action.response.code,
+        data: action.response
+      }
+    case 'ADD_DUPLICATE_ATTEMPT':
+      return {
+        pending: true,
+        selected: state.selected,
+        data: state.data
+      }
+    case 'ADD_DUPLICATE_FAILURE':
+      return {
+        pending: false,
+        error: true,
+        selected: state.selected,
+        data: state.data
+      }
+    case 'ADD_DUPLICATE_SUCCESS':
+      return {
+        pending: false,
+        error: false,
+        selected: action.response.code,
+        data: action.response
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
