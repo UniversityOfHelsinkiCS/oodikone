41a28851af568344199a1b6669887550295c1b8c
ikuisma <ilkka.a.kuisma@gmail.com>
2018-06-05 17:18:52 +0300

    Implemented new test for viewing student credit accumulation graph.

--- superbara_tests/tests/population_statistics_show_students.rb
+++ superbara_tests/tests/population_statistics_show_students.rb
@@ -1,5 +1,5 @@
-run '../common'
-focus
+first_student_selector = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(4) > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(1)" 
+credit_accumulation_graph = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div > div.creditAccumulationGraph__graphContainer___2cC5j"
 
 visit $basepath
 wait do
@@ -7,3 +7,16 @@ wait do
 end
 click_text "Population statistics"
 
+run 'helpers/population_statistics'
+
+click_text "show"
+
+assert do
+    has_text? "Student names hidden"
+end
+
+(find first_student_selector).click
+
+wait 15 do
+    has_css? credit_accumulation_graph
+end
