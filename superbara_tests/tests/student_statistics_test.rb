03e97ceb9adea2f8a9161f17af6b73ed9844ea5d
ikuisma <ilkka.a.kuisma@gmail.com>
2018-06-05 15:29:18 +0300

    Two first tests made with superbara.

---
+++ superbara_tests/tests/student_statistics_test.rb
@@ -0,0 +1,18 @@
+visit "localhost:8081"
+
+wait do
+    has_text? "Student statistics"
+end
+
+click_text "Student statistics"
+student_search_input = find "input"
+student_search_input.type "Paksula Matti"
+click_text "Student names hidden"
+click_text "012843501"
+
+wait do
+    has_text? "Filosofian maisteri"
+end
+assert do
+    has_text? "Filosofian maisteri"
+end
