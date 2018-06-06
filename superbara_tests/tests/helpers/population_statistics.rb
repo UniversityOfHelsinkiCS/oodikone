dcba353b2d3a9c2b60656cbd83fdc562a8c9c534
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-06-06 14:04:55 +0300

    new superbara tests

--- superbara_tests/tests/helpers/population_statistics.rb
+++ superbara_tests/tests/helpers/population_statistics.rb
@@ -1,19 +1,20 @@
 enrollment_year_select = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div > form > div.fields.populationSearchForm__enrollmentSelectorGroup___2l8ON > div.field.populationSearchForm__yearSelect___OLVSL > div > input"
 study_rights_select = "#rightGroup > div > div"
 
-(find enrollment_year_select).click
-
-4.times do
+wait 15 do
+    (find enrollment_year_select).click
+end
+2.times do
     type :backspace
 end
 
-type "2015"
+type "15"
 
 (find study_rights_select).click
 
 click_text "Bachelor of Science, Computer Science"
 click_text "See population"
 
-wait 15 do
-    has_text? "Credit statistics"
+wait 60 do
+    find("#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(2)")
 end
--- superbara_tests/tests/population_statistics.rb
+++ superbara_tests/tests/population_statistics.rb
@@ -1,21 +1,22 @@
-visit $basepath
+focus
+visit $basepath + "/populations"
 
 male_radio_label = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(6) > form > div > div:nth-child(2) > div:nth-child(1) > label"
 sex_filter_add_button = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(1) > div > div:nth-child(6) > form > div > div:nth-child(3) > button"
+population_td = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(2)"
 
-wait do
-    find "a", text: "Population statistics"
-end.click
 
 run "./helpers/population_statistics"
 
 click_text "add"
-
+value_start = (find population_td).text.to_i
 (find male_radio_label).click
 (find sex_filter_add_button).click
 
-assert do
-    has_text? "Showing only male students."
-end
+value_end = (find population_td).text.to_i
 
-scroll 1.5
+assert "Filter is on and is filtering population" do
+    has_text? "Showing only male students."
+    value_start > value_end
+    highcharts = find("#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > div")
+end
