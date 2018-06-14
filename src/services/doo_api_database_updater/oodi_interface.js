7f9aa8cf50beebec155ed0597808e230074b8a5d
ikuisma <ilkka.a.kuisma@gmail.com>
2018-06-14 09:55:15 +0300

    Refactored oodi_interface to use axios instance.

--- src/services/doo_api_database_updater/oodi_interface.js
+++ src/services/doo_api_database_updater/oodi_interface.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 const data_mapper = require('./oodi_data_mapper')
 const base_url = process.env.OODI_ADDR
 const https = require('https')
-const logger = require('../../util/logger')
 
 const instance = axios.create({
   httpsAgent: new https.Agent({  
@@ -20,112 +19,39 @@ const addTokenToUrl = (url) => {
   return `${url}?token=${process.env.TOKEN}`
 }
 
-const httpsAgent = new https.Agent({  
-  rejectUnauthorized: false
-})
-
-const getStudent = (studentNumber) => {
+const getStudent = async studentNumber => {
   const url = addTokenToUrl(`${base_url}/students/${studentNumber}/info`)
-  return axios.get(url, { httpsAgent })
-    .then(response => {
-      return data_mapper.getStudentFromData(response.data)
-    })
-    .catch(error => {
-      return null
-    })
+  const response = await instance.get(url)
+  return data_mapper.getStudentFromData(response.data)
 }
 
-const getStudentStudyRights = (studentNumber) => {
+const getStudentStudyRights = async studentNumber => {
   const url = addTokenToUrl(`${base_url}/students/${studentNumber}/studyrights`)
-  return axios.get(url, { httpsAgent })
-    .then( response => {
-      return response.data.data.map(data => data_mapper.getStudyRightFromData(data, studentNumber))
-    })
-    .catch(error => {
-      console.log('error getStudentStudyRights\n' + error)
-    })
+  const response = await instance.get(url)
+  return response.data.data.map(data => data_mapper.getStudyRightFromData(data, studentNumber))
 }
 
-const getOrganisation = (organisationId) => {
-  return axios.get(base_url + '/organisations/' + organisationId + '?language_code=en')
-    .then(response => {
-      return data_mapper.getOrganisationFromData(response.data)
-    })
-    .catch(error => {
-      console.log('error getOrganization\n' + error)
-    })
-}
-
-const getTeacherDetails = (courseCode, date) => {
-  return axios.get(base_url + '/courses/' + courseCode + '/' + date + '/teacherdetails')
-    .then(response => {
-      return response.data
-    })
-    .catch(error => {
-      console.log('error getTeacherDetails\n' + error)
-    })
-}
-
-// TODO function not in use?xw
-const getStudentNumbers = () => {
-  return axios.get(base_url + '/programs/students/since/01.01.1965')
-    .then(response => {
-      return data_mapper.getStudentNumbersFromProgramData(response.data)
-    })
-    .catch(error => {
-      console.log('error with getStudentNumbers\n' + error)
-    })
-}
-
-
-const getStudentCourseCredits = (studentNumber) => {
-  return axios.get(base_url + '/credits/' + studentNumber)
-    .then(response => {
-      return data_mapper.getCourseCreditsFromData(response.data)
-    })
-    .catch(error => {
-      console.log('error with getStudentCourseCredits\n' + error)
-    })
-}
-
-const getFaculties = () => {
+const getFaculties = async () => {
   const url = addTokenToUrl(`${base_url}/codes/faculties`)
-  return axios.get(url, { httpsAgent })
-    .then(response => {
-      return response.data.data
-    })
-    .catch(error => {
-      logger.error(`Error getting organization codes: ${error.message}`)
-    })
+  const response = await instance.get(url)
+  return response.data.data
 }
 
-const getStudyAttainments = (studentNumber) => {
+const getStudyAttainments = async studentNumber => {
   const url = addTokenToUrl(`${base_url}/students/${studentNumber}/studyattainments`)
-  return axios.get(url, { httpsAgent })
-    .then(response => {
-      return response.data.data
-    }).catch(error => {
-      logger.error(`Error getting study attainments for student ${studentNumber}: ${error.message}`)
-    })
+  const response = await instance.get(url)
+  return response.data.data
 }
 
-const getTeacherInfo = id => {
+const getTeacherInfo = async id => {
   const url = addTokenToUrl(`${base_url}/teachers/${id}/info`)
-  return axios.get(url, { httpsAgent })
-    .then(response => {
-      return data_mapper.getTeacherFromData(response.data.data)
-    }).catch(error => {
-      logger.error(`Error getting teacher info for teacher ${id}: ${error.message}`)
-    })
+  const response = await instance.get(url)
+  return data_mapper.getTeacherFromData(response.data.data)
 }
 
 module.exports = {
   getStudentStudyRights,
   getStudent,
-  getOrganisation,
-  getStudentCourseCredits,
-  getStudentNumbers,
-  getTeacherDetails,
   getFaculties,
   getStudyAttainments,
   getTeacherInfo
