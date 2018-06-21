95e98746e484bc046c670714e6677dcdde5cfacc
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-06-21 10:41:09 +0300

    removed clog

--- src/components/CreditGraphTooltip/index.jsx
+++ src/components/CreditGraphTooltip/index.jsx
@@ -58,7 +58,6 @@ const CreditGraphTooltip = (props) => {
     const {
       title, credits, date, grade, passed
     } = payload[0].payload
-    console.log(payload[0].payload)
 
     return (
       <Card>
