c5dff7ac6384a3f2a496d2e9d99407b53d64ab3c
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-06-21 15:57:55 +0300

    added new population statistics test

--- superbara_tests/tests/helpers/population_statistics.rb
+++ superbara_tests/tests/helpers/population_statistics.rb
@@ -1,9 +1,5 @@
-enrollment_year_select = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div > form > div.fields.populationSearchForm__enrollmentSelectorGroup___2l8ON > div.field.populationSearchForm__yearSelect___OLVSL > div > input"
-study_rights_select = "#rightGroup > div > div"
-
-
 wait 15 do
-    (find enrollment_year_select).click
+    find("label", text:"Enrollment year").find(:xpath, "..").find("input").click
 end
 2.times do
     type :backspace
@@ -11,11 +7,12 @@ end
 
 type "15"
 
-(find study_rights_select).click
+find("label", text:"Study rights").find(:xpath, "..").click
 
-click_text "Bachelor of Science, Computer Science"
-click_text "See population"
+type "Tietojenkäsittelytieteen"
+click_text "Tietojenkäsittelytieteen koulutusohjelma"
+click_button "See population"
 
 wait 60 do
-    find("#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(2)")
+    has_text? "Credit accumulation"
 end
--- superbara_tests/tests/population_statistics_show_students.rb
+++ superbara_tests/tests/population_statistics_show_students.rb
@@ -1,6 +1,3 @@
-first_student_selector = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div:nth-child(2) > div:nth-child(5) > div:nth-child(3) > table > tbody > tr:nth-child(1) > td:nth-child(1)" 
-credit_accumulation_graph = "#root > div > main > div:nth-child(3) > div.ui.segment.layout__contentSegment___M-cZG > div > div.creditAccumulationGraph__graphContainer___2cC5j"
-
 
 visit "localhost:8081"
 
