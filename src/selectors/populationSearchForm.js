e5b3f00cf51cd95c98bec4e7ab063dd58e041e08
ikuisma <ilkka.a.kuisma@gmail.com>
2018-07-03 14:30:02 +0300

    Added course codes to degree and studyprogram dropdown lists.

--- src/selectors/populationSearchForm.js
+++ src/selectors/populationSearchForm.js
@@ -4,7 +4,7 @@ import { groupBy } from 'lodash'
 const getUnits = units => units.data
 
 export const mapRightsToDropdown = rights =>
-  groupBy(rights.map(r => ({ key: r.id, value: r.id, text: r.name, type: r.type })), 'type')
+  groupBy(rights.map(r => ({ key: r.id, value: r.id, text: r.name, type: r.type, description: r.id })), 'type')
 
 
 export const makeMapRightsToDropDown = () => createSelector(
