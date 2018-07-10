1ff6b7b31f450c61d03ffd8dcfc51e2ee6f493b2
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-07-10 10:10:59 +0300

    reset

--- superbara_tests/tests/student_statistics.rb
+++ superbara_tests/tests/student_statistics.rb
@@ -7,6 +7,9 @@ end
 find("a", text:"Student statistics").click
 student_search_input = find "input"
 student_search_input.type "Paksula"
+wait do
+    has_text? "012843501"
+end
 click_text "Student names hidden"
 click_text "012843501"
 
