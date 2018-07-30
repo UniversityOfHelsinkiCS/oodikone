32474be9ea45e2e47c0ba87952ce8956ab03a4f6
totutotu <tuomo.torppa@gmail.com>
2018-07-30 11:07:24 +0300

    Updater to use cert

--- src/conf-backend.js
+++ src/conf-backend.js
@@ -5,6 +5,9 @@ const frontend_addr = process.env.FRONT_URL
 const redis = process.env.REDIS
 const TOKEN_SECRET = process.env.TOKEN_SECRET
 const DB_SCHEMA = process.env.DB_SCHEMA || 'public'
+const CERT_PATH = process.env.CERT_PATH // production/staging only
+const KEY_PATH = process.env.KEY_PATH // production/staging only
+
 const OODI = {
   test: 'http://localhost',
   anon: process.env.OODI_ADDR_ANON
@@ -13,5 +16,5 @@ const OODI = {
 const OODI_ADDR = OODI[process.env.NODE_ENV] || process.env.OODI_ADDR
 
 module.exports = {
-  frontend_addr, DB_URL, redis, TOKEN_SECRET, DB_SCHEMA, OODI_ADDR
+  frontend_addr, DB_URL, redis, TOKEN_SECRET, DB_SCHEMA, OODI_ADDR, CERT_PATH, KEY_PATH
 }
