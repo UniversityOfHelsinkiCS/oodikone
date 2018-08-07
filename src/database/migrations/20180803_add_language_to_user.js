6e925de8bb9b3c3f04ff137bff9cbac655d524ea
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-08-07 11:36:48 +0300

    accidantal commentation of migration file reverted :D

--- src/database/migrations/20180803_add_language_to_user.js
+++ src/database/migrations/20180803_add_language_to_user.js
@@ -1,10 +1,10 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // return queryInterface.addColumn('users', 'language',
-    //   {
-    //     type: Sequelize.STRING,
-    //     defaultValue: 'fi'
-    //   })
+    return queryInterface.addColumn('users', 'language',
+      {
+        type: Sequelize.STRING,
+        defaultValue: 'fi'
+      })
   },
   down: (queryInterface) => {
     return queryInterface.removeColumn('users', 'language')
