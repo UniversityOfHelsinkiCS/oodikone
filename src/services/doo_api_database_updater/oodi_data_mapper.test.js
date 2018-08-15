f68df0a8d37c0f309136ce05343c827eb0149ed6
ikuisma <ilkka.a.kuisma@gmail.com>
2018-08-15 10:39:17 +0300

    mapping of enrollment date from api data + tests.

--- src/services/doo_api_database_updater/oodi_data_mapper.test.js
+++ src/services/doo_api_database_updater/oodi_data_mapper.test.js
@@ -77,4 +77,50 @@ describe('studyattainment data mapping to credit tests', () => {
     expect(credit.course_code).toBe('LID_001')
   })
 
-}) 
+})
+
+describe('semester enrollment data mapping tests', () => {
+
+  const studentnumber = '12345678'
+
+  const data = {
+    'full_time_student': 'true',
+    'yths_code': null,
+    'semester_enrollment_type_code': 1,
+    'absence_reason_code': null,
+    'payment_amount': null,
+    'yths_description': null,
+    'student_union_member': 'true',
+    'semester_enrollment_date': '2011-08-30T21:00:00.000Z',
+    'payment_date': null,
+    'semester_code': 123,
+    'doctoral_current_year_estimate': null,
+    'doctoral_previous_year': null
+  }
+
+  describe('mapping of enrolled data', () => {
+    const mapping = mapper.semesterEnrollmentFromData(data, studentnumber)
+
+    test('mapped semestercode should be integer 123', () => {
+      expect(mapping.semestercode).toBe(123)
+    })
+
+    test('mapped enrollmenttype code should be integer 1', () => {
+      expect(mapping.enrollmenttype).toBe(1)
+    })
+
+    test('mapped studentnumber should be correct', () => {
+      expect(mapping.studentnumber).toBe(studentnumber)
+    })
+
+    test('mapped enrollment_date should be instance of Date', () => {
+      expect(mapping.enrollment_date instanceof Date).toBe(true)
+    })
+
+    test('mapped enrollment_date should be equal to Date object with same day, month and year', () => {
+      expect(mapping.enrollment_date).toEqual(new Date('2011-08-30'))
+    })
+
+  })
+
+})
