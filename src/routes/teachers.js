f367b0f22ebd30c1b23afff9d9127cb5c1405e23
ikuisma <ilkka.a.kuisma@gmail.com>
2018-08-17 15:49:13 +0300

    Service function + endpoint for getting credits for teacher by id.

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -7,4 +7,10 @@ router.get('/teachers', async (req, res) => {
   res.json(result)
 })
 
+router.get('/teachers/:id', async (req, res) => {
+  const { id } = req.params
+  const result = await teachers.teacherStats(id)
+  res.json(result)
+})
+
 module.exports = router
