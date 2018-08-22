9391d071aced12df4e83e32fb6adf73d4674fe0e
ikuisma <ilkka.a.kuisma@gmail.com>
2018-08-22 12:13:34 +0300

    Add 'before' parameter for getSemesters-function, modify semester endpoint to only return semesters before the current server date.

--- src/routes/semesters.js
+++ src/routes/semesters.js
@@ -2,7 +2,8 @@ const router = require('express').Router()
 const { getSemestersAndYears } = require('../services/semesters')
 
 router.get('/semesters/codes', async(req, res) => {
-  const providers = await getSemestersAndYears()
+  const before = new Date()
+  const providers = await getSemestersAndYears(before)
   res.json(providers)
 })
 
--- src/services/semesters.js
+++ src/services/semesters.js
@@ -1,7 +1,14 @@
+const { Op } = require('sequelize')
 const { Semester } = require('../models/index')
 
-const getSemestersAndYears = async () => {
-  const semesters = Semester.findAll()
+const getSemestersAndYears = async before => {
+  const semesters = await Semester.findAll({
+    where: {
+      startdate: {
+        [Op.lt]: before
+      }
+    }
+  })
   const result = semesters.reduce((acc, semester) => {
     const { semestercode, name, yearcode, yearname }  = semester
     const semesters = { ...acc.semesters, [semestercode]: { semestercode, name, yearcode }}
