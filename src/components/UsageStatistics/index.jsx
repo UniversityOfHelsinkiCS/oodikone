c8ad64a785987e7cab6e0253791d71bcadf8ab4a
Matti Luukkainen <mluukkai@iki.fi>
2018-08-23 18:16:09 +0300

    users requests

--- src/components/UsageStatistics/index.jsx
+++ src/components/UsageStatistics/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Container, Header, Table } from 'semantic-ui-react'
+import { Container, Header, Table, Icon } from 'semantic-ui-react'
 import { callApi } from '../../apiConnection'
+
+import styles from '../StudentInfoCard/studentInfoCard.css'
 
 class UsageStatistics extends Component { //eslint-disable-line
   state = null
@@ -29,6 +31,51 @@ class UsageStatistics extends Component { //eslint-disable-line
       count: this.state.byEndpoint[endpoint].length
     }))
 
+    if (this.state.user) {
+      const entries = this.state.all
+        .filter(e => e.username === this.state.user)
+        .sort((e1, e2) => e2.time - e1.time)
+
+      const usersName = entries[0].name
+
+      const toTime = (stamp) => {
+        const zeroed = v => (v < 10 ? `0${v}` : v)
+
+        const date = new Date(stamp * 1000)
+        return `${date.getFullYear()}${zeroed(date.getUTCMonth() + 1)}${zeroed(date.getDate())} ${zeroed(date.getHours())}:${zeroed(date.getMinutes())}:${zeroed(date.getSeconds())}`
+      }
+
+      return (
+        <div>
+          <h3>
+            {usersName} ({this.state.user})
+            <Icon
+              onClick={() => this.setState({ user: null })}
+              name="remove"
+              className={styles.controlIcon}
+            />
+          </h3>
+          <Table celled>
+            <Table.Body>
+              {entries.map(entry => (
+                <Table.Row key={entry.id}>
+                  <Table.Cell>
+                    {entry.method}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {entry.URL}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {toTime(entry.time)}
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      )
+    }
+
     return (
       <div>
         <h3>by user</h3>
@@ -37,7 +84,7 @@ class UsageStatistics extends Component { //eslint-disable-line
           <Table.Body>
             {users.sort(byCount).map(user => (
               <Table.Row key={user.name}>
-                <Table.Cell>
+                <Table.Cell onClick={() => this.setState({ user: user.name })}>
                   {user.name}
                 </Table.Cell>
                 <Table.Cell>
