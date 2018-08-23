7cbe19b0413cbf8d100d12bd704df82ac573a3a5
ikuisma <ilkka.a.kuisma@gmail.com>
2018-08-23 08:34:15 +0300

    Fix teacher statistics query params to match new API.

--- src/redux/teacherStatistics.js
+++ src/redux/teacherStatistics.js
@@ -3,11 +3,12 @@ import { callController } from '../apiConnection/index'
 
 const prefix = 'GET_TEACHER_STATISTICS_'
 
-export const getTeacherStatistics = (year, providers) => {
+export const getTeacherStatistics = (semesterStart, semesterEnd, providers) => {
   const route = '/teachers/stats'
   const params = {
     providers,
-    startYearCode: year
+    semesterStart,
+    semesterEnd
   }
   return callController(route, prefix, [], 'get', params, params)
 }
