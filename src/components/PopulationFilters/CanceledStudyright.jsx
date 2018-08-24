2e5f91decf1253fe3531669bafc3b16c87574784
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-08-24 17:55:29 +0300

    canceled studyright filter

---
+++ src/components/PopulationFilters/CanceledStudyright.jsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Dropdown, Icon, Form, Segment, Button } from 'semantic-ui-react'
+import { shape, func, arrayOf, string } from 'prop-types'
+
+import { canceledStudyright } from '../../populationFilters'
+import { removePopulationFilter, setPopulationFilter } from '../../redux/populationFilters'
+
+class CanceledStudyright extends Component {
+  static propTypes = {
+    filter: shape({}).isRequired,
+    removePopulationFilter: func.isRequired,
+    setPopulationFilter: func.isRequired,
+    studyrights: arrayOf(string).isRequired
+  }
+
+  state = {
+    cancel: 'true'
+  }
+
+  options = [{ value: 'false', text: 'haven\'t' }, { value: 'true', text: 'have' }]
+
+  handleChange = (e, { value }) => {
+    this.setState({ cancel: value })
+  }
+
+  handleCancel = () => {
+    this.props.setPopulationFilter(canceledStudyright({
+      studyrights: this.props.studyrights,
+      cancel: this.state.cancel
+    }))
+  }
+
+  clearFilter = () => {
+    this.props.removePopulationFilter(this.props.filter.id)
+  }
+
+  render() {
+    const { filter } = this.props
+    if (filter.notSet) {
+      return (
+        <Segment>
+          <Form>
+            <Form.Group inline>
+              <Form.Field>
+                <label>Filter students that </label>
+              </Form.Field>
+              <Form.Field>
+                <Dropdown
+                  fluid
+                  placeholder="have/haven't"
+                  name="complemented"
+                  onChange={this.handleChange}
+                  options={this.options}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label> canceled this studyright </label>
+              </Form.Field>
+              <Form.Field>
+                <Button
+                  onClick={this.handleCancel}
+                >
+                  set filter
+                </Button>
+              </Form.Field>
+            </Form.Group>
+          </Form>
+        </Segment>
+      )
+    }
+
+    return (
+      <Segment>
+        {filter.params.cancel === 'true' ? 'Showing students that have canceled this studyright' : 'Showing students that have not canceled this studyright'}
+        <span style={{ float: 'right' }}>
+          <Icon name="remove" onClick={this.clearFilter} />
+        </span>
+      </Segment>
+    )
+  }
+}
+const mapStateToProps = ({ populations }) => ({
+  studyrights: populations.query.studyRights
+})
+
+
+export default connect(
+  mapStateToProps,
+  { setPopulationFilter, removePopulationFilter }
+)(CanceledStudyright)
