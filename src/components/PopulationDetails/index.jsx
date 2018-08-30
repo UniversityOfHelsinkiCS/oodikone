929ed9d8e86640c684857c9fcb150adc6db4053a
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2018-08-30 15:17:48 +0300

    eslint

--- src/components/PopulationDetails/index.jsx
+++ src/components/PopulationDetails/index.jsx
@@ -67,7 +67,8 @@ class PopulationDetails extends Component {
                   />
                 </Tab.Pane>
               )
-            }]} />)
+            }]}
+        />)
     }
     return (
       <Segment>
