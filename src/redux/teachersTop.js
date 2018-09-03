49e473625e5a2689fa856a17a2a746b9b89b3dd6
ikuisma <ilkka.a.kuisma@gmail.com>
2018-09-03 12:58:32 +0300

    Add category arg to get top teachers action.

--- src/redux/teachersTop.js
+++ src/redux/teachersTop.js
@@ -3,9 +3,9 @@ import { callController } from '../apiConnection/index'
 
 const prefix = 'GET_TOP_TEACHERS_'
 
-export const getTopTeachers = (yearcode) => {
+export const getTopTeachers = (yearcode, category) => {
   const route = '/teachers/top'
-  const params = { yearcode }
+  const params = { yearcode, category }
   return callController(route, prefix, [], 'get', params, params)
 }
 
