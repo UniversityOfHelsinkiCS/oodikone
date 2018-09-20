e046afe66d6a71cb6f37b41535c70bdeb80f3b77
ikuisma <ilkka.a.kuisma@gmail.com>
2018-09-20 09:11:07 +0300

    Refactor summary tab to use redux and selectors instead of own state.

--- src/components/CourseStatistics/SummaryTab/index.jsx
+++ src/components/CourseStatistics/SummaryTab/index.jsx
@@ -1,47 +1,15 @@
 import React, { Component } from 'react'
 import { Table, Form } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { shape, arrayOf } from 'prop-types'
+import { shape, arrayOf, func } from 'prop-types'
 import selectors from '../../../selectors/courseStats'
-
-const INITIAL = {
-  programme: selectors.ALL.value
-}
+import { fields, setValue } from '../../../redux/coursesSummaryForm'
 
 class SummaryTab extends Component {
-    state={ ...INITIAL }
-
-    summaryStatistics = () => {
-      const { statistics: s, programmes } = this.props
-      const { programme: code } = this.state
-      const prog = programmes.find(p => p.key === code)
-      const filterStudent = studentnumber => new Set(prog.students).has(studentnumber)
-      return Object.entries(s).map((entry) => {
-        const [coursecode, data] = entry
-        const { statistics, name } = data
-        const summary = {
-          passed: 0,
-          failed: 0
-        }
-        statistics.forEach((groupstat) => {
-          const { passed, failed } = groupstat.attempts.classes
-          summary.passed += passed.filter(filterStudent).length
-          summary.failed += failed.filter(filterStudent).length
-        })
-        const passrate = (100 * summary.passed) / (summary.passed + summary.failed)
-        summary.passrate = !passrate ? null : passrate.toFixed(2)
-        return {
-          coursecode,
-          name,
-          summary
-        }
-      })
-    }
-
-    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+    handleChange = (e, { name, value }) => this.props.setValue(name, value)
 
     render() {
-      const stats = this.summaryStatistics()
+      const { statistics } = this.props
       const options = this.props.programmes.map(p => ({
         ...p,
         label: {
@@ -58,14 +26,14 @@ class SummaryTab extends Component {
               selection
               fluid
               label="Study programme"
-              name="programme"
+              name={fields.programme}
               onChange={this.handleChange}
-              value={this.state.programme}
+              value={this.props.form[fields.programme]}
             />
           </Form>
           <Table
             headerRow={['Course', 'Passed', 'Failed', 'Passrate']}
-            tableData={stats}
+            tableData={statistics}
             renderBodyRow={({ coursecode, name, summary }) => ({
               key: coursecode,
               cells: [name, summary.passed, summary.failed, !summary.passrate ? '' : `${summary.passrate} %`]
@@ -77,13 +45,20 @@ class SummaryTab extends Component {
 }
 
 SummaryTab.propTypes = {
-  statistics: shape({}).isRequired,
-  programmes: arrayOf(shape({})).isRequired
+  statistics: arrayOf(shape({})).isRequired,
+  programmes: arrayOf(shape({})).isRequired,
+  form: shape({}).isRequired,
+  setValue: func.isRequired
 }
 
-const mapStateToProps = state => ({
-  statistics: state.courseStats.data,
-  programmes: selectors.getAllStudyProgrammes(state)
-})
+const mapStateToProps = (state) => {
+  const programmes = selectors.getAllStudyProgrammes(state)
+  const programme = state.courseSummaryForm[fields.programme]
+  return {
+    form: state.courseSummaryForm,
+    statistics: selectors.summaryStatistics(state, { programmes, programme }),
+    programmes
+  }
+}
 
-export default connect(mapStateToProps)(SummaryTab)
+export default connect(mapStateToProps, { setValue })(SummaryTab)
