49e7625b8d290d44f35a9da6b6df97a30a90f116
ikuisma <ilkka.a.kuisma@gmail.com>
2018-09-25 10:22:27 +0300

    Router with wrapper methods for catching async/await callback errors.

---
+++ src/routes/router.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const logger = require('../util/logger')
+
+const wrapWithErrorHandler = fn => (req, res) => Promise.resolve(fn(req, res))
+  .catch(error => {
+    logger.error(error)
+    res.status(500).send('Internal server error')
+  })
+
+const routerWithWrapper = () => {
+  const router = express.Router()
+  return {
+    router,
+    wrapper: {
+      get: (url, callback) => router.get(url, wrapWithErrorHandler(callback))
+    }
+  }
+}
+
+module.exports = { routerWithWrapper }
