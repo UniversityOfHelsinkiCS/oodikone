1e6b9a7218e6701066cc80b52ee72e32666716b9
ikuisma <ilkka.a.kuisma@gmail.com>
2018-10-15 12:22:18 +0300

    Implemented chaos monkey button to sandbox for crashing the app

--- src/components/Sandbox/index.jsx
+++ src/components/Sandbox/index.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import { Header, Segment, Grid, Message } from 'semantic-ui-react'
+import { Header, Segment, Grid, Message, Button } from 'semantic-ui-react'
 import sharedStyles from '../../styles/shared'
 import style from './index.css'
 
 class SandboxContainer extends Component {
-    state={}
+    state={
+      crash: false
+    }
 
     render() {
+      if (this.state.crash) {
+        throw new Error('Oo oo ah ah!')
+      }
       return (
         <div className={style.container}>
           <Header className={sharedStyles.segmentTitle} content="Sandbox" size="large" />
@@ -17,6 +22,11 @@ class SandboxContainer extends Component {
                   compact
                   content={(<i>{'"I don\'t like sand. It\'s coarse and rough and irritating and it gets everywhere."'}</i>)}
                 />
+              </Grid.Row>
+              <Grid.Row>
+                <Button.Group>
+                  <Button negative content="Chaos Monkey" icon="trash" onClick={() => this.setState({ crash: true })} />
+                </Button.Group>
               </Grid.Row>
             </Grid>
           </Segment>
