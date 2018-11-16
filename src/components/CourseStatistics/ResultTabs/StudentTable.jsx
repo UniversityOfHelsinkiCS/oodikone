bf0ee501cd420f5ad6ccd1110a4a7eb3e56b0293
ikuisma <ilkka.a.kuisma@gmail.com>
2018-11-16 16:49:40 +0200

    New automagical SortableTable component + sortable tables to course statistics

--- src/components/CourseStatistics/ResultTabs/StudentTable.jsx
+++ src/components/CourseStatistics/ResultTabs/StudentTable.jsx
@@ -1,34 +1,37 @@
 import React from 'react'
-import { Table, Header } from 'semantic-ui-react'
+import { Header } from 'semantic-ui-react'
 import { shape, string, number, oneOfType, arrayOf } from 'prop-types'
+import SortableTable from '../SortableTable'
 
-const StudentTable = ({ stats, name }) => (
-  <div>
-    <Header as="h3" content={name} textAlign="center" />
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell content="Time" />
-          <Table.HeaderCell content="Passed on first try" />
-          <Table.HeaderCell content="Passed after retry" />
-          <Table.HeaderCell content="Failed on first try" />
-          <Table.HeaderCell content="Failed after retry" />
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {stats.map(stat => (
-          <Table.Row key={stat.code}>
-            <Table.Cell content={stat.name} singleLine />
-            <Table.Cell content={stat.students.categories.passedFirst || 0} />
-            <Table.Cell content={stat.students.categories.passedRetry || 0} />
-            <Table.Cell content={stat.students.categories.failedFirst || 0} />
-            <Table.Cell content={stat.students.categories.failedRetry || 0} />
-          </Table.Row>
-          ))}
-      </Table.Body>
-    </Table>
-  </div>
-)
+const StudentTable = ({ stats, name }) => {
+  const formatted = stats.map((statistic) => {
+    const { name: n, code, students } = statistic
+    const {
+      passedFirst = 0,
+      passedRetry = 0,
+      failedFirst = 0,
+      failedRetry = 0
+    } = students.categories
+    return ({ name: n, code, passedFirst, passedRetry, failedFirst, failedRetry })
+  })
+  return (
+    <div>
+      <Header as="h3" content={name} textAlign="center" />
+      <SortableTable
+        getRowKey={s => s.code}
+        tableProps={{ celled: true }}
+        columns={[
+          { key: 'TIME', title: 'Time', getRowVal: s => s.code, getRowContent: s => s.name, cellProps: { width: 4, singleLine: true } },
+          { key: 'PASS_FIRST', title: 'Passed on first try', getRowVal: s => s.passedFirst, cellProps: { width: 3 } },
+          { key: 'PASS_RETRY', title: 'Passed after retry', getRowVal: s => s.passedRetry, cellProps: { width: 3 } },
+          { key: 'FAIL_FIRST', title: 'Failed on first try', getRowVal: s => s.failedFirst, cellProps: { width: 3 } },
+          { key: 'FAIL_RETRY', title: 'Failed on retry', getRowVal: s => s.failedRetry, cellProps: { width: 3 } }
+        ]}
+        data={formatted}
+      />
+    </div>
+  )
+}
 
 StudentTable.propTypes = {
   stats: arrayOf(shape({})).isRequired,
