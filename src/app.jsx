a968a7bc909f63848dd910f3765ee95ef44f86b1
Uotila Terho <terho.uotila@helsinki.fi>
2018-11-23 14:31:23 +0200

    do not send sentry errors from development enviroment

--- src/app.jsx
+++ src/app.jsx
@@ -16,13 +16,17 @@ import reducers from './redux'
 import { handleRequest } from './apiConnection'
 import Main from './components/Main'
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+
 const av = navigator.appVersion
 console.log(av) // eslint-disable-line
 if (av.indexOf('MSIE') !== -1 || av.indexOf('Trident/') !== -1) {
   alert("Internet Explorer is not supported. Please use a web browser from this decade. e.g. Google Chrome or Firefox.") // eslint-disable-line
 }
 try {
-  Sentry.init({ dsn: 'https://02d07bd40f404cc0965f38f06183d9fb@toska.cs.helsinki.fi/3' }) // eslint-disable-line
+  if (IS_PRODUCTION) {
+    Sentry.init({ dsn: 'https://02d07bd40f404cc0965f38f06183d9fb@toska.cs.helsinki.fi/3' }) // eslint-disable-line
+  }
 } catch (e) {
   console.log(e) // eslint-disable-line
 }
@@ -30,7 +34,7 @@ try {
 const translations = require('./i18n/translations.json')
 
 // eslint-disable-next-line
-const composeEnhancers = (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composeEnhancers = (!IS_PRODUCTION && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, handleRequest)))
 store.dispatch(initialize(AVAILABLE_LANGUAGES, { defaultLanguage: DEFAULT_LANG }))
