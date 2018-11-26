22b84a446af97d395a0ff3995fe2d44fceab7a27
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-11-26 17:57:05 +0200

    Fix BEFORE semester count

--- test/jest/utils/semester.test.js
+++ test/jest/utils/semester.test.js
@@ -10,6 +10,7 @@ describe('Semester utils tests', () => {
     expect(semesterUtils.getPassingSemester(2018, moment('2019-09-01'))).toBe('1-FALL')
     expect(semesterUtils.getPassingSemester(2018, moment('2020-05-01'))).toBe('1-SPRING')
     expect(semesterUtils.getPassingSemester(2018, moment('2026-05-01'))).toBe('LATER')
+    expect(semesterUtils.getPassingSemester(2018, moment('2018-05-01'))).toBe('BEFORE')
   })
 
 })
