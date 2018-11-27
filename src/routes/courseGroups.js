3d7454fe2e295db1492fec0b6469ddd4ffc1a34f
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-11-27 13:00:32 +0200

    teachers to service

--- src/routes/courseGroups.js
+++ src/routes/courseGroups.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 
-const EP_TEACHERS = ['017715', '019051', '053532', '028579', '036199', '083257', '089822', '128474']
-const KP_TEACHERS = ['032147', '012926', '066993']
+const CourseGroupService = require('../services/courseGroups')
 
 router.get('/courseGroups', async (req, res) => {
   res.json([
@@ -11,15 +10,9 @@ router.get('/courseGroups', async (req, res) => {
 })
 
 router.get('/courseGroups/:id/teachers', async (req, res) => {
-  if (req.params.id === '1') {
-    return res.json(EP_TEACHERS)
-  }
+  const teachers = CourseGroupService.getTeachersForCourseGroup(parseInt(req.params.id, 10))
 
-  if (req.params.id === '2') {
-    return res.json(KP_TEACHERS)
-  }
-
-  return res.send(404)
+  return teachers ? res.send(teachers) : res.send(404)
 })
 
 module.exports = router
