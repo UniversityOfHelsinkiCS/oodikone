c4707b136b925a7b72565521dba5c90a2bb23f7d
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-11-27 12:17:28 +0200

    Add courseGroups teachers endpoint & tests

--- src/routes/courseGroups.js
+++ src/routes/courseGroups.js
@@ -7,4 +7,21 @@ router.get('/courseGroups', async (req, res) => {
   ])
 })
 
+router.get('/courseGroups/:id/teachers', async (req, res) => {
+  if (req.params.id === '1') {
+    return res.json([
+      '017715',
+      '019051',
+      '053532',
+      '028579',
+      '036199',
+      '083257',
+      '089822',
+      '128474'
+    ])
+  }
+
+  return res.send(404)
+})
+
 module.exports = router
