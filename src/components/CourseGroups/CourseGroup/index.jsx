d1eb3f093a5ab66c1050e50724bf1806d9c75929
eero3 <eero.siren@kolumbus.fi>
2018-11-27 09:08:19 +0200

    added navigation and sensible way of doing it with routes

--- src/components/CourseGroups/CourseGroup/index.jsx
+++ src/components/CourseGroups/CourseGroup/index.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Segment, Header, Button } from 'semantic-ui-react'
+import { string, func, shape } from 'prop-types'
+import { withRouter } from 'react-router'
 
-const CourseGroup = () => (
-  <div>Course Group</div>
-)
+import { getCompiledPath } from '../../../common'
+import { routes } from '../../../constants'
 
-export default CourseGroup
+import styles from './courseGroup.css'
+
+class CourseGroup extends Component {
+  static propTypes = {
+    groupId: string.isRequired,
+    history: shape({
+      push: func.isRequired
+    }).isRequired
+  }
+
+  state = {}
+
+  render() {
+    const { groupId, history } = this.props
+    const navigateTo = route => history.push(getCompiledPath(route, {}))
+    return (
+      <Segment>
+        <Header size="medium" className={styles.courseGroupHeader}>
+          {groupId}
+          <Button
+            icon="reply"
+            onClick={() => navigateTo(routes.courseGroups.route)}
+            className={styles.returnIconButton}
+          />
+        </Header>
+      </Segment>
+    )
+  }
+}
+
+export default withRouter(CourseGroup)
