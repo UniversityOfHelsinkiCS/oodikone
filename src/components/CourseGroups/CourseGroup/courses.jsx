75ba497d31bbccb6928235eedb66bd8566ae3b03
eero3 <eero.siren@kolumbus.fi>
2018-11-27 17:52:23 +0200

    added teachers and courses components

---
+++ src/components/CourseGroups/CourseGroup/courses.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Header, Segment } from 'semantic-ui-react'
+
+const Courses = () => (
+  <Segment>
+    <Header size="medium" content="Courses" />
+  </Segment>
+)
+
+export default Courses
