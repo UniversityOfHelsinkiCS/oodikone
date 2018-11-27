ba87eab8dc4a3ef24bd673bcc3a60849b67130dc
eero3 <eero.siren@kolumbus.fi>
2018-11-27 17:52:46 +0200

    added courseGroups by id route and mocked data for it

--- src/services/courseGroups.js
+++ src/services/courseGroups.js
@@ -1,7 +1,9 @@
+const teachers = require('./teachers')
+
 const EP_TEACHERS = ['017715', '019051', '053532', '028579', '036199', '083257', '089822', '128474']
 const KP_TEACHERS = ['032147', '012926', '066993']
 
-const getTeachersForCourseGroup = courseGroupId => {
+const getTeachersForCourseGroup = (courseGroupId) => {
   if (courseGroupId === 1) {
     return EP_TEACHERS
   }
@@ -11,6 +13,31 @@ const getTeachersForCourseGroup = courseGroupId => {
   }
 }
 
+const getCourseGroup = async (courseGroupId) => {
+  if (courseGroupId === 1) {
+    return {
+      id: 1,
+      name: 'Erityispedagogiikka',
+      totalStudents: 10,
+      totalCredits: 100,
+      totalCourses: 1000,
+      teachers: await teachers.getTeachersByIds(EP_TEACHERS)
+    }
+  }
+
+  if (courseGroupId === 2) {
+    return {
+      id: 2,
+      name: 'Kasvatuspsykologia',
+      totalStudents: 10,
+      totalCredits: 100,
+      totalCourses: 1000,
+      teachers: await teachers.getTeachersByIds(KP_TEACHERS)
+    }
+  }
+}
+
 module.exports = {
-  getTeachersForCourseGroup
+  getTeachersForCourseGroup,
+  getCourseGroup
 }
