aa67ce70e253114a53ab89809f5b6a1c1c05cc7f
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-11-27 12:36:18 +0200

    Add more teachers

--- src/routes/courseGroups.js
+++ src/routes/courseGroups.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 
+const EP_TEACHERS = ['017715', '019051', '053532', '028579', '036199', '083257', '089822', '128474']
+const KP_TEACHERS = ['032147', '012926', '066993']
+
 router.get('/courseGroups', async (req, res) => {
   res.json([
     { id: 1, name: 'Erityispedagogiikka' },
@@ -9,16 +12,11 @@ router.get('/courseGroups', async (req, res) => {
 
 router.get('/courseGroups/:id/teachers', async (req, res) => {
   if (req.params.id === '1') {
-    return res.json([
-      '017715',
-      '019051',
-      '053532',
-      '028579',
-      '036199',
-      '083257',
-      '089822',
-      '128474'
-    ])
+    return res.json(EP_TEACHERS)
+  }
+
+  if (req.params.id === '2') {
+    return res.json(KP_TEACHERS)
   }
 
   return res.send(404)
