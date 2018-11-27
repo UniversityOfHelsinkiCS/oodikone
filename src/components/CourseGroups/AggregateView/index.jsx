97c1700c62a0919861d002c07699b9de37e96c8a
eero3 <eero.siren@kolumbus.fi>
2018-11-27 09:08:19 +0200

    added navigation and sensible way of doing it with routes

--- src/components/CourseGroups/AggregateView/index.jsx
+++ src/components/CourseGroups/AggregateView/index.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Header, Segment, Button } from 'semantic-ui-react'
+import { withRouter } from 'react-router'
+import { func, shape } from 'prop-types'
 
-const AggregateView = () => (
-  <div>Aggregate view</div>
-)
+import { getCompiledPath } from '../../../common'
+import { routes } from '../../../constants'
 
-export default AggregateView
+
+const AggregateView = ({ history }) => {
+  const navigateToCourseGroup = courseGroupId =>
+    history.push(getCompiledPath(routes.courseGroups.route, { courseGroupId }))
+
+  return (
+    <Segment>
+      <Header size="medium">Group statistics</Header>
+      <Button onClick={() => navigateToCourseGroup('groupId')}>navigation test</Button>
+    </Segment>
+  )
+}
+
+AggregateView.propTypes = {
+  history: shape({
+    push: func.isRequired
+  }).isRequired
+}
+
+export default withRouter(AggregateView)
