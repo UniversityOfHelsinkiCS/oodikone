5801a1c46c513b399d52d5680b70167eefc65048
eero3 <eero.siren@kolumbus.fi>
2018-11-28 18:03:22 +0200

    Merge branch 'course-groups' of https://github.com/UniversityOfHelsinkiCS/oodikone2-backend into course-group-teachers
    # Conflicts:
    #	src/services/courseGroups.js

--- src/services/courseGroups.js
+++ src/services/courseGroups.js
@@ -1,11 +1,5 @@
 const Promise = require('bluebird')
-
 const { sequelize } = require('../database/connection')
-
-const { sequelize } = require('../database/connection')
-const { Op } = require('sequelize')
-const { Teacher } = require('../models/index')
-
 
 const EP_TEACHERS = ['017715', '019051', '053532', '028579', '036199', '083257', '089822', '128474']
 const KP_TEACHERS = ['032147', '012926', '066993']
@@ -13,7 +7,6 @@ const KP_TEACHERS = ['032147', '012926', '066993']
 const COURSE_GROUP_NAMES = { 1: 'Erityispedagogiikka', 2: 'Kasvatuspsykologia' }
 const STATISTICS_START_SEMESTER = 135
 
-const getTeachersForCourseGroup = courseGroupId => {
 const getTeachersForCourseGroup = (courseGroupId) => {
   if (courseGroupId === 1) {
     return EP_TEACHERS
@@ -24,6 +17,20 @@ const getTeachersForCourseGroup = (courseGroupId) => {
   }
 }
 
+const getCourseGroupInfoByTeacherIds = teacherIds => sequelize.query(
+  `select
+        count(distinct c.course_code) as courses,
+        sum(credits) as credits,
+        count(distinct student_studentnumber) as students
+      from credit_teachers ct
+        left join credit c on ct.credit_id = c.id
+      where
+        ct.teacher_id in (:teacherIds)
+      and
+        c.semestercode >= ${STATISTICS_START_SEMESTER}`,
+  { replacements: { teacherIds }, type: sequelize.QueryTypes.SELECT }
+)
+
 const getCourseGroupsWithTotals = () => {
   const courseGroupIds = [1, 2]
 
@@ -31,17 +38,7 @@ const getCourseGroupsWithTotals = () => {
     const teachers = getTeachersForCourseGroup(courseGroupId)
     const name = COURSE_GROUP_NAMES[courseGroupId]
 
-    const statistics = await sequelize.query(
-      `select
-        sum(credits) as credits,
-        count(distinct student_studentnumber) as students
-      from credit_teachers ct
-      left join credit c on ct.credit_id = c.id
-      where
-        ct.teacher_id in (:teachers)
-      and
-        c.semestercode >= ${STATISTICS_START_SEMESTER}`,
-      { replacements: { teachers }, type: sequelize.QueryTypes.SELECT })
+    const statistics = await getCourseGroupInfoByTeacherIds(teachers)
 
     return {
       id: courseGroupId,
@@ -52,41 +49,43 @@ const getCourseGroupsWithTotals = () => {
   })
 }
 
-const getTeachersByIds = teacherIds => Teacher.findAll({
-  attributes: ['name', 'code', 'id'],
-  where: {
-    id: {
-      [Op.in]: teacherIds
-    }
-  }
-})
-
+const getTeachersByIds = async teacherIds => sequelize.query(
+  `select        
+        t.id as id,
+        t.code as code,
+        t.name as name,
+        count(distinct c.course_code) as courses,
+        sum(credits) as credits,
+        count(distinct student_studentnumber) as students
+      from credit_teachers ct
+        left join credit c on ct.credit_id = c.id
+        left join teacher t on ct.teacher_id = t.id     
+      where
+        semestercode >= ${STATISTICS_START_SEMESTER}
+      and
+        ct.teacher_id in (:teacherIds)
+      group by t.id, t.name, t.code`,
+  { replacements: { teacherIds }, type: sequelize.QueryTypes.SELECT }
+)
 
 const getCourseGroup = async (courseGroupId) => {
-  if (courseGroupId === 1) {
-    return {
-      id: 1,
-      name: 'Erityispedagogiikka',
-      totalStudents: 10,
-      totalCredits: 100,
-      totalCourses: 1000,
-      teachers: await getTeachersByIds(EP_TEACHERS)
-    }
-  }
+  const teacherIds = getTeachersForCourseGroup(courseGroupId)
+  const name = COURSE_GROUP_NAMES[courseGroupId]
+  const statistics = await getCourseGroupInfoByTeacherIds(teacherIds)
+  const teachers = await getTeachersByIds(teacherIds)
 
-  if (courseGroupId === 2) {
-    return {
-      id: 2,
-      name: 'Kasvatuspsykologia',
-      totalStudents: 10,
-      totalCredits: 100,
-      totalCourses: 1000,
-      teachers: await getTeachersByIds(KP_TEACHERS)
-    }
+  const { credits, students, courses } = statistics[0]
+
+  return {
+    id: courseGroupId,
+    name,
+    teachers,
+    totalCredits: credits,
+    totalStudents: students,
+    totalCourses: courses
   }
 }
 
-const STATISTICS_START_SEMESTER = 135
 const getCoursesByTeachers = async teacherIds => sequelize.query(
   `select        
         course_code as coursecode,
@@ -110,8 +109,7 @@ const getCoursesByTeachers = async teacherIds => sequelize.query(
 
 module.exports = {
   getTeachersForCourseGroup,
-  getCourseGroupsWithTotals
-  getTeachersForCourseGroup,
+  getCourseGroupsWithTotals,
   getCourseGroup,
   getCoursesByTeachers
 }
