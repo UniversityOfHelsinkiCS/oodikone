434aa5a9442e58b61e5aaeafe0965393e08510bd
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-11-28 17:19:50 +0200

    Show aggregated data for course groups

--- src/components/CourseGroups/AggregateView/index.jsx
+++ src/components/CourseGroups/AggregateView/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Header, Segment, Button } from 'semantic-ui-react'
+import { Header, Segment, Table } from 'semantic-ui-react'
 import { withRouter } from 'react-router'
 import { func, shape } from 'prop-types'
 
@@ -8,27 +8,60 @@ import { routes } from '../../../constants'
 
 import { callApi } from '../../../apiConnection'
 
+const getCourseGroupPath = courseGroupId => getCompiledPath(routes.courseGroups.route, { courseGroupId })
+
 class AggregateView extends Component {
   state = {
+    isLoading: true,
     courseGroups: []
   }
 
   componentDidMount() {
     callApi('/courseGroups')
       .then((res) => {
-        console.log(res.data)
-        this.setState({ courseGroups: res.data })
+        this.setState({
+          courseGroups: res.data,
+          isLoading: false
+        })
       })
   }
 
+  handleNavigation = (e, courseGroupId) => {
+    e.preventDefault()
+    this.props.history.push(getCourseGroupPath(courseGroupId))
+  }
+
   render() {
-    const navigateToCourseGroup = courseGroupId =>
-      this.props.history.push(getCompiledPath(routes.courseGroups.route, { courseGroupId }))
+    const { isLoading, courseGroups } = this.state
 
     return (
-      <Segment>
+      <Segment loading={isLoading}>
         <Header size="medium">Group statistics</Header>
-        <Button onClick={() => navigateToCourseGroup('groupId')}>navigation test</Button>
+        <Table>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell content="Course group" />
+              <Table.HeaderCell content="Credits" />
+              <Table.HeaderCell content="Students" />
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {courseGroups.map(courseGroup => (
+              <Table.Row key={courseGroup.id}>
+                <Table.Cell>
+                  <a
+                    href={getCourseGroupPath(courseGroup.id)}
+                    onClick={e => this.handleNavigation(e, courseGroup.id)}
+                  >
+                    {courseGroup.name}
+                  </a>
+                </Table.Cell>
+                <Table.Cell content={courseGroup.credits} />
+                <Table.Cell content={courseGroup.students} />
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
       </Segment>
     )
   }
