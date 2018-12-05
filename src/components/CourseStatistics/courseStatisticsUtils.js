950fb88fe4dc2a6ca85be871c60fa004ed232636
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-12-05 18:31:51 +0200

    Fix year selecting

--- src/components/CourseStatistics/courseStatisticsUtils.js
+++ src/components/CourseStatistics/courseStatisticsUtils.js
@@ -2,7 +2,8 @@ import moment from 'moment'
 
 const MIN_YEAR = 1899
 const MAX_YEAR = 2112
-const MIN_SELECTABLE_SEMESTER = '1950-51'
+const MIN_DEFAULT_YEAR = 2000
+const MIN_DEFAULT_SEMESTER = '2000-01'
 
 const isSpring = date => moment(date).month() < 7
 const isPre2016Course = course => !Number.isNaN(Number(course.code.charAt(0)))
@@ -12,13 +13,13 @@ const getYearText = (year, spring) => (spring ? getSemesterText(year - 1, year) 
 
 const getCourseYears = course => ({
   startYear: moment(course.startdate).year(),
-  endYear: moment(course.enddate).year()
+  endYear: moment(course.max_attainment_date || course.enddate).year()
 })
 
 const getActiveYears = (course) => {
   const { startYear, endYear } = getCourseYears(course)
   const startYearText = getYearText(startYear, isSpring(course.startdate))
-  const endYearText = getYearText(endYear, isSpring(course.enddate))
+  const endYearText = getYearText(endYear, isSpring(course.max_attainment_date || course.enddate))
 
   if (endYear === MAX_YEAR && isPre2016Course(course)) {
     return `— ${getYearText(2016, false)}`
@@ -40,15 +41,15 @@ const getCourseSemesters = (course) => {
 
   return {
     start: getYearText(startYear, isSpring(course.startdate)),
-    end: getYearText(endYear, isSpring(course.enddate))
+    end: getYearText(endYear, isSpring(course.max_attainment_date || course.enddate))
   }
 }
 
 const getStartAndEndYearValues = (course, years) => {
   const { start, end } = getCourseSemesters(course)
 
-  const startYear = Number(start.substring(0, 4)) < 1950
-    ? years.find(year => year.text === MIN_SELECTABLE_SEMESTER)
+  const startYear = Number(start.substring(0, 4)) < MIN_DEFAULT_YEAR
+    ? years.find(year => year.text === MIN_DEFAULT_SEMESTER)
     : years.find(year => year.text === start)
 
   const endYear = Number(start.substring(0, 4) === MAX_YEAR) ? undefined : years.find(year => year.text === end)
