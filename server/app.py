02686770697b107760b6236f2e7656e9ca781976
Mikko Tiainen <mikko.o.tiainen@helsinki.fi>
2018-12-05 14:24:18 +0200

    added route to graduation server endpoint

--- server/app.py
+++ server/app.py
@@ -10,6 +10,9 @@ from keras import backend
 import pandas as pd
 import json
 import networkx as nx
+from operator import itemgetter
+import itertools
+
 
 app = Flask(__name__)
 load_dotenv()
@@ -29,7 +32,7 @@ dimensions = [
 ]
 
 def map_old_to_new(data):
-  jsonData=json.load(open('./data/oldToNew.json', 'rb'))
+  jsonData=json.load(open('../data/oldToNew.json', 'rb'))
   oldToNew = pd.DataFrame(jsonData, index=range(len(jsonData)), columns=["old", "new"])
   oldToNew["old"] = jsonData.keys()
   oldToNew["new"] = jsonData.values()
@@ -46,29 +49,32 @@ def ping():
   print('someone is pinging')
   return 'pong'
 
+# This is to calculate the different groups
+# It will assign each student to "above", "below" and "average" group on each dimension.
+# Use this for what the fuck group comparisons.
 @app.route('/calc-groups')
 def calc_groups():
   pipeline = [
     { "$group": {
-       "_id": "$Kysely",
-       "avgSBI": { "$avg": "$SBI" },
-       "avgOrganised": { "$avg": "$Organised" },
-       "avgSurface": { "$avg": "$Surface" },
-       "avgDeep": { "$avg": "$Deep" },
-       "avgSE": { "$avg": "$SE" },
-       "avgIntRel": { "$avg": "$IntRel" },
-       "avgPeer": { "$avg": "$Peer" },
-       "avgAlign": { "$avg": "$Align" },
-       "avgConsFeed": { "$avg": "$ConsFeed" },
-       "sdSBI": { "$avg": "$SBI" },
-       "sdOrganised": { "$stdDevSamp": "$Organised" },
-       "sdSurface": { "$stdDevSamp": "$Surface" },
-       "sdDeep": { "$stdDevSamp": "$Deep" },
-       "sdSE": { "$stdDevSamp": "$SE" },
-       "sdIntRel": { "$stdDevSamp": "$IntRel" },
-       "sdPeer": { "$stdDevSamp": "$Peer" },
-       "sdAlign": { "$stdDevSamp": "$Align" },
-       "sdConsFeed": { "$stdDevSamp": "$ConsFeed" }
+      "_id": "$Kysely",
+      "avgSBI": { "$avg": "$SBI" },
+      "avgOrganised": { "$avg": "$Organised" },
+      "avgSurface": { "$avg": "$Surface" },
+      "avgDeep": { "$avg": "$Deep" },
+      "avgSE": { "$avg": "$SE" },
+      "avgIntRel": { "$avg": "$IntRel" },
+      "avgPeer": { "$avg": "$Peer" },
+      "avgAlign": { "$avg": "$Align" },
+      "avgConsFeed": { "$avg": "$ConsFeed" },
+      "sdSBI": { "$avg": "$SBI" },
+      "sdOrganised": { "$stdDevSamp": "$Organised" },
+      "sdSurface": { "$stdDevSamp": "$Surface" },
+      "sdDeep": { "$stdDevSamp": "$Deep" },
+      "sdSE": { "$stdDevSamp": "$SE" },
+      "sdIntRel": { "$stdDevSamp": "$IntRel" },
+      "sdPeer": { "$stdDevSamp": "$Peer" },
+      "sdAlign": { "$stdDevSamp": "$Align" },
+      "sdConsFeed": { "$stdDevSamp": "$ConsFeed" }
       }
     }
   ]
@@ -145,26 +151,30 @@ def get_averages():
       'ConstFeed': cons_feed
     }
   return json_util.dumps(grade_students)
+
 @app.route("/suggest_new_course")
 def suggest_new_course():
   done_courses = request.args.getlist("doneCourses[]")
+  period = request.args.getlist("period")
+  if not period:
+    period = 274
   done_courses = map_old_to_new(pd.DataFrame(done_courses, columns=["code"]))["code"].unique()
   g = pickle.load(open('../models/graph_pruned.sav', 'rb'))
 
-  now = 274 # TODO GET REALTIME
+  now = period # 274 # TODO GET REALTIME
   edges = []
   suggested = {}
   for course in done_courses:
     for edge in g[course]:
       if edge in done_courses or g[course][edge]["period"] < now - 8:
         continue
-      weight = (g[course][edge]["weight"] + (g[course][edge]["count"] * 0.0005))
+      weight = (g[course][edge]["weight"] + (g[course][edge]["count"] * 0.01))
       if edge in suggested:
         if suggested[edge] > weight:
           continue
       suggested[edge] = weight
-  print(suggested)
-  top_three = sorted(suggested.items(), key=lambda kv: kv[1])[-3:]
+  # print(suggested)
+  top_three = sorted(suggested.items(), key=lambda kv: kv[1])[-7:]
   return json_util.dumps([tuplez[0] for tuplez in top_three])
 
 @app.route('/grade_estimate')
@@ -193,6 +203,91 @@ def grade_estimate():
       model = load_model(course)
       res = model.predict(np.array(data))
   return str(np.argmax(res))
+
+@app.route('/suggest_route_to_graduation')
+def suggest_route_to_graduation(done_courses=[]):
+  done_courses = map_old_to_new(pd.DataFrame(done_courses, columns=["code"]))["code"].unique()
+  g = pickle.load(open('../models/graph_acyclic.sav', 'rb'))
+  compulsory = pd.read_json("../data/degree_structure.json")
+  compulsory = compulsory["CS"].map(lambda x: x["courses"] if 'courses' in x else [x[0]["courses"], x[1]["courses"]])
+  compulsory = compulsory[0] + compulsory[1] + compulsory[2] + compulsory[3][0] + compulsory[3][1]
+  
+  def find_path(graph, start, path=[]):
+    path = path + [start]
+    s = start
+    top_three = ["none", "none", "none"]
+    top_three_v = [-100,-100,-100]
+    top_comp = {}
+    # Initialize dict with arrays for each compulsory course
+    for comp in compulsory:
+      top_comp[comp] = []
+    # Initialize the top three courses for first period. Why? How?
+    for v in graph[s]:
+      if v not in path and any(graph[s][v]["grade"] > i for i in top_three_v) and len(graph[v]) > 0:
+        idx = np.argmin(top_three_v)
+        top_three[idx] = v
+        top_three_v[idx] = graph[s][v]["grade"]
+    path = path + top_three
+
+    def naive_bois(path, graph, i):
+      top_three = ["none", "none", "none"]
+      top_three_v = [-100,-100,-100]
+      for v in path[-3:-1]:
+        if v == "none":
+          continue
+        for pot in graph[v]:
+          period = pot.split("_")[0]
+          course_name = pot.split("_")[1]
+          grade = graph[v][pot]["grade"]
+          count = graph[v][pot]["count"]
+          grade += count * 0.05
+          # if re.search(r"^3[0-9]_TKT20014", pot):
+          #   return path + [pot]
+
+          if course_name in compulsory:
+            top_comp[course_name] = top_comp[course_name] + [(grade + count * 0.05, period, count)]
+
+          if course_name not in [z.split("_")[1] for z in path[1:]] and any(grade > i for i in top_three_v) and len(graph[pot]) > 0 and pot not in top_three:
+              idx = np.argmin(top_three_v)
+              top_three[idx] = pot
+              top_three_v[idx] = grade
+        
+      path = path + [y for y in top_three if y != "none"]
+      if i  == 13:
+        return path
+      return naive_bois(path, graph, i + 1)
+      
+    path = naive_bois(path, graph, 0)
+
+    for key in top_comp.keys():
+      filtered = [x for x in top_comp[key] if x[2] > 4]
+      top_comp_options = [(k, max(g, key=lambda a: a[0])) for k, g in itertools.groupby(filtered, itemgetter(1))]
+      top_comp[key] = top_comp_options
+    sorted_top_comp = sorted(top_comp.keys(), key=lambda a: len(top_comp[a]))
+    for c in path:
+      course = c.split("_")
+      if len(course) > 1 and course[1] in sorted_top_comp:
+        sorted_top_comp.remove(course[1])
+    # print(sorted_top_comp)
+    for comp_course in sorted_top_comp:
+      cur_course = top_comp[comp_course]
+      handled = False
+      for period in cur_course:
+        if handled:
+          break
+        for path_i, path_course in enumerate(path):
+          pc = path_course.split("_")
+          if pc[0] == period[0] and pc[1] not in sorted_top_comp:
+            handled = True
+            path[path_i] = period[0] + "_" + comp_course
+            break
+    # print(path)
+    return path
+
+  path = find_path(g,"start")
+  # print(path)
+  #print(routes)
+  return json_util.dumps(path)
 
 @app.route('/<course_code>')
 def get_cluster(course_code):
