d1b4eaddc2479a1a0f2f59d3d34264e368dd6e80
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-12-05 18:24:13 +0200

    Use very fast sql

--- src/services/doo_api_database_updater/update_max_attainment_dates.js
+++ src/services/doo_api_database_updater/update_max_attainment_dates.js
@@ -1,28 +1,13 @@
-const Promise = require('bluebird')
-const logger = require('../../util/logger')
 const { sequelize } = require('../../database/connection')
 
 const updateMaxAttainmentDates = async () => {
-  const courseCodes = await sequelize.query('select code from course', { type: sequelize.QueryTypes.SELECT })
-
-  await Promise.each(courseCodes, async (row) => {
-    const courseCode = row.code
-
-    try {
-      const maxDate = await sequelize.query(
-        'select course_code, max(attainment_date) from credit where course_code = :courseCode group by course_code',
-        { replacements: { courseCode }, type: sequelize.QueryTypes.SELECT }
-      )
-      const maxAttainmentDate = maxDate[0].max
-
-      await sequelize.query(
-        'update course set max_attainment_date = :maxAttainmentDate where code = :courseCode',
-        { replacements: { maxAttainmentDate, courseCode }, type: sequelize.QueryTypes.UPDATE }
-      )
-    } catch (err) {
-      logger.info(`No attainments for course ${courseCode}`)
-    }
-  })
+  await sequelize.query(
+    `UPDATE course
+     SET max_attainment_date = cr.max
+     FROM (select course_code, max(attainment_date) from credit group by course_code) cr
+     WHERE course.code=cr.course_code`,
+    { type: sequelize.QueryTypes.UPDATE }
+  )
 }
 
 module.exports = {
