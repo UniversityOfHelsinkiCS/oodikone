df4e3d1d099b5abb247bed475c102c748a6baacf
totutotu <tuomo.torppa@gmail.com>
2018-12-07 14:04:03 +0200

    min attainment date to course model

--- scripts/update_max_attainment_dates.js
+++ scripts/update_max_attainment_dates.js
@@ -1,11 +1,11 @@
 const logger = require('../src/util/logger')
-const { updateMaxAttainmentDates } = require('../src/services/doo_api_database_updater/update_max_attainment_dates')
+const { updateAttainmentDates } = require('../src/services/doo_api_database_updater/update_attainment_dates')
 
 const run = async () => {
   try {
     logger.info('starting')
 
-    await updateMaxAttainmentDates()
+    await updateAttainmentDates()
 
     logger.info('complete')
     process.exit(0)
