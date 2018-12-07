8ffd995afc87b04c48b73481f6897e92da5b8985
eero3 <eero.siren@kolumbus.fi>
2018-12-07 09:47:26 +0200

    added tests and fixed route

--- src/routes/courseGroups.js
+++ src/routes/courseGroups.js
@@ -7,25 +7,25 @@ const BASE_PATH = '/course-groups'
 router.get(BASE_PATH, async (req, res) => {
   const semesterCode = req.query.semester
   const courseGroups = await CourseGroupService.getCourseGroupsWithTotals(semesterCode)
-  res.json(courseGroups)
+  return res.json(courseGroups)
 })
 
 router.get(`${BASE_PATH}/academic-years`, async (req, res) => {
   const academicYears = await CourseGroupService.getAcademicYears()
-  res.json(academicYears)
+  return res.json(academicYears)
 })
 
-router.get(`${BASE_PATH}/teachers`, async (req, res) => {
+router.get(`${BASE_PATH}/courses`, async (req, res) => {
   const { teacherIds: ids, semester } = req.query
-  const teacherIds = JSON.parse(ids)
+  const teacherIds = ids && JSON.parse(ids)
 
-  if (!Array.isArray(teacherIds)) {
-    res.send(400)
+  if (!teacherIds || !Array.isArray(teacherIds)) {
+    return res.sendStatus(400)
   }
 
   const courses = await CourseGroupService.getCoursesByTeachers(teacherIds, semester)
 
-  return courses ? res.send(courses) : res.send(404)
+  return courses ? res.send(courses) : res.sendStatus(404)
 })
 
 router.get(`${BASE_PATH}/:id`, async (req, res) => {
@@ -33,7 +33,7 @@ router.get(`${BASE_PATH}/:id`, async (req, res) => {
   const { semester } = req.query
   const groupData = await CourseGroupService.getCourseGroup(Number(id), semester)
 
-  return groupData ? res.send(groupData) : res.send(404)
+  return groupData ? res.send(groupData) : res.sendStatus(404)
 })
 
 module.exports = router
--- test/endpoint/courseGroups.test.js
+++ test/endpoint/courseGroups.test.js
@@ -5,20 +5,20 @@ const { forceSyncDatabase } = require('../../src/database/connection')
 
 const { sequelize } = require('../../src/models/index')
 
-jest.mock('../../src/services/redis', () => ({
-  __esModule: true,
-  redisClient: {
-    getAsync: jest.fn(() => Promise.resolve()),
-    setAsync: jest.fn(() => Promise.resolve())
-  }
-}))
-
 const uid = 'tktl'
 const payload = { userId: uid, name: '', enabled: true, admin: true }
 
 const token = jwt.sign(payload, conf.TOKEN_SECRET, {
   expiresIn: '24h'
 })
+
+const API_PATH = '/api/course-groups'
+
+const MOCK_ACADEMIC_YEARS = [
+  { yearname: '2018-19', semestercode: 160 },
+  { yearname: '2017-18', semestercode: 159 },
+  { yearname: '2016-17', semestercode: 158 }
+]
 
 beforeAll(async () => {
   await forceSyncDatabase()
@@ -27,6 +27,21 @@ beforeAll(async () => {
 afterAll(async () => {
   await sequelize.close()
 })
+
+beforeEach(() => {
+  jest.mock('../../src/services/redis', () => ({
+    __esModule: true,
+    redisClient: {
+      getAsync: jest.fn(() => Promise.resolve()),
+      setAsync: jest.fn(() => Promise.resolve())
+    }
+  }))
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
 
 describe('Course groups endpoint tests', () => {
   test('Ping test', async () => {
@@ -38,31 +53,119 @@ describe('Course groups endpoint tests', () => {
     app.close()
   })
 
-  test('Get list of available course groups', async () => {
-    const app = require('../../src/app')
-    const expectedArray = [{
-      credits: null,
-      id: 1,
-      name: 'Erityispedagogiikka',
-      students: 0
-    },
-    {
-      credits: null,
-      id: 2,
-      name: 'Kasvatuspsykologia',
-      students: 0
-    }
-    ]
-    sequelize.query = jest.fn(() => Promise.resolve(expectedArray))
+  describe('/api/course-groups', () => {
+    test('Returns list of mocked course groups', async () => {
+      const app = require('../../src/app')
 
-    const res = await supertest(app)
-      .get('/api/course-groups')
-      .set('x-access-token', token)
-      .set('uid', uid)
+      const expectedCourseGroups = [{
+        credits: null,
+        id: 1,
+        name: 'Erityispedagogiikka',
+        students: 0
+      },
+      {
+        credits: null,
+        id: 2,
+        name: 'Kasvatuspsykologia',
+        students: 0
+      }
+      ]
+
+      sequelize.query = jest.fn(() => Promise.resolve(expectedCourseGroups))
+
+      const res = await supertest(app)
+        .get(API_PATH)
+        .set('x-access-token', token)
+        .set('uid', uid)
 
 
-    expect(res.status).toBe(200)
-    expect(res.body).toEqual(expectedArray)
-    app.close()
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual(expectedCourseGroups)
+      app.close()
+    })
+  })
+
+  describe('/api/course-groups/academic-years', () => {
+    test('Returns mocked academic years', async () => {
+      const app = require('../../src/app')
+
+      sequelize.query = jest.fn(() => Promise.resolve(MOCK_ACADEMIC_YEARS))
+
+      const res = await supertest(app)
+        .get(`${API_PATH}/academic-years`)
+        .set('x-access-token', token)
+        .set('uid', uid)
+
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual(MOCK_ACADEMIC_YEARS)
+      app.close()
+    })
+  })
+
+  describe('/api/course-groups/courses', () => {
+    test('Returns 400 bad request when called without teacherIds parameter', async () => {
+      const app = require('../../src/app')
+
+      const res = await supertest(app)
+        .get(`${API_PATH}/courses`)
+        .set('x-access-token', token)
+        .set('uid', uid)
+
+      expect(res.status).toBe(400)
+      app.close()
+    })
+
+    test('Returns 400 bad request when called with non-array teacherIds parameter', async () => {
+      const app = require('../../src/app')
+
+      const res = await supertest(app)
+        .get(`${API_PATH}/courses/?teacherIds=21314`)
+        .set('x-access-token', token)
+        .set('uid', uid)
+
+      expect(res.status).toBe(400)
+      app.close()
+    })
+
+    test('Returns mocked courses by teachers list', async () => {
+      const app = require('../../src/app')
+
+      const expectedTeachers = [
+        { coursecode: 'EDUK111',
+          coursenames:
+            { en: "Bachelor's Thesis",
+              fi: 'Kandidaatin tutkielma ja seminaari',
+              sv: 'Kandidatavhandling och seminarium'
+            },
+          teachercode: 'testio',
+          teachername: 'testi test testila',
+          credits: 10,
+          students: 1 },
+        { coursecode: 'EDUK123',
+          coursenames:
+            { en: "Course",
+              fi: 'Kurssi',
+              sv: 'kursser'
+            },
+          teachercode: 'testi2',
+          teachername: 'toinen to testi',
+          credits: 12,
+          students: 1 }]
+
+      sequelize.query = jest.fn()
+      sequelize.query.mockReturnValueOnce(Promise.resolve(MOCK_ACADEMIC_YEARS))
+      sequelize.query.mockReturnValueOnce(Promise.resolve(expectedTeachers))
+
+      const ids = ['021314', '021345']
+
+      const res = await supertest(app)
+        .get(`${API_PATH}/courses/?teacherIds=${JSON.stringify(ids)}`)
+        .set('x-access-token', token)
+        .set('uid', uid)
+
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual(expectedTeachers)
+      app.close()
+    })
   })
 })
