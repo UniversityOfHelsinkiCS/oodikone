423a563a1c0dd1f358fd6ae16e296450e7a263ce
ikuisma <ilkka.a.kuisma@gmail.com>
2018-12-17 12:09:31 +0200

    Use axios instance in userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,30 +1,30 @@
 const axios = require('axios')
+const { USERSERVICE_URL } = require('../conf-backend')
+
+const client = axios.create({ baseURL: USERSERVICE_URL })
 
 const ping = async () => {
-  const url = `${process.env.USERSERVICE_URL}/ping`
+  const url = '/ping'
   const response = await axios.get(url)
   return response.data
 }
 
 const byUsername = async (uid) => {
-  const url = `${process.env.USERSERVICE_URL}/user/${uid}`
-  const response = await axios.get(url)
+  const url = `/user/${uid}`
+  const response = await client.get(url)
   return response.data
 }
 
 const updateUser = async (uid, fields) => {
-  const url = `${process.env.USERSERVICE_URL}/user/${uid}`
-  const response = await axios.put(url, fields )
+  const url = `/user/${uid}`
+  const response = await client.put(url, fields)
   return response.data
 }
 
 const createUser = async (username, full_name, email) => {
-  const url = `${process.env.USERSERVICE_URL}/user`
-
-  const response = await axios.post(url, {
+  const response = await client.post('/user', {
     username, full_name, email
   })
-
   return response.data
 }
 
