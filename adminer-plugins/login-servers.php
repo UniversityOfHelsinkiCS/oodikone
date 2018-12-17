d4dc427d8375d42fb7284a7e7c987826a4b926ed
ikuisma <ilkka.a.kuisma@gmail.com>
2018-12-17 10:58:58 +0200

    Added Adminer to development setup

---
+++ adminer-plugins/login-servers.php
@@ -0,0 +1,11 @@
+<?php
+require_once('plugins/login-servers.php');
+
+/** Set supported servers
+    * @param array array($domain) or array($domain => $description) or array($category => array())
+    * @param string
+    */
+return new AdminerLoginServers(
+    $servers = array("db" => "db"),
+    $driver = "pgsql"
+);
