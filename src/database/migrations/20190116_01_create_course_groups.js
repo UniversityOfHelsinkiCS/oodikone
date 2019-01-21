6f2b7acf5efb826a88e83b250963db11c0e777bc
Rochet2 <tqbattlenet@gmail.com>
2019-01-21 12:36:33 +0200

    Fix migration dates and comment them out. Uncomment later!

---
+++ src/database/migrations/20190116_01_create_course_groups.js
@@ -0,0 +1,55 @@
+
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // await queryInterface.createTable('course_groups',
+    //   {
+    //     id: {
+    //       primaryKey: true,
+    //       type: Sequelize.BIGINT,
+    //       autoIncrement: true
+    //     },
+    //     name: {
+    //       type: Sequelize.STRING,
+    //       unique: true
+    //     },
+    //     createdAt: {
+    //       type: Sequelize.DATE
+    //     },
+    //     updatedAt: {
+    //       type: Sequelize.DATE
+    //     }
+    //   })
+    // await queryInterface.createTable('teacher_course_group',
+    //   {
+    //     id: {
+    //       primaryKey: true,
+    //       type: Sequelize.BIGINT,
+    //       autoIncrement: true
+    //     },
+    //     teacher_id: {
+    //       type: Sequelize.STRING,
+    //       references: {
+    //         model: 'teacher',
+    //         key: 'id'
+    //       }
+
+    //     },
+    //     course_group_id: {
+    //       type: Sequelize.BIGINT,
+    //       references: {
+    //         model: 'course_groups',
+    //         key: 'id'
+    //       }
+    //     },
+    //     createdAt: {
+    //       type: Sequelize.DATE
+    //     },
+    //     updatedAt: {
+    //       type: Sequelize.DATE
+    //     }
+    //   })
+  },
+  down: async () => {
+  }
+}
