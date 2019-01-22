6f92120adcd7feddf5aa5a041a77f71cfb8490a6
Rochet2 <tqbattlenet@gmail.com>
2019-01-22 17:02:10 +0200

    Refactor TeacherSearch to be more generic

---
+++ src/components/TeacherSearchTab/index.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+import { shape } from 'prop-types'
+import { Message } from 'semantic-ui-react'
+import { withRouter } from 'react-router'
+import TeacherSearch from '../TeacherSearch'
+
+const TeacherSearchTab = ({ history }) => (
+  <Fragment>
+    <Message header="Teacher search" content="Search for a teacher and click the search result to view their individual statistics from their entire career. " />
+    <TeacherSearch
+      onClick={teacher => (history.push(`/teachers/${teacher.id}`))}
+      icon="level up alternate"
+    />
+  </Fragment>
+)
+
+TeacherSearchTab.propTypes = {
+  history: shape({}).isRequired
+}
+
+export default withRouter(TeacherSearchTab)
