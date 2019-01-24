0eb2437ea05c78cb0733d570f693f8a0efae0f6f
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-01-24 16:12:39 +0200

    Granular api authorization

--- src/routes/elementdetails.js
+++ src/routes/elementdetails.js
@@ -4,27 +4,16 @@ const { getMandatoryCourses } = require('../services/courses')
 
 router.get('/studyprogrammes', async (req, res) => {
   try {
-    const { admin, czar } = req.decodedToken
-    if (!(admin || czar)) {
-      res.status(403).json([])
-      return
-    } else {
-      const studyrights = await getAllDegreesAndProgrammes()
-      res.json(studyrights)
-    }
+    const studyrights = await getAllDegreesAndProgrammes()
+    res.json(studyrights)
   } catch (err) {
     res.status(500).json(err)
   }
 })
 
 router.get('/v2/studyprogrammes', async (req, res) => {
-  const { admin } = req.decodedToken
-  if (!admin) {
-    res.status(401).send('No admin rights')
-  } else {
-    const associations = await getAssociations()
-    res.json(associations)
-  }
+  const associations = await getAssociations()
+  res.json(associations)
 })
 
 router.get('/v2/studyprogrammes/:id/mandatory_courses', async (req, res) => {
