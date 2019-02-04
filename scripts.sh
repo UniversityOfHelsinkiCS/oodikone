175d4c330fdda3aec5d690a2e8a227d35c4f135a
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-02-04 12:33:38 +0200

    Added userdb restore from db dump, userservice default to trunk-branch  nd fixed symlink creation.
    Co-authored-by: Jami Kousa <jami.kousa@helsinki.fi>

--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-ABS_DIR_PATH=$(realpath $(dirname "$0"))
+DIR_PATH=$(dirname "$0")
 BACKUP_DIR=backups
 REPOS=repos
 PSQL_DB_BACKUP="$BACKUP_DIR/latest-pg.bak"
+USER_DB_BACKUP="$BACKUP_DIR/latest-user-pg.bak"
 
 init_dirs () {
     mkdir -p $REPOS $BACKUP_DIR
@@ -30,7 +31,7 @@ pull_git_repositories () {
     git clone -b trunk https://github.com/UniversityOfHelsinkiCS/oodikone2-backend.git
     git clone -b trunk https://github.com/UniversityOfHelsinkiCS/oodikone2-frontend.git
     git clone https://github.com/UniversityOfHelsinkiCS/oodilearn.git
-    git clone https://github.com/UniversityOfHelsinkiCS/oodikone2-userservice.git
+    git clone -b trunk https://github.com/UniversityOfHelsinkiCS/oodikone2-userservice.git
     popd
 }
 
@@ -46,6 +47,10 @@ restore_psql_from_backup () {
     cat $PSQL_DB_BACKUP | docker exec -i -u postgres oodi_db psql -d tkt_oodi
 }
 
+restore_userdb_from_backup () {
+    cat $USER_DB_BACKUP | docker exec -i -u postgres oodi_user_db psql -d user_db
+}
+
 restore_mongodb_from_backup () {
     docker exec -t mongo_db mongorestore -d oodilearn "/dump"
 }
@@ -56,14 +61,23 @@ db_oodikone_reset () {
     restore_psql_from_backup
 }
 
+ping_psql () {
+    echo "Pinging psql in container $1"
+    for i in 1 2 3 4 5; do docker exec -u postgres $1 pg_isready && break || echo "Waiting..." && sleep 10; done
+}
+
 db_setup_full () {
     echo "Getting backups from the Oodikone server, this will prompt you for your password. "
     get_oodikone_server_backup
     echo "Unpacking compressed files"
     unpack_oodikone_server_backup
     echo "Restoring PostgreSQL from backup"
+    ping_psql "oodi_db"
     restore_psql_from_backup
     echo "Restoring MongoDB from backup"
+    echo "Restore user db from backup"
+    ping_psql "oodi_user_db"
+    restore_userdb_from_backup
     restore_mongodb_from_backup
     echo "Database setup finished"
 }
@@ -73,8 +87,8 @@ docker_build () {
 }
 
 create_symlink_git_hooks () {
-    ln -f "$ABS_DIR_PATH/git-hooks/frontend/pre-push" repos/oodikone2-frontend/.git/hooks/pre-push
-    ln -f "$ABS_DIR_PATH/git-hooks/backend/pre-push" repos/oodikone2-backend/.git/hooks/pre-push
+    ln -f "$DIR_PATH/git-hooks/frontend/pre-push" repos/oodikone2-frontend/.git/hooks/pre-push
+    ln -f "$DIR_PATH/git-hooks/backend/pre-push" repos/oodikone2-backend/.git/hooks/pre-push
 }
 
 show_instructions () {
