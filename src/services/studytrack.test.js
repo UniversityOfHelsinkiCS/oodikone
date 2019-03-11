4ad0bf4a34d7706e30240699b956c63241086a82
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-03-11 15:02:09 +0200

    Added graduated student calculation to studytrack productivity stats.

--- src/services/studytrack.test.js
+++ src/services/studytrack.test.js
@@ -3,12 +3,17 @@ const {
   getCreditsForProvider,
   isThesis,
   productivityStatsFromCredits,
-  productivityStatsForProvider
+  productivityStatsForProvider,
+  findGraduated,
+  graduatedStatsFromStudyrights,
+  combineStatistics,
+  productivityStatsForStudytrack
 } = require('./studytrack')
 const { sequelize } = require('../models')
 const { readFileSync } = require('fs')
 
 const provider = '500-M010'
+const studytrack = 'MH50_010'
 
 beforeAll(async () => {
   const query = readFileSync('./src/services/studytrack.test.sql', 'utf8')
@@ -77,17 +82,102 @@ test('productivityStatsFromCredits calculates stats correctly', () => {
     { year: 2016, credits: 5, thesis: true },
     { year: 2015, credits: 5, thesis: true },
     { year: 2015, credits: 5, thesis: true },
-    { year: 2015, credits: 10, thesis: false },
+    { year: 2015, credits: 10, thesis: false }
   ]
   const stats = productivityStatsFromCredits(credits)
-  expect(stats.length).toBe(2)
-  expect(stats).toContainEqual({ year: 2016, credits: 15, thesis: 1 })
-  expect(stats).toContainEqual({ year: 2015, credits: 20, thesis: 2 })
+  expect(stats).toMatchObject({
+    2016: { year: 2016, credits: 15, thesis: 1 },
+    2015: { year: 2015, credits: 20, thesis: 2 }
+  })
 })
 
 test('productivityStats integrates correctly', async () => {
   const stats = await productivityStatsForProvider(provider)
-  expect(stats.length).toBe(2)
-  expect(stats).toContainEqual({ year: 2015, thesis: 1, credits: 40 })
-  expect(stats).toContainEqual({ year: 2016, thesis: 0, credits: 5 })
+  expect(stats).toMatchObject({
+    2015: { year: 2015, thesis: 1, credits: 40 },
+    2016: { year: 2016, thesis: 0, credits: 5 }
+  })
+})
+
+test('findGraduated finds graduated studyright', async () => {
+  const studyrights = await findGraduated(studytrack)
+  const match = studyrights.find(sr => sr.studyrightid === '10')
+  expect(match).toBeTruthy()
+})
+
+test('findGraduated formats graduated studyrights correctly', async () => {
+  const studyrights = await findGraduated(studytrack)
+  expect(studyrights).toContainEqual({ studyrightid: '10', year: 2016 })
+})
+
+test('findGraduated does not return studyrights that are not graduated', async () => {
+  const studyrights = await findGraduated(studytrack)
+  const match = studyrights.find(sr => sr.id === '11')
+  expect(match).toBeFalsy()
+})
+
+test('findGraduated does return graduated studyrights from other studytracks', async () => {
+  const studyrights = await findGraduated(studytrack)
+  const match = studyrights.find(sr => sr.id === '12')
+  expect(match).toBeFalsy()
+})
+
+test('graduatedStatsFromStudyrights calculates stats correctly', () => {
+  const studyrights = [
+    { year: 2015 },
+    { year: 2015 },
+    { year: 2014 }
+  ]
+  const stats = graduatedStatsFromStudyrights(studyrights)
+  expect(stats).toMatchObject({
+    2015: 2,
+    2014: 1
+  })
+})
+
+test('combineStatistics returns correctly formatted array', () => {
+  const creditStats = {
+    2015: { year: 2015, thesis: 1, credits: 40 },
+    2016: { year: 2016, thesis: 0, credits: 5 },
+    2014: { year: 2014, thesis: 0, credits: 20 }
+  }
+  const studyrightStats = {
+    2015: 2,
+    2016: 1
+  }
+  const stats = combineStatistics(creditStats, studyrightStats)
+  expect(stats).toContainEqual({
+    year: 2015,
+    thesis: 1,
+    credits: 40,
+    graduated: 2 
+  })
+  expect(stats).toContainEqual({
+    year: 2014,
+    thesis: 0,
+    credits: 20,
+    graduated: 0
+  })
+  expect(stats).toContainEqual({
+    year: 2016,
+    thesis: 0,
+    credits: 5,
+    graduated: 1
+  })
+})
+
+test('productivityStatsForStudytrack integrates', async () => {
+  const stats = await productivityStatsForStudytrack(studytrack)
+  expect(stats).toContainEqual({
+    year: 2015,
+    graduated: 0,
+    thesis: 1,
+    credits: 40
+  })
+  expect(stats).toContainEqual({
+    year: 2016,
+    graduated: 1,
+    thesis: 0,
+    credits: 5
+  })
 })
