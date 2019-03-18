8d29d888d1c5ba04084382b917ebebc3175ed4cc
totutotu <tuomo.torppa@gmail.com>
2019-03-18 14:53:36 +0200

    Nicer throughput table

--- src/components/StudyProgramme/ThroughputTable/index.jsx
+++ src/components/StudyProgramme/ThroughputTable/index.jsx
@@ -1,29 +1,56 @@
 import React from 'react'
-import { Header, Loader } from 'semantic-ui-react'
+import { Header, Loader, Table } from 'semantic-ui-react'
 import { shape, number, arrayOf, bool, string } from 'prop-types'
 import SortableTable from '../../SortableTable'
 
 const ThroughputTable = ({ throughput, loading, error }) => {
   const morethan = x => (total, amount) => amount >= x ? total + 1 : total // eslint-disable-line
-  const columns = [
-    { key: 'year', title: 'Start year', getRowVal: year => year.year },
-    { key: 'students', title: 'Students', getRowVal: year => year.credits.length },
-    { key: 'credits30', title: 'Credits >= 30', getRowVal: year => year.credits.reduce(morethan(30), 0) },
-    { key: 'credits60', title: 'Credits >= 60', getRowVal: year => year.credits.reduce(morethan(60), 0) },
-    { key: 'credits90', title: 'Credits >= 90', getRowVal: year => year.credits.reduce(morethan(90), 0) },
-    { key: 'credits120', title: 'Credits >= 120', getRowVal: year => year.credits.reduce(morethan(120), 0) },
-    { key: 'credits150', title: 'Credits >= 150', getRowVal: year => year.credits.reduce(morethan(150), 0) },
-    { key: 'thesisM', title: 'Thesis (Master)', getRowVal: year => year.thesisM },
-    { key: 'thesisB', title: 'Thesis (Bachelor)', getRowVal: year => year.thesisB },
-    { key: 'graduatedM', title: 'Graduated (Master)', getRowVal: year => year.graduatedM },
-    { key: 'graduatedB', title: 'Graduated (Bachelor)', getRowVal: year => year.graduatedB }
-  ]
   if (error) return <h1>Oh no so error {error}</h1>
+  const data = throughput.filter(year => year.credits.length > 0)
   return (
     <React.Fragment>
       <Header>Population progress</Header>
       <Loader active={loading} inline="centered">Loading...</Loader>
-      <SortableTable columns={columns} data={throughput.filter(year => year.credits.length > 0)} getRowKey={row => row.year} />
+      <Table celled structured>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell rowSpan='2'>Year</Table.HeaderCell>
+        <Table.HeaderCell rowSpan='2'>Students</Table.HeaderCell>
+        <Table.HeaderCell colSpan='5'>Credits</Table.HeaderCell>
+        <Table.HeaderCell colSpan='2'>Thesis</Table.HeaderCell>
+        <Table.HeaderCell colSpan='2'>Graduated</Table.HeaderCell>
+      </Table.Row>
+      <Table.Row>
+        <Table.HeaderCell>>= 30</Table.HeaderCell>
+        <Table.HeaderCell>>= 60</Table.HeaderCell>
+        <Table.HeaderCell>>= 90</Table.HeaderCell>
+        <Table.HeaderCell>>= 120</Table.HeaderCell>
+        <Table.HeaderCell>>= 150</Table.HeaderCell>
+        <Table.HeaderCell>Master</Table.HeaderCell>
+        <Table.HeaderCell>Bachelor</Table.HeaderCell>
+        <Table.HeaderCell>Master</Table.HeaderCell>
+        <Table.HeaderCell>Bachelor</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body> 
+          {data.map(year => (
+            <Table.Row key={year.year}>
+              <Table.Cell>{year.year}</Table.Cell>
+              <Table.Cell>{year.credits.length}</Table.Cell>
+              <Table.Cell>{year.credits.reduce(morethan(30), 0)}</Table.Cell>
+              <Table.Cell>{year.credits.reduce(morethan(60), 0)}</Table.Cell>
+              <Table.Cell>{year.credits.reduce(morethan(90), 0)}</Table.Cell>
+              <Table.Cell>{year.credits.reduce(morethan(120), 0)}</Table.Cell>
+              <Table.Cell>{year.credits.reduce(morethan(150), 0)}</Table.Cell>
+              <Table.Cell>{year.thesisM}</Table.Cell>
+              <Table.Cell>{year.thesisB}</Table.Cell>
+              <Table.Cell>{year.graduatedM}</Table.Cell>
+              <Table.Cell>{year.graduatedB}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
     </React.Fragment>
   )
 }
@@ -31,7 +58,7 @@ const ThroughputTable = ({ throughput, loading, error }) => {
 ThroughputTable.propTypes = {
   throughput: arrayOf(shape({
     year: string,
-    credits: number,
+    credits: arrayOf(number),
     thesisM: number,
     thesisB: number,
     graduatedB: number,
