6cc46e6ff42869dc20e7d5c705dec96b55a8bc4a
totutotu <tuomo.torppa@gmail.com>
2019-03-22 09:59:46 +0200

    Graduated to single cell -> approximation removed

--- src/components/StudyProgramme/ThroughputTable/index.jsx
+++ src/components/StudyProgramme/ThroughputTable/index.jsx
@@ -15,9 +15,9 @@ const ThroughputTable = ({ throughput, loading, error }) => {
           <Table.Row>
             <Table.HeaderCell rowSpan="2">Year</Table.HeaderCell>
             <Table.HeaderCell rowSpan="2">Students</Table.HeaderCell>
+            <Table.HeaderCell rowSpan="2">Graduated</Table.HeaderCell>
             <Table.HeaderCell colSpan="5">Credits</Table.HeaderCell>
             <Table.HeaderCell colSpan="2">Thesis</Table.HeaderCell>
-            <Table.HeaderCell colSpan="2">Graduated</Table.HeaderCell>
           </Table.Row>
           <Table.Row>
             <Table.HeaderCell content=">= 30" />
@@ -27,8 +27,6 @@ const ThroughputTable = ({ throughput, loading, error }) => {
             <Table.HeaderCell content=">= 150" />
             <Table.HeaderCell content="Master" />
             <Table.HeaderCell content="Bachelor" />
-            <Table.HeaderCell content="Master" />
-            <Table.HeaderCell content="Bachelor" />
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -36,6 +34,7 @@ const ThroughputTable = ({ throughput, loading, error }) => {
             <Table.Row key={year.year}>
               <Table.Cell>{year.year}</Table.Cell>
               <Table.Cell>{year.credits.length}</Table.Cell>
+              <Table.Cell>{year.graduated}</Table.Cell>
               <Table.Cell>{year.credits.reduce(morethan(30), 0)}</Table.Cell>
               <Table.Cell>{year.credits.reduce(morethan(60), 0)}</Table.Cell>
               <Table.Cell>{year.credits.reduce(morethan(90), 0)}</Table.Cell>
@@ -43,8 +42,6 @@ const ThroughputTable = ({ throughput, loading, error }) => {
               <Table.Cell>{year.credits.reduce(morethan(150), 0)}</Table.Cell>
               <Table.Cell>{year.thesisM}</Table.Cell>
               <Table.Cell>{year.thesisB}</Table.Cell>
-              <Table.Cell>{year.graduatedM}</Table.Cell>
-              <Table.Cell>{year.graduatedB}</Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
@@ -59,8 +56,7 @@ ThroughputTable.propTypes = {
     credits: arrayOf(number),
     thesisM: number,
     thesisB: number,
-    graduatedB: number,
-    graduatedM: number
+    graduated: number
   })).isRequired,
   loading: bool.isRequired,
   error: bool.isRequired
