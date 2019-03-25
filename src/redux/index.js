58d9ebc6d524cb521ebcae61546584efeb5548de
totutotu <tuomo.torppa@gmail.com>
2019-03-25 14:27:23 +0200

    removed depracated reducer and old code

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux'
 import { localeReducer as locale } from 'react-localize-redux'
-
-import department from './department'
 import users from './users'
 import populations from './populations'
 import populationCourses from './populationCourses'
@@ -16,7 +14,6 @@ import students from './students'
 import errors from './errors'
 import courses from './courses'
 import courseInstances from './courseInstances'
-import courseStatistics from './courseStatistics'
 import graphSpinner from './graphSpinner'
 import settings from './settings'
 import courseCodeDuplicates from './coursecodeduplicates'
@@ -44,7 +41,6 @@ import oodilearnPopulationCourseSelect from './oodilearnPopulationCourseSelect'
 
 export default combineReducers({
   locale,
-  department,
   users,
   populations,
   populationCourses,
@@ -59,7 +55,6 @@ export default combineReducers({
   errors,
   courses,
   courseInstances,
-  courseStatistics,
   graphSpinner,
   settings,
   courseCodeDuplicates,
