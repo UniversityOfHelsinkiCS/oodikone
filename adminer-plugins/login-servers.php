7142e130a1246e96de87a69edae3bd8098060c07
Rochet2 <tqbattlenet@gmail.com>
2019-03-27 17:41:21 +0200

    Add analytics DB

--- adminer-plugins/login-servers.php
+++ adminer-plugins/login-servers.php
@@ -6,6 +6,6 @@ require_once('plugins/login-servers.php');
     * @param string
     */
 return new AdminerLoginServers(
-    $servers = array("db" => "db", "user_db" => "user_db"),
+    $servers = array("db" => "db", "user_db" => "user_db", "analytics_db" => "analytics_db"),
     $driver = "pgsql"
 );
