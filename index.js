dcadbe33cab8c209e822a0a2acbb0a1ff3af147c
Rochet2 <tqbattlenet@gmail.com>
2019-03-27 11:13:37 +0200

    Remove unused stuff and add travis build

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const express = require('express')
 const app = express()
 const port = 4568
 const bodyParser = require('body-parser')
-const checkSecret = require('./src/middlewares/secret')
 
 app.use(bodyParser.json())
-app.use(checkSecret)
 
 app.get('/ping', (req, res) => res.json({ message: 'pong '}))
 
