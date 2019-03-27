67365ad2ab25eb6e0e2e367bcda1081dde9f423f
Rochet2 <tqbattlenet@gmail.com>
2019-03-27 11:08:05 +0200

    Add test data endpoint

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ app.use(checkSecret)
 
 app.get('/ping', (req, res) => res.json({ message: 'pong '}))
 
+let fetching = false
+let data = null
+app.get('/data', (req, res) => {
+  if (fetching) {
+    return res.json(data)
+  }
+  if (!data) {
+    data = { data: "datahere" } // recalculate?
+  }
+  return res.json(data)
+})
+
 app.listen(port, () => console.log(`Analytics listening on port ${port}!`))
