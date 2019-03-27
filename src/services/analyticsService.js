074d3945f7d1ec92552357192f2bd51e4021e926
Rochet2 <tqbattlenet@gmail.com>
2019-03-27 19:14:06 +0200

    Implement throughput analytics cache

--- src/services/analyticsService.js
+++ src/services/analyticsService.js
@@ -9,17 +9,29 @@ const ping = async () => {
 }
 
 const getProductivity = async (id) => {
-  const response = await client.get(`/data/${id}`)
+  const response = await client.get(`/productivity/${id}`)
   return response.data
 }
 
 const setProductivity = async (data) => {
-  const response = await client.post('/data', { data })
+  const response = await client.post('/productivity', { data })
+  return response.data
+}
+
+const getThroughput = async (id) => {
+  const response = await client.get(`/throughput/${id}`)
+  return response.data
+}
+
+const setThroughput = async (data) => {
+  const response = await client.post('/throughput', { data })
   return response.data
 }
 
 module.exports = {
   ping,
   getProductivity,
-  setProductivity
+  setProductivity,
+  getThroughput,
+  setThroughput
 }
