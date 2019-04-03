5de618b3a4a2af0a124c754ffcff035dff5fb73e
totutotu <tuomo.torppa@gmail.com>
2019-04-03 12:56:38 +0300

    secret + travis.yml

--- src/conf-usageservice.js
+++ src/conf-usageservice.js
@@ -1,9 +1,12 @@
-const PORT = process.env.PORT
-const DB_URL = process.env.DB_URL
 const DB_SCHEMA = process.env.DB_SCHEMA ? process.env.DB_SCHEMA : 'public'
+const DB_URL = process.env.DB_URL
+const PORT = process.env.PORT
+const SECRET = process.env.SECRET
+
 
 module.exports = {
-  PORT,
+  DB_SCHEMA,
   DB_URL,
-  DB_SCHEMA
+  PORT,
+  SECRET
 }
