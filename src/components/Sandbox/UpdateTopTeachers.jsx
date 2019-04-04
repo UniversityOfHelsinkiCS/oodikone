27f56f04ad955ec25f7bf0428586ea28d0fcaa9d
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-04-04 10:47:10 +0300

    Refactor update top teachers component to stop using SSE.

--- src/components/Sandbox/UpdateTopTeachers.jsx
+++ src/components/Sandbox/UpdateTopTeachers.jsx
@@ -1,33 +1,34 @@
 import React, { Component } from 'react'
-import { Button, Progress } from 'semantic-ui-react'
+import { Button, Progress, Message } from 'semantic-ui-react'
 import { createConfiguredAxios } from '../../apiConnection'
 import KeyValueTable from '../Postman/KeyValueTable'
-import { API_BASE_PATH } from '../../constants'
 
 const TASK_URL = '/tasks/topteachers'
-const STREAM_URL = `${API_BASE_PATH}/status/topteachers`
 
 class UpdateTopTeachers extends Component {
   state={
     status: undefined,
     progress: undefined,
-    computing: false
+    computing: false,
+    fetching: false,
+    error: undefined
   }
 
   componentDidMount() {
     this.checkStatus()
-    const eventSource = new EventSource(STREAM_URL)
-    eventSource.onmessage = ({ data }) => {
-      const { progress, computing, ...rest } = JSON.parse(data)
-      this.setState({ status: rest, progress, computing })
-    }
   }
 
   checkStatus = async () => {
-    const axios = await createConfiguredAxios()
-    const { data } = await axios.get(TASK_URL)
-    const { progress, computing, ...rest } = data
-    this.setState({ status: rest, progress, computing })
+    this.setState({ fetching: true })
+    try {
+      const axios = await createConfiguredAxios()
+      const { data } = await axios.get(TASK_URL)
+      const { progress, computing, ...rest } = data
+      this.setState({ status: rest, progress, computing })
+    } catch (error) {
+      this.setState({ error })
+    }
+    this.setState({ fetching: false })
   }
 
   doUpdate = async () => {
@@ -38,17 +39,24 @@ class UpdateTopTeachers extends Component {
   }
 
   render() {
-    const { status, computing, progress } = this.state
+    const { status, computing, progress, fetching, error } = this.state
     return (
       <React.Fragment>
-        <Button
-          fluid
-          content="Update Top Teachers"
-          icon="refresh"
-          onClick={this.doUpdate}
-          loading={computing}
-          disabled={computing}
-        />
+        { error && <Message error content={error} /> }
+        <Button.Group fluid widths="8">
+          <Button
+            content="Update Top Teachers"
+            icon="refresh"
+            onClick={this.doUpdate}
+            disabled={computing}
+          />
+          <Button
+            content="Check Status"
+            icon="clock outline"
+            onClick={this.checkStatus}
+            loading={fetching}
+          />
+        </Button.Group>
         { status && <KeyValueTable data={status} /> }
         { !!computing && <Progress autoSuccess active={computing} percent={progress} />}
       </React.Fragment>
