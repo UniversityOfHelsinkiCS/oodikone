03a6253afc6236b1da7ed6ad50dfb6ed1323d5cd
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-04-04 06:00:10 +0300

    Add updated timestamp to teacher leader board.

--- src/components/TeacherStatisticsTable/index.jsx
+++ src/components/TeacherStatisticsTable/index.jsx
@@ -41,7 +41,7 @@ class TeacherStatisticsTable extends Component {
 
       const sortDirection = name => (selected === name ? direction : null)
 
-      return (statistics.length === 0 ? <Segment content="No statistics found for the given query." /> :
+      return (statistics.length === 0 ? <Segment basic content="No statistics found for the given query." /> :
       <Table structured celled textAlign="center" sortable>
         <Table.Header>
           <Table.Row>
