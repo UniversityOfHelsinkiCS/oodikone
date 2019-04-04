19043d03d4d7bd51c9ec75151208210b135471c2
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-04-04 16:46:59 +0300

    dynamic table cells in depending on set thesis in studyprogramme views

--- src/components/StudyProgramme/ThroughputTable/index.jsx
+++ src/components/StudyProgramme/ThroughputTable/index.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Header, Loader, Table } from 'semantic-ui-react'
 import { shape, number, arrayOf, bool, string } from 'prop-types'
 
-const ThroughputTable = ({ throughput, loading, error }) => {
+const ThroughputTable = ({ throughput, thesis, loading, error }) => {
   const morethan = x => (total, amount) => amount >= x ? total + 1 : total // eslint-disable-line
   if (error) return <h1>Oh no so error {error}</h1>
   const data = throughput ? throughput.filter(year => year.credits.length > 0) : []
+  let thesisTypes = []
+  if (thesis) {
+    thesisTypes = thesis.map(t => t.thesisType)
+  }
   return (
     <React.Fragment>
       <Header>Population progress</Header>
@@ -17,7 +21,7 @@ const ThroughputTable = ({ throughput, loading, error }) => {
             <Table.HeaderCell rowSpan="2">Students</Table.HeaderCell>
             <Table.HeaderCell rowSpan="2">Graduated</Table.HeaderCell>
             <Table.HeaderCell colSpan="5">Credits</Table.HeaderCell>
-            <Table.HeaderCell colSpan="2">Thesis</Table.HeaderCell>
+            <Table.HeaderCell colSpan={thesisTypes.length}>Thesis</Table.HeaderCell>
           </Table.Row>
           <Table.Row>
             <Table.HeaderCell content=">= 30" />
@@ -25,8 +29,8 @@ const ThroughputTable = ({ throughput, loading, error }) => {
             <Table.HeaderCell content=">= 90" />
             <Table.HeaderCell content=">= 120" />
             <Table.HeaderCell content=">= 150" />
-            <Table.HeaderCell content="Master" />
-            <Table.HeaderCell content="Bachelor" />
+            {thesisTypes.includes('MASTER') && <Table.HeaderCell content="Master" />}
+            {thesisTypes.includes('BACHELOR') && <Table.HeaderCell content="Bachelor" />}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -43,8 +47,8 @@ const ThroughputTable = ({ throughput, loading, error }) => {
                   <Table.Cell>{year.credits.reduce(morethan(90), 0)}</Table.Cell>
                   <Table.Cell>{year.credits.reduce(morethan(120), 0)}</Table.Cell>
                   <Table.Cell>{year.credits.reduce(morethan(150), 0)}</Table.Cell>
-                  <Table.Cell>{year.thesisM}</Table.Cell>
-                  <Table.Cell>{year.thesisB}</Table.Cell>
+                  {thesisTypes.includes('MASTER') ? <Table.Cell>{year.thesisM}</Table.Cell> : null}
+                  {thesisTypes.includes('BACHELOR') ? <Table.Cell>{year.thesisB}</Table.Cell> : null}
                 </Table.Row>
               ))}
         </Table.Body>
@@ -61,12 +65,20 @@ ThroughputTable.propTypes = {
     thesisB: number,
     graduated: number
   })), // eslint-disable-line
+  thesis: arrayOf(shape({
+    programmeCode: string,
+    courseCode: string,
+    thesisType: string,
+    createdAt: string,
+    updatedAt: string
+  })),
   loading: bool.isRequired,
   error: bool.isRequired
 }
 
 ThroughputTable.defaultProps = {
-  throughput: null
+  throughput: null,
+  thesis: undefined
 }
 
 export default ThroughputTable
