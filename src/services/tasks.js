e02be933f024452381c0fb4ff4f90f06a019b010
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2019-04-04 10:49:41 +0300

    Remove status endpoint due to nginx configuration difficulties.

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -1,4 +1,3 @@
-const EventEmitter = require('events')
 const { findAndSaveTopTeachers } = require('./topteachers')
 
 const TOPTEACHER = {
@@ -8,43 +7,30 @@ const TOPTEACHER = {
   progress: undefined
 }
 
-const emitter = new EventEmitter()
-
-const publish = (payload) => {
-  emitter.emit(TOPTEACHER.task, payload)
-}
-
-const subscribe = (callback) => emitter.on(TOPTEACHER.task, callback)
-
 const startTopTeacherUpdate = async (from, to) => {
   TOPTEACHER.computing = true
   TOPTEACHER.status = 'Update started'
   TOPTEACHER.progress = 0
-  publish(TOPTEACHER)
   try {
     for (let year=from; year<=to; year++) {
       await findAndSaveTopTeachers(year)
       TOPTEACHER.status = `Top teachers from year with id ${year} updated.`
-      TOPTEACHER.progress = (from / (to - from))
-      publish(TOPTEACHER)
+      TOPTEACHER.progress = ((year - from) / (to - from)) * 100
     }
   } catch (e) {
     TOPTEACHER.status = JSON.parse(e)
-    publish(TOPTEACHER)
   }
   TOPTEACHER.computing = false
   TOPTEACHER.progress = 100
   TOPTEACHER.status = 'Update finished'
-  publish(TOPTEACHER)
 }
 
 const getTaskStatus = () => ({ ...TOPTEACHER })
 
-const getTeacherUpdateStatus = () => getTaskStatus(TOPTEACHER)
+const getTeacherUpdateStatus = () => getTaskStatus()
 
 module.exports = {
   getTaskStatus,
   startTopTeacherUpdate,
-  getTeacherUpdateStatus,
-  subscribe
+  getTeacherUpdateStatus
 }
