fadc988bd65669195c99a0bce5df6ac71132157e
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-04-23 13:21:19 +0300

    stop cypress:run from creating a video

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -6,6 +6,3 @@ npm test  --prefix services/oodikone2-analytics
 npm run  --prefix services/oodikone2-userservice test_docker
 npm test  --prefix services/oodikone2-usageservice
 npm run cypress:run
-
-
-exit 1
