21f20c12ab540b70422167dba1a72c1045ccb01c
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-05-03 17:53:31 +0300

    inital sudoku updater_writer

---
+++ services/updater_writer/index.js
@@ -0,0 +1,21 @@
+const stan = require('node-nats-streaming').connect('updaterNATS', process.env.HOSTNAME, process.env.NATS_URI);
+const fs = require('fs')
+
+console.log(`STARTING WITH ${process.env.HOSTNAME} as id`)
+const opts = stan.subscriptionOptions();
+opts.setManualAckMode(true);
+opts.setAckWait(15 * 1000); // 5min
+opts.setDeliverAllAvailable()
+opts.setDurableName('durable')
+opts.setMaxInFlight(1)
+stan.on('connect', function () {
+
+  const sub = stan.subscribe('UpdateWrite', 'updater.workers', opts)
+
+  sub.on('message', async (msg) => {
+    const studentData = msg.getData()
+    fs.writeFileSync('./updater/test_student.json', studentData)
+    msg.ack()
+  });
+
+})
