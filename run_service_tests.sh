c860c587f50d04191649feb88da3e06740e8f40b
Rochet2 <tqbattlenet@gmail.com>
2019-05-04 02:39:51 +0300

    Run tests in docker instead of locally

--- run_service_tests.sh
+++ run_service_tests.sh
@@ -6,5 +6,5 @@ npm run --prefix services/oodikone2-backend test_docker
 npm test --prefix services/oodikone2-analytics
 npm run --prefix services/oodikone2-userservice test_docker
 npm test --prefix services/oodikone2-usageservice
-npm test --prefix services/oodikone2-frontend
+npm run --prefix services/oodikone2-frontend test_docker
 
