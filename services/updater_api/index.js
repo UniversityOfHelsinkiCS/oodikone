2b9b6fc6ea4c0d1f29557cc945166fcf92a98818
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-05-03 14:14:48 +0300

    updater api with nats

--- services/updater_api/index.js
+++ services/updater_api/index.js
@@ -1,26 +1,40 @@
-const kafka = require('kafka-node')
+var stan = require('node-nats-streaming').connect('updaterNATS', process.env.HOSTNAME, process.env.NATS_URI);
+const fs = require('fs');
+const { updateStudent } = require('./doo_api_database_updater/database_updater')
 
-const client = new kafka.KafkaClient({ kafkaHost: 'kafka:9092' })
 
-const consumer = new kafka.Consumer(
-  client,[
-    { topic: 'studentnumbers' }
-  ]
-)
+console.log(`STARTING WITH ${process.env.HOSTNAME} as id`)
+var opts = stan.subscriptionOptions();
+opts.setManualAckMode(true);
+opts.setAckWait(15 * 1000); // 5min
+opts.setDeliverAllAvailable()
+opts.setDurableName('durable')
+stan.on('connect', function () {
 
-consumer.on('message', (message) => {
-  const { value } = message
-  console.log(message)
-  if (value === '00000000') {
-    console.log('should update pre-updater data')
-  } else if (value === '99999999') {
-    console.log('should update after-updater data')
-  } else {
-    console.log('update student')
-  }
+  const sub = stan.subscribe('Update', 'updater.workers', opts)
+  const prioSub = stan.subscribe('Priority_Update', 'updater.workers', opts)
+
+  sub.on('message', async (msg) => {
+    const studentnumber = msg.getData()
+    // TODO: check that its a valid studentnumber and just ack it if its not
+    try {
+      const student = await updateStudent(studentnumber)
+      // TODO: check that student data is properly structured(?)
+      stan.publish('UpdateWrite', JSON.stringify(student), (err, guid) => {
+        if (err) {
+          return err
+        } else {
+          msg.ack()
+          console.log(studentnumber, 'read from api and published for writing', guid)
+        }
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  });
+
+  prioSub.on('message', async (msg) => {
+    await updateStudent(msg.getData())
+    msg.ack()
+  });
 })
-
-consumer.on('error', (err) => {
-  console.log('ERROR ERROR ', err)
-})
-
