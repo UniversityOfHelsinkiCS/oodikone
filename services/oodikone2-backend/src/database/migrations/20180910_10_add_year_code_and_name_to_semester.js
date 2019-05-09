29b015744c55545aa9a27da8ba542235983b5b41
Rochet2 <tqbattlenet@gmail.com>
2019-05-09 13:42:15 +0300

    Was moved to monorepo/shared/models

--- services/oodikone2-backend/src/database/migrations/20180910_10_add_year_code_and_name_to_semester.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('semesters', 'yearcode', { type: Sequelize.INTEGER })
-    await queryInterface.addColumn('semesters', 'yearname', { type: Sequelize.STRING })
-  },
-  down: async () => {
-  }
-}
