b8a3e047f2387a96f33442ff29e91b3687143758
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-05-14 13:14:05 +0300

    scheduler

---
+++ services/updater_scheduler/models.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: false, useFindAndModify: false })
+
+
+const scheduleSchema = new mongoose.Schema(
+  {
+    task: String,
+    status: String,
+    type: String,
+    updatedAt: Date
+  }
+)
+
+module.exports = mongoose.model('Schedule', scheduleSchema) 
