2a6ce61b0209bae141e0223db3186354eac41c75
totutotu <tuomo.torppa@gmail.com>
2019-05-14 15:57:58 +0300

    Removed redis, set throughput to psql

--- services/oodikone2-analytics/index.js
+++ services/oodikone2-analytics/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const _ = require('lodash')
 const morgan = require('morgan')
-const { redisClient } = require('./src/services/redis')
-const { Productivity } = require('./src/models')
+const { Productivity, Throughput } = require('./src/models')
 
 const app = express()
 const port = 4568
@@ -13,25 +12,12 @@ app.use(morgan('tiny'))
 
 app.get('/ping', (req, res) => res.json({ message: 'pong'}))
 
-const getCached = async (key) => {
-  try {
-    const cached = await redisClient.getAsync(key)
-    if (cached) return JSON.parse(cached)
-  } catch (e) {
-    console.error(e)
-  }
-  return null
-}
-
-const setCached = async (key, data) => {
-  try {
-    await redisClient.setAsync(key, JSON.stringify(data))
-  } catch (e) {
-    console.error(e)
-  }
-}
-
 const formatProductivity = stats => {
+  const { id, updatedAt: lastUpdated, data, status } = stats
+  return { [id] : { lastUpdated, data, status } }
+}
+
+const formatThroughput = stats => {
   const { id, updatedAt: lastUpdated, data, status } = stats
   return { [id] : { lastUpdated, data, status } }
 }
@@ -62,22 +48,25 @@ app.patch('/productivity', async (req, res) => {
 
 app.get('/throughput/:id', async (req, res) => {
   const { id } = req.params
-  const data = await getCached('throughput')
-  if (data && data[id]) res.json({ [id]: data[id] })
-  else res.json(null)
+  const result = await Throughput.findByPk(id)
+  if (!result) {
+    res.json(null)
+  } else {
+    res.json(formatThroughput(result))
+  }
 })
 
 app.post('/throughput', async (req, res) => {
-  const data = await getCached('throughput')
-  const newdata = _.extend(data, req.body.data)
-  await setCached('throughput', newdata)
-  res.status(200).end()
+  const { data } = req.body
+  const [saved] = await Throughput.upsert({ ...data, status: 'DONE' }, { returning: true })
+  const result = formatThroughput(saved)
+  res.json(result)
 })
 
 app.patch('/throughput', async (req, res) => {
-  const data = await getCached('throughput')
-  await setCached('throughput', _.merge(data, req.body.data))
+  for (let [id, data] of Object.entries(req.body.data)) {
+    await Throughput.upsert({ ...data, id })  
+  }
   res.status(200).end()
 })
-
 app.listen(port, () => console.log(`Analytics listening on port ${port}!`))
