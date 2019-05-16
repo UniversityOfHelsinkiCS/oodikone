afc8bf88f9b130ff57289bd98fbac1473782cf73
Rochet2 <tqbattlenet@gmail.com>
2019-05-16 09:09:12 +0300

    Enable updater_writer and backend to use shared folder, fix dockerignores

--- services/oodikone2-backend/src/database/migrations/20180807_add_attainment_date_column.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('credit', 'attainment_date', { type: Sequelize.DATE })
-  },
-  down: async (queryInterface) => {
-    queryInterface.removeColumn('credit', 'attainment_date')
-  }
-}
