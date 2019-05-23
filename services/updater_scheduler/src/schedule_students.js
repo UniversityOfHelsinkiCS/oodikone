27d04097adc0b7052f6f378a5c58d6f6202a0452
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-05-23 13:51:44 +0300

    add ramp up time for scheduling students to give doo api some time to spin up more machines ( hopefully )

--- services/updater_scheduler/src/schedule_students.js
+++ services/updater_scheduler/src/schedule_students.js
@@ -2,11 +2,12 @@ const { stan } = require('./nats_connection')
 const Schedule = require('../models')
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-const scheduleActiveStudents = async () => {
-  const tasks = [...(await Schedule.find({ type: 'student', active: true }))]
-  console.log(tasks.length, 'tasks to schedule')
+const publish  = async (tasks) => {
+  let rampup = 300 
   for (const task of tasks) {
+    if (rampup > 1) {
+      rampup = rampup - 1
+    }
     stan.publish('UpdateApi', task.task, (err, guid) => {
       console.log(guid)
       if (err) {
@@ -18,27 +19,18 @@ const scheduleActiveStudents = async () => {
         console.log('publish failed')
       }
     })
-    await sleep(15)
+    await sleep(15 * rampup)
   }
+}
+const scheduleActiveStudents = async () => {
+  const tasks = [...(await Schedule.find({ type: 'student', active: true }))]
+  console.log(tasks.length, 'tasks to schedule')
+  publish()
 }
 const scheduleAllStudentsAndMeta = async () => {
 
   const tasks = ['meta', ...(await Schedule.find({ type: 'student' }))]
-  console.log(await Schedule.find())
   console.log(tasks.length, 'tasks to schedule')
-  for (const task of tasks) {
-    stan.publish('UpdateApi', task.task, (err, guid) => {
-      console.log(guid)
-      if (err) {
-        console.log('publish failed')
-      }
-    })
-    stan.publish('status', `${task.task}:SCHEDULED`, (err) => {
-      if (err) {
-        console.log('publish failed')
-      }
-    })
-    await sleep(15)
-  }
+  publish()
 }
 module.exports = { scheduleActiveStudents, scheduleAllStudentsAndMeta }
