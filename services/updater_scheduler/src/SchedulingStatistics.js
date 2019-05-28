2c941337e103d6dba80a59da54d62061a019e476
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-05-28 13:34:04 +0300

    change graylog data

--- services/updater_scheduler/src/SchedulingStatistics.js
+++ services/updater_scheduler/src/SchedulingStatistics.js
@@ -2,17 +2,18 @@ const Schedule = require('../models')
 
 const getOldestTasks = async () => {
   const oldestTask = await Schedule.find({ type: 'student' }).sort({ updatedAt: 1 }).limit(1)
-  const oldestActiveTask = await Schedule.find({ type: 'student', active: true }).sort({ updatedAt: 1 }).limit(1)
+  const oldestActiveTask = await Schedule.find({ type: 'student', active: true, status: 'DONE' }).sort({ updatedAt: 1 }).limit(1)
 
-  return { oldestTask, oldestActiveTask }
+  return {
+    oldestChangedStatus: { studentnumber: oldestTask.task, updatedAt: oldestTask.updatedAt }, oldestActiveStudentDone: { studentnumber: oldestActiveTask.task, updatedAt: oldestActiveTask.updatedAt }
+  }
 }
-
 const getCurrentStatus = async () => {
   const allTasksScheduled = await Schedule.find({ type: 'student', status: 'SCHEDULED' })
   const allTasksFetched = await Schedule.find({ type: 'student', status: 'FETCHED' })
   const allTasksDone = await Schedule.find({ type: 'student', status: 'DONE' })
-  
-  return `allTasksScheduled: ${allTasksScheduled.length} | allTasksFetched: ${allTasksFetched.length} | allTasksDone: ${allTasksDone.length}`
+
+  return { allTasksScheduled: allTasksScheduled.length, allTasksFetched: allTasksFetched.length, allTasksDone: allTasksDone.length }
 }
 
 module.exports = { getOldestTasks, getCurrentStatus }
