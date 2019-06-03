7826b40344ca0a80a2e8e11783315ef7e788a9b9
Rochet2 <tqbattlenet@gmail.com>
2019-06-03 12:14:09 +0300

    Implement sorting to overview programme listing

--- services/oodikone2-frontend/src/components/StudyProgramme/StudyProgrammeSelector/index.jsx
+++ services/oodikone2-frontend/src/components/StudyProgramme/StudyProgrammeSelector/index.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { arrayOf, func, string, bool } from 'prop-types'
-import { sortBy } from 'lodash'
-import { Loader } from 'semantic-ui-react'
+import { arrayOf, func, string, bool, shape } from 'prop-types'
+import { Loader, Message } from 'semantic-ui-react'
 import { getDegreesAndProgrammes } from '../../../redux/populationDegreesAndProgrammes'
-import Table from '../../SearchResultTable'
 import { getTextIn } from '../../../common'
-
-const headers = [
-  'name',
-  'code'
-]
+import SortableTable from '../../SortableTable'
 
 class StudyProgrammeSelector extends Component {
   static propTypes = {
     getDegreesAndProgrammes: func.isRequired,
     handleSelect: func.isRequired,
-    studyprogrammes: arrayOf(arrayOf(string)), // eslint-disable-line
-    selected: bool.isRequired
+    studyprogrammes: arrayOf(shape({ name: shape({}), code: string })),
+    selected: bool.isRequired,
+    language: string.isRequired
+  }
+
+  static defaultProps = {
+    studyprogrammes: null
   }
 
   componentDidMount() {
@@ -27,16 +26,32 @@ class StudyProgrammeSelector extends Component {
   }
 
   render() {
-    const { studyprogrammes, selected } = this.props
+    const { studyprogrammes, selected, language } = this.props
     if (selected) return null
     if (!studyprogrammes) return <Loader active>Loading</Loader>
+
+    const headers = [
+      {
+        key: 'name',
+        title: 'name',
+        getRowContent: prog => getTextIn(prog.name, language)
+      },
+      {
+        key: 'code',
+        title: 'code',
+        getRowContent: prog => prog.code
+      }
+    ]
+    if (studyprogrammes == null) {
+      return <Message>You do not have access to any programmes</Message>
+    }
     return (
-      <Table
-        headers={headers}
-        rows={studyprogrammes}
-        rowClickFn={(e, row) => this.props.handleSelect(row)}
-        selectable
-        noResultText="No study programmes"
+      <SortableTable
+        columns={headers}
+        getRowKey={programme => programme.code}
+        getRowProps={programme => ({ onClick: () => this.props.handleSelect(programme.code), style: { cursor: 'pointer' } })}
+        data={studyprogrammes}
+        defaultdescending
       />
     )
   }
@@ -47,9 +62,9 @@ const mapStateToProps = ({ populationDegreesAndProgrammes, settings }) => {
   const { language } = settings
 
   return {
-    studyprogrammes: programmes ? sortBy(Object.values(programmes).filter(programme =>
-      programme.code.includes('_'))
-      .map(programme => [getTextIn(programme.name, language), programme.code]), '0') : null
+    studyprogrammes: programmes ? Object.values(programmes).filter(programme =>
+      programme.code.includes('_')) : null,
+    language
   }
 }
 
