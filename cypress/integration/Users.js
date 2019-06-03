e50ca0d20bffb23a02e71e44128c56f108c9d21c
Rochet2 <tqbattlenet@gmail.com>
2019-06-03 15:50:37 +0300

    Add tests for superlogin/mocking

---
+++ cypress/integration/Users.js
@@ -0,0 +1,48 @@
+
+describe('Users tests', () => {
+  beforeEach(() => {
+    cy.server({
+      onAnyRequest: function (route, proxy) {
+        if (Cypress.config().baseUrl.includes("http://localhost:1337/")) {
+          proxy.xhr.setRequestHeader('uid', 'admink')
+          proxy.xhr.setRequestHeader('shib-session-id', 'mock-shibboleth')
+          proxy.xhr.setRequestHeader('hygroupcn', 'grp-oodikone-users')
+          proxy.xhr.setRequestHeader('edupersonaffiliation', 'asdasd')
+        }
+      }
+    })
+    console.log(Cypress.config().baseUrl)
+    cy.visit(Cypress.config().baseUrl)
+    cy.reload()
+    cy.contains("Users").click()
+    cy.contains("Enable or disable access to Oodikone")
+  })
+
+  it('Admin mocking user shows "mocking as"-button', () => {
+    cy.contains("mocking").should('not.exist')
+    cy.contains("tr", "Normaalikäyttäjä").within(($row) => {
+      cy.contains('button', 'Edit').click()
+    })
+
+    cy.get('i.spy').click()
+    cy.contains("mocking as normk")
+  })
+
+  it("Admin mocking normal user shows only the mocked user's programmes", () => {
+    cy.contains("mocking").should('not.exist')
+    cy.contains("tr", "Normaalikäyttäjä").within(($row) => {
+      cy.contains('button', 'Edit').click()
+    })
+    cy.contains("Access rights").siblings().within(($row) => {
+      cy.get("div[role='listitem']").should('have.length', 1).contains("Tietojenkäsittelytieteen kandiohjelma")
+    })
+
+    cy.get('i.spy').click()
+    cy.contains("Study programme").click().siblings().contains("Search by class").click()
+    cy.contains("label", "Study programme")
+    cy.contains("label", "Study programme").siblings().within(($row) => {
+      cy.get("div[role='option']").should('have.length', 1).contains("Tietojenkäsittelytieteen kandiohjelma")
+    })
+  })
+
+})
