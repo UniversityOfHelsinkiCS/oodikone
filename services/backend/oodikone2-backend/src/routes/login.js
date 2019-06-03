779934698606b5b30e93f3efa7563aae3ea924d8
Rochet2 <tqbattlenet@gmail.com>
2019-06-03 18:30:04 +0300

    Fix mockedby impersonation

--- services/backend/oodikone2-backend/src/routes/login.js
+++ services/backend/oodikone2-backend/src/routes/login.js
@@ -44,26 +44,6 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/superlogin/:uid', async (req, res) => {
-  try {
-    const asUser = req.params.uid
-    const uid = req.headers['uid']
-    if (req.headers['shib-session-id'] && uid) {
-      console.log('super')
-      const token = await userService.superlogin(uid, asUser)
-      res.status(200).json({ token })
-    } else {
-      res.status(401).json({
-        message: `Not enough headers login, uid:
-        ${req.headers.uid} session-id ${req.headers['shib-session-id']}`
-      }).end()
-    }
-  } catch (err) {
-    console.log(err.message)
-    res.status(401).json({ message: 'problem with login', err: err.message })
-  }
-})
-
 router.delete('/logout', async (req, res) => {
   try {
     const logoutUrl = req.headers.shib_logout_url
