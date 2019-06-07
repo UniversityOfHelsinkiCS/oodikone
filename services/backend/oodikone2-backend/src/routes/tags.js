95d823d32668037497e69e119f4b076f1bbb2a6f
esakemp <esa.kemppainen@helsinki.fi>
2019-06-07 12:08:10 +0300

    get tags from database to backend to frontend

---
+++ services/backend/oodikone2-backend/src/routes/tags.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const Tags = require('../services/tags')
+
+
+router.get('/tags', async (req, res) => {
+  console.log('request got to routes')
+  try {
+    const tags = await Tags.findTags()
+    console.log('response', tags)
+    res.status(200).json(tags)
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err)
+  }
+})
+
+router.post('/studytracktags', async (req, res) => {
+  const { tag } = req.params
+  try {
+    const result = await Tags.createNewTag(tag)
+    res.status(200).json(result)
+  } catch (err) {
+    console.log(err)
+    res.status(400).json(err.message)
+  }
+})
+
+module.exports = router
