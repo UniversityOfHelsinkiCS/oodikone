d631c69819cdfc66d96bd0428e7774d87ebf2b1d
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-06-28 17:49:23 +0300

    faculties route created

---
+++ services/oodikone2-frontend/src/components/Faculty/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { withRouter } from 'react-router'
+import { Header, Segment } from 'semantic-ui-react'
+import FacultySelector from './FacultySelector'
+
+const Faculty = () => (
+  <div className="segmentContainer">
+    <Header className="segmentTitle" size="large">
+      Faculties
+    </Header>
+    <Segment className="contentSegment" >
+      <FacultySelector />
+    </Segment>
+  </div>
+)
+
+
+export default withRouter(Faculty)
