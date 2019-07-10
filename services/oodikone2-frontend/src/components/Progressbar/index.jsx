cab98792dc8e8cdf9714b15d0c0cdcab6846aeab
Mäkinen Sasu S <sasu.makinen@helsinki.fi>
2019-07-10 15:55:21 +0300

    perlerkelrek

--- services/oodikone2-frontend/src/components/Progressbar/index.jsx
+++ services/oodikone2-frontend/src/components/Progressbar/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { Progress } from 'semantic-ui-react'
+import { useState, useEffect } from 'react'
+// import { Progress } from 'semantic-ui-react'
 import { number, bool } from 'prop-types'
 
 const Progressbar = ({ time, pending }) => {
