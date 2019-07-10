51d809135d1fc20241d59d492805334977200d3a
Rochet2 <tqbattlenet@gmail.com>
2019-07-10 16:01:40 +0300

    Trunkate broken migrations

--- services/backend/shared/migrations/20190702_02_drop_schema_kone.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.dropSchema('kone_data', { transaction })
-    })
-  },
-  down: async () => {
-  }
-}
