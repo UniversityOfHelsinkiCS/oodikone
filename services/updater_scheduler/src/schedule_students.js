f6962766aba5ea070e510c25ea7876bc0542e7d3
Rochet2 <tqbattlenet@gmail.com>
2019-07-25 13:39:14 +0300

    Add lint to updater scheduler and api

--- services/updater_scheduler/src/schedule_students.js
+++ services/updater_scheduler/src/schedule_students.js
@@ -25,7 +25,7 @@ const publish = async (tasks, priority = false) => {
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    [array[i], array[j]] = [array[j], array[i]]
   }
   return array
 }
