55f75a6bcd973bb22c3f2276417cf68ef3607e20
esakemp <esa.kemppainen@helsinki.fi>
2019-07-26 11:13:11 +0300

    make population of course look better

--- services/oodikone2-frontend/src/components/CourseStudents/index.jsx
+++ services/oodikone2-frontend/src/components/CourseStudents/index.jsx
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { shape, func, bool } from 'prop-types'
 import { getTranslate } from 'react-localize-redux'
+import { Segment, Header, Popup } from 'semantic-ui-react'
 
 import qs from 'query-string'
 
@@ -10,6 +11,10 @@ import { getCoursePopulation, getCoursePopulationCourses } from '../../redux/cou
 import CreditAccumulationGraphHighCharts from '../CreditAccumulationGraphHighCharts'
 import PopulationStudents from '../PopulationStudents'
 import PopulationCourses from '../PopulationCourseStats'
+import infoTooltips from '../../common/InfoToolTips'
+import InfoBox from '../InfoBox'
+import SegmentDimmer from '../SegmentDimmer'
+
 
 const CourseStudents = ({ getCoursePopulationDispatch, getCoursePopulationCoursesDispatch, studentData, courses, pending, history, translate }) => {
   const parseQueryFromUrl = () => {
@@ -22,25 +27,45 @@ const CourseStudents = ({ getCoursePopulationDispatch, getCoursePopulationCourse
     getCoursePopulationDispatch({ coursecode: query.coursecode, yearcode: query.yearcode })
     getCoursePopulationCoursesDispatch({ coursecode: query.coursecode, yearcode: query.yearcode })
   }, [])
+  const { CreditAccumulationGraph, CoursesOf } = infoTooltips.PopulationStatistics
 
   const selectedStudents = studentData.students ? studentData.students.map(student => student.studentNumber) : []
 
   return (
-    <div>
+    <div className="segmentContainer">
       {studentData.students ? (
         <div>
-          <CreditAccumulationGraphHighCharts
-            students={studentData.students}
-            selectedStudents={selectedStudents}
-            title={`${translate('populationStatistics.sampleId')}`}
-            translate={translate}
+          <Segment>
+            <Header size="medium" dividing>
+              {translate('populationStatistics.graphSegmentHeader')} (for {selectedStudents.length} students)
+              <InfoBox content={CreditAccumulationGraph} />
+            </Header>
+            <CreditAccumulationGraphHighCharts
+              students={studentData.students}
+              selectedStudents={selectedStudents}
+              title={`${translate('populationStatistics.sampleId')}`}
+              translate={translate}
 
-          />
-          <PopulationCourses
-            courses={courses}
-            pending={pending}
-            selectedStudents={selectedStudents}
-          />
+            />
+          </Segment>
+          <Segment>
+            <Header size="medium" dividing >
+              <Popup
+                trigger={<Header.Content>{translate('populationCourses.header')}</Header.Content>}
+                content="Sort by clicking columns. Click course name to limit observed population to students who
+                  participated to the course."
+                wide
+                position="top left"
+              />
+              <InfoBox content={CoursesOf} />
+            </Header>
+            <SegmentDimmer translate={translate} isLoading={pending} />
+            <PopulationCourses
+              courses={courses}
+              pending={pending}
+              selectedStudents={selectedStudents}
+            />
+          </Segment>
           <PopulationStudents
             samples={studentData.students}
             selectedStudents={selectedStudents}
