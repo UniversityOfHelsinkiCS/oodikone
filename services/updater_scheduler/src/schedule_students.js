84869f7f5306c3690c06ce1205f57756d3d7c9e8
Rochet2 <tqbattlenet@gmail.com>
2019-07-29 16:12:01 +0300

    Comment out unused shuffle for eslint

--- services/updater_scheduler/src/schedule_students.js
+++ services/updater_scheduler/src/schedule_students.js
@@ -22,13 +22,14 @@ const publish = async (tasks, priority = false) => {
     await sleep(15 * rampup)
   }
 }
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
-  }
-  return array
-}
+
+// const shuffleArray = (array) => {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]]
+//   }
+//   return array
+// }
 
 const scheduleActiveStudents = async () => {
   const tasks = [...await Schedule.find({ type: 'student', active: true }).sort({ updatedAt: 1 })]
