25c383957cd4bb98f10b99552ab65ff812473169
esakemp <esa.kemppainen@helsinki.fi>
2019-07-30 10:38:55 +0300

    course population filter stuff

---
+++ services/oodikone2-frontend/src/components/CourseStudentsFilters/index.jsx
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { Segment, Header, Button, Form, Radio } from 'semantic-ui-react'
+import { func, arrayOf, bool, shape } from 'prop-types'
+import _ from 'lodash'
+
+import CreditsLessThan from '../PopulationFilters/CreditsLessThan'
+import CreditsAtLeast from '../PopulationFilters/CreditsAtLeast'
+import GradeMeanFilter from '../PopulationFilters/GradeMeanFilter'
+import CourseParticipation from '../PopulationFilters/CourseParticipation'
+import SexFilter from '../PopulationFilters/SexFilter'
+import InfoBox from '../InfoBox'
+import infotooltips from '../../common/InfoToolTips'
+import {
+  clearPopulationFilters, setComplementFilter
+} from '../../redux/populationFilters'
+
+
+const componentFor = {
+  CreditsAtLeast,
+  CreditsLessThan,
+  GradeMeanFilter,
+  SexFilter,
+  CourseParticipation
+}
+
+const CourseStudentsFilters = ({ samples, filters, clearPopulationFiltersDispatch, complemented, setComplementFilterDispatch }) => {
+  const [visible, setVisible] = useState(false)
+
+  const renderAddFilters = () => {
+    const { Add } = infotooltips.PopulationStatistics.Filters
+    const allFilters = _.union(Object.keys(componentFor))
+
+    const setFilters = _.union(
+      filters.map(f => f.type),
+      filters.filter(f => f.type === 'Preset').map(f => f.id),
+    )
+
+    const unsetFilters = _.uniq(_.difference(allFilters, setFilters))
+    if (unsetFilters.length === 0) {
+      return null
+    }
+    if (!visible) {
+      return (
+        <Segment>
+          <Header>Add filters <InfoBox content={Add} /></Header>
+          <Button
+            onClick={() => setVisible(true)}
+          >
+            add
+          </Button>
+        </Segment>
+      )
+    }
+    return (
+      <Segment>
+        <Header>Add filters <InfoBox content={Add} /></Header>
+        {unsetFilters.map(filterName => React.createElement(componentFor[filterName], { filter: { notSet: true }, key: filterName, samples }))}
+        <Button onClick={() => setVisible(false)}>cancel</Button>
+      </Segment>
+    )
+  }
+
+  const renderSetFilters = () => {
+    const setFilters = filters.map(f => f.type)
+    const { Filters } = infotooltips.PopulationStatistics.Filters
+    if (setFilters.length === 0) {
+      return null
+    }
+
+    return (
+      <Segment>
+        <Header>Filters <InfoBox content={Filters} /></Header>
+        {filters.map(filter => React.createElement(componentFor[filter.type], { filter, key: filter.id, samples }))}
+        <Form>
+          <Form.Group inline>
+            <Form.Field>
+              <label>Show excluded students only</label>
+            </Form.Field>
+            <Form.Field>
+              <Radio
+                toggle
+                checked={complemented}
+                onClick={setComplementFilterDispatch}
+              />
+            </Form.Field>
+          </Form.Group>
+        </Form>
+        <Button onClick={clearPopulationFiltersDispatch}>clear all filters</Button>
+      </Segment>
+    )
+  }
+  return (
+    <div>
+      {renderAddFilters()}
+      {renderSetFilters()}
+    </div>
+  )
+}
+
+CourseStudentsFilters.propTypes = {
+  filters: arrayOf(shape([])).isRequired,
+  complemented: bool.isRequired,
+  samples: arrayOf(shape([])).isRequired,
+  clearPopulationFiltersDispatch: func.isRequired,
+  setComplementFilterDispatch: func.isRequired
+}
+
+const mapStateToProps = ({ populationFilters }) => ({
+  filters: populationFilters.filters,
+  complemented: populationFilters.complemented
+})
+
+export default connect(mapStateToProps, {
+  clearPopulationFiltersDispatch: clearPopulationFilters,
+  setComplementFilterDispatch: setComplementFilter
+})(CourseStudentsFilters)
