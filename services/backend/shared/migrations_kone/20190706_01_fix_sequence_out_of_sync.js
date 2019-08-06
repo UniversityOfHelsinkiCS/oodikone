baa9f2dddb6fc15654e88dfcb28a04b1ea762496
Rochet2 <tqbattlenet@gmail.com>
2019-08-06 18:29:29 +0300

    fix lint

--- services/backend/shared/migrations_kone/20190706_01_fix_sequence_out_of_sync.js
+++ services/backend/shared/migrations_kone/20190706_01_fix_sequence_out_of_sync.js
@@ -1,12 +1,12 @@
 module.exports = {
   up: async (queryInterface) => {
     return queryInterface.sequelize.transaction(async transaction => {
-      await queryInterface.sequelize.query(`SELECT setval('mandatory_courses_id_seq', (SELECT MAX(id) FROM mandatory_courses))`, { transaction })
-      await queryInterface.sequelize.query(`SELECT setval('course_groups_id_seq', (SELECT MAX(id) FROM course_groups))`, { transaction })
-      await queryInterface.sequelize.query(`SELECT setval('mandatory_course_labels_id_seq', (SELECT MAX(id) FROM mandatory_course_labels))`, { transaction })
-      await queryInterface.sequelize.query(`SELECT setval('tag_tag_id_seq', (SELECT MAX(tag_id) FROM tag))`, { transaction })
-      await queryInterface.sequelize.query(`SELECT setval('tag_student_id_seq', (SELECT MAX(id) FROM tag_student))`, { transaction })
-      await queryInterface.sequelize.query(`SELECT setval('teacher_course_group_id_seq', (SELECT MAX(id) FROM teacher_course_groups))`, { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'mandatory_courses_id_seq\', (SELECT MAX(id) FROM mandatory_courses))', { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'course_groups_id_seq\', (SELECT MAX(id) FROM course_groups))', { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'mandatory_course_labels_id_seq\', (SELECT MAX(id) FROM mandatory_course_labels))', { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'tag_tag_id_seq\', (SELECT MAX(tag_id) FROM tag))', { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'tag_student_id_seq\', (SELECT MAX(id) FROM tag_student))', { transaction })
+      await queryInterface.sequelize.query('SELECT setval(\'teacher_course_group_id_seq\', (SELECT MAX(id) FROM teacher_course_groups))', { transaction })
     })
   },
   down: async () => {
