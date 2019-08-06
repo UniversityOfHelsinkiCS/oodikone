91209252cbcbf377e345e7ac2cd47dfe5e5a4d98
Rochet2 <tqbattlenet@gmail.com>
2019-08-06 18:06:53 +0300

    Fix all out of sync sequences

--- services/backend/shared/migrations_kone/20190706_00_fix_sequence_out_of_sync.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  up: async (queryInterface) => {
-    await queryInterface.sequelize.query(`SELECT setval('mandatory_courses_id_seq', (SELECT MAX(id) FROM mandatory_courses))`)
-  },
-  down: async () => {
-  }
-}
