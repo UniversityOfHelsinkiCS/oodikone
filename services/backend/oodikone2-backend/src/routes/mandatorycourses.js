fc8839e9fb0fc4ac0cbb7224bba717188c5af5ec
Rochet2 <tqbattlenet@gmail.com>
2019-08-16 19:27:18 +0300

    Fix https://github.com/UniversityOfHelsinkiCS/oodikone/issues/1243

--- services/backend/oodikone2-backend/src/routes/mandatorycourses.js
+++ services/backend/oodikone2-backend/src/routes/mandatorycourses.js
@@ -4,56 +4,49 @@ const MandatoryCourses = require('../services/mandatoryCourses')
 router.delete('/:programme', async (req, res) => {
   const { programme } = req.params
   const { course } = req.body
-  if (programme && course) {
-    try {
-      await MandatoryCourses.remove(programme, course)
-      res.status(200).json(course)
-    } catch (err) {
-      console.error(err)
-      res.status(400).json(err.message)
-    }
-  } else {
-    res.status(422).end()
+  if (!programme || !course) return res.status(400).end()
+  try {
+    await MandatoryCourses.remove(programme, course)
+    res.status(200).json(course)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json(err.message)
   }
 })
 
 router.post('/:programme', async (req, res) => {
   const { programme } = req.params
   const { course } = req.body
-  if (programme && course) {
-    try {
-      await MandatoryCourses.create(programme, course)
-      const newCourse = await MandatoryCourses.find(programme, course)
-      res.status(201).json({ name: newCourse.course.name, code: newCourse.course_code, label: newCourse.label })
-    } catch (err) {
-      console.error(err)
-      res.status(400).json(err.message)
-    }
-  } else {
-    res.status(422).end()
+  if (!programme || !course) return res.status(400).end()
+  try {
+    await MandatoryCourses.create(programme, course)
+    const newCourse = await MandatoryCourses.find(programme, course)
+    if (!newCourse) return res.status(400).end()
+    res.status(201).json({ name: newCourse.course.name, code: newCourse.course_code, label: newCourse.label })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json(err.message)
   }
 })
 
 router.post('/:programme/label/:course', async (req, res) => {
   const { programme, course } = req.params
   const { label } = req.body
-  if (programme && course) {
-    try {
-      await MandatoryCourses.updateLabel(programme, course, label)
-      const updatedCourse = await MandatoryCourses.find(programme, course)
-      res
-        .status(200)
-        .json({
-          name: updatedCourse.course.name,
-          code: updatedCourse.course_code,
-          label: updatedCourse.mandatory_course_label
-        })
-    } catch (err) {
-      console.error(err)
-      res.status(400).json(err.message)
-    }
-  } else {
-    res.status(422).end()
+  if (!programme || !course) return res.status(400).end()
+  try {
+    await MandatoryCourses.updateLabel(programme, course, label)
+    const updatedCourse = await MandatoryCourses.find(programme, course)
+    if (!updatedCourse) return res.status(400).end()
+    res
+      .status(200)
+      .json({
+        name: updatedCourse.course.name,
+        code: updatedCourse.course_code,
+        label: updatedCourse.mandatory_course_label
+      })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json(err.message)
   }
 })
 
