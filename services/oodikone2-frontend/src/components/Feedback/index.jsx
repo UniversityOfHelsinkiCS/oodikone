c235c62d03ca127b3460f379b5b197e92cc32384
esakemp <esa.kemppainen@helsinki.fi>
2019-08-26 14:18:32 +0300

    feedbakc changes

--- services/oodikone2-frontend/src/components/Feedback/index.jsx
+++ services/oodikone2-frontend/src/components/Feedback/index.jsx
@@ -1,32 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { Form, TextArea, Button, Confirm } from 'semantic-ui-react'
+import { Form, TextArea, Button, Modal } from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 
 import { sendFeedbackAction } from '../../redux/feedback'
 
-const Feedback = ({ sendFeedback, success }) => {
+const Feedback = ({ sendFeedback, success, pending, error }) => {
   const [feedback, setFeedback] = useState('')
-  const [confirm, setConfirm] = useState(false)
+  const [showError, setError] = useState(false)
   const [show, setShow] = useState(false)
 
   useEffect(() => {
     if (success) {
       setFeedback('')
-      setConfirm(false)
       setShow(true)
       setTimeout(() => {
         setShow(false)
       }, 10000)
-    } else {
-      setConfirm(false)
     }
-  }, [success])
-  // just a dummy commit
-  const open = () => setConfirm(true)
-
-  const close = () => setConfirm(false)
+    if (error) {
+      setError(true)
+      setTimeout(() => {
+        setError(false)
+      }, 10000)
+    }
+  }, [pending])
 
   const handleTyping = ({ target }) => {
     setFeedback(target.value)
@@ -46,20 +45,32 @@ const Feedback = ({ sendFeedback, success }) => {
           </div>
           <p>Thank you for contacting us. We will contact you soon.</p>
         </div>) : null}
+      {showError === true ? (
+        <div className="ui negative message" align="center" style={{ maxWidth: 1000 }}>
+          <div className="header">
+            Your question/feedback was not sent
+          </div>
+          <p>An error occured while trying to send your message. Please try again.</p>
+        </div>) : null}
       <Form >
         Questions/feedback:
         <div>
-          <TextArea placeholder="Tell us more" style={{ minHeight: 400, maxWidth: 1000, maxHeight: 400 }} onChange={handleTyping} value={feedback} />
+          <TextArea placeholder="Tell us more" style={{ minHeight: 400, maxWidth: 1000 }} onChange={handleTyping} value={feedback} />
         </div>
         <div>
-          <Button onClick={open}> submit</Button>
-          <Confirm
-            open={confirm}
-            onCancel={close}
-            onConfirm={handleSubmit}
-            content="Sending mail to Toska"
-            cancelButton="Cancel"
-            confirmButton="Confirm"
+          <Modal
+            trigger={<Button disabled={feedback.length < 1 || pending}> submit</Button>}
+            header="Sending mail to Toska"
+            actions={[
+              'Cancel',
+              {
+                key: 'send',
+                content: 'Send',
+                positive: true,
+                disabled: pending,
+                onClick: event => handleSubmit(event)
+              }
+            ]}
           />
         </div>
       </Form>
@@ -68,12 +79,16 @@ const Feedback = ({ sendFeedback, success }) => {
 }
 
 const mapStateToProps = ({ feedback }) => ({
-  success: feedback.success
+  success: feedback.success,
+  pending: feedback.pending,
+  error: feedback.error
 })
 
 Feedback.propTypes = {
   sendFeedback: PropTypes.func.isRequired,
-  success: PropTypes.bool.isRequired
+  success: PropTypes.bool.isRequired,
+  pending: PropTypes.bool.isRequired,
+  error: PropTypes.bool.isRequired
 }
 
 export default withRouter(connect(mapStateToProps, { sendFeedback: sendFeedbackAction })(Feedback))
