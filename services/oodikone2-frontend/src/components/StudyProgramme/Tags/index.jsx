3a0c90878f39e8d5816f0b3b91b424ae07fcc833
esakemp <esa.kemppainen@helsinki.fi>
2019-09-02 12:58:11 +0300

    garbo

--- services/oodikone2-frontend/src/components/StudyProgramme/Tags/index.jsx
+++ services/oodikone2-frontend/src/components/StudyProgramme/Tags/index.jsx
@@ -1,15 +1,20 @@
 import React, { useState, useEffect } from 'react'
+import Datetime from 'react-datetime'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Button, List, Label, Icon, Header, Confirm, Form } from 'semantic-ui-react'
 import { arrayOf, string, shape, func } from 'prop-types'
 
 import TagModal from '../TagModal'
+import { reformatDate } from '../../../common'
 import { getTagsByStudytrackAction, createTagAction, deleteTagAction } from '../../../redux/tags'
+
+const YEAR_DATE_FORMAT = 'YYYY'
 
 const Tags = ({ createTag, deleteTag, getTagsByStudytrack, tags, studyprogramme }) => {
   const [tagname, setTagname] = useState('')
   const [confirm, setConfirm] = useState(null)
+  const [year, setYear] = useState('')
 
   useEffect(() => {
     getTagsByStudytrack(studyprogramme)
@@ -21,25 +26,32 @@ const Tags = ({ createTag, deleteTag, getTagsByStudytrack, tags, studyprogramme 
     setConfirm(null)
   }
 
+  const handleTagYearSelect = (momentYear) => {
+    setYear(reformatDate(momentYear, YEAR_DATE_FORMAT))
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
     const newTag = {
       tagname: tagname.trim(),
-      studytrack: studyprogramme
+      studytrack: studyprogramme,
+      year
     }
     createTag(newTag)
     setTagname('')
+    setYear('')
   }
 
   const handleChange = ({ target }) => {
     setTagname(target.value)
   }
 
+
   const rows = tags.map(tag => (
     <List.Item key={tag.tag_id}>
       <List.Content>
         <Label>
-          {tag.tagname} <Icon name="delete" link onClick={() => setConfirm(tag.tag_id)} />
+          tagname: {tag.tagname}, year: {tag.year}  <Icon name="delete" link onClick={() => setConfirm(tag.tag_id)} />
         </Label>
         <Confirm
           open={confirm === tag.tag_id}
@@ -57,13 +69,37 @@ const Tags = ({ createTag, deleteTag, getTagsByStudytrack, tags, studyprogramme 
     <List>
       <Form>
         <Form.Group>
-          <Form.Input onChange={handleChange} value={tagname} action={<Button disabled={!tagname.trim() || tags.find(t => t.tagname === tagname.trim())} onClick={handleSubmit}>add new tag</Button>} />
+          <Form.Field>
+            <label>
+              Tag name
+            </label>
+            <Form.Input
+              onChange={handleChange}
+              value={tagname}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>
+              Associated start year
+            </label>
+            <Datetime
+              className="yearSelectInput"
+              control={Datetime}
+              dateFormat={YEAR_DATE_FORMAT}
+              timeFormat={false}
+              renderYear={(props, selectableYear) => <td {...props}>{selectableYear}</td>}
+              closeOnSelect
+              value={year}
+              onChange={handleTagYearSelect}
+            />
+          </Form.Field>
+          <Button disabled={!tagname.trim() || tags.find(t => t.tagname === tagname.trim()) || !year} onClick={handleSubmit}> add new tag </Button>
           <TagModal tags={tags} studytrack={studyprogramme} />
         </Form.Group>
       </Form>
       <Header size="medium">Study programme tags</Header>
       {rows}
-    </List>
+    </List >
   )
 }
 
