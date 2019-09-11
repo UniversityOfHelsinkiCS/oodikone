25e1c5891de1360233583568217cb4d422e9973d
Warro <olli.warro@gmail.com>
2019-09-11 12:23:56 +0300

    Fixed migration for real this time

--- services/oodikone2-userservice/src/database/migrations/20190910_00_faculty_accessgroup.js
+++ services/oodikone2-userservice/src/database/migrations/20190910_00_faculty_accessgroup.js
@@ -1,14 +1,41 @@
-const { AccessGroup } = require('../../models')
-const accessGroup = {
-  group_code: 'faculties',
-  group_info: 'grants access to faculty statistics',
-}
-
 module.exports = {
-  up: async () => {
-    AccessGroup.create(accessGroup)
+  up: async (queryInterface) => {
+    return new Promise(async (res) => {
+      // For some reason sequelize tried to create a new
+      // access group with an already existing id (= 8).
+      // Therefore we need to "update" sequelize's auto increment
+      // by deleting the corrupted id.
+      const buggedAccessGroup = (await queryInterface.sequelize.query(
+        `SELECT * FROM access_groups WHERE id = ?`, {
+          replacements: [8],
+          type: queryInterface.sequelize.QueryTypes.SELECT
+        }
+      ))[0]
+
+      if (buggedAccessGroup) {
+        queryInterface.bulkDelete('access_groups', { id: 8 })
+        queryInterface.bulkInsert('access_groups', [
+          {
+            group_code: buggedAccessGroup.group_code,
+            group_info: buggedAccessGroup.group_info,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        ])
+      }
+
+      queryInterface.bulkInsert('access_groups', [
+        {
+          group_code: 'faculties',
+          group_info: 'grants access to faculty statistics',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ])
+
+      res()
+    })
   },
   down: async () => {
-    AccessGroup.destroy({ where: { group_code: ['faculties'] } })
   }
 }
