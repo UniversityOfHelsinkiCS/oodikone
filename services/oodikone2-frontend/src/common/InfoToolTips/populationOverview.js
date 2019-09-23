f3ecc8f742d60db8c40f70a5395cadb94d6cad6f
esakemp <40692959+esakemp@users.noreply.github.com>
2019-09-23 10:57:36 +0300

    Update populationOverview.js

--- services/oodikone2-frontend/src/common/InfoToolTips/populationOverview.js
+++ services/oodikone2-frontend/src/common/InfoToolTips/populationOverview.js
@@ -1,3 +1,4 @@
 export default {
-  Overview: 'Tämä koontisivu sisältää koulutusohjelman vuosikurssien perustiedot. Mustista nuolista voi siirtyä tarkastelemaan tietyn vuosikurssin tarkempia tietoja.'
+  Overview: `Tämä koontisivu sisältää koulutusohjelman vuosikurssien perustiedot. 
+  Mustista nuolista voi siirtyä tarkastelemaan tietyn vuosikurssin tarkempia tietoja.`
 }
