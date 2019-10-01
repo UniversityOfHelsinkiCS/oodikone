32c245f2598be90d55c32b3e9b1b68be423d9fd6
esakemp <esa.kemppainen@helsinki.fi>
2019-10-01 15:11:54 +0300

    fix participation in custom and course pop

--- services/backend/oodikone2-backend/src/services/populations.js
+++ services/backend/oodikone2-backend/src/services/populations.js
@@ -689,8 +689,8 @@ const bottlenecksOf = async (query, studentnumberlist) => {
       const courses = await findCourses(studentnumbers, dateMonthsFromNow(startDate, months))
       return [allstudents, courses]
     } else {
-      const allstudents = studentnumberlist
-      const courses = await findCourses(allstudents, new Date())
+      const allstudents = studentnumberlist.reduce((numbers, num) => ({ ...numbers, [num]: true }), {})
+      const courses = await findCourses(studentnumberlist, new Date())
       return [allstudents, courses]
     }
   }
