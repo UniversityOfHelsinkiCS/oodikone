0caa827c936de12b0477770819ea694e67730ed5
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-10-01 16:37:34 +0300

    Add and run prettier on usageservice

--- services/oodikone2-usageservice/src/models.js
+++ services/oodikone2-usageservice/src/models.js
@@ -1,39 +1,42 @@
 const Sequelize = require('sequelize')
 const { sequelize } = require('./database/connection')
 
-const UsageStatistic = sequelize.define('usage_statistics', {
-  id: {
-    primaryKey: true,
-    type: Sequelize.STRING
+const UsageStatistic = sequelize.define(
+  'usage_statistics',
+  {
+    id: {
+      primaryKey: true,
+      type: Sequelize.STRING
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    time: {
+      type: Sequelize.INTEGER
+    },
+    admin: {
+      type: Sequelize.BOOLEAN
+    },
+    method: {
+      type: Sequelize.STRING
+    },
+    URL: {
+      type: Sequelize.STRING
+    },
+    status: {
+      type: Sequelize.INTEGER
+    },
+    data: {
+      type: Sequelize.JSONB
+    }
   },
-  username: {
-    type: Sequelize.STRING
-  },
-  name: {
-    type: Sequelize.STRING
-  },
-  time: {
-    type: Sequelize.INTEGER
-  },
-  admin: {
-    type: Sequelize.BOOLEAN
-  },
-  method: {
-    type: Sequelize.STRING
-  },
-  URL: {
-    type: Sequelize.STRING
-  },
-  status: {
-    type: Sequelize.INTEGER
-  },
-  data: {
-    type: Sequelize.JSONB
-  },
-
-}, {
-  timestamps: false
-})
+  {
+    timestamps: false
+  }
+)
 
 module.exports = {
   UsageStatistic,
