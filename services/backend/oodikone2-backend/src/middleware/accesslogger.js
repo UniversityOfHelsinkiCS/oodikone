f3e895f1fd76cbc1572b41bbcf8d338de824d259
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-10-04 14:21:43 +0300

    Add "req-route" to access logger
    The req-route field grabs req.route.path, which will let us group response
    times by the actual route instead of by a specific URL.

--- services/backend/oodikone2-backend/src/middleware/accesslogger.js
+++ services/backend/oodikone2-backend/src/middleware/accesslogger.js
@@ -10,6 +10,7 @@ const accessLogger = morgan((tokens, req, res) => {
   const fields = ['method', 'url', 'status', 'response-time', 'remote-addr', 'remote-user', 'user-agent', 'referrer']
   const meta = req.decodedToken
   fields.forEach(field => (meta[field] = tokens[field](req, res)))
+  meta['req-route'] = req.route.path
 
   const message = [
     req.decodedToken.name,
