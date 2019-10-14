b63b420b0528324d91fdd0a00b131c7ba96418de
Joona Heikkilä <joona.o.heikkila@gmail.com>
2019-10-14 18:25:10 +0300

    Enable tsa metrics, attempt 2

---
+++ services/backend/oodikone2-backend/src/routes/tsaAnalytics.js
@@ -0,0 +1,27 @@
+const Raven = require('raven')
+const router = require('express').Router()
+const tsaService = require('../services/tsaService')
+
+router.post('/event', (req, res) => {
+  const { group, name, label, value } = req.body
+
+  if (!group || !name) {
+    return res.status(400).json({ error: 'group and name are required' })
+  }
+
+  // don't await here because frontend doesn't care if it succeeds
+  tsaService.sendTsaEvent(req.decodedToken.userId, { group, name, label, value }).catch(e => {
+    let ravenOpts = { req }
+    // if error was caused by a 400, dig out the JSON response for Sentry
+    if (e && e.isAxiosError && e.response && e.response.data) {
+      ravenOpts.extra = {
+        errorData: e.response.data
+      }
+    }
+    Raven.captureException(e, ravenOpts)
+  })
+
+  res.status(200).end()
+})
+
+module.exports = router
