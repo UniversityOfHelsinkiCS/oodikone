ef618f34fbde5193fd73f2c1759baecb81e99e0f
Warro <olli.warro@gmail.com>
2019-10-24 19:10:25 +0300

    Optimizing updater writer

--- services/backend/updater_writer/updater/database_updater.js
+++ services/backend/updater_writer/updater/database_updater.js
@@ -1,5 +1,5 @@
 const { sequelize } = require('../database/connection')
-const { sortBy, flatMap } = require('lodash')
+const { sortBy, flatMap, uniqBy, sortedUniqBy } = require('lodash')
 
 const {
   Student,
@@ -25,58 +25,86 @@ const {
 } = require('../models/index')
 const { updateAttainmentDates } = require('./update_attainment_dates')
 
-const awaitforEach = async (arr, func) => {
-  for (let i = 0; i < arr.length; ++i) {
-    await func(arr[i], i, arr)
-  }
-}
-
 const updateAttainments = async (studyAttainments, transaction) => {
-  // sort data to avoid deadlocks
-  const courses = sortBy(studyAttainments.map(e => e.course), 'code')
+  // Sort data to avoid deadlocks. If there are duplicate primary keys in the same array, then bulkCreate won't work.
+  const courses = sortedUniqBy(sortBy(studyAttainments.map(e => e.course), 'code'), 'code')
   const disciplines = sortBy(flatMap(studyAttainments, e => e.course.disciplines || []), 'discipline_id', 'course_id')
-  const providers = sortBy(flatMap(studyAttainments, e => e.course.providers), 'providercode')
+  const providers = sortedUniqBy(
+    sortBy(flatMap(studyAttainments, e => e.course.providers), 'providercode'),
+    'providercode'
+  )
   const courseproviders = sortBy(flatMap(studyAttainments, e => e.course.courseproviders), 'coursecode', 'providercode')
-  const credits = sortBy(studyAttainments.map(e => e.credit), 'id')
-  const teachers = sortBy(flatMap(studyAttainments, e => e.teachers || []), 'id')
+  const credits = sortedUniqBy(sortBy(studyAttainments.map(e => e.credit), 'id'), 'id')
+  const teachers = sortedUniqBy(sortBy(flatMap(studyAttainments, e => e.teachers || []), 'id'), 'id')
   const creditTeachers = sortBy(flatMap(studyAttainments, e => e.creditTeachers), 'teacher_id', 'credit_id')
 
-  for (const course of courses) {
-    await Course.upsert(course, { transaction })
-  }
-  for (const courseDiscipline of disciplines) {
-    await CourseDisciplines.upsert(courseDiscipline, { transaction })
-  }
-  for (const provider of providers) {
-    await Provider.upsert(provider, { transaction })
-  }
-  // must be after providers inserted
-  for (const courseProvider of courseproviders) {
-    await CourseProvider.upsert(courseProvider, { transaction })
-  }
-  for (const credit of credits) {
-    await Credit.upsert(credit, { transaction })
-  }
-  for (const teacher of teachers) {
-    await Teacher.upsert(teacher, { transaction })
-  }
-  // must be after teachers inserted
-  for (const creditTeacher of creditTeachers) {
-    await CreditTeacher.upsert(creditTeacher, { transaction })
-  }
+  await Promise.all([
+    Course.bulkCreate(courses, {
+      transaction,
+      updateOnDuplicate: [
+        'name',
+        'latest_instance_date',
+        'is_study_module',
+        'coursetypecode',
+        'startdate',
+        'enddate',
+        'max_attainment_date',
+        'min_attainment_date'
+      ]
+    }),
+    Provider.bulkCreate(providers, {
+      transaction,
+      updateOnDuplicate: ['name']
+    }),
+    Credit.bulkCreate(credits, {
+      transaction,
+      updateOnDuplicate: [
+        'grade',
+        'student_studentnumber',
+        'credits',
+        'ordering',
+        'createddate',
+        'lastmodifieddate',
+        'credittypecode',
+        'attainment_date',
+        'course_code',
+        'semestercode',
+        'isStudyModule'
+      ]
+    }),
+    Teacher.bulkCreate(teachers, {
+      transaction,
+      updateOnDuplicate: ['code', 'name']
+    })
+  ])
+
+  await Promise.all([
+    // must be after courses
+    CourseDisciplines.bulkCreate(disciplines, {
+      transaction,
+      ignoreDuplicates: true
+    }),
+    // must be after providers
+    CourseProvider.bulkCreate(courseproviders, {
+      transaction,
+      ignoreDuplicates: true
+    }),
+    // must be after teachers
+    CreditTeacher.bulkCreate(creditTeachers, {
+      transaction,
+      ignoreDuplicates: true
+    })
+  ])
 }
 
 const updateStudyRights = async (studyRights, transaction) => {
-  // sort data to avoid deadlocks
-  const studyRightExtents = sortBy(studyRights.map(e => e.studyRightExtent), 'extentcode')
-  const studyrights = sortBy(studyRights.map(e => e.studyright), 'studyrightid')
-  const elementDetails = sortBy(flatMap(studyRights, e => e.elementDetails), 'code')
-  const studyRightElements = sortBy(
-    flatMap(studyRights, e => e.studyRightElements),
-    'startdate',
-    'enddate',
-    'studyrightid',
-    'code'
+  // Sort data to avoid deadlocks. If there are duplicate primary keys in the same array, then bulkCreate won't work.
+  const studyRightExtents = sortedUniqBy(sortBy(studyRights.map(e => e.studyRightExtent), 'extentcode'), 'extentcode')
+  const studyrights = sortedUniqBy(sortBy(studyRights.map(e => e.studyright), 'studyrightid'), 'studyrightid')
+  const elementDetails = sortedUniqBy(sortBy(flatMap(studyRights, e => e.elementDetails), 'code'), 'code')
+  const studyRightElements = uniqBy(
+    sortBy(flatMap(studyRights, e => e.studyRightElements), 'startdate', 'enddate', 'studyrightid', 'code'),
+    s => `${s.startdate}${s.enddate}${s.studyrightid}${s.code}`
   )
   const transfers = sortBy(
     flatMap(studyRights, e => e.transfers),
@@ -87,21 +115,44 @@ const updateStudyRights = async (studyRights, transaction) => {
     'targetcode'
   )
 
-  for (const studyRightExtent of studyRightExtents) {
-    await StudyrightExtent.upsert(studyRightExtent, { transaction })
-  }
-  for (const studyright of studyrights) {
-    await Studyright.create(studyright, { transaction })
-  }
-  for (const elementdetail of elementDetails) {
-    await ElementDetails.upsert(elementdetail, { transaction })
-  }
-  for (const StudyRightElement of studyRightElements) {
-    await StudyrightElement.create(StudyRightElement, { transaction })
-  }
-  for (const transfer of transfers) {
-    await Transfers.upsert(transfer, { transaction })
-  }
+  await Promise.all([
+    StudyrightExtent.bulkCreate(studyRightExtents, {
+      transaction,
+      updateOnDuplicate: ['name']
+    }),
+
+    Studyright.bulkCreate(studyrights, {
+      transaction,
+      updateOnDuplicate: [
+        'canceldate',
+        'cancelorganisation',
+        'enddate',
+        'givendate',
+        'graduated',
+        'prioritycode',
+        'startdate',
+        'studystartdate',
+        'organization_code',
+        'student_studentnumber',
+        'extentcode'
+      ]
+    }),
+
+    ElementDetails.bulkCreate(elementDetails, {
+      transaction,
+      updateOnDuplicate: ['name', 'type']
+    }),
+
+    StudyrightElement.bulkCreate(studyRightElements, {
+      transaction,
+      updateOnDuplicate: ['startdate', 'enddate', 'studyrightid', 'code', 'studentnumber']
+    }),
+
+    Transfers.bulkCreate(transfers, {
+      transaction,
+      updateOnDuplicate: ['transferdate', 'studentnumber', 'studyrightid', 'sourcecode', 'targetcode']
+    })
+  ])
 }
 
 const deleteStudentStudyrights = async (studentnumber, transaction) => {
@@ -113,14 +164,18 @@ const updateStudent = async student => {
   let { studentInfo, studyAttainments, semesterEnrollments, studyRights } = student
 
   // sort data to avoid deadlocks
-  semesterEnrollments = sortBy(semesterEnrollments, 'studentnumber', 'semestercode')
+  semesterEnrollments = uniqBy(sortBy(semesterEnrollments, 'studentnumber', 'semestercode'), 'id')
 
   const transaction = await sequelize.transaction()
   try {
     await deleteStudentStudyrights(studentInfo.studentnumber, transaction) // this needs to be done because Oodi just deletes deprecated studyrights from students ( big yikes )
 
     await Student.upsert(studentInfo, { transaction })
-    await awaitforEach(semesterEnrollments, async SE => await SemesterEnrollment.upsert(SE, { transaction }))
+
+    await SemesterEnrollment.bulkCreate(semesterEnrollments, {
+      transaction,
+      updateOnDuplicate: ['enrollmenttype', 'studentnumber', 'semestercode', 'enrollment_date']
+    })
 
     if (studyAttainments) await updateAttainments(studyAttainments, transaction)
 
@@ -147,12 +202,33 @@ const updateMeta = async ({
   const transaction = await sequelize.transaction()
 
   try {
-    await awaitforEach(courseTypeCodes, async cT => await CourseType.upsert(cT, { transaction }))
-    await awaitforEach(faculties, async org => await Organisation.upsert(org, { transaction }))
-    await awaitforEach(courseRealisationsTypes, async cR => await CourseRealisationType.upsert(cR, { transaction }))
-    await awaitforEach(semesters, async s => await Semester.upsert(s, { transaction }))
-    await awaitforEach(creditTypeCodes, async cTC => await CreditType.upsert(cTC, { transaction }))
-    await awaitforEach(disciplines, async d => await Discipline.upsert(d, { transaction }))
+    await Promise.all([
+      CourseType.bulkCreate(uniqBy(courseTypeCodes, 'coursetypecode'), {
+        transaction,
+        updateOnDuplicate: ['name']
+      }),
+      Organisation.bulkCreate(uniqBy(faculties, 'code'), {
+        transaction,
+        updateOnDuplicate: ['name']
+      }),
+      CourseRealisationType.bulkCreate(uniqBy(courseRealisationsTypes, 'realisationtypecode'), {
+        transaction,
+        updateOnDuplicate: ['name']
+      }),
+      Semester.bulkCreate(uniqBy(semesters, 'semestercode'), {
+        transaction,
+        updateOnDuplicate: ['name', 'startdate', 'enddate', 'yearcode', 'yearname']
+      }),
+      CreditType.bulkCreate(uniqBy(creditTypeCodes, 'credittypecode'), {
+        transaction,
+        updateOnDuplicate: ['name']
+      }),
+      Discipline.bulkCreate(uniqBy(disciplines, 'discipline_id'), {
+        transaction,
+        updateOnDuplicate: ['name']
+      })
+    ])
+
     await transaction.commit()
   } catch (err) {
     await transaction.rollback()
