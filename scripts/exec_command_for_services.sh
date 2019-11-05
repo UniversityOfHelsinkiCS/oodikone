2ce908ff9653b76e0dfea6f3c811ad347abd2a00
Warro <olli.warro@gmail.com>
2019-11-05 16:10:02 +0200

    Added params to exec_command_for_service.sh

--- scripts/exec_command_for_services.sh
+++ scripts/exec_command_for_services.sh
@@ -1,27 +1,65 @@
 #!/bin/bash
+#
+# This script is used to execute npm commands inside multiple running docker services.
+#
+# Params:
+#   -c (required). The npm command to execute in the services.
+#     Example:
+#       ./exec_command_for_services.sh -c lint
+#
+#   -e (optional). Sets the script to stop on first error.
+#     Example:
+#       ./exec_command_for_services.sh -e -c test
+#
+#   -p (optional). Additional params for the npm command.
+#
+# You can also run f.ex. "./exec_command_for_services.sh -c lint backend frontend"
+# to run the specified command in some specific services (in this case, backend & frontend).
 
-# stop on first error
-set -e
+while getopts 'ec:p::' flag; do
+  case "${flag}" in
+    # Stop on first error
+    e) set -e ;;
+    c) COMMAND=${OPTARG};;
+    p) PARAMS=${OPTARG};;
+  esac
+done
 
-services=(
-  backend
-  updater_writer
-  updater_api
-  updater_scheduler
-  userservice
-  frontend
-  analytics
-  userservice
-  usageservice
-)
+# Check that command is given
+if [ -z $COMMAND ]; then
+  echo "-c (command) missing"
+  exit
+fi
 
+# Remove opts from argument list
+shift $(( OPTIND - 1 ))
+
+# Service names given as arguments
+WHITELISTED_SERVICES=${@}
+
+if [ ! -z "$WHITELISTED_SERVICES" ]; then
+  services=$WHITELISTED_SERVICES
+else
+  services=(
+    backend
+    updater_writer
+    updater_api
+    updater_scheduler
+    userservice
+    frontend
+    analytics
+    userservice
+    usageservice
+  )
+fi
 
 RED='\033[0;31m'
 Green='\033[0;32m'
 NC='\033[0m'
 
-echo -e "Executing command ${RED}$1${NC} $([ -n "$2" ] && echo "with param(s) ${RED}${@:2}${NC} ")to services ${Green}${services[@]}${NC}"
+echo -e "Executing command ${RED}$COMMAND${NC} $([ -n "$PARAMS" ] && echo "with param(s) ${RED}${PARAMS[@]}${NC} ")to services ${Green}${services[@]}${NC}"
 
+# Execute the given npm command in given services
 for service in "${services[@]}";
-  do docker-compose exec -T $service npm run "$1" --if-present -- "${@:2}"
+  do docker-compose exec -T $service npm run "$COMMAND" --if-present -- "${PARAMS[@]}"
 done
