c7bebfc251211bc89893cb16ca435788ede8f810
Esa Kemppainen <40692959+esakemp@users.noreply.github.com>
2019-11-05 12:57:37 +0200

    tagfilter

--- services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
+++ services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
@@ -53,7 +53,8 @@ export default {
       Vaihtaminen tarkoittaa sitä, kun opiskelija vaihtaa koulutusohjelmaansa kesken opinto-oikeutensa.
       (eikä opiskelijaa joka saa opinto-oikeuden joltakin toiselta instituutiolta)`,
     PriorityStudyright: ``,
-    TransferToStudyrightFilter: ``
+    TransferToStudyrightFilter: ``,
+    TagFilter: ``
   },
   CreditAccumulationGraph: `Opiskelijoiden kumulatiiviset opintopistekertymät opiskelijakohtaisina kuvaajina. Kun
     hiiren vie kuvaajien päälle, saa näkyviin jokaista kuvaajaa vastaavan opiskelijanumeron. Opiskelijanumeron
