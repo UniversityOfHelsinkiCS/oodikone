4f7cc3ce29ad48eed8bc9b473b804bf62df22256
Warro <olli.warro@gmail.com>
2019-11-07 11:35:30 +0200

    tests pls

--- cypress/integration/Population_statistics.js
+++ cypress/integration/Population_statistics.js
@@ -270,7 +270,7 @@ describe('Population Statistics tests', () => {
     cy.contains("See population").click()
     setPopStatsUntil("September 2019", ["with cancelled"])
 
-    cy.contains("button", "add").click()
+    cy.contains("Add filters").siblings().contains("button", "add").click({ force: true })
     cy.contains("Add filters").siblings().within(() => {
       cy.get(".form").should('have.length', 9)
     })
