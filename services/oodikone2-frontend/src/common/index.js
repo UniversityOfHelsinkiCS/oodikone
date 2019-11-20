a6e82bcad9e2953e615584b0b05ac720a2bb16ef
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2019-11-21 00:58:10 +0200

    Remove document title tracker
    piwik-react-router does it on location change

--- services/oodikone2-frontend/src/common/index.js
+++ services/oodikone2-frontend/src/common/index.js
@@ -5,7 +5,6 @@ import Datetime from 'react-datetime'
 import { uniqBy, filter, maxBy } from 'lodash'
 import pathToRegexp from 'path-to-regexp'
 import qs from 'query-string'
-import TSA from './tsa'
 import { API_DATE_FORMAT, DISPLAY_DATE_FORMAT, SEARCH_HISTORY_VERSION } from '../constants'
 import toskaLogo from '../assets/toska.png'
 import irtomikko from '../assets/irtomikko.png'
@@ -360,7 +359,6 @@ export const useProgress = loading => {
 export const useTitle = title => {
   useEffect(() => {
     document.title = title ? `${title} - Oodikone` : 'Oodikone'
-    TSA.Matomo.setDocumentTitle(document.title)
   }, [title])
 }
 
