e2e5b5e23e26200eec0621b414ecb3b88393e6bd
esakemp <esa.kemppainen@helsinki.fi>
2019-12-09 12:30:01 +0200

    infotooltips bettering

--- services/oodikone2-frontend/src/common/InfoToolTips/populationOverview.js
+++ services/oodikone2-frontend/src/common/InfoToolTips/populationOverview.js
@@ -1,4 +1,12 @@
 export default {
   Overview: `Tämä koontisivu sisältää koulutusohjelman vuosikurssien perustiedot. 
-  Mustista nuolista voi siirtyä tarkastelemaan tietyn vuosikurssin tarkempia tietoja.`
+  Mustista nuolista voi siirtyä tarkastelemaan tietyn vuosikurssin tarkempia tietoja.`,
+  Year: `placeholder year`,
+  Students: `placeholder students`,
+  Started: `placeholder started`,
+  Cancelled: `placeholder cancelled`,
+  Graduated: `placeholder graduated`,
+  Transferred: `placeholder transferred`,
+  Credits: `placeholder credits`,
+  Thesis: `placeholder thesis`
 }
