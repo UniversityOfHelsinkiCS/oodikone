8ec567d486f52b21102c9951211751dff5b046eb
Joona Heikkilä <21111572+cxcorp@users.noreply.github.com>
2020-01-20 18:00:41 +0200

    Analytics track student name visibility toggle

--- services/oodikone2-frontend/src/components/StudentNameVisibilityToggle/index.jsx
+++ services/oodikone2-frontend/src/components/StudentNameVisibilityToggle/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { connect } from 'react-redux'
 import { string, func, bool } from 'prop-types'
 import { Radio } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
 
+import TSA from '../../common/tsa'
 import { toggleStudentNameVisibility } from '../../redux/settings'
 
-const StudentNameVisibilityToggle = ({ showNames, toggleLabel, toggleStudentNameVisibility: toggle }) => (
-  <div style={{ marginTop: 15, marginBottom: 10 }}>
-    <Radio toggle label={toggleLabel} checked={showNames} onChange={toggle} />
-  </div>
-)
+const StudentNameVisibilityToggle = ({ showNames, toggleLabel, toggleStudentNameVisibility: toggle }) => {
+  const handleChange = useCallback(() => {
+    TSA.Matomo.sendEvent('Common', 'Toggle student name visibility', showNames ? 'hide' : 'show')
+    toggle()
+  }, [showNames, toggle])
+
+  return (
+    <div style={{ marginTop: 15, marginBottom: 10 }}>
+      <Radio toggle label={toggleLabel} checked={showNames} onChange={handleChange} />
+    </div>
+  )
+}
 
 StudentNameVisibilityToggle.propTypes = {
   showNames: bool.isRequired,
