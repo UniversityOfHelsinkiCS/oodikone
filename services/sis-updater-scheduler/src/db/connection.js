4b4f694e85cb79626182479bc26740cee82edf82
Warro <olli.warro@gmail.com>
2020-01-24 15:24:46 +0200

    sis-updater to sis-updater-scheduler, added sis-updater-nats conf

---
+++ services/sis-updater-scheduler/src/db/connection.js
@@ -0,0 +1,43 @@
+const knex = require('knex')
+const EventEmitter = require('events')
+const { SIS_IMPORTER_HOST, SIS_IMPORTER_USER, SIS_IMPORTER_PASSWORD, SIS_IMPORTER_DATABASE } = process.env
+
+class KnexConnection extends EventEmitter {
+  constructor() {
+    super()
+    this.RETRY_ATTEMPTS = 15
+  }
+
+  async connect(attempt = 1) {
+    try {
+      this.knex = knex({
+        client: 'pg',
+        version: '9.6.3',
+        connection: {
+          host: SIS_IMPORTER_HOST,
+          user: SIS_IMPORTER_USER,
+          password: SIS_IMPORTER_PASSWORD,
+          database: SIS_IMPORTER_DATABASE
+        },
+        pool: {
+          min: 0,
+          max: 5
+        }
+      })
+      await this.knex.raw('select 1+1 as result')
+      this.emit('connect')
+    } catch (e) {
+      if (attempt > this.RETRY_ATTEMPTS) {
+        this.emit('error', e)
+        return
+      }
+      console.log(`Knex database connection failed! Attempt ${attempt}/${this.RETRY_ATTEMPTS}`)
+      setTimeout(() => this.connect(attempt + 1), 1000 * attempt)
+    }
+  }
+}
+
+const knexConnection = new KnexConnection()
+module.exports = {
+  knexConnection
+}
