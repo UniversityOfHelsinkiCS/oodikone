488a3bf0d536958cb285ac39cfbdcc9a2d77df16
Warro <olli.warro@gmail.com>
2020-01-24 10:36:51 +0200

    db_sis

--- services/sis-updater/src/db/connection.js
+++ services/sis-updater/src/db/connection.js
@@ -1,20 +1,28 @@
 const knex = require('knex')
 const EventEmitter = require('events')
-const { SIS_IMPORTER_HOST, SIS_IMPORTER_USER, SIS_IMPORTER_PASSWORD, SIS_IMPORTER_DATABASE } = process.env
+const Sequelize = require('sequelize')
+const { SIS_IMPORTER_HOST, SIS_IMPORTER_USER, SIS_IMPORTER_PASSWORD, SIS_IMPORTER_DATABASE, SIS_DB_URL } = process.env
+
+const sequelize = new Sequelize(SIS_DB_URL, {
+  pool: {
+    min: 0,
+    max: 5
+  }
+})
 
 class DbConnections extends EventEmitter {
   constructor() {
     super()
     this.KNEX_CONNECTION = 'knexConnected'
     this.SEQUELIZE_CONNECTION = 'sequelizeConnected'
+    this.RETRY_ATTEMPTS = 15
     this[this.KNEX_CONNECTION] = false
     this[this.SEQUELIZE_CONNECTION] = false
   }
 
   establish(connection) {
     this[connection] = true
-    // TODO: Check sequelize connection as well
-    if (this[this.KNEX_CONNECTION]) this.emit('connected')
+    if (this[this.KNEX_CONNECTION] && this[this.SEQUELIZE_CONNECTION]) this.emit('connected')
   }
 
   async connect(attempt = 1) {
@@ -37,14 +45,18 @@ class DbConnections extends EventEmitter {
         await this.knex.raw('select 1+1 as result')
         this.establish(this.KNEX_CONNECTION)
       }
-      // TODO: Establish sequelize connection
+
+      if (!this[this.SEQUELIZE_CONNECTION]) {
+        await sequelize.authenticate()
+        this.sequelize = sequelize
+        this.establish(this.SEQUELIZE_CONNECTION)
+      }
     } catch (e) {
-      if (attempt === 15) {
-        this.emit('error')
-        console.log(`Connection to the databases failed after ${attempt} attempts`, e)
+      if (attempt > this.RETRY_ATTEMPTS) {
+        this.emit('error', e)
         return
       }
-      console.log(`Connection to the databases failed! Attempt ${attempt} of 15`)
+      console.log(`Some database connection failed! Attempt ${attempt}/${this.RETRY_ATTEMPTS}`)
       setTimeout(() => this.connect(attempt + 1), 1000 * attempt)
     }
   }
