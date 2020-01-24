bf4933ae535dbb05f6fef6519477dc427f030812
Joona Heikkilä <joona.o.heikkila@gmail.com>
2020-01-24 11:38:09 +0200

    add else

--- services/backend/oodikone2-backend/src/services/studyrights.js
+++ services/backend/oodikone2-backend/src/services/studyrights.js
@@ -325,16 +325,14 @@ const calculateAssociationsFromDb = async (chunksize = 100000) => {
             code,
             programmes: {}
           }
-        }
-        if (type === StudyRightType.STUDYTRACK) {
+        } else if (type === StudyRightType.STUDYTRACK) {
           associations.studyTracks[code] = associations.studyTracks[code] || {
             type,
             name,
             code,
             programmes: {}
           }
-        }
-        if (type === StudyRightType.PROGRAMME) {
+        } else if (type === StudyRightType.PROGRAMME) {
           associations.programmes[code] = associations.programmes[code] || {
             type: type,
             name: name,
@@ -370,8 +368,7 @@ const calculateAssociationsFromDb = async (chunksize = 100000) => {
                   name: name,
                   code: code
                 }
-              }
-              if (e.type === StudyRightType.STUDYTRACK) {
+              } else if (e.type === StudyRightType.STUDYTRACK) {
                 const momentenddate = moment(enddate)
                 const estartdate = moment(e.startdate)
                 const eenddate = moment(e.enddate)
