6e74d28a8f3e52427b3f32124798f3f5025b83bf
Warro <olli.warro@gmail.com>
2020-01-27 17:03:08 +0200

    improved sis updater scheduling

--- services/sis-updater-scheduler/src/utils/stan.js
+++ services/sis-updater-scheduler/src/utils/stan.js
@@ -6,12 +6,6 @@ const stan = natsStreaming.connect('sis-updater-nats', HOSTNAME, {
   token: SIS_NATS_TOKEN
 })
 
-const opts = stan.subscriptionOptions()
-opts.setManualAckMode(true)
-opts.setAckWait(60 * 1000 * 15)
-opts.setMaxInFlight(5)
-
 module.exports = {
-  stan,
-  opts
+  stan
 }
--- services/sis-updater-worker/src/config.js
+++ services/sis-updater-worker/src/config.js
@@ -1,1 +1,2 @@
+module.exports.NATS_GROUP = 'sis-updater-nats.workers'
 module.exports.SIS_UPDATER_SCHEDULE_CHANNEL = 'SIS_UPDATER_SCHEDULE_CHANNEL'
