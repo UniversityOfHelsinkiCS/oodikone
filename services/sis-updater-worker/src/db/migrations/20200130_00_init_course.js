465bf914101a10e6d0a448fd8ad6e95292fa196a
Warro <olli.warro@gmail.com>
2020-01-30 16:35:17 +0200

    courses to sis_db

---
+++ services/sis-updater-worker/src/db/migrations/20200130_00_init_course.js
@@ -0,0 +1,46 @@
+const { STRING, DATE, JSONB, BOOLEAN } = require('sequelize')
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.createTable('course', {
+      id: {
+        type: STRING,
+        primaryKey: true
+      },
+      name: {
+        type: JSONB
+      },
+      code: {
+        type: STRING
+      },
+      latest_instance_date: {
+        type: DATE
+      },
+      is_study_module: {
+        type: BOOLEAN
+      },
+      coursetypecode: {
+        type: STRING
+      },
+      startdate: {
+        type: DATE
+      },
+      enddate: {
+        type: DATE
+      },
+      max_attainment_date: {
+        type: DATE
+      },
+      min_attainment_date: {
+        type: DATE
+      },
+      created_at: {
+        type: DATE
+      },
+      updated_at: {
+        type: DATE
+      }
+    })
+  },
+  down: async () => {}
+}
