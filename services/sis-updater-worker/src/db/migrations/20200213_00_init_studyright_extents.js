e002e9b28a23ba64f7cbc963cecf9c6d10e89d38
Warro <olli.warro@gmail.com>
2020-02-13 09:47:11 +0200

    studyright extents to db

---
+++ services/sis-updater-worker/src/db/migrations/20200213_00_init_studyright_extents.js
@@ -0,0 +1,22 @@
+const { DATE, JSONB, INTEGER } = require('sequelize')
+
+module.exports = {
+  up: async queryInterface => {
+    await queryInterface.createTable('studyright_extents', {
+      extentcode: {
+        type: INTEGER,
+        primaryKey: true
+      },
+      name: {
+        type: JSONB
+      },
+      createdAt: {
+        type: DATE
+      },
+      updatedAt: {
+        type: DATE
+      }
+    })
+  },
+  down: async () => {}
+}
