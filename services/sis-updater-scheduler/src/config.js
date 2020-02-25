816949fd18b6c1f06d731f50f1b6da111739e354
Warro <olli.warro@gmail.com>
2020-02-25 13:57:10 +0200

    improved sis-updater logging

--- services/sis-updater-scheduler/src/config.js
+++ services/sis-updater-scheduler/src/config.js
@@ -2,3 +2,7 @@ module.exports.SIS_UPDATER_SCHEDULE_CHANNEL = 'SIS_UPDATER_SCHEDULE_CHANNEL'
 module.exports.CHUNK_SIZE = 100
 module.exports.isDev = process.env.NODE_ENV === 'development'
 module.exports.SECRET_TOKEN = process.env.SECRET_TOKEN
+module.exports.REDIS_TOTAL_META_KEY = 'TOTAL_META'
+module.exports.REDIS_TOTAL_STUDENTS_KEY = 'TOTAL_STUDENTS'
+module.exports.REDIS_TOTAL_META_DONE_KEY = 'TOTAL_META_DONE'
+module.exports.REDIS_TOTAL_STUDENTS_DONE_KEY = 'TOTAL_STUDENTS_DONE'
--- services/sis-updater-worker/src/index.js
+++ services/sis-updater-worker/src/index.js
@@ -1,11 +1,26 @@
 const { stan, opts } = require('./utils/stan')
 const { dbConnections } = require('./db/connection')
 const { update } = require('./updater')
-const { SIS_UPDATER_SCHEDULE_CHANNEL, NATS_GROUP } = require('./config')
+const { get: redisGet, incrby: redisIncrementBy } = require('./utils/redis')
+const {
+  SIS_UPDATER_SCHEDULE_CHANNEL,
+  NATS_GROUP,
+  REDIS_TOTAL_META_KEY,
+  REDIS_TOTAL_STUDENTS_KEY,
+  REDIS_TOTAL_META_DONE_KEY,
+  REDIS_TOTAL_STUDENTS_DONE_KEY
+} = require('./config')
 
 const msgParser = f => async msg => {
   try {
-    await f(JSON.parse(msg.getData()))
+    const msgData = JSON.parse(msg.getData())
+    await f(msgData)
+    const totalScheduled = await redisGet(msgData.type === 'students' ? REDIS_TOTAL_STUDENTS_KEY : REDIS_TOTAL_META_KEY)
+    const totalDone = await redisIncrementBy(
+      msgData.type === 'students' ? REDIS_TOTAL_STUDENTS_DONE_KEY : REDIS_TOTAL_META_DONE_KEY,
+      msgData.entityIds.length
+    )
+    console.log(`UPDATED ${msgData.type === 'students' ? 'STUDENTS' : 'META'}: ${totalDone}/${totalScheduled}`)
   } catch (e) {
     console.log('Updating failed', e)
   } finally {
