53a61921e016cf48cafd69a600bcac615979a780
Warro <olli.warro@gmail.com>
2020-03-03 16:32:03 +0200

    refactoring and optimizing sis-updater

--- services/sis-updater-worker/src/utils/redis.js
+++ services/sis-updater-worker/src/utils/redis.js
@@ -2,6 +2,14 @@ const redis = require('redis')
 const redisLock = require('redis-lock')
 const { promisify } = require('util')
 
+const redisRetry = ({ attempt, error }) => {
+  if (attempt > 100) {
+    throw new Error('Lost connection to Redis...', error)
+  }
+
+  return Math.min(attempt * 100, 5000)
+}
+
 const redisPromisify = async (func, ...params) =>
   new Promise((res, rej) => {
     func.call(client, ...params, (err, data) => {
@@ -11,7 +19,8 @@ const redisPromisify = async (func, ...params) =>
   })
 
 const client = redis.createClient({
-  url: process.env.REDIS_URI
+  url: process.env.REDIS_URI,
+  retry_strategy: redisRetry
 })
 
 const lock = promisify(redisLock(client))
--- services/sis-updater-worker/src/utils/stan.js
+++ services/sis-updater-worker/src/utils/stan.js
@@ -4,7 +4,9 @@ const { HOSTNAME, SIS_NATS_URI, SIS_NATS_TOKEN } = process.env
 
 const stan = natsStreaming.connect('sis-updater-nats', HOSTNAME, {
   url: SIS_NATS_URI,
-  token: SIS_NATS_TOKEN
+  token: SIS_NATS_TOKEN,
+  maxReconnectAttempts: -1,
+  waitOnFirstConnect: true
 })
 
 const opts = stan.subscriptionOptions()
