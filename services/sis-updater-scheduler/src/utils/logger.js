e01f40ca3a353eaaefee6e5ab296c919199e7f4d
Warro <olli.warro@gmail.com>
2020-03-16 16:05:23 +0200

    sis-updater logging

---
+++ services/sis-updater-scheduler/src/utils/logger.js
@@ -0,0 +1,40 @@
+const winston = require('winston')
+const Log2gelf = require('winston-log2gelf')
+const { combine, timestamp, printf, splat } = winston.format
+const { LOG_PORT, LOG_HOST, LOG_HOSTNAME, NODE_ENV } = process.env
+
+const customFormat = printf(({ level, message, timestamp, ...rest }) => {
+  return `${timestamp} ${level}: ${message} ${JSON.stringify(rest)}`
+})
+
+const transports = []
+
+if (LOG_PORT && LOG_HOST) {
+  transports.push(
+    new Log2gelf({
+      hostname: LOG_HOSTNAME || 'sis-updater-scheduler',
+      host: LOG_HOST,
+      port: LOG_PORT,
+      protocol: 'http',
+      environment: NODE_ENV,
+      service: 'SIS-UPDATER-SCHEDULER'
+    })
+  )
+}
+
+if (process.env.NODE_ENV !== 'test') {
+  transports.push(new winston.transports.File({ filename: 'debug.log' }))
+}
+
+transports.push(
+  new winston.transports.Console({
+    level: 'debug',
+    format: combine(splat(), timestamp(), customFormat)
+  })
+)
+
+const logger = winston.createLogger({ transports })
+
+module.exports = {
+  logger
+}
