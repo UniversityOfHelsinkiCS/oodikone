8794b4b66e0d5e084c6e6c0772103924c7aa7ad3
esakemp <esa.kemppainen@helsinki.fi>
2020-04-17 14:24:04 +0300

    ui betterments

--- services/oodikone2-frontend/src/components/CoolDataScience/ProtoC.jsx
+++ services/oodikone2-frontend/src/components/CoolDataScience/ProtoC.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import Highcharts from 'highcharts'
 import ReactHighcharts from 'react-highcharts'
-import { Segment, Loader, Dimmer, Checkbox, Button, Message, Divider, Radio } from 'semantic-ui-react'
+import { Segment, Loader, Dimmer, Checkbox, Button, Message, Icon } from 'semantic-ui-react'
 import _ from 'lodash'
 
 import { callApi } from '../../apiConnection'
@@ -25,9 +25,7 @@ const defaultConfig = () => {
       }
     },
     legend: {
-      layout: 'horizontal',
-      borderWidth: 1,
-      backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF'
+      enabled: false,
     },
     tooltip: {
       shared: true,
@@ -77,6 +75,7 @@ const makeConfig = (sortedOrgs, onOrgClicked) => {
             // use setImmediate so the click handler can finish
             // before datamangels begins so that the browser is responsive
             setImmediate(() => onOrgClicked(clickedOrg))
+            e.point.update()
           }
         }
       }
@@ -135,6 +134,22 @@ const makeConfig = (sortedOrgs, onOrgClicked) => {
       style: {
         display: 'none'
       }
+    },
+    responsive: {
+      rules: [
+        {
+          condition: {
+            callback: function() {
+              console.log(series)
+            }
+          },
+          chartOptions: {
+            legend: {
+              enabled: false
+            }
+          }
+        }
+      ]
     },
     xAxis: {
       categories: sortedOrgs.map(org => org.name)
@@ -311,6 +326,15 @@ const ProtoC = () => {
         {!isLoading && data && drilldownOrg && (
           <ProgrammeDrilldown org={drilldownOrg} sorter={sorter} sortDir={sortDir} />
         )}
+        <div align="center" style={{ margin: '10px' }}>
+          <span style={{ border: '1px solid black', padding: '4px' }}>
+            <Icon style={{ marginLeft: '10px', color: '#6ab04c' }} name="circle" size="small" /> 3v tahdissa
+            <Icon style={{ marginLeft: '10px', color: '#f9ca24' }} name="circle" size="small" /> 4v tahdissa
+            <Icon style={{ marginLeft: '10px', color: '#ff7979' }} name="circle" size="small" /> ei tahdissa
+            <Icon style={{ marginLeft: '10px', color: '#7f8c8d' }} name="circle" size="small" /> tällä hetkellä
+            peruutettu
+          </span>
+        </div>
         <div align="center">
           <Checkbox
             label="Include only at least once enrolled students"
