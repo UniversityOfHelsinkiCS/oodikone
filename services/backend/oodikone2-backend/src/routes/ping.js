c6925d09b703b439eb7349c12ce0f93eef837e03
Matti Luukkainen <mluukkai@iki.fi>
2020-04-27 22:30:08 +0300

    betterment

--- services/backend/oodikone2-backend/src/routes/ping.js
+++ services/backend/oodikone2-backend/src/routes/ping.js
@@ -12,18 +12,19 @@ wrapper.get('/ping', async (req, res) => {
 wrapper.get('/v3/mandatory_courses/:code', async (req, res) => {
   const courses = []
 
-  const flatten = module => {
+  const dumb_flatten = module => {
     if (module.children) {
       module.children.forEach(child => {
-        flatten(child)
+        dumb_flatten(child)
       })
-    } else if (module.code && !module.code.startsWith('KK')) {
+    } else if (module.code && !module.code.startsWith('KK') && !courses.map(c => c.code).includes(module.code)) {
       courses.push({
         name: {
           fi: module.name,
           en: module.name
         },
-        code: module.code
+        code: module.code,
+        label: { id: `${courses.length}`, label: module.code.slice(0,4) , orderNumber: courses.length }
       })
     }
   }
@@ -47,8 +48,13 @@ wrapper.get('/v3/mandatory_courses/:code', async (req, res) => {
 
   const code = req.params.code
   const response = await axios.get(`http://sis-updater-scheduler:8082/v1/courses/${code}?token=dev`)
-  //flatten(response.data)
-  better_flatten(response.data.children[0])
+
+  dumb_flatten(response.data)
+  //better_flatten(response.data.children[0])
+
+  const byCode = (c1, c2) => c1.code < c2.code ? -1 : 1
+
+  courses.sort(byCode)
   res.json(courses)
 })
 
