cd8882bd31640b62c3d0ca686372e204fa8db8b8
Warro <olli.warro@gmail.com>
2020-04-30 10:59:37 +0300

    remember user CDS status settings

--- services/oodikone2-frontend/src/common/hooks.js
+++ services/oodikone2-frontend/src/common/hooks.js
@@ -248,3 +248,27 @@ export const useSisFeatureToggle = () => {
 
   return isSisModeActive
 }
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
+
+  const setValue = value => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value
+      setStoredValue(valueToStore)
+      window.localStorage.setItem(key, JSON.stringify(valueToStore))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return [storedValue, setValue]
+}
