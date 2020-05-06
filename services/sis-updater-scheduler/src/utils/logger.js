48933b33ee6075b7ea83cb948664443a7846ce6e
Warro <olli.warro@gmail.com>
2020-05-06 10:48:45 +0300

    fulfill Joona's legacy

--- services/sis-updater-scheduler/src/utils/logger.js
+++ services/sis-updater-scheduler/src/utils/logger.js
@@ -15,7 +15,7 @@ if (LOG_PORT && LOG_HOST) {
       hostname: LOG_HOSTNAME || 'sis-updater-scheduler',
       host: LOG_HOST,
       port: LOG_PORT,
-      protocol: LOG_PROTOCOL || 'http',
+      protocol: LOG_PROTOCOL || 'https',
       protocolOptions: {
         path: LOG_PATH || '/gelf'
       },
