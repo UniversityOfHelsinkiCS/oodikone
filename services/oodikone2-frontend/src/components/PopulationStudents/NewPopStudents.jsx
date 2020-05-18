f497de87deb6fa4b51c2de40d37311cfdd5dcb11
atte <atte.h.1998@gmail.com>
2020-05-18 10:17:16 +0300

    Rename 'Mandatory courses' -> 'Courses'

--- services/oodikone2-frontend/src/components/PopulationStudents/NewPopStudents.jsx
+++ services/oodikone2-frontend/src/components/PopulationStudents/NewPopStudents.jsx
@@ -564,7 +564,7 @@ class PopulationStudents extends Component {
         )
       },
       {
-        menuItem: 'Mandatory courses',
+        menuItem: 'Courses',
         render: () => (
           <Tab.Pane>
             <div style={{ display: 'flex' }}>
