5b11cda4a085255ad54e8c370450c19a0982ca2a
esakemp <esa.kemppainen@helsinki.fi>
2020-06-02 10:26:54 +0300

    hover labels

--- services/oodikone2-frontend/src/components/CourseStatistics/ProgrammeDropdown/DropdownItem.jsx
+++ services/oodikone2-frontend/src/components/CourseStatistics/ProgrammeDropdown/DropdownItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Label } from 'semantic-ui-react'
+import { Label, Popup } from 'semantic-ui-react'
 import { string, oneOfType, number } from 'prop-types'
 
 const styles = {
@@ -16,22 +16,43 @@ const styles = {
   }
 }
 
-const DropdownItem = ({ name, code, size }) => (
-  <div style={styles.container}>
-    <div style={styles.text}>
-      <Label content={size} icon="user" size="tiny" />
-      <span style={{ paddingLeft: '1rem' }}>{name}</span>
+const DropdownItem = ({ name, code, size, description }) =>
+  ['ALL', 'OTHER', 'EXCLUDED'].includes(code) ? (
+    <Popup
+      size="small"
+      content={description}
+      trigger={
+        <div style={styles.container}>
+          <div style={styles.text}>
+            <Label content={size} icon="user" size="tiny" />
+            <span style={{ paddingLeft: '1rem' }}>{name}</span>
+          </div>
+          <div style={styles.students}>
+            <Label content={code} size="tiny" />
+          </div>
+        </div>
+      }
+    />
+  ) : (
+    <div style={styles.container}>
+      <div style={styles.text}>
+        <Label content={size} icon="user" size="tiny" />
+        <span style={{ paddingLeft: '1rem' }}>{name}</span>
+      </div>
+      <div style={styles.students}>
+        <Label content={code} size="tiny" />
+      </div>
     </div>
-    <div style={styles.students}>
-      <Label content={code} size="tiny" />
-    </div>
-  </div>
-)
+  )
 
+DropdownItem.defaultProps = {
+  description: ''
+}
 DropdownItem.propTypes = {
   name: string.isRequired,
   code: string.isRequired,
-  size: oneOfType([number, string]).isRequired
+  size: oneOfType([number, string]).isRequired,
+  description: string
 }
 
 export default DropdownItem
