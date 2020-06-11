91c470cc2d7d302792dc81f0b04a3b6d0039756f
atte <atte.h.1998@gmail.com>
2020-06-11 12:50:16 +0300

    Refactor courses of population

---
+++ services/oodikone2-frontend/src/components/PopulationCourseStats/PassFail/index.jsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import { Table, Icon, Popup, Item } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { getTextIn } from '../../../common'
+
+import FilterToggleIcon from '../../FilterToggleIcon'
+import SortableHeaderCell from '../SortableHeaderCell'
+import { UsePopulationCourseContext } from '../PopulationCourseContext'
+
+const PassFail = () => {
+  const { language } = useSelector(({ settings }) => settings)
+  const {
+    courseStatistics,
+    onSortableColumnHeaderClick,
+    filterInput,
+    tableColumnNames,
+    isActiveCourse,
+    onGoToCourseStatisticsClick,
+    onCourseNameCellClick,
+    sortCriteria,
+    reversed,
+    translate
+  } = UsePopulationCourseContext()
+  const getSortableHeaderCell = (label, columnName, rowSpan = 1) => (
+    <SortableHeaderCell
+      content={label}
+      columnName={columnName}
+      onClickFn={onSortableColumnHeaderClick}
+      activeSortColumn={sortCriteria}
+      reversed={reversed}
+      rowSpan={rowSpan}
+    />
+  )
+
+  const getTableHeader = () => (
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell colSpan="4" content={translate('populationCourses.course')} />
+        {getSortableHeaderCell(translate('populationCourses.students'), tableColumnNames.STUDENTS, 2)}
+        <Table.HeaderCell colSpan="3" content={translate('populationCourses.passed')} />
+        <Table.HeaderCell colSpan="2" content={translate('populationCourses.failed')} />
+        <Table.HeaderCell colSpan="2" content={translate('populationCourses.attempts')} />
+        <Table.HeaderCell colSpan="2" content={translate('populationCourses.percentageOfPopulation')} />
+      </Table.Row>
+      <Table.Row>
+        {filterInput('nameFilter', 'populationCourses.name', '3')}
+        {filterInput('codeFilter', 'populationCourses.code')}
+        {getSortableHeaderCell(translate('populationCourses.number'), tableColumnNames.PASSED)}
+        {getSortableHeaderCell(translate('populationCourses.passedAfterRetry'), tableColumnNames.RETRY_PASSED)}
+        {getSortableHeaderCell(translate('populationCourses.percentage'), tableColumnNames.PERCENTAGE)}
+        {getSortableHeaderCell(translate('populationCourses.number'), tableColumnNames.FAILED)}
+        {getSortableHeaderCell(translate('populationCourses.failedManyTimes'), tableColumnNames.FAILED_MANY)}
+        {getSortableHeaderCell(translate('populationCourses.number'), tableColumnNames.ATTEMPTS)}
+        {getSortableHeaderCell(translate('populationCourses.perStudent'), tableColumnNames.PER_STUDENT)}
+        {getSortableHeaderCell(translate('populationCourses.passed'), tableColumnNames.PASSED_OF_POPULATION)}
+        {getSortableHeaderCell(translate('populationCourses.attempted'), tableColumnNames.TRIED_OF_POPULATION)}
+      </Table.Row>
+    </Table.Header>
+  )
+
+  const getCourseRow = courseStats => {
+    const { course, stats } = courseStats
+    const { code, name } = course
+    const {
+      failed,
+      passed,
+      retryPassed,
+      failedMany,
+      attempts,
+      percentage,
+      perStudent,
+      passedOfPopulation,
+      triedOfPopulation
+    } = stats
+    const isActive = isActiveCourse(course)
+    return (
+      <Table.Row key={code} active={isActive}>
+        <Popup
+          trigger={
+            <Table.Cell className="filterCell clickableCell">
+              <FilterToggleIcon isActive={isActive} onClick={() => onCourseNameCellClick(code)} />
+            </Table.Cell>
+          }
+          content={
+            isActive ? (
+              <span>
+                Poista rajaus kurssin <b>{getTextIn(name, language)}</b> perusteella
+              </span>
+            ) : (
+              <span>
+                Rajaa opiskelijat kurssin <b>{getTextIn(name, language)}</b> perusteella
+              </span>
+            )
+          }
+          position="top right"
+        />
+        <Table.Cell content={getTextIn(name, language)} className="nameCell" />
+        <Table.Cell className="iconCell clickableCell">
+          <p>
+            <Item
+              as={Link}
+              to={`/coursestatistics?courseCodes=["${encodeURIComponent(
+                code
+              )}"]&separate=false&unifyOpenUniCourses=false`}
+            >
+              <Icon name="level up alternate" onClick={() => onGoToCourseStatisticsClick(code)} />
+            </Item>
+          </p>
+        </Table.Cell>
+        <Table.Cell content={code} />
+        <Table.Cell content={passed + failed} />
+        <Table.Cell content={passed} />
+        <Table.Cell content={retryPassed} />
+        <Table.Cell content={`${percentage.toFixed(2)} %`} />
+        <Table.Cell content={failed} />
+        <Table.Cell content={failedMany} />
+        <Table.Cell content={attempts} />
+        <Table.Cell content={perStudent.toFixed(2)} />
+        <Table.Cell content={`${passedOfPopulation.toFixed(2)}  %`} />
+        <Table.Cell content={`${triedOfPopulation.toFixed(2)}  %`} />
+      </Table.Row>
+    )
+  }
+
+  return (
+    <Table className="fixed-header" celled sortable>
+      {getTableHeader()}
+      <Table.Body>{courseStatistics.map(getCourseRow)}</Table.Body>
+    </Table>
+  )
+}
+
+export default PassFail
---
+++ services/oodikone2-frontend/src/components/PopulationCourseStats/Students/index.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react'
+import { Table } from 'semantic-ui-react'
+import { shape, arrayOf, string, func, bool, element, oneOfType } from 'prop-types'
+import { sortBy } from 'lodash'
+
+const verticalTitle = title => {
+  // https://stackoverflow.com/a/41396815
+  return <div className="tableVerticalTitle">{title}</div>
+}
+
+const DIRECTIONS = {
+  ASC: 'ascending',
+  DESC: 'descending'
+}
+
+const Students = ({ defaultdescending, defaultsortkey, columns, data, tableProps, getRowKey }) => {
+  const [direction, setDirection] = useState(defaultdescending ? DIRECTIONS.DESC : DIRECTIONS.ASC)
+  const [selected, setSelected] = useState(defaultsortkey == null ? columns[0].key : defaultsortkey)
+  const [sortedRows, setSortedRows] = useState(data)
+
+  const handleSort = column => {
+    if (selected === column) {
+      setDirection(direction === DIRECTIONS.ASC ? DIRECTIONS.DESC : DIRECTIONS.ASC)
+    } else {
+      setSelected(column)
+      setDirection(DIRECTIONS.DESC)
+    }
+  }
+
+  useEffect(() => {
+    const column = columns.find(c => c.key === selected)
+    if (!column) {
+      setSortedRows(data)
+      return
+    }
+    const { getRowVal } = column
+    const sorted = sortBy(data, [getRowVal])
+    setSortedRows(direction === DIRECTIONS.ASC ? sorted : sorted.reverse())
+  }, [selected])
+
+  return (
+    <Table sortable {...tableProps} className="fixed-header" striped>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell key="general" content={<b>Students:</b>} colSpan="2" style={{ textAlign: 'right' }} />
+          {sortedRows
+            .filter(row => row.studentNumber)
+            .map(row => (
+              <Table.HeaderCell
+                className="rotatedTableHeader"
+                key={getRowKey(row)}
+                content={verticalTitle(row.studentNumber)}
+              />
+            ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Course</Table.Cell>
+          <Table.Cell>Total</Table.Cell>
+          {sortedRows
+            .filter(row => row.studentNumber)
+            .map(row => (
+              <Table.Cell key={getRowKey(row)} content={columns[2].getRowVal(row)} />
+            ))}
+        </Table.Row>
+        {columns
+          .filter(col => col.cellProps)
+          .filter(col => data[0][col.code] && data[0][col.code] > 0)
+          .map(col => (
+            <Table.Row key={col.key}>
+              <Table.Cell key="name" content={col.title} onClick={() => handleSort(col.key)} />
+              <Table.Cell key="totals" content={data[0][col.code]} />
+              {sortedRows
+                .filter(row => row.studentNumber)
+                .map(row => (
+                  <Table.Cell key={getRowKey(row)} content={col.getRowContent(row)} />
+                ))}
+            </Table.Row>
+          ))}
+      </Table.Body>
+    </Table>
+  )
+}
+
+Students.propTypes = {
+  tableProps: shape({}),
+  getRowKey: func.isRequired,
+  getRowProps: func,
+  columns: arrayOf(
+    shape({
+      key: string.isRequired,
+      title: oneOfType([element, string]),
+      headerProps: shape({}),
+      getRowVal: func,
+      getRowContent: func,
+      getCellProps: func,
+      cellProps: shape({}),
+      group: bool,
+      children: arrayOf()
+    })
+  ).isRequired,
+  data: arrayOf(shape({})).isRequired,
+  defaultdescending: bool,
+  defaultsortkey: string,
+  collapsingHeaders: bool,
+  showNames: bool,
+  chunkifyBy: string
+}
+
+Students.defaultProps = {
+  tableProps: undefined,
+  getRowProps: undefined,
+  defaultdescending: false,
+  defaultsortkey: null,
+  collapsingHeaders: false,
+  showNames: undefined,
+  chunkifyBy: undefined
+}
+
+export default Students
