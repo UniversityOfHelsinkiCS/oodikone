85d49ea67e7db81147c7d04db61874d08a93e12c
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-06-24 21:25:39 +0300

    Implement selecting courses in course filter

--- services/oodikone2-frontend/src/components/FilterTray/filters/Courses.jsx
+++ services/oodikone2-frontend/src/components/FilterTray/filters/Courses.jsx
@@ -4,48 +4,51 @@ import { Form, Dropdown } from 'semantic-ui-react'
 import { createStore, useStore } from 'react-hookstore'
 import FilterCard from './common/FilterCard'
 
-const mockOptions = [
-  {
-    key: 'asd1',
-    text: 'Tietorakenteet ja algoritmit',
-    value: '1'
-  },
-  {
-    key: 'asd2',
-    text: 'Ohjelmoinnin peruskurssi',
-    value: '2'
-  },
-  {
-    key: 'asd3',
-    text: 'Ohjelmoinnin jatkokurssi',
-    value: '3'
-  }
-]
-
 export const dataStoreName = 'courseFilterDataStore'
 createStore(dataStoreName, [])
 
 const Courses = ({ filterControl }) => {
   const [courseStats] = useStore(dataStoreName)
-  console.log(courseStats)
+  const [selectedCourses, setSelectedCourses] = useState([])
+
+  const onChange = (_, { value }) =>
+    setSelectedCourses(prev => prev.concat(courseStats.find(course => course.course.code === value)))
 
   // Wrestle course stats into something semantic-ui eats without throwing up.
   const options = courseStats
     .filter(course => course.stats.students > Math.round(filterControl.filteredStudents.length * 0.3))
-    .map(stat => ({
-      key: `course-filter-option-${stat.course.code}`,
-      text: stat.course.name.fi,
-      value: stat.course.code
+    .filter(course => !selectedCourses.includes(course))
+    .map(course => ({
+      key: `course-filter-option-${course.course.code}`,
+      text: course.course.name.fi,
+      value: course.course.code
     }))
-  console.log(options)
 
   return (
     <FilterCard title="Courses">
       <Form>
-        <Dropdown options={options} placeholder="Select A Course" selection className="mini" fluid button />
+        {selectedCourses.map(course => (
+          <div key={`course-filter-selected-course-${course.course.code}`}>{course.course.name.fi}</div>
+        ))}
+        <Dropdown
+          options={options}
+          placeholder="Select A Course"
+          selection
+          className="mini"
+          fluid
+          button
+          value=""
+          onChange={onChange}
+        />
       </Form>
     </FilterCard>
   )
+}
+
+Courses.propTypes = {
+  filterControl: PropTypes.shape({
+    filteredStudents: PropTypes.arrayOf(PropTypes.object).isRequired
+  }).isRequired
 }
 
 export default Courses
