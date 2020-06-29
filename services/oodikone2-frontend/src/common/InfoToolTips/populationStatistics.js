cfe1d3dacf817b404eb01c5090e873ada437830a
esakemp <esa.kemppainen@helsinki.fi>
2020-06-29 10:36:51 +0300

    infofixing

--- services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
+++ services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
@@ -103,10 +103,8 @@ export default {
     täsmää puolivuosi-lukumäärien summan kanssa, johtuen tuplakirjauksista Oodissa.`
   },
   Students: {
-    AccordionTitle: `**Students** Taulukko näyttää oletusarvoisesti vain 
-    opiskelijanumerot, mutta harmaata liukukytkintä klikkaamalla saa näkyviin opiskelijoiden nimet.`,
-    Infobox: `**Students:** harmaata "show" painiketta klikkaamalla saa listan kaikista populaation opiskelijoista
-    (huom. suodattimet vaikuttavat tähän listaan!). Taulukko näyttää oletusarvoisesti vain 
+    AccordionTitle: `**Students** Taulukko joka näyttää informaatiota opiskelijoista.`,
+    Infobox: `**Students:** Taulukko näyttää oletusarvoisesti vain 
     opiskelijanumerot, mutta harmaata liukukytkintä klikkaamalla saa näkyviin opiskelijoiden nimet.
     <br> <br>
     **GENERAL:**
