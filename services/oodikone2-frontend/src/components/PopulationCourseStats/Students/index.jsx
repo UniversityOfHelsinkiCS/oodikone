51647b9880ea330907bbb89d568ebdd8186ce25f
atte <atte.h.1998@gmail.com>
2020-07-06 15:37:14 +0300

    remove useless useEffect

--- services/oodikone2-frontend/src/components/PopulationCourseStats/Students/index.jsx
+++ services/oodikone2-frontend/src/components/PopulationCourseStats/Students/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react'
+import React, { useState, useMemo } from 'react'
 import { Table, Icon, Popup, Item } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
