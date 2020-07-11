b2f85d21fdca51c4889747f1da7d8122564091b7
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-07-11 16:06:17 +0300

    Refactor tests to have less copy-paste

--- cypress/integration/Student_statistics.js
+++ cypress/integration/Student_statistics.js
@@ -2,17 +2,7 @@
 
 describe('Student Statistics tests', () => {
     beforeEach(() => {
-        cy.server({
-            onAnyRequest: function (route, proxy) {
-                if (Cypress.config().baseUrl.includes("http://nginx/")) {
-                    proxy.xhr.setRequestHeader('uid', 'tktl')
-                    proxy.xhr.setRequestHeader('shib-session-id', 'mock-shibboleth')
-                    proxy.xhr.setRequestHeader('hygroupcn', 'grp-oodikone-users')
-                    proxy.xhr.setRequestHeader('edupersonaffiliation', 'asdasd')
-                }
-            }
-        })
-        cy.visit(Cypress.config().baseUrl)
+        cy.init()
         cy.contains("Student statistics").click()
         cy.contains("Student names hidden")
     })
--- cypress/integration/Studyprogramme_overview.js
+++ cypress/integration/Studyprogramme_overview.js
@@ -9,17 +9,7 @@ const deleteTag = (name) => {
 
 describe('Studyprogramme overview', () => {
   beforeEach(() => {
-    cy.server({
-      onAnyRequest: function (route, proxy) {
-        if (Cypress.config().baseUrl.includes("http://nginx/")) {
-          proxy.xhr.setRequestHeader('uid', 'tktl')
-          proxy.xhr.setRequestHeader('shib-session-id', 'mock-shibboleth')
-          proxy.xhr.setRequestHeader('hygroupcn', 'grp-oodikone-users')
-          proxy.xhr.setRequestHeader('edupersonaffiliation', 'asdasd')
-        }
-      }
-    })
-    cy.visit(Cypress.config().baseUrl)
+    cy.init()
     cy.contains("Study programme").click().siblings().contains("Overview").click()
     cy.contains("Study Programme", { timeout: 100000 })
   })
