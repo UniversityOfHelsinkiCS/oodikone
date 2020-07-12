f127e0368539cc485cf1a1a27a22f664902062be
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-07-12 12:03:52 +0300

    Moar filtre testen

--- cypress/integration/Filters.js
+++ cypress/integration/Filters.js
@@ -21,11 +21,33 @@ describe("Population Statistics", () => {
 
   it("Graduation filter works", () => {
     cy.cs("graduatedFromProgrammeFilter-header").click()
-    cy.cs("graduatedFromProgrammeFilter-dropdown").click().children(".menu").children().eq(0).click()
+    cy.selectFromDropdown("graduatedFromProgrammeFilter-dropdown", 0)
     checkFilteringResult(210)
-    cy.cs("graduatedFromProgrammeFilter-dropdown").click().children(".menu").children().eq(1).click()
+    cy.selectFromDropdown("graduatedFromProgrammeFilter-dropdown", 1)
     checkFilteringResult(9)
     cy.cs("graduatedFromProgrammeFilter-clear").click()
+    checkFilteringResult(219, true)
+  })
+
+  it("Transfer filter works", () => {
+    cy.cs("transferredToProgrammeFilter-header").click()
+    cy.cs("transferredToProgrammeFilter-have").click()
+    checkFilteringResult(25)
+    cy.cs("transferredToProgrammeFilter-havenot").click()
+    checkFilteringResult(194)
+    cy.cs("transferredToProgrammeFilter-clear").click()
+    checkFilteringResult(219, true)
+  })
+
+  it("Enrollment filter works", () => {
+    cy.cs("enrollmentStatusFilter-header").click()
+    cy.selectFromDropdown("enrollmentStatusFilter-status", 0)
+    checkFilteringResult(219, true)
+    cy.selectFromDropdown("enrollmentStatusFilter-semesters", [0, 0])
+    checkFilteringResult(202)
+    cy.selectFromDropdown("enrollmentStatusFilter-status", 1)
+    checkFilteringResult(13)
+    cy.cs("enrollmentStatusFilter-clear").click()
     checkFilteringResult(219, true)
   })
 })
