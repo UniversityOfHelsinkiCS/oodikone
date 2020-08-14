a9068c71b27c9392621aed6b069c92e6d3c40f14
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-08-14 12:50:03 +0300

    Smol refactor

--- services/oodikone2-frontend/src/components/FilterTray/useAnalytics.jsx
+++ services/oodikone2-frontend/src/components/FilterTray/useAnalytics.jsx
@@ -17,12 +17,14 @@ export default () => {
   const [state, setState] = useContext(FilterAnalyticsContext)
   const category = `Filters (${state})`
 
+  const sendEvent = (event, name, value = undefined) => state && TSA.Matomo.sendEvent(category, event, name, value)
+
   const setTarget = name => setState(name)
   const unsetTarget = () => setState(null)
-  const setFilter = (name, value) => state && TSA.Matomo.sendEvent(category, 'Set Filter', name, value)
-  const clearFilter = name => state && TSA.Matomo.sendEvent(category, 'Clear Filter', name)
-  const openTray = () => state && TSA.Matomo.sendEvent(category, 'Toggle Filter Tray', 'Open')
-  const closeTray = () => state && TSA.Matomo.sendEvent(category, 'Toggle Filter Tray', 'Close')
+  const setFilter = (name, value) => sendEvent('Set Filter', name, value)
+  const clearFilter = name => sendEvent('Clear Filter', name)
+  const openTray = () => sendEvent('Toggle Filter Tray', 'Open')
+  const closeTray = () => sendEvent('Toggle Filter Tray', 'Close')
 
   return {
     setTarget,
