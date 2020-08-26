5094ce7c9187b84786b584508cabcf4ed569cb10
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-08-26 20:47:21 +0300

    Refactor credits gained table to use new toggle component

--- services/oodikone2-frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.jsx
+++ services/oodikone2-frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Popup, Button, Icon } from 'semantic-ui-react'
 import { useLocation } from 'react-router-dom'
 import { getMonths } from '../../../common/query'
 import useCreditFilter from '../../FilterTray/filters/CreditsEarned/useCreditFilter'
-import { contextKey as filterTrayContextKey } from '../../FilterTray'
 import { contextKey as creditFilterContextKey } from '../../FilterTray/filters/CreditsEarned'
-import useFilterTray from '../../FilterTray/useFilterTray'
-import useAnalytics from '../../FilterTray/useAnalytics'
+import ExternalFilterToggle from '../../FilterTray/ExternalFilterToggle'
 
 const ExternalCreditFilterToggle = ({ min, max }) => {
   const { currentValue: currentFilterValue, setRequestedValue } = useCreditFilter()
-  const [, setFilterTrayOpen] = useFilterTray(filterTrayContextKey)
-  const [, setCreditFilterOpen] = useFilterTray(creditFilterContextKey)
-  const filterAnalytics = useAnalytics()
-  const name = 'Credit Filter'
 
   const months = getMonths(useLocation())
   const limitedMax = max === 0 ? 1 : max
@@ -23,27 +16,14 @@ const ExternalCreditFilterToggle = ({ min, max }) => {
   const currentMax = currentFilterValue.max === '' ? null : Number(currentFilterValue.max)
   const active = currentMin === min && currentMax === limitedMax
 
-  const updateFilters = () => {
-    if (active) {
-      setRequestedValue({ min: null, max: null })
-      filterAnalytics.clearFilterViaTable(name)
-    } else {
-      setRequestedValue({ min, max: limitedMax })
-      setFilterTrayOpen(true)
-      setCreditFilterOpen(true)
-      filterAnalytics.setFilterViaTable(name)
-    }
-  }
-
   return (
-    <Popup
-      content={`Rajaa opiskelijat ensimmäisen ${months} kuukauden aikana saatujen opintopisteiden perusteella`}
-      size="mini"
-      trigger={
-        <Button onClick={updateFilters} size="mini" icon basic={!active} primary={active}>
-          <Icon name="filter" />
-        </Button>
-      }
+    <ExternalFilterToggle
+      filterPanelContextKey={creditFilterContextKey}
+      applyFilter={() => setRequestedValue({ min, max: limitedMax })}
+      clearFilter={() => setRequestedValue({ min: null, max: null })}
+      active={active}
+      popupContent={`Rajaa opiskelijat ensimmäisen ${months} kuukauden aikana saatujen opintopisteiden perusteella`}
+      filterName="Credit Filter"
     />
   )
 }
