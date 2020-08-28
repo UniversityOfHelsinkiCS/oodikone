8eb878cf021b3f4f3d68ed4aa10aff83e40174f5
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-08-28 19:52:43 +0300

    Remove double title from custom population

--- services/oodikone2-frontend/src/components/CustomPopulation/CustomPopulationCourses/index.jsx
+++ services/oodikone2-frontend/src/components/CustomPopulation/CustomPopulationCourses/index.jsx
@@ -1,46 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { func, shape, arrayOf, string, bool } from 'prop-types'
-import { Segment, Header, Popup } from 'semantic-ui-react'
-import { getTranslate } from 'react-localize-redux'
-import SegmentDimmer from '../../SegmentDimmer'
+import { shape, arrayOf, string, bool } from 'prop-types'
 import PopulationCourseStats from '../../PopulationCourseStats'
 
-const CustomPopulationCourses = ({ translate, courses, pending, selectedStudents, query }) => {
+const CustomPopulationCourses = ({ courses, pending, selectedStudents, query }) => {
   return (
-    <Segment>
-      <Header size="medium" dividing>
-        <Popup
-          trigger={<Header.Content>{translate('populationCourses.header')}</Header.Content>}
-          content="Sort by clicking columns. Click course name to limit observed population to students who participated to the course."
-          wide
-          position="top left"
-        />
-      </Header>
-      <SegmentDimmer translate={translate} isLoading={pending} />
-      <PopulationCourseStats
-        courses={courses}
-        query={query}
-        pending={pending}
-        selectedStudents={selectedStudents}
-        customPopulation
-      />
-    </Segment>
+    <PopulationCourseStats
+      courses={courses}
+      query={query}
+      pending={pending}
+      selectedStudents={selectedStudents}
+      customPopulation
+    />
   )
 }
 
 CustomPopulationCourses.propTypes = {
   pending: bool.isRequired,
   courses: shape([]).isRequired,
-  translate: func.isRequired,
   selectedStudents: arrayOf(string).isRequired,
   query: shape({}).isRequired
 }
 
-const mapStateToProps = ({ localize, populationCourses }) => ({
+const mapStateToProps = ({ populationCourses }) => ({
   courses: populationCourses.data,
   pending: populationCourses.pending,
-  translate: getTranslate(localize),
   query: populationCourses.query
 })
 
