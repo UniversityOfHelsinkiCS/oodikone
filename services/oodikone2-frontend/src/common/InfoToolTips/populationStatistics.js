9256e20ba6d16e3edb733ff1e55990b9ae6b0a96
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-08-31 21:16:10 +0300

    Update course population - students infobox

--- services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
+++ services/oodikone2-frontend/src/common/InfoToolTips/populationStatistics.js
@@ -79,24 +79,6 @@ export default {
   Students: {
     AccordionTitle: `**Students** Taulukko joka näyttää informaatiota opiskelijoista.`
   },
-  CoursePopulationStudents: {
-    Infobox: `**Students:** harmaata _Show_-painiketta klikkaamalla saa listan kaikista populaation opiskelijoista.
-    (Huom. suodattimet vaikuttavat tähän listaan!) Taulukko näyttää oletusarvoisesti vain 
-    opiskelijanumerot, mutta harmaata liukukytkintä klikkaamalla saa näkyviin opiskelijoiden nimet.
-    <br> <br>
-    **Student Number:**Klikkaamalla opiskelijanumeron vieressä olevaa sinistä nuolta, siirtyy "student statistic" -näkymään valitun opiskelijan osalta.
-    <br> <br>
-    **All Credits:** listaa opiskelijan kaikki opintopisteet (myös aiemmat suoritukset, 
-      eli ne jotka tehty ennen valittuun tarkastelujaksoon ja populaatioon kuulumista)
-    <br> <br>
-    **Tags:**
-    Opiskelijoille voi luoda tageja ryhmittelyä varten kohdassa _Study Program > Overview > haluttu koulutusohjelma_.
-    <br> <br>
-    **Study Programme:** Opiskelijan koulutusohjelma.
-    <br> <br>
-    **Start Year at Uni:** Aloitusvuosi yliopistolla.
-    `
-  },
   CreditStatistics: {
     AccordionTitle: `**Credit Statistics** Opintopistekertymä-kategoria skaalautuvat valitun tarkastelujakson mukaan. `
   },
