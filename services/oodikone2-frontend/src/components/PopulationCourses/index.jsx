1753545abf5bf63a63e0607f91387a3839e35a5a
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-04 10:58:23 +0300

    Fix linting

--- services/oodikone2-frontend/src/components/PopulationCourses/index.jsx
+++ services/oodikone2-frontend/src/components/PopulationCourses/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { func, shape, arrayOf, string, bool } from 'prop-types'
 import { Segment } from 'semantic-ui-react'
