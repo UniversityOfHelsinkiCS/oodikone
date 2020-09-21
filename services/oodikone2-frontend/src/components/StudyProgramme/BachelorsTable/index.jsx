befdf4e40aa5e6bff894cae9dc5228fd2cf52abc
Roni Lindholm <roni.lindholm@helsinki.fi>
2020-09-21 11:32:34 +0300

    Fix lint

--- services/oodikone2-frontend/src/components/StudyProgramme/BachelorsTable/index.jsx
+++ services/oodikone2-frontend/src/components/StudyProgramme/BachelorsTable/index.jsx
@@ -2,48 +2,45 @@ import React from 'react'
 import { Table, Header, Grid, Segment } from 'semantic-ui-react'
 import { shape, any, arrayOf, bool } from 'prop-types'
 
-
-const BachelorsTable = ({ bachelors, loading }) => {
-  return (
-    <>
-      <Header>
-        <Grid columns={2}>
-          <Grid.Row>
-            <Grid.Column>
-              Cool options table here
-              <Header.Subheader>Last updated: tomorrow</Header.Subheader>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Header>
-      <div>Statistics from n+1 years</div>
-      <Segment basic loading={loading} style={{ overflowX: 'auto' }}>
-        <Table selectable>
-          {bachelors && bachelors.data
-            ? bachelors.data.map(item => (
-                <>
-                  <Table.Header>
+const BachelorsTable = ({ bachelors, loading }) => (
+  <>
+    <Header>
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            Cool options table here
+            <Header.Subheader>Last updated: tomorrow</Header.Subheader>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Header>
+    <div>Statistics from n+1 years</div>
+    <Segment basic loading={loading} style={{ overflowX: 'auto' }}>
+      <Table selectable>
+        {bachelors && bachelors.data
+          ? bachelors.data.map(item => (
+              <>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>{item.year}</Table.HeaderCell>
+                    <Table.HeaderCell>Lukumäärä</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  {item.bachelors.map(bachelor => (
                     <Table.Row>
-                      <Table.HeaderCell>{item.year}</Table.HeaderCell>
-                      <Table.HeaderCell>Lukumäärä</Table.HeaderCell>
+                      <Table.Cell>{bachelor.name}</Table.Cell>
+                      <Table.Cell>{bachelor.amount}</Table.Cell>
                     </Table.Row>
-                  </Table.Header>
-                  <Table.Body>
-                    {item.bachelors.map(bachelor => (
-                      <Table.Row>
-                        <Table.Cell>{bachelor.name}</Table.Cell>
-                        <Table.Cell>{bachelor.amount}</Table.Cell>
-                      </Table.Row>
-                    ))}
-                  </Table.Body>
-                </>
-              ))
-            : null}
-        </Table>
-      </Segment>
-    </>
-  )
-}
+                  ))}
+                </Table.Body>
+              </>
+            ))
+          : null}
+      </Table>
+    </Segment>
+  </>
+)
 
 BachelorsTable.propTypes = {
   bachelors: shape({
