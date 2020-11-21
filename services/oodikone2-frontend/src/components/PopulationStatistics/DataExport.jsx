a379483823ce2939113fe9f2e8815f488741ae91
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-11-21 18:58:13 +0200

    Minthify data export button UX :9000:

--- services/oodikone2-frontend/src/components/PopulationStatistics/DataExport.jsx
+++ services/oodikone2-frontend/src/components/PopulationStatistics/DataExport.jsx
@@ -1,7 +1,7 @@
 import { sortBy } from 'lodash'
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Button, Dropdown, Icon } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 import xlsx from 'xlsx'
 import { getStudentTotalCredits, getTextIn, reformatDate } from '../../common'
 import sendEvent from '../../common/sendEvent'
