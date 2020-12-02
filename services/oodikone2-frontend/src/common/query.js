ba26f2095e5d50f8c4857471fdb50a10e16d1bcf
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-12-02 17:00:32 +0200

    Refactor

--- services/oodikone2-frontend/src/common/query.js
+++ services/oodikone2-frontend/src/common/query.js
@@ -1,13 +1,16 @@
 /**
- * Global definitions for Population Statistics HTTP GET query.
+ * Global definitions for query params.
  */
 import qs from 'qs'
 
-const parseParams = location => qs.parse(location.search, { ignoreQueryPrefix: true })
+/**
+ * All query params from given `location`.
+ * @param {object} location React-Router's `location` object.
+ */
+export const queryParamsFromUrl = location => qs.parse(location.search, { ignoreQueryPrefix: true })
 
 /**
  * Get `months` parameter from query.
  * @param {object} location React-Router's `location` object.
  */
-/* eslint-disable-next-line import/prefer-default-export */
-export const getMonths = location => parseParams(location).months
+export const getMonths = location => queryParamsFromUrl(location).months
