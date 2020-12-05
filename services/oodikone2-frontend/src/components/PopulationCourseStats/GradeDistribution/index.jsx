271c58f9ff1b6f3d9526086d9908584221e943af
Darake <adamson.gert@hotmail.com>
2020-12-05 15:21:14 +0200

    bring back old courses of population & split components

--- services/oodikone2-frontend/src/components/PopulationCourseStats/GradeDistribution/index.jsx
+++ services/oodikone2-frontend/src/components/PopulationCourseStats/GradeDistribution/index.jsx
@@ -1,148 +1,22 @@
 import React from 'react'
-import { Table, Popup, Item, Icon } from 'semantic-ui-react'
-import { replace, sortBy, omit } from 'lodash'
-import { Link } from 'react-router-dom'
-import { shape, string, instanceOf, func } from 'prop-types'
+import { Table, Icon } from 'semantic-ui-react'
+import { instanceOf, func } from 'prop-types'
 import { useSelector } from 'react-redux'
 import { getTextIn } from '../../../common'
-import FilterToggleIcon from '../../FilterToggleIcon'
-import SortableHeaderCell from '../SortableHeaderCell'
 import { UsePopulationCourseContext } from '../PopulationCourseContext'
-import useCourseFilter from '../../FilterTray/filters/Courses/useCourseFilter'
-import { useLanguage } from '../../../common/hooks'
+import GradeDistributionHeader from './GradeDistributionHeader'
+import GradeDistributionRow from './GradeDistributionRow'
 
 const gradeTypes = [1, 2, 3, 4, 5]
 
-const formatGradeDistribution = grades =>
-  replace(
-    JSON.stringify(
-      sortBy(Object.entries(grades).map(([key, value]) => ({ [key]: value.count })), o => -Object.keys(o)),
-      null,
-      1
-    ),
-    /\[\n|{\n*|{\s|}|\s*}|]|"|,/g,
-    ''
-  )
-
-const CourseRow = ({ courseStatistics }) => {
-  const language = useLanguage()
-  const { courseIsSelected } = useCourseFilter()
-
-  const { onCourseNameCellClick, onGoToCourseStatisticsClick } = UsePopulationCourseContext()
-
-  const { course, grades } = courseStatistics
-  const { name, code } = course
-
-  const isActive = courseIsSelected(course.code)
-  let attempts = 0
-  let failedGrades = 0
-  let otherPassed = 0
-
-  if (grades) {
-    const countSumReducer = (acc, cur) => acc + cur.count
-    const gradeValues = grades ? Object.values(grades) : null
-    attempts = gradeValues.reduce(countSumReducer, 0)
-    failedGrades = gradeValues.filter(g => g.status.failingGrade).reduce(countSumReducer, 0)
-    otherPassed = Object.values(omit(grades, gradeTypes))
-      .filter(g => g.status.passingGrade || g.status.improvedGrade)
-      .reduce(countSumReducer, 0)
-  }
-  return (
-    <Table.Row active={isActive}>
-      <Popup
-        trigger={
-          <Table.Cell className="filterCell clickableCell">
-            <FilterToggleIcon isActive={isActive} onClick={() => onCourseNameCellClick(code)} />
-          </Table.Cell>
-        }
-        content={
-          isActive ? (
-            <span>
-              Poista rajaus kurssin <b>{getTextIn(name, language)}</b> perusteella
-            </span>
-          ) : (
-            <span>
-              Rajaa opiskelijat kurssin <b>{getTextIn(name, language)}</b> perusteella
-            </span>
-          )
-        }
-        position="top right"
-      />
-      <Table.Cell content={getTextIn(name, language)} className="nameCell" />
-      <Table.Cell className="iconCell">
-        <p>
-          <Item
-            as={Link}
-            to={`/coursestatistics?courseCodes=["${encodeURIComponent(
-              code
-            )}"]&separate=false&unifyOpenUniCourses=false`}
-          >
-            <Icon name="level up alternate" onClick={() => onGoToCourseStatisticsClick(code)} />
-          </Item>
-        </p>
-      </Table.Cell>
-      <Table.Cell content={code} />
-      <Table.Cell content={attempts} />
-      <Table.Cell content={failedGrades} />
-      {gradeTypes.map(g => (
-        <Table.Cell content={grades[g] ? grades[g].count || 0 : 0} key={code + g} />
-      ))}
-      <Table.Cell content={otherPassed} />
-    </Table.Row>
-  )
-}
-
-const CoursePopUpRow = ({ courseStatistics }) => {
-  const { course, grades } = courseStatistics
-  const { code } = course
-  return (
-    <Popup
-      key={code}
-      flowing
-      hoverable
-      inverted
-      position="top right"
-      hideOnScroll
-      content={grades ? <pre>{formatGradeDistribution(grades)}</pre> : 'Nothing to see here'}
-      trigger={<CourseRow courseStatistics={courseStatistics} />}
-    />
-  )
-}
-
 const GradeDistribution = ({ expandedGroups, toggleGroupExpansion }) => {
-  const {
-    modules,
-    onSortableColumnHeaderClick,
-    filterInput,
-    reversed,
-    sortCriteria,
-    tableColumnNames
-  } = UsePopulationCourseContext()
+  const { modules } = UsePopulationCourseContext()
 
   const { language } = useSelector(({ settings }) => settings)
 
   return (
     <Table celled sortable className="fixed-header">
-      <Table.Header>
-        <Table.Row>
-          {filterInput('nameFilter', 'Name', '3')}
-          {filterInput('codeFilter', 'Code')}
-
-          <SortableHeaderCell
-            content="Attempts"
-            columnName={tableColumnNames.STUDENTS}
-            onClickFn={onSortableColumnHeaderClick}
-            activeSortColumn={sortCriteria}
-            reversed={reversed}
-          />
-
-          <Table.HeaderCell>0</Table.HeaderCell>
-          {gradeTypes.map(g => (
-            <Table.HeaderCell content={g} key={g} />
-          ))}
-          <Table.HeaderCell content="Other passed" />
-        </Table.Row>
-      </Table.Header>
+      <GradeDistributionHeader gradeTypes={gradeTypes} />
       <Table.Body>
         {modules.map(({ module, courses }) => (
           <React.Fragment key={module.code}>
@@ -157,20 +31,14 @@ const GradeDistribution = ({ expandedGroups, toggleGroupExpansion }) => {
               <Table.Cell colSpan="8" />
             </Table.Row>
             {expandedGroups.has(module.code) &&
-              courses.map(course => <CoursePopUpRow key={course.course.code} courseStatistics={course} />)}
+              courses.map(course => (
+                <GradeDistributionRow key={course.course.code} courseStatistics={course} gradeTypes={gradeTypes} />
+              ))}
           </React.Fragment>
         ))}
       </Table.Body>
     </Table>
   )
-}
-
-CourseRow.propTypes = {
-  courseStatistics: shape({ course: shape({ code: string }) }).isRequired
-}
-
-CoursePopUpRow.propTypes = {
-  courseStatistics: shape({ course: shape({ code: string }) }).isRequired
 }
 
 GradeDistribution.propTypes = {
--- services/oodikone2-frontend/src/components/PopulationCourseStats/PassFail/index.jsx
+++ services/oodikone2-frontend/src/components/PopulationCourseStats/PassFail/index.jsx
@@ -1,133 +1,17 @@
 import React from 'react'
-import { Table, Icon, Popup, Item } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Table } from 'semantic-ui-react'
 import { func, instanceOf } from 'prop-types'
-import { getTextIn } from '../../../common'
-import FilterToggleIcon from '../../FilterToggleIcon'
-import SortableHeaderCell from '../SortableHeaderCell'
 import { UsePopulationCourseContext } from '../PopulationCourseContext'
-import useCourseFilter from '../../FilterTray/filters/Courses/useCourseFilter'
 import CollapsibleModuleTable from '../CollapsibleModuleTable'
-import { useLanguage } from '../../../common/hooks'
+import PassFailRow from './PassFailRow'
+import PassFailHeader from './PassFailHeader'
 
 const PassFail = ({ expandedGroups, toggleGroupExpansion }) => {
-  const language = useLanguage()
-  const { courseIsSelected } = useCourseFilter()
-  const {
-    onSortableColumnHeaderClick,
-    filterInput,
-    tableColumnNames,
-    onGoToCourseStatisticsClick,
-    onCourseNameCellClick,
-    sortCriteria,
-    reversed,
-    modules
-  } = UsePopulationCourseContext()
-  const getSortableHeaderCell = (label, columnName, rowSpan = 1) => (
-    <SortableHeaderCell
-      content={label}
-      columnName={columnName}
-      onClickFn={onSortableColumnHeaderClick}
-      activeSortColumn={sortCriteria}
-      reversed={reversed}
-      rowSpan={rowSpan}
-    />
-  )
-
-  const getTableHeader = () => (
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell colSpan="4" content="Course" />
-        {getSortableHeaderCell('Students', tableColumnNames.STUDENTS, 2)}
-        <Table.HeaderCell colSpan="3" content="Passed" />
-        <Table.HeaderCell colSpan="2" content="Failed" />
-        <Table.HeaderCell colSpan="2" content="Attempts" />
-        <Table.HeaderCell colSpan="2" content="percentage of population" />
-      </Table.Row>
-      <Table.Row>
-        {filterInput('nameFilter', 'Name', '3')}
-        {filterInput('codeFilter', 'Code')}
-        {getSortableHeaderCell('n', tableColumnNames.PASSED)}
-        {getSortableHeaderCell('after retry', tableColumnNames.RETRY_PASSED)}
-        {getSortableHeaderCell('percentage', tableColumnNames.PERCENTAGE)}
-        {getSortableHeaderCell('n', tableColumnNames.FAILED)}
-        {getSortableHeaderCell('many times', tableColumnNames.FAILED_MANY)}
-        {getSortableHeaderCell('n', tableColumnNames.ATTEMPTS)}
-        {getSortableHeaderCell('per student', tableColumnNames.PER_STUDENT)}
-        {getSortableHeaderCell('Passed', tableColumnNames.PASSED_OF_POPULATION)}
-        {getSortableHeaderCell('attempted', tableColumnNames.TRIED_OF_POPULATION)}
-      </Table.Row>
-    </Table.Header>
-  )
-
-  const getCourseRow = courseStats => {
-    const { course, stats } = courseStats
-    const { code, name } = course
-    const {
-      failed,
-      passed,
-      retryPassed,
-      failedMany,
-      attempts,
-      percentage,
-      perStudent,
-      passedOfPopulation,
-      triedOfPopulation
-    } = stats
-    const isActive = courseIsSelected(course.code)
-    return (
-      <Table.Row key={code} active={isActive} data-cy={name.fi}>
-        <Popup
-          trigger={
-            <Table.Cell className="filterCell clickableCell">
-              <FilterToggleIcon isActive={isActive} onClick={() => onCourseNameCellClick(code)} />
-            </Table.Cell>
-          }
-          content={
-            isActive ? (
-              <span>
-                Poista rajaus kurssin <b>{getTextIn(name, language)}</b> perusteella
-              </span>
-            ) : (
-              <span>
-                Rajaa opiskelijat kurssin <b>{getTextIn(name, language)}</b> perusteella
-              </span>
-            )
-          }
-          position="top right"
-        />
-        <Table.Cell content={getTextIn(name, language)} className="nameCell" />
-        <Table.Cell className="iconCell clickableCell">
-          <p>
-            <Item
-              as={Link}
-              to={`/coursestatistics?courseCodes=["${encodeURIComponent(
-                code
-              )}"]&separate=false&unifyOpenUniCourses=false`}
-              data-cy={`coursestats-link-${code}`}
-            >
-              <Icon name="level up alternate" onClick={() => onGoToCourseStatisticsClick(code)} />
-            </Item>
-          </p>
-        </Table.Cell>
-        <Table.Cell content={code} />
-        <Table.Cell content={passed + failed} />
-        <Table.Cell content={passed} />
-        <Table.Cell content={retryPassed} />
-        <Table.Cell content={`${percentage.toFixed(2)} %`} />
-        <Table.Cell content={failed} />
-        <Table.Cell content={failedMany} />
-        <Table.Cell content={attempts} />
-        <Table.Cell content={perStudent.toFixed(2)} />
-        <Table.Cell content={`${passedOfPopulation.toFixed(2)}  %`} />
-        <Table.Cell content={`${triedOfPopulation.toFixed(2)}  %`} />
-      </Table.Row>
-    )
-  }
+  const { modules } = UsePopulationCourseContext()
 
   return (
     <Table className="fixed-header" celled sortable>
-      {getTableHeader()}
+      <PassFailHeader />
       <Table.Body>
         <CollapsibleModuleTable
           emptyColSpan={12}
@@ -135,7 +19,7 @@ const PassFail = ({ expandedGroups, toggleGroupExpansion }) => {
           expandedGroups={expandedGroups}
           toggleGroupExpansion={toggleGroupExpansion}
         >
-          {courses => courses.map(getCourseRow)}
+          {courses => courses.map(stats => <PassFailRow key={stats.course.code} courseStats={stats} />)}
         </CollapsibleModuleTable>
       </Table.Body>
     </Table>
