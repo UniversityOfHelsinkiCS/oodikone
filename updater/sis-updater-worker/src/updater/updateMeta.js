6eb6f1efe5d818ff19ad772be3e16968b75884d7
atte <atte.h.1998@gmail.com>
2020-12-11 10:46:25 +0200

    Create semesters manually

--- updater/sis-updater-worker/src/updater/updateMeta.js
+++ updater/sis-updater-worker/src/updater/updateMeta.js
@@ -72,7 +72,55 @@ const updateCourseTypes = async studyLevels => {
 }
 
 const updateSemesters = async studyYears => {
-  const semesters = flattenDeep(Object.entries(groupBy(studyYears, 'org')).map(mapSemester))
+  // TIME TO HACK
+  const startYear = 1950
+  const endYear = 2025
+
+  const org = 'hy-university-root-id'
+
+  const semesters = []
+
+  let semestercode = 1
+  let yearcode = 1
+
+  for (let year = startYear; year <= endYear; year++) {
+    semesters.push({
+      composite: `${org}-${semestercode}`,
+      semestercode: semestercode++,
+      name: {
+        en: `Autumn ${year}`,
+        fi: `Syksy ${year}`,
+        sv: `Hösten ${year}`
+      },
+      startdate: `${year}-08-01`,
+      enddate: `${year}-01-01`,
+      yearcode,
+      org,
+      yearname: `${year}-${year+1}`,
+      term_index: 0,
+      start_year: year
+    })
+
+    semesters.push({
+      composite: `${org}-${semestercode}`,
+      semestercode: semestercode++,
+      name: {
+        en: `Spring ${year+1}`,
+        fi: `Kevät ${year+1}`,
+        sv: `Våren ${year+1}`
+      },
+      startdate: `${year+1}-01-01`,
+      enddate: `${year+1}-08-01`,
+      yearcode,
+      org,
+      yearname: `${year}-${year+1}`,
+      term_index: 1,
+      start_year: year
+    })
+    yearcode++;
+  }
+
+  //const semesters = flattenDeep(Object.entries(groupBy(studyYears, 'org')).map(mapSemester))
   await bulkCreate(Semester, semesters)
 }
 
