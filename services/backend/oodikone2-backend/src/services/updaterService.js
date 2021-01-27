be21a52c12f3ef6c77eea1522887c1fc93f0c04d
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-01-27 21:19:10 +0200

    Fix updater aborting

--- services/backend/oodikone2-backend/src/services/updaterService.js
+++ services/backend/oodikone2-backend/src/services/updaterService.js
@@ -69,11 +69,6 @@ const rescheduleFetched = async () => {
   return response.data
 }
 
-const abort = async () => {
-  const response = await client.get('/abort')
-  return response.data
-}
-
 module.exports = {
   ping,
   updateStudents,
@@ -87,6 +82,5 @@ module.exports = {
   rescheduleScheduled,
   rescheduleFetched,
   updateNoStudents,
-  updateDaily,
-  abort
+  updateDaily
 }
