e1c6cf6996c8cb17d5688ad2f82d04a3fe4eabb0
Kalle <38327916+Sendouc@users.noreply.github.com>
2021-02-02 17:06:40 +0200

    closes #2560 start of studyright admin only

--- services/oodikone2-frontend/src/components/PopulationStudents/StudentTable/GeneralTab.jsx
+++ services/oodikone2-frontend/src/components/PopulationStudents/StudentTable/GeneralTab.jsx
@@ -276,12 +276,14 @@ const GeneralTab = ({
   })
 
   if (!(coursePopulation || customPopulation)) {
-    columns.push({
-      key: 'studystartdate',
-      title: 'start of studyright',
-      getRowVal: s => new Date(studentToStudyrightStartMap[s.studentNumber]).getTime(),
-      getRowContent: s => reformatDate(studentToStudyrightStartMap[s.studentNumber], 'YYYY-MM-DD')
-    })
+    if (isAdmin) {
+      columns.push({
+        key: 'studystartdate',
+        title: 'start of studyright',
+        getRowVal: s => new Date(studentToStudyrightStartMap[s.studentNumber]).getTime(),
+        getRowContent: s => reformatDate(studentToStudyrightStartMap[s.studentNumber], 'YYYY-MM-DD')
+      })
+    }
 
     // potentially will replace the 'start of studyright' column - both present for validation
     columns.push({
