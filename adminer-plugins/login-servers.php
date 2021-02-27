3098fff910424c96310ba4af0524d3bf5c34d4f3
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-02-27 15:29:04 +0200

    fix login servers style, add sis-importer db

--- adminer-plugins/login-servers.php
+++ adminer-plugins/login-servers.php
@@ -1,11 +1,17 @@
 <?php
 require_once('plugins/login-servers.php');
+$driver = "pgsql";
 
 /** Set supported servers
-    * @param array array($domain) or array($domain => $description) or array($category => array())
-    * @param string
-    */
+  * @param array array($description => array("server" => , "driver" => "server|pgsql|sqlite|..."))
+*/
 return new AdminerLoginServers(
-    $servers = array("db" => "db", "db_kone" => "db_kone", "user_db" => "user_db", "analytics_db" => "analytics_db", "db_sis" => "db_sis"),
-    $driver = "pgsql"
+    $servers = array(
+        "db" => array("server" => "db", "driver" => $driver),
+        "db_kone" => array("server" => "db_kone", "driver" => $driver),
+        "user_db" => array("server" => "user_db", "driver" => $driver),
+        "analytics_db" => array("server" => "analytics_db", "driver" => $driver),
+        "db_sis" => array("server" => "db_sis", "driver" => $driver),
+        "sis-importer-db" => array("server" => "sis-importer-db", "driver" => $driver)
+    )
 );
