d56422a280b8a11a6015f6de2a81a5af1f0b3cba
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-06 13:09:15 +0200

    Add option to get student numbers from db when doing diffffff

--- services/backend/oodikone2-backend/src/sisoodi_diff/students/getStudentNumbers.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/students/getStudentNumbers.js
@@ -1,4 +1,6 @@
 const { readFileSync } = require('fs')
+const { Op, fn, col, where } = require('sequelize')
+const { StudyrightElement } = require('../../models')
 
 const getStudentNumbersFromFile = (n = 10) => {
   try {
@@ -11,14 +13,33 @@ const getStudentNumbersFromFile = (n = 10) => {
   }
 }
 
-const getStudentNumbers = () => {
+const getStudentNumbersFromProgramme = async studyProgrammeCode => {
+  try {
+    const studentNumbers = await StudyrightElement.findAll({
+      attributes: ['studentnumber'],
+      where: {
+        code: {
+          [Op.eq]: studyProgrammeCode
+        },
+        [Op.and]: where(fn('DATE_PART', 'year', col('startdate')), 2018)
+      },
+      raw: true
+    }).map(sn => sn.studentnumber)
+
+    console.log('n', studentNumbers.length)
+    return studentNumbers.slice(0, 5)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getStudentNumbers = async () => {
   const firstArg = process.argv[2]
   const secondArg = process.argv[3]
 
   switch (firstArg) {
     case 'p':
-      console.log('programme')
-      break
+      return await getStudentNumbersFromProgramme(secondArg)
     case 'n':
       return getStudentNumbersFromFile(secondArg)
     case undefined:
