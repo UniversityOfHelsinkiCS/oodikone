7af501207a4cf01c57ae5ee64190c76e55dc87e5
Jami Kousa <jami.kousa@helsinki.fi>
2021-03-09 13:28:08 +0200

    remove log2gelf

--- updater/sis-updater-worker/src/utils/logger.js
+++ updater/sis-updater-worker/src/utils/logger.js
@@ -1,29 +1,11 @@
 const winston = require('winston')
-const Log2gelf = require('winston-log2gelf')
 const { combine, timestamp, printf, splat } = winston.format
-const { LOG_PORT, LOG_HOST, LOG_HOSTNAME, LOG_PROTOCOL, LOG_PATH, NODE_ENV } = process.env
 
 const customFormat = printf(({ level, message, timestamp, ...rest }) => {
   return `${timestamp} ${level}: ${message} ${JSON.stringify(rest)}`
 })
 
 const transports = []
-
-if (LOG_PORT && LOG_HOST) {
-  transports.push(
-    new Log2gelf({
-      hostname: LOG_HOSTNAME || 'sis-updater-worker',
-      host: LOG_HOST,
-      port: LOG_PORT,
-      protocol: LOG_PROTOCOL || 'https',
-      protocolOptions: {
-        path: LOG_PATH || '/gelf'
-      },
-      environment: NODE_ENV,
-      service: 'SIS-UPDATER-WORKER'
-    })
-  )
-}
 
 if (process.env.NODE_ENV !== 'test') {
   transports.push(new winston.transports.File({ filename: 'debug.log' }))
