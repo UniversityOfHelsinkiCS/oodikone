fe1c81e2217134902058f2e09b80aa71139ef5eb
Jami Kousa <jami.kousa@helsinki.fi>
2021-03-10 15:41:53 +0200

    advanced prod logging

--- updater/sis-updater-scheduler/src/utils/logger.js
+++ updater/sis-updater-scheduler/src/utils/logger.js
@@ -1,9 +1,6 @@
 const winston = require('winston')
+const { isDev } = require('../config')
 const { combine, timestamp, printf, splat } = winston.format
-
-const customFormat = printf(({ level, message, timestamp, ...rest }) => {
-  return `${timestamp} ${level}: ${message} ${JSON.stringify(rest)}`
-})
 
 const transports = []
 
@@ -11,12 +8,36 @@ if (process.env.NODE_ENV !== 'test') {
   transports.push(new winston.transports.File({ filename: 'debug.log' }))
 }
 
-transports.push(
-  new winston.transports.Console({
-    level: 'debug',
-    format: combine(splat(), timestamp(), customFormat)
+if (isDev) {
+  const devFormat = printf(({ level, message, timestamp, ...rest }) => {
+    return `${timestamp} ${level}: ${message} ${JSON.stringify(rest)}`
   })
-)
+
+  transports.push(
+    new winston.transports.Console({
+      level: 'debug',
+      format: combine(splat(), timestamp(), devFormat)
+    })
+  )
+}
+
+if (!isDev) {
+  const levels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    http: 3,
+    verbose: 4,
+    debug: 5,
+    silly: 6
+  }
+
+  const prodFormat = winston.format.printf(({ level, ...rest }) => JSON.stringify({
+    level: levels[level], ...rest
+  }))
+
+  transports.push(new winston.transports.Console({ format: prodFormat }))
+}
 
 const logger = winston.createLogger({ transports })
 
