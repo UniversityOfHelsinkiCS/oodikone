dcf3a890017808e36925de7ca5c137fbbb889577
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-10 18:20:51 +0200

    Ignore gert's list of courses

--- services/backend/oodikone2-backend/src/sisoodi_diff/students/compareCourses.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/students/compareCourses.js
@@ -1,6 +1,16 @@
 const { sum } = require('lodash')
+const ignoredCourses = require('./ignoredCourses')
+
+const excludeIgnoredCourses = courses =>
+  courses.filter(({ course }) => {
+    const { code } = course
+    return !ignoredCourses.includes(code)
+  })
 
 const compareTotalCredits = (oodi, sis, msg) => {
+  oodi = excludeIgnoredCourses(oodi)
+  sis = excludeIgnoredCourses(sis)
+
   const sumOodi = sum(oodi.map(course => course.credits))
   const sumSis = sum(sis.map(course => course.credits))
 
