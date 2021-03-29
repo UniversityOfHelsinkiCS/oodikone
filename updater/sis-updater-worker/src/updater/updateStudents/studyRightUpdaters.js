3a7e6c88cf21d94bc1bf10046abf0d2b72e80d02
Matti Luukkainen <mluukkai@iki.fi>
2021-03-29 20:48:00 +0300

    unfak varhaiskasvatistiedefukap

--- updater/sis-updater-worker/src/updater/updateStudents/studyRightUpdaters.js
+++ updater/sis-updater-worker/src/updater/updateStudents/studyRightUpdaters.js
@@ -124,7 +124,9 @@ const updateStudyRightElements = async (groupedStudyRightSnapshots, moduleGroupI
         // according to Eija Airio this is the right way to get the date... at least when studyright has changed
         let startDate = snapshot.first_snapshot_date_time
         // fix for varhaiskasvatus, see https://github.com/UniversityOfHelsinkiCS/oodikone/issues/2741
-        if ( snapshot.accepted_selection_path && snapshot.accepted_selection_path.educationPhase1GroupId ==='hy-DP-114256570') {
+
+        if ( snapshot.accepted_selection_path && snapshot.accepted_selection_path.educationPhase1GroupId ==='hy-DP-114256570' && snapshot.accepted_selection_path.educationPhase1ChildGroupId === 'otm-ebd2a5bb-190b-49cc-bccf-44c7e5eef14b') {
+          console.log(JSON.stringify(snapshot, null, 2))
           startDate = snapshot.valid.startDate
         }
 
