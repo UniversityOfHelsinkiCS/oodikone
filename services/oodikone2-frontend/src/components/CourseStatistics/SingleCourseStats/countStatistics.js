fa4bfd38bb8a17ccdbe71f95560b8d7f3cee20f0
Saara Satokangas <saarasat@gmail.com>
2021-04-06 09:53:39 +0300

    Separate totals into its own function and file

---
+++ services/oodikone2-frontend/src/components/CourseStatistics/SingleCourseStats/countStatistics.js
@@ -0,0 +1,75 @@
+export const countTotalStats = (formattedStats, userHasAccessToAllStats) => {
+  const totals = formattedStats.reduce(
+    (acc, curr) => {
+      if (curr.rowObfuscated) {
+        return acc
+      }
+      const passed = acc.attempts.categories.passed + curr.attempts.categories.passed
+      const failed = acc.attempts.categories.failed + curr.attempts.categories.failed
+      const cgrades = acc.attempts.grades
+
+      Object.keys(curr.attempts.grades).forEach(grade => {
+        if (!cgrades[grade]) cgrades[grade] = 0
+        cgrades[grade] += curr.attempts.grades[grade]
+      })
+      const { passedFirst, failedFirst } = curr.students.categories
+
+      const newPassedFirst = passedFirst
+        ? acc.students.categories.passedFirst + passedFirst
+        : acc.students.categories.passedFirst
+      const newFailedFirst = failedFirst
+        ? acc.students.categories.failedFirst + failedFirst
+        : acc.students.categories.failedFirst
+
+      const sgrades = acc.students.grades
+
+      Object.keys(curr.students.grades).forEach(grade => {
+        if (!sgrades[grade]) sgrades[grade] = 0
+        sgrades[grade] += curr.students.grades[grade]
+      })
+
+      return {
+        ...acc,
+        coursecode: curr.coursecode,
+        attempts: { categories: { passed, failed }, grades: cgrades },
+        students: { categories: { passedFirst: newPassedFirst, failedFirst: newFailedFirst }, grades: sgrades }
+      }
+    },
+    {
+      code: 9999,
+      name: 'Total',
+      coursecode: '000',
+      userHasAccessToAllStats,
+      attempts: {
+        categories: {
+          passed: 0,
+          failed: 0
+        },
+        passRate: 0,
+        grades: {}
+      },
+      students: {
+        categories: {
+          passedFirst: 0,
+          failedFirst: 0
+        },
+        grades: {},
+        total: 0,
+        passRate: 0,
+        failRate: 0
+      }
+    }
+  )
+
+  // Count pass- and failrates also for "Total"-lines
+  const { passedFirst = 0, passedRetry = 0, failedFirst = 0, failedRetry = 0 } = totals.students.categories
+  const total = passedFirst + passedRetry + failedFirst + failedRetry
+  totals.students.total = total
+  totals.students.passRate = (passedFirst + passedRetry) / total
+  totals.students.failRate = (failedFirst + failedRetry) / total
+
+  const { failed, passed } = totals.attempts.categories
+  totals.attempts.passRate = (100 * passed) / (passed + failed)
+
+  return totals
+}
