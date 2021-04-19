f0eb329884b99f1c5991deb5ccaa9e35dd4ce706
Saara Satokangas <saarasat@gmail.com>
2021-04-19 15:22:17 +0300

    Fix wrong toggle-position

--- services/oodikone2-frontend/src/components/CourseStatistics/ResultTabs/index.jsx
+++ services/oodikone2-frontend/src/components/CourseStatistics/ResultTabs/index.jsx
@@ -45,7 +45,7 @@ const ResultTabs = props => {
     const currentTab = params[1].panes[activeIndex]
     sendAnalytics(`Current tab '${currentTab.menuItem.content}'`, 'Course statistics')
     setTab(...params)
-    setViewMode(resetViewMode ? viewModeNames.ATTEMPTS : viewMode)
+    setViewMode(resetViewMode ? viewMode : viewModeNames.ATTEMPTS)
   }
 
   const getRadioButton = (firstLabel, secondLabel, value, setValue) => (
