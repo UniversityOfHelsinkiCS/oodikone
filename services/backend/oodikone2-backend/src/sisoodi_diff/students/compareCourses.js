94be1d9c9d099e3df54901994a4c03a97f28b7c9
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-04-27 12:47:40 +0300

    Clean up

--- services/backend/oodikone2-backend/src/sisoodi_diff/students/compareCourses.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/students/compareCourses.js
@@ -5,8 +5,6 @@ const excludeIgnoredCourses = require('./excludeIgnoredCourses')
 const compareCourses = async (data, msg) => {
   data.courses = await excludeIgnoredCourses(data)
 
-  //msg = compareTotalCredits(oodi, sis, msg)
-  //msg = compareLength(oodi, sis, msg)
   msg = compareCoursesPairwise(data, msg)
   msg = compareStudyModules(data, msg)
   return msg
