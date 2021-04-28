f53a77feb7bb0f51f401dfd468e3c641140726ed
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-04-28 15:39:05 +0300

    Move programmemodule query to single function

--- services/backend/oodikone2-backend/src/servicesV2/programmeModules.js
+++ services/backend/oodikone2-backend/src/servicesV2/programmeModules.js
@@ -5,15 +5,12 @@ const { Op } = Sequelize
 const { dbConnections: sisConnections } = require('../databaseV2/connection')
 const { sequelizeKone } = require('../database/connection')
 
-const byProgrammeCode = async code => {
+const recursivelyGetModuleAndChildren = async (code, type) => {
   // TODO use only sis connection once sis is stable
   const connection = sisConnections.established ? sisConnections.sequelize : sequelizeKone
-
-  // recursively get a module and all of its children and all of the children of the children...
   try {
     const [result] = await connection.query(
-      `
-      WITH RECURSIVE children as (
+      `WITH RECURSIVE children as (
         SELECT DISTINCT pm.*, 0 AS module_order, NULL::jsonb AS label_name, NULL AS label_code FROM programme_modules pm
         WHERE pm.code = ?
         UNION ALL
@@ -21,15 +18,19 @@ const byProgrammeCode = async code => {
         FROM children c, programme_modules pm, programme_module_children pmc
         WHERE c.id = pmc.parent_id AND pm.id = pmc.child_id
         GROUP BY pm.id, c.name, c.code, c.order
-      ) SELECT * FROM children WHERE type = 'course'
-    `,
-      { replacements: [code] }
+      ) SELECT * FROM children WHERE type = ?`,
+      { replacements: [code, type] }
     )
+    return result
   } catch (e) {
-    console.error("Error when searching programme module with code: ", code)
-    console.error("Details: ", e)
+    console.error('Error when searching with code: ', code)
+    console.error('Details: ', e)
     return []
   }
+}
+
+const byProgrammeCode = async code => {
+  const result = await recursivelyGetModuleAndChildren(code, 'course')
 
   const excluded = await ExcludedCourse.findAll({
     where: {
@@ -59,31 +60,8 @@ const byProgrammeCode = async code => {
 const addExcludedCourses = async (programmecode, coursecodes) => {
   return ExcludedCourse.bulkCreate(coursecodes.map(c => ({ programme_code: programmecode, course_code: c })))
 }
-// just copy pasted from above since almost same query
 const modulesByProgrammeCode = async code => {
-  const connection = sisConnections.established ? sisConnections.sequelize : sequelizeKone
-  try {
-    const [result] = await connection.query(
-      `
-      WITH RECURSIVE children as (
-        SELECT DISTINCT pm.*, 0 AS module_order, NULL::jsonb AS label_name, NULL AS label_code FROM programme_modules pm
-        WHERE pm.code = ?
-        UNION ALL
-        SELECT pm.*, c.order AS module_order, c.name AS label_name, c.code AS label_code
-        FROM children c, programme_modules pm, programme_module_children pmc
-        WHERE c.id = pmc.parent_id AND pm.id = pmc.child_id
-        GROUP BY pm.id, c.name, c.code, c.order
-      ) SELECT * FROM children WHERE type = 'module'
-    `,
-      { replacements: [code] }
-    )
-  } catch (e) {
-    console.error("Error when searching programme module with code: ", code)
-    console.error("Details: ", e)
-    return []
-  }
-
-  return result
+  return await recursivelyGetModuleAndChildren(code, 'module')
 }
 
 const removeExcludedCourses = async ids => {
