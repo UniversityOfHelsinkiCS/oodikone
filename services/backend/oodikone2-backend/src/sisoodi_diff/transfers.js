255ad2281d26b7d6d8f5eeb8aef353524b355c9f
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-19 12:58:31 +0300

    Add from / to counter

--- services/backend/oodikone2-backend/src/sisoodi_diff/transfers.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/transfers.js
@@ -5,54 +5,47 @@ const { Transfers, StudyrightElement } = require('../models')
 const { Transfer: SISTransfer } = require('../modelsV2')
 const { Op } = require('sequelize')
 
-let total = 0
+let totalFrom = 0
+let totalTo = 0
 
-const populationDiff = async (programme) => {
+const transferDiff = async (programme) => {
+  [true, false].forEach(async fromProgramme => {
+    const where = fromProgramme ? {sourcecode: programme} : {targetcode: programme}
 
-  const resultOodi = (await Transfers.findAll({
-    where: {
-      sourcecode: programme
+    const resultOodi = (await Transfers.findAll({
+      where: where
+    })).map(s => s.studentnumber)
+
+    const resultSis = (await SISTransfer.findAll({
+      where: where
+    })).map(s => s.studentnumber)
+
+    let sisOnly = _.difference(resultSis, resultOodi)
+    let oodiOnly = _.difference(resultOodi, resultSis)
+
+    if (oodiOnly.length === 0 && sisOnly.length === 0) return
+
+    const both = new Set([...resultSis, ...resultOodi])
+    if (fromProgramme) {
+      totalFrom += both.size
+    } else {
+      totalTo += both.size
     }
-  })).map(s => s.studentnumber)
 
-  const resultSis = (await SISTransfer.findAll({
-    where: {
-      sourcecode: programme
+    // Check for possible causes and group together 
+    const direction = fromProgramme ? "Transferred from" : "Transferred to"
+    console.log(`=== ${direction} ${programme} ===`)
+
+    if (oodiOnly.length > 0) {
+      console.log("in oodi:")
+      console.log(oodiOnly.join('\n'))
     }
-  })).map(s => s.studentnumber)
 
-
-  let sisOnly = _.difference(resultSis, resultOodi)
-  let oodiOnly = _.difference(resultOodi, resultSis)
-
-  if (oodiOnly.length === 0 && sisOnly.length === 0) return
-
-  const both = new Set([...resultSis, ...resultOodi])
-  total += both.size
-
-  // Check for possible causes and group together
-  console.log(`=== ${programme} ===`)
-  console.log("Both size: ", both.size)
-
-  if (oodiOnly.length > 0) {
-    console.log("oodionly:")
-    console.log(oodiOnly.join('\n'))
-  }
-
-  if (sisOnly.length > 0) {
-    console.log("sisonly:")
-    console.log(sisOnly.join('\n'))
-  }
-}
-
-const programmeDiff = async programme => {
-  console.log('\n' + programme)
-  await populationDiff(programme)
-  // const years = ['2017', '2018', '2019', '2020']
-  // console.log('\n' + programme)
-  // for (const year of years) {
-  //   await populationDiff(programme, year)
-  // }
+    if (sisOnly.length > 0) {
+      console.log("in sis:")
+      console.log(sisOnly.join('\n'))
+    }
+  })
 }
 
 const masterCodes = async () => {
@@ -87,46 +80,39 @@ const bscCodes = async () => {
 
 const msc = async () => {
   const programmes = await masterCodes()
-  console.log(programmes.join('\n'))
   for (let programme of programmes) {
-    await programmeDiff(programme)
+    await transferDiff(programme)
   }
 }
 
 const bsc = async () => {
   const programmes = await bscCodes()
   for (let programme of programmes) {
-    await programmeDiff(programme)
+    await transferDiff(programme)
   }
 }
 
 const main = async () => {
-  // print moar/less
-  verbose = true
 
   const what = process.argv.slice(2)
 
   if (what.includes('all')) {
     await bsc()
     await msc()
-  }
-
-  if (what.includes('msc')) {
+  } else if (what.includes('msc')) {
     await msc()
-  }
-
-  if (what.includes('bsc')) {
+  } else if (what.includes('bsc')) {
     await bsc()
-  }
-
-  for (let i = 0; i < what.length; i++) {
-    const programme = what[i]
-    if (programme.startsWith('KH') || programme.startsWith('MH')) {
-      await programmeDiff(programme)
+  } else {
+    for (let i = 0; i < what.length; i++) {
+      const programme = what[i]
+      if (programme.startsWith('KH') || programme.startsWith('MH')) {
+        await transferDiff(programme)
+      }
     }
   }
-
-  console.log("== TOTAL: ", total)
+  console.log("== TOTAL FROM: ", totalFrom)
+  console.log("== TOTAL TO: ", totalTo)
 }
 
 main()
