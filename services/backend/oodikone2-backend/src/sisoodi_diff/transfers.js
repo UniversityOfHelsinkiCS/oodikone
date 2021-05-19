6762330aef90b178fb330c15b028e75b25b30407
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-19 16:07:35 +0300

    Add total counting to transfers diff script

--- services/backend/oodikone2-backend/src/sisoodi_diff/transfers.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/transfers.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 const _ = require('lodash')
-const { Transfers, StudyrightElement } = require('../models')
+const { Transfers, StudyrightElement, Studyright } = require('../models')
 const { Transfer: SISTransfer } = require('../modelsV2')
 const { Op } = require('sequelize')
 
@@ -21,105 +21,110 @@ const { Op } = require('sequelize')
 //     (COUNT(studentnumber) > 1) AND
 //     (COUNT(sourcecode) > 1) AND
 //     (COUNT(targetcode) > 1);
+//
+const findGraduatedAndTransferredOnSameDay = async (transfers) => {
+  const graduationDatesByStudentnumber = (await Studyright.findAll({
+    where: {
+      student_studentnumber: {
+        [Op.in]: transfers.map(s => s.studentnumber)
+      }
+    }
+  })).reduce((acc, curr) => {
+    if (curr.graduated) {
+      const sn = curr.student_studentnumber
+      return {...acc, [sn]: acc.sn ? [...acc.sn, new Date(curr.enddate).toString()] : [new Date(curr.enddate).toString()]}
+    }
+    return acc
+  }, {})
 
-const transferDiff = async (programme) => {
-  [true, false].forEach(async fromProgramme => {
-    const where = fromProgramme ? {sourcecode: programme} : {targetcode: programme}
+  return transfers.filter(transfer => 
+    graduationDatesByStudentnumber[transfer.studentnumber] && 
+      graduationDatesByStudentnumber[transfer.studentnumber].includes(new Date(transfer.transferdate).toString())
+  ).map(s => s.studentnumber)
+}
 
-    const resultOodi = (await Transfers.findAll({
-      where: where
-    })).map(s => s.studentnumber)
+// FromProgramme --> check sourcecode, otherwise to programm --> targetcode
+const transferDiff = async (programme, fromProgramme = true) => {
+  const where = fromProgramme ? {sourcecode: programme} : {targetcode: programme}
 
-    const resultSis = (await SISTransfer.findAll({
-      where: where
-    })).map(s => s.studentnumber)
+  let totals = [0, 0, 0]
 
-    let sisOnly = _.difference(resultSis, resultOodi)
-    let oodiOnly = _.difference(resultOodi, resultSis)
+  const transfersOodi = (await Transfers.findAll({
+    where: where
+  }))
+  const resultOodi = transfersOodi.map(s => s.studentnumber)
 
-    if (oodiOnly.length === 0 && sisOnly.length === 0) return
+  const transfersSis = (await SISTransfer.findAll({
+    where: where
+  }))
+  const resultSis = transfersSis.map(s => s.studentnumber)
 
-    const both = new Set([...resultSis, ...resultOodi])
+  let sisOnly = _.difference(resultSis, resultOodi)
+  let oodiOnly = _.difference(resultOodi, resultSis)
 
-    // Check for possible causes and group together 
-    const direction = fromProgramme ? "Transferred from" : "Transferred to"
-    console.log(`=== ${direction} ${programme} ===`)
+  if (oodiOnly.length === 0 && sisOnly.length === 0) return totals
 
+  const direction = fromProgramme ? "Transferred from" : "Transferred to"
+  console.log(`=== ${direction} ${programme} ===`)
+
+  if (oodiOnly.length > 0) {
+    const graduatedAndTransferredOnSameDay = await findGraduatedAndTransferredOnSameDay(transfersOodi)
+    oodiOnly = _.difference(oodiOnly, graduatedAndTransferredOnSameDay)
+    console.log("in oodi:")
+    if (graduatedAndTransferredOnSameDay.length > 0) {
+      console.log("graduated (probably from bsc) and was transferred to new program on same day")
+      console.log(graduatedAndTransferredOnSameDay.join('\n'))
+      totals[0] = graduatedAndTransferredOnSameDay.length
+    }
     if (oodiOnly.length > 0) {
-      console.log("in oodi:")
+      console.log("other reason")
       console.log(oodiOnly.join('\n'))
+      totals[1] = oodiOnly.length
     }
+  }
 
-    if (sisOnly.length > 0) {
-      console.log("in sis:")
-      console.log(sisOnly.join('\n'))
-    }
-  })
+  if (sisOnly.length > 0) {
+    console.log("in sis:")
+    console.log(sisOnly.join('\n'))
+    totals[2] = oodiOnly.length
+  }
+
+  return totals
 }
 
-const masterCodes = async () => {
+const findProgrammes = async () => {
   return (
     await StudyrightElement.findAll({
       attributes: ['code'],
       where: {
         code: {
-          [Op.like]: 'MH%'
+          [Op.like]: { [Op.any]: ['MH%', 'KH%']}
         }
       },
       group: ['code'],
       order: ['code']
     })
   ).map(s => s.code)
-}
-
-const bscCodes = async () => {
-  return (
-    await StudyrightElement.findAll({
-      attributes: ['code'],
-      where: {
-        code: {
-          [Op.like]: 'KH%'
-        }
-      },
-      group: ['code'],
-      order: ['code']
-    })
-  ).map(s => s.code)
-}
-
-const msc = async () => {
-  const programmes = await masterCodes()
-  for (let programme of programmes) {
-    await transferDiff(programme)
-  }
-}
-
-const bsc = async () => {
-  const programmes = await bscCodes()
-  for (let programme of programmes) {
-    await transferDiff(programme)
-  }
 }
 
 const main = async () => {
-
-  const what = process.argv.slice(2)
-
-  if (what.includes('all')) {
-    await bsc()
-    await msc()
-  } else if (what.includes('msc')) {
-    await msc()
-  } else if (what.includes('bsc')) {
-    await bsc()
-  } else {
-    for (let i = 0; i < what.length; i++) {
-      const programme = what[i]
-      if (programme.startsWith('KH') || programme.startsWith('MH')) {
-        await transferDiff(programme)
-      }
-    }
+  let fromTotals = [0, 0, 0]
+  let toTotals = [0, 0, 0]
+  const programmes = await findProgrammes()
+  console.log(programmes)
+  for (const programme of programmes) {
+    fromTotals = _.unzipWith([fromTotals, await transferDiff(programme, fromProgramme = true)], _.add)
+    toTotals = _.unzipWith([toTotals, await transferDiff(programme, fromProgramme = false)], _.add)
   }
+  console.log("1. Total diff transferred from")
+  console.log("oodi, graduated & transferred on the same day", fromTotals[0])
+  console.log("oodi, other", fromTotals[1])
+  console.log("sis, other", fromTotals[2])
+
+  console.log("2. Total diff transferred to")
+  console.log("oodi, graduated & transferred on the same day", toTotals[0])
+  console.log("oodi, other", toTotals[1])
+  console.log("sis, other", toTotals[2])
 }
 
 main()
