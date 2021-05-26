3c0fce848460754db62d6a41c5a29911c1139eb4
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-26 14:39:36 +0300

    Add first recursive differ for trends

--- services/backend/oodikone2-backend/src/sisoodi_diff/trends.js
+++ services/backend/oodikone2-backend/src/sisoodi_diff/trends.js
@@ -1,6 +1,29 @@
 const sisTrends = require('../servicesV2/trends')
 const oodiTrends = require('../services/coolDataScience')
 
+const recursiveDiff = (oodi, sis) => {
+  Object.keys(oodi).forEach(child => {
+    if (oodi[child].drill) {
+      recursiveDiff(oodi[child].drill, sis[child].drill)
+    } else {
+      // Go through each years diff
+      Object.keys(oodi[child].yearly).forEach(year => {
+        if (!sis[child]) {
+          console.log("Doesn't exist in sis data: ", oodi[child].name.fi)
+        } else {
+          const oodiCredits = oodi[child].yearly[year].acc
+          const sisCredits = sis[child].yearly[year].acc
+          if (oodiCredits != sisCredits) {
+            console.log("=== Acc diffs for ", oodi[child].name.fi)
+            console.log("Oodi: ", oodiCredits)
+            console.log("Sis: ", oodiCredits)
+          }
+        }
+      })
+    }
+  })
+}
+
 // Do the diff
 const main = async () => {
   const showByYear = 'false'
@@ -11,11 +34,9 @@ const main = async () => {
   const oodiData = await oodiTrends.getStatus(date.getTime(), showByYear)
   const sisData = await sisTrends.getStatus(date.getTime(), showByYear)
 
-  console.log("=== OODI === ")
-  console.log(JSON.stringify(oodiData.H30, null, 2))
-  
-  console.log("=== SIS === ")
-  console.log(JSON.stringify(sisData.H30, null, 2))
+  // diff lääkis
+  recursiveDiff(oodiData.H30.drill, sisData.H30.drill)
+
 }
 
 main()
