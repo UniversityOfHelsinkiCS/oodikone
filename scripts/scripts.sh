59d0e584b2724e3402f87fe0e793144136ff0643
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-05-31 12:15:48 +0300

    Refactor setup script, drop unnecessary stuff

--- scripts/scripts.sh
+++ scripts/scripts.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
+# Constant filenames and paths used in scripts
 USER_DATA_FILE_PATH="hyuserdata"
-ANONDB_DIR=anonyymioodi
 BACKUP_DIR=backups
-
-PSQL_DB_BACKUP="$ANONDB_DIR/anon.sqz"
-USER_DB_BACKUP="$ANONDB_DIR/user-dump.sqz"
-KONE_DB_BACKUP="$ANONDB_DIR/anon_kone.sqz"
-ANALYTICS_DB_BACKUP="$ANONDB_DIR/analytics-dump.sqz"
 
 PSQL_REAL_DB_BACKUP="$BACKUP_DIR/latest-pg.sqz"
 KONE_REAL_DB_BACKUP="$BACKUP_DIR/latest-kone-pg.sqz"
@@ -17,6 +12,8 @@ SIS_REAL_DB_BACKUP="$BACKUP_DIR/latest-sis.sqz"
 
 # Remember username during runtime
 username=""
+
+### === GENERIC HELPER FUNCTIONS ===
 
 get_username() {
   # Check if username has already been set
@@ -37,39 +34,17 @@ get_username() {
   username=$(head -n 1 < $USER_DATA_FILE_PATH)
 }
 
+retry () {
+  for i in {1..60}; do
+    "$@" && break || echo "Retry attempt $i failed, waiting..." && sleep 10;
+  done
+}
+
 docker-compose-dev () {
     npm run docker:oodikone:dev -- "$@"
 }
 
-retry () {
-    for i in {1..60}
-    do
-        "$@" && break || echo "Retry attempt $i failed, waiting..." && sleep 10;
-    done
-}
-
-init_dirs () {
-    mkdir -p $BACKUP_DIR nginx nginx/cache nginx/letsencrypt
-    touch nginx/error.log
-    touch nginx/log
-}
-
-echo_path () {
-  pwd
-}
-
-get_oodikone_server_backup() {
-    get_username
-    echo "Using your Uni Helsinki username: $username"
-    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@oodikone.cs.helsinki.fi:/home/tkt_oodi/backups/* "$BACKUP_DIR/"
-    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@svm-77.cs.helsinki.fi:/home/tkt_oodi/backups/* "$BACKUP_DIR/"
-    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@svm-96.cs.helsinki.fi:/home/updater_user/backups/* "$BACKUP_DIR/"
-}
-
-get_anon_oodikone() {
-    rm -rf anonyymioodi
-    git clone --depth=1 git@github.com:UniversityOfHelsinkiCS/anonyymioodi.git
-}
+### === PSQL HELPER FUNCTIONS ===
 
 restore_psql_from_backup () {
     echo ""
@@ -90,14 +65,18 @@ ping_psql () {
 drop_psql () {
     echo "Dropping psql in container $1 with db name $2"
     retry docker exec -u postgres "$1" pg_isready --dbname="$2"
-    #docker exec -u postgres $1 psql -c "DROP DATABASE $2" || echo "container $1 DB $2 doesn't exists"
     docker exec -u postgres "$1" dropdb "$2" || echo "container $1 DB $2 does not exist"
 }
+
+### === FUNCTIONS TO RUN CLI OPTIONS ===
 
 run_importer_setup () {
     get_username
     echo "Using your Uni Helsinki username: $username"
-    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@importer:/home/importer_user/importer-db/backup/importer-db.sqz "$BACKUP_DIR/"
+
+    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" \
+    "$username@importer:/home/importer_user/importer-db/backup/importer-db.sqz" "$BACKUP_DIR/"
+
     docker-compose -f dco.data.yml up -d sis-importer-db
     ping_psql "sis-importer-db" "importer-db"
     restore_psql_from_backup "$BACKUP_DIR/importer-db.sqz" sis-importer-db importer-db
@@ -122,8 +101,9 @@ run_full_real_data_reset () {
 
     docker-compose-dev down
     docker-compose-dev up -d user_db db_kone analytics_db db_sis sis-importer-db
-    
+
     echo "Restoring PostgreSQL from backup. This might take a while."
+
     ping_psql "db_kone" "db_kone_real"
     restore_psql_from_backup $KONE_REAL_DB_BACKUP db_kone db_kone_real
     ping_psql "oodi_user_db" "user_db_real"
@@ -134,6 +114,7 @@ run_full_real_data_reset () {
     restore_psql_from_backup $SIS_REAL_DB_BACKUP db_sis db_sis_real
     ping_psql "sis-importer-db" "importer-db"
     restore_psql_from_backup "$BACKUP_DIR/importer-db.sqz" sis-importer-db importer-db
+
     echo "Database setup finished"
 
     docker-compose-dev down
@@ -157,8 +138,7 @@ install_local_npm_packages () {
     cd ../../../
 }
 
-show_instructions () {
-    cat scripts/assets/instructions.txt
+init_dirs () {
 }
 
 run_full_setup () {
@@ -166,17 +146,18 @@ run_full_setup () {
     install_local_npm_packages
     echo "Init dirs"
     init_dirs
-    echo "Getting backups from the Oodikone server, this will prompt you for your password. "
-    get_oodikone_server_backup
-    echo "Getting anon backups from the private repository. "
-    get_anon_oodikone
+    echo "Setting up needed databases"
+    run_full_real_data_reset
     echo "Building images"
     docker-compose-dev build
-    echo "Setup oodikone db from dump."
-    docker-compose-dev up -d db user_db db_kone analytics_db db_sis
-    db_setup_full
-    db_anon_setup_full
-    docker-compose-dev down
     npm run docker:up:real
-    show_instructions
+    cat scripts/assets/instructions.txt
+}
+
+get_oodikone_server_backup() {
+    get_username
+    echo "Using your Uni Helsinki username: $username"
+    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@oodikone.cs.helsinki.fi:/home/tkt_oodi/backups/* "$BACKUP_DIR/"
+    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@svm-77.cs.helsinki.fi:/home/tkt_oodi/backups/* "$BACKUP_DIR/"
+    scp -r -o ProxyCommand="ssh -l $username -W %h:%p melkki.cs.helsinki.fi" $username@svm-96.cs.helsinki.fi:/home/updater_user/backups/* "$BACKUP_DIR/"
 }
