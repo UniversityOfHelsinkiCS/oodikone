af3060ef8e207d62b94c6b573374839618dcd9e1
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-06-07 17:30:34 +0300

    Clean stuff from run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,6 @@
 
 set -Eeuo pipefail # Fail fast if script fails
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
 usage() {
   cat <<EOF
 Usage: $(basename "${BASH_SOURCE[0]}") option version command --flag
@@ -25,9 +23,9 @@ EOF
 
 setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    NOFORMAT='\033[0m' RED='\033[0;31m' BLUE='\033[0;34m'
   else
-    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+    NOFORMAT='' RED='' BLUE=''
   fi
 }
 
