d609aa18a1a873a6f644fb9acad2b5eb086b37bb
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-06-15 16:11:37 +0300

    Be verbose when cloning for debugging

--- scripts/functions_for_setup.sh
+++ scripts/functions_for_setup.sh
@@ -115,7 +115,7 @@ reset_databases() {
 
 reset_all_anonymous_data() {
   infomsg "Downloading anonymous dumps"
-  rm -rf "$ANON_DUMP_DIR" && git clone "$ANON_DUMPS_GIT_URL" "$ANON_DUMP_DIR"
+  rm -rf "$ANON_DUMP_DIR" && git clone --verbose "$ANON_DUMPS_GIT_URL" "$ANON_DUMP_DIR"
   reset_databases "anon" ${DATABASES[*]} "$OODI_DB_NAME" #Remove oodi anon setup when oodi-db is deprecated
 }
 
