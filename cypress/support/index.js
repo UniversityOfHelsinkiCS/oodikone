854915c245211a64aac5d0ef0183cead430b23b9
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-06-28 14:20:55 +0300

    Restore custom population tests + run prettier

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -13,5 +13,5 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-import './commands'
-import './populationStatistics'
+import "./commands";
+import "./populationStatistics";
--- cypress/support/populationStatistics.js
+++ cypress/support/populationStatistics.js
@@ -1,8 +1,8 @@
 /// <reference types="Cypress" />
 
 Cypress.Commands.add("selectStudyProgramme", (name) => {
-  cy.cs("navbar-studyProgramme").click()
-  cy.cs("navbar-class").click()
-  cy.cs("select-study-programme").click().children().contains(name).click()
-  cy.contains("See population").click()
-})
+  cy.cs("navbar-studyProgramme").click();
+  cy.cs("navbar-class").click();
+  cy.cs("select-study-programme").click().children().contains(name).click();
+  cy.contains("See population").click();
+});
