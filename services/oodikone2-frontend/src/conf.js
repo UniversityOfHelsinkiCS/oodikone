360f772b343ec7fe481a2f456cd09e101cc1f754
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-07-10 21:11:13 +0300

    Add sentry config

--- services/oodikone2-frontend/src/conf.js
+++ services/oodikone2-frontend/src/conf.js
@@ -2,7 +2,10 @@ const isProduction = process.env.NODE_ENV === 'production'
 
 const GIT_SHA = process.env.REACT_APP_GIT_SHA || ''
 
+const TAG = process.env.TAG || ''
+
 module.exports = {
   isProduction,
-  GIT_SHA
+  GIT_SHA,
+  TAG
 }
---
+++ services/oodikone2-frontend/src/util/sentry.js
@@ -0,0 +1,15 @@
+import * as Sentry from '@sentry/browser'
+import { inProduction, GIT_SHA, TAG } from '../conf'
+import { BASE_PATH } from '../constants'
+
+const initializeSentry = () => {
+  if (!inProduction || BASE_PATH !== '/' || TAG !== 'staging' || TAG !== 'latest') return
+
+  Sentry.init({
+    dsn: 'https://020b79f0cbb14aad94cc9d69a1ea9d52@sentry.cs.helsinki.fi/2',
+    environment: TAG,
+    release: GIT_SHA
+  })
+}
+
+export default initializeSentry
