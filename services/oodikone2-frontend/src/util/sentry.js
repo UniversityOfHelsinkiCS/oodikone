f5f115a26a885daf131277327d4021993bfce489
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-07-10 21:19:04 +0300

    Fix typos and use isProduction everywhere

--- services/oodikone2-frontend/src/util/sentry.js
+++ services/oodikone2-frontend/src/util/sentry.js
@@ -1,9 +1,9 @@
 import * as Sentry from '@sentry/browser'
-import { inProduction, GIT_SHA, TAG } from '../conf'
+import { isProduction, GIT_SHA, TAG } from '../conf'
 import { BASE_PATH } from '../constants'
 
 const initializeSentry = () => {
-  if (!inProduction || BASE_PATH !== '/' || TAG !== 'staging' || TAG !== 'latest') return
+  if (!isProduction || BASE_PATH !== '/' || TAG !== 'staging' || TAG !== 'latest') return
 
   Sentry.init({
     dsn: 'https://020b79f0cbb14aad94cc9d69a1ea9d52@sentry.cs.helsinki.fi/2',
