a49f84309409d06f5cffa38c2d87e02c15a74c55
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-07-13 16:05:39 +0300

    Remove storybook from frontend

--- services/frontend/src/components/AccessDenied/index.storybook.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react' // eslint-disable-line
-import AccessDenied from '.'
-
-storiesOf('AccessDenied', module).add('with text', () => <AccessDenied itWasError />)
