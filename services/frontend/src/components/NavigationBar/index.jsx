6274206a3f52578cb37d36789f381d113240d2e8
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-07-16 10:40:54 +0300

    Add working adminerurls

--- services/frontend/src/components/NavigationBar/index.jsx
+++ services/frontend/src/components/NavigationBar/index.jsx
@@ -9,17 +9,7 @@ import { logout as logoutAction } from '../../redux/auth'
 import './navigationBar.css'
 import LanguagePicker from '../LanguagePicker'
 import { useIsAdmin } from '../../common/hooks'
-import { isDev } from '../../conf'
-
-const { USER_ADMINER_URL, ADMINER_URL, KONE_ADMINER_URL, SIS_ADMINER_URL, SIS_IMPORTER_ADMINER_URL } = process.env
-
-const adminerUrls = [
-  { url: ADMINER_URL, text: 'Database' },
-  { url: USER_ADMINER_URL, text: 'User database' },
-  { url: KONE_ADMINER_URL, text: 'Kone database' },
-  { url: SIS_ADMINER_URL, text: 'Sis database' },
-  { url: SIS_IMPORTER_ADMINER_URL, text: 'Sis importer database' },
-]
+import { isDev, adminerUrls } from '../../conf'
 
 const allNavigationItems = {
   populations: {
