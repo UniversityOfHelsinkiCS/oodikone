b2a6a0a21e9a2a84c044b19b332dfca5a3c5967d
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-07-23 17:08:41 +0300

    Fix users tests to work with different user types

--- cypress/integration/Users.js
+++ cypress/integration/Users.js
@@ -1,69 +1,63 @@
 /// <reference types="Cypress" />
 
 describe('Users tests', () => {
-  beforeEach(() => {
-    cy.init()
-    cy.contains('Users').click()
-    cy.contains('Oodikone users')
+  describe('Using user with just grp-oodikone-user, no other rights', () => {
+    it('shows only frontpage, trends feedback', () => {
+      cy.init('', 'norights') // login with norights user
+      cy.get('.navBar').contains('Trends').should('exist')
+      cy.get('.navBar').contains('Give feedback').should('exist')
+
+      cy.get('.navBar').contains('Study programme').should('not.exist')
+      cy.get('.navBar').contains('Student statistics').should('not.exist')
+      cy.get('.navBar').contains('Course statistics').should('not.exist')
+      cy.get('.navBar').contains('Teachers').should('not.exist')
+      cy.get('.navBar').contains('Users').should('not.exist')
+      cy.get('.navBar').contains('Faculty').should('not.exist')
+      cy.get('.navBar').contains('Updater').should('not.exist')
+      cy.get('.navBar').contains('Sandbox').should('not.exist')
+    })
   })
 
-  it("Admin mocking normal user shows only the mocked user's programmes", () => {
-    cy.contains('mocking').should('not.exist')
-    cy.contains('tr', 'Normaalikäyttäjä').within($row => {
-      cy.contains('.button', 'Edit').click()
-    })
-    cy.contains('Access rights')
-      .siblings()
-      .within($row => {
-        cy.get("div[role='listitem']").should('have.length', 1).contains('Tietojenkäsittelytieteen kandiohjelma')
-      })
-    cy.route('POST', '/api/superlogin/normk').as('superlogin')
-    cy.get('i.spy').click()
-    cy.wait('@superlogin')
-    cy.contains('mocking as normk')
-    cy.wait(1000)
-    cy.contains('Study programme').click().siblings().contains('Search by class').click()
-    cy.contains('label', 'Study programme')
-    cy.contains('label', 'Study programme')
-      .siblings()
-      .within($row => {
-        cy.get("div[role='option']").should('have.length', 1).contains('Tietojenkäsittelytieteen kandiohjelma')
-      })
-  })
+  // TODO: Add user right checks for onlycoursestats user (should be like above + should
+  // show course stats
+  //
+  // TODO: Add user right checks for basic user (should show also study programme,
+  // student stats, course stats
 
-  it('Admin mocking courseStatistics-user can see only Trends and Course Statistics', () => {
-    cy.contains('mocking').should('not.exist')
-    cy.contains('tr', 'User manager').within($row => {
-      cy.contains('.button', 'Edit').click()
+  describe('Using as admin', () => {
+    beforeEach(() => {
+      cy.init('/users', 'admin')
     })
 
-    cy.get('[data-cy=access-groups-form]').click().contains('courseStatistics').click()
-    cy.get('[data-cy=access-groups-save]').click()
-
-    cy.route('POST', '/api/superlogin/usermk').as('superlogin')
-    cy.get('i.spy').click()
-    cy.wait('@superlogin')
-    cy.contains('mocking as usermk')
-    cy.wait(1000)
-
-    cy.get('[data-cy=navbar-studyProgramme').should('not.exist')
-    cy.get('[data-cy=navbar-students]').should('not.exist')
-    cy.get('[data-cy=navbar-teachers]').should('not.exist')
-    cy.get('[data-cy=navbar-users').should('not.exist')
-
-    cy.contains('Stop mocking').click()
-    cy.wait(1000)
-
-    cy.contains('Users').click()
-    cy.contains('Oodikone users')
-    cy.contains('tr', 'User manager').within($row => {
-      cy.contains('.button', 'Edit').click()
+    it('should see more stuff than others', () => {
+      cy.get('.navBar').contains('Study programme').should('exist')
+      cy.get('.navBar').contains('Student statistics').should('exist')
+      cy.get('.navBar').contains('Course statistics').should('exist')
+      // TODO: admin should probably see this as well, since can access this page: cy.get('.navBar').contains('Teachers').should('exist')
+      cy.get('.navBar').contains('Users').should('exist')
+      cy.get('.navBar').contains('Trends').should('exist')
     })
-    cy.contains('Access Groups')
-      .siblings()
-      .within($row => {
-        cy.get("i[class='dropdown icon clear']").click()
+
+    it("mocking normal user shows only the mocked user's programmes", () => {
+      cy.contains('mocking').should('not.exist')
+      cy.contains('tr', 'basic').within($row => {
+        cy.contains('.button', 'Edit').click()
       })
-    cy.get('[data-cy=access-groups-save]').click()
+      cy.intercept({
+        method: 'POST',
+        path: '/api/superlogin/basic',
+      }).as('superlogin')
+      cy.get('i.spy').click()
+      cy.wait('@superlogin')
+      cy.contains('mocking as basic')
+      cy.wait(1000)
+      cy.contains('Study programme').click().siblings().contains('Search by class').click()
+      cy.contains('label', 'Study programme')
+      cy.contains('label', 'Study programme')
+        .siblings()
+        .within($row => {
+          cy.get("div[role='option']").should('have.length', 2).contains('Tietojenkäsittelytieteen kandiohjelma')
+        })
+    })
   })
 })
