03484e68f3e0e1c930b1ad889e14697ca251f696
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-14 17:38:04 +0300

    Cleanup unused stuff from faculties reducer

--- services/frontend/src/redux/faculties.js
+++ services/frontend/src/redux/faculties.js
@@ -6,24 +6,6 @@ export const getFaculties = () => {
   return callController(route, prefix)
 }
 
-export const getUserFaculties = () => {
-  const route = '/facultystats/faculties'
-  const prefix = 'GET_USER_FACULTIES_'
-  return callController(route, prefix)
-}
-
-export const getFacultiesYearlyStats = () => {
-  const route = '/facultystats/yearlystats'
-  const prefix = 'GET_FACULTIES_YEARLY_STATS_'
-  return callController(route, prefix)
-}
-
-export const getFacultyProgrammes = () => {
-  const route = '/facultystats/programmes'
-  const prefix = 'GET_FACULTY_PROGRAMMES_'
-  return callController(route, prefix)
-}
-
 const reducer = (
   state = {
     data: [],
@@ -55,57 +37,6 @@ const reducer = (
         data: action.response,
         pending: false,
         error: true,
-      }
-    case 'GET_USER_FACULTIES_ATTEMPT':
-      return {
-        ...state,
-        userFacultiesPending: true,
-        userFacultiesError: false,
-      }
-    case 'GET_USER_FACULTIES_FAILURE':
-      return {
-        ...state,
-        userFacultiesPending: false,
-        userFacultiesError: true,
-      }
-    case 'GET_USER_FACULTIES_SUCCESS':
-      return {
-        ...state,
-        data: action.response,
-        userFacultiesPending: false,
-        userFacultiesError: false,
-      }
-    case 'GET_FACULTIES_YEARLY_STATS_FAILURE':
-      return {
-        ...state,
-        pending: false,
-        error: true,
-      }
-    case 'GET_FACULTIES_YEARLY_STATS_SUCCESS':
-      return {
-        ...state,
-        yearlyStats: action.response,
-        pending: false,
-        error: true,
-      }
-    case 'GET_FACULTY_PROGRAMMES_ATTEMPT':
-      return {
-        ...state,
-        pending: true,
-        error: false,
-      }
-    case 'GET_FACULTY_PROGRAMMES_FAILURE':
-      return {
-        ...state,
-        pending: false,
-        error: true,
-      }
-    case 'GET_FACULTY_PROGRAMMES_SUCCESS':
-      return {
-        ...state,
-        facultyProgrammes: action.response,
-        pending: false,
-        error: false,
       }
     default:
       return state
