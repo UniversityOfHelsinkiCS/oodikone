3a9d71824419ff7d4a43764fee4784f460803757
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-17 09:31:23 +0300

    Remove old semesters oodikone code

--- services/backend/src/routes/semesters.js
+++ services/backend/src/routes/semesters.js
@@ -1,11 +1,5 @@
 const router = require('express').Router()
-const { getSemestersAndYears } = require('../services/semesters')
 const semestersV2 = require('../routesV2/semesters')
 const useSisRouter = require('../util/useSisRouter')
-
-router.get('/semesters/codes', async (req, res) => {
-  const providers = await getSemestersAndYears()
-  res.json(providers)
-})
 
 module.exports = useSisRouter(semestersV2, router)
