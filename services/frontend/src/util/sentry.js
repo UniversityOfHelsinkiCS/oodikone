93ceb0940e1826ca4cbc3e6dbe06ebcf880c5ddb
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-23 19:33:19 +0300

    Add moar sentry goodies to frontend

--- services/frontend/src/util/sentry.js
+++ services/frontend/src/util/sentry.js
@@ -1,13 +1,16 @@
 import * as Sentry from '@sentry/browser'
-import { SENTRY_RELEASE, SENTRY_ENVIRONMENT, runningInCypress } from '../conf'
+import { Integrations } from '@sentry/tracing'
+import { sentryRelease, sentryEnvironment, runningInCypress } from '../conf'
 
 const initializeSentry = () => {
-  if (!SENTRY_RELEASE || !SENTRY_ENVIRONMENT || runningInCypress) return
+  if (!sentryRelease || !sentryEnvironment || runningInCypress) return
 
   Sentry.init({
     dsn: 'https://020b79f0cbb14aad94cc9d69a1ea9d52@sentry.cs.helsinki.fi/2',
-    environment: SENTRY_ENVIRONMENT,
-    release: SENTRY_RELEASE,
+    environment: sentryEnvironment,
+    release: sentryRelease,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
   })
 }
 
