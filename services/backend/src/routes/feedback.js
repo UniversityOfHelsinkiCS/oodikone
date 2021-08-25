758f47c94f69c3222172fe674300d0fc2a4ba9c7
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-25 10:04:22 +0300

    Migrate feedbacks to use pate

--- services/backend/src/routes/feedback.js
+++ services/backend/src/routes/feedback.js
@@ -1,28 +1,25 @@
 const router = require('express').Router()
-const mailservice = require('../services/mailservice')
 const userService = require('../services/userService')
+const { sendFeedbackToToska } = require('../services/mailservice')
 
 router.post('/email', async (req, res) => {
   const { content } = req.body
   const { uid } = req.headers
   const { email, full_name } = await userService.byUsername(uid)
-  const formattedEmail = content
-    .split('\n\n')
-    .map(line => `<p> ${line} </p>`)
-    .join('')
-  const feedback = mailservice.feedback(formattedEmail, uid, email, full_name)
 
-  if (process.env.SMTP !== undefined && content) {
-    await mailservice.transporter.sendMail(feedback, error => {
-      if (error) {
-        console.log('Error occured')
-        res.status(400).json(error).end()
-      } else {
-        console.log('Message sent succesfully!')
-        res.status(200).json('success').end()
-      }
-      mailservice.transporter.close()
-    })
+  const result = await sendFeedbackToToska({
+    feedbackContent: content,
+    userId: uid,
+    userEmail: email,
+    userFullName: full_name,
+  })
+  if (!result) {
+    const errorMessage = 'Error occured when sending email'
+    console.log(errorMessage)
+    res.status(400).json(errorMessage).end()
+  } else {
+    console.log('Message sent succesfully!')
+    res.status(200).json('success').end()
   }
 })
 
