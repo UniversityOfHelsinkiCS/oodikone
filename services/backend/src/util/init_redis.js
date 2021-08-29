90ca7dc3ff0405c93c7e6aa6ede724b15aa8641d
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-08-29 10:01:46 +0300

    Remove bunch of unused utils from backend

--- services/backend/src/util/init_redis.js
+++
@@ -1,9 +0,0 @@
-const { redisClient } = require('../services/redis')
-
-const initializeDuplicates = async () => {
-  await redisClient.setAsync('duplicates', '{}')
-  console.log('Initiated redis')
-  process.exit()
-}
-
-initializeDuplicates()
