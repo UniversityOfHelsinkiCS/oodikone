b2a01c9dbf53b95cd28fffc81a2d1179a5c55551
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-09-13 17:23:44 +0300

    header fixes

--- services/backend/src/routes/feedback.js
+++ services/backend/src/routes/feedback.js
@@ -5,19 +5,21 @@ const logger = require('../util/logger')
 
 router.post('/email', async (req, res) => {
   const { content } = req.body
-  const { uid } = req.headers
-  const { email, full_name } = await userService.byUsername(uid)
+  const {
+    decodedToken: { userId },
+  } = req
+  const { email, full_name } = await userService.byUsername(userId)
 
   const result = await sendFeedbackToToska({
     feedbackContent: content,
-    userId: uid,
+    userId,
     userEmail: email,
     userFullName: full_name,
   })
   if (result.error) {
     return res.status(400).json(result).end()
   }
-  logger.info(`${uid} succesfully sent some feedback mail to toska`)
+  logger.info(`${userId} succesfully sent some feedback mail to toska`)
 
   res.status(200).json('success').end()
 })
