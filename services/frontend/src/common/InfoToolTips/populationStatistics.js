f83d626a031e522910fadc46add3e7fc8de4c276
Saara Satokangas <saarasat@gmail.com>
2021-09-21 13:29:32 +0300

    Fix info Population stats: coursePopulation

--- services/frontend/src/common/InfoToolTips/populationStatistics.js
+++ services/frontend/src/common/InfoToolTips/populationStatistics.js
@@ -59,6 +59,32 @@ export default {
   - **Study programme:** haluttu koulutusohjelma.
   - **Select tag (Optional):** itsetehty tai valmis tägi.
   - **Study track (Optional):** opintosuunta.`,
+  Students: `**Students**
+
+  Taulukko näyttää oletusarvoisesti vain opiskelijanumerot, mutta harmaata liukukytkintä klikkaamalla saa näkyviin opiskelijoiden nimet.
+  
+  **General**
+  
+  Klikkaamalla opiskelijanumeron vieressä olevaa sinistä nuolta, siirtyy "student statistic" näkymään valitun opiskelijan osalta.
+  
+  - **credits since start of studyright:**  listaa opiskelijan suoritetut opintopisteet valitun tarkastelujakson / populaation ajalta.
+  - **all credits:** listaa opiskelijan kaikki opintopisteet (myös aiemmat suoritukset, eli ne jotka tehty ennen valittuun tarkastelujaksoon ja populaatioon kuulumista)
+  - **transferred from:** opiskelijan vanha koulutusohjelma, josta opiskelija on siirtynyt uuteen.
+  - **studytracks:** opintosuunta
+  
+  **Mandatory Courses**
+  
+  Tällä välilehdellä näkyy, mitä pakollisiksi määriteltyjä kursseja opiskelija on suorittanut.
+  Kurssit voi määrittää kohdassa "Study program" > "Overview" > haluttu koulutusohjelma. Scrollatessa kurssitaulukkoa alaspäin, kurssin nimen saa näkyviin viemällä kursorin halutun solun kohdalle.
+  
+  **Tags**
+  
+  Opiskelijoille voi luoda tag:eja ryhmittelyä varten kohdassa "Study Program" > "Overview" > haluttu koulutusohjelma.
+  
+  **Download**
+  
+  Klikkaamalla tätä harmaata kuvaketta, saa ladattua .xlsx tiedoston, joka sisältää kaikki "General" ja "Mandatory Courses" välilehtien tiedot.
+  `,
   Filters: {
     Add: `Harmaata ”add” painiketta klikkaamalla aukeaa filters- eli suodatinvalikko, 
     josta voi lisätä haluamiaan suodattimia, jotka rajaavat valittua populaatiota.`,
