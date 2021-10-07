799a29fcac7d4caea87b2b1b6649529c8a3754d4
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-10-07 11:12:30 +0300

    Refactor userpage to use hooks instead of HOCs

--- services/frontend/src/selectors/users.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect'
-
-const getUsers = users => (!users.pending && !users.error ? users.data : [])
-
-export const sortUsers = users => users.sort((a, b) => a.full_name.localeCompare(b.full_name))
-
-export const makeSortUsers = () => createSelector(getUsers, sortUsers)
