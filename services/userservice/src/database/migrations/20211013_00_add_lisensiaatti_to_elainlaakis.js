ad351b7d732aac60503e7d579af5fdc3888eb0bd
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-10-13 09:54:14 +0300

    Connect eläinlääkis lisensiaatti to faculty

---
+++ services/userservice/src/database/migrations/20211013_00_add_lisensiaatti_to_elainlaakis.js
@@ -0,0 +1,8 @@
+module.exports = {
+  up: async queryInterface =>
+    queryInterface.sequelize.query(`
+      --- add lisensiaatti to lääkis faculty
+      INSERT INTO faculty_programmes VALUES ('H90', 'MH90_001', 'NOW()', 'NOW()');
+    `),
+  down: async () => {},
+}
