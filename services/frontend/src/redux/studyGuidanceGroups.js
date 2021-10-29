ee8013cba2ac8db4a7bd05ba938fd963f84dd959
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-10-29 13:38:50 +0300

    Add initial support for adding study guidance group programme & year in front

--- services/frontend/src/redux/studyGuidanceGroups.js
+++ services/frontend/src/redux/studyGuidanceGroups.js
@@ -1,13 +1,20 @@
 import { callController } from '../apiConnection'
 import itemreducer from './common/itemreducer'
 
-const prefix = 'GET_STUDY_GUIDANCE_GROUPS_'
+const getPrefix = 'GET_STUDY_GUIDANCE_GROUPS_'
+const changeTagsPrefix = 'CHANGE_STUDY_GUIDANCE_GROUP_TAGS_'
 
 export const getStudyGuidanceGroups = () => {
   const route = '/studyguidancegroups'
-  return callController(route, prefix)
+  return callController(route, getPrefix)
 }
 
-const reducer = itemreducer(prefix)
+export const changeStudyGuidanceGroupTags = (groupId, tags) => {
+  const route = `/studyguidancegroups/${groupId}/tags`
+  return callController(route, changeTagsPrefix, tags, 'put')
+}
 
-export default reducer
+const getReducer = itemreducer(getPrefix)
+const changeTagsReducer = itemreducer(changeTagsPrefix)
+
+export default { getReducer, changeTagsReducer }
