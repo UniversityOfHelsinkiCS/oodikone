1bb5ddba96b89c51fa3b1b1e752429f777436008
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-02 19:31:58 +0200

    Finalize form on frontend

--- services/frontend/src/redux/studyGuidanceGroups.js
+++ services/frontend/src/redux/studyGuidanceGroups.js
@@ -1,20 +1,80 @@
-import { callController } from '../apiConnection'
-import itemreducer from './common/itemreducer'
+import { callController, actionTypes } from '../apiConnection'
 
 const getPrefix = 'GET_STUDY_GUIDANCE_GROUPS_'
-const changeTagsPrefix = 'CHANGE_STUDY_GUIDANCE_GROUP_TAGS_'
+const getTypes = {
+  ...actionTypes(getPrefix),
+}
 
 export const getStudyGuidanceGroups = () => {
   const route = '/studyguidancegroups'
   return callController(route, getPrefix)
 }
 
+const changeTagsPrefix = 'CHANGE_STUDY_GUIDANCE_GROUP_TAGS_'
+const changeTagsTypes = {
+  ...actionTypes(changeTagsPrefix),
+}
+
 export const changeStudyGuidanceGroupTags = (groupId, tags) => {
   const route = `/studyguidancegroups/${groupId}/tags`
   return callController(route, changeTagsPrefix, tags, 'put')
 }
+const initialState = {
+  pending: false,
+  error: false,
+  data: [],
+}
 
-const getReducer = itemreducer(getPrefix)
-const changeTagsReducer = itemreducer(changeTagsPrefix)
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case getTypes.attempt:
+      return {
+        ...state,
+        error: false,
+        pending: true,
+      }
+    case getTypes.failure:
+      return {
+        ...state,
+        pending: false,
+        error: true,
+      }
+    case getTypes.success:
+      return {
+        ...state,
+        pending: false,
+        error: false,
+        data: action.response,
+      }
+    case changeTagsTypes.attempt:
+      return {
+        ...state,
+        error: false,
+        pending: false,
+      }
+    case changeTagsTypes.failure:
+      return {
+        ...state,
+        pending: false,
+        error: true,
+      }
+    case changeTagsTypes.success:
+      return {
+        ...state,
+        pending: false,
+        error: false,
+        data: state.data.map(group =>
+          group.id === action.response.studyGuidanceGroupId
+            ? {
+                ...state.data.find(group => group.id === action.response.studyGuidanceGroupId),
+                tags: action.response,
+              }
+            : group
+        ),
+      }
+    default:
+      return state
+  }
+}
 
-export default { getReducer, changeTagsReducer }
+export default reducer
