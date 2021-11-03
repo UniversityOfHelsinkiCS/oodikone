e702f5f6afb2083023c786cb7d7f2ea74c208b38
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-11-03 12:11:30 +0200

    Cleanup studyguidancegroup reducer

--- services/frontend/src/redux/studyGuidanceGroups.js
+++ services/frontend/src/redux/studyGuidanceGroups.js
@@ -1,23 +1,15 @@
+import { createReducer } from '@reduxjs/toolkit'
 import { callController, actionTypes } from '../apiConnection'
+
+const baseUrl = '/studyguidancegroups'
 
 const getPrefix = 'GET_STUDY_GUIDANCE_GROUPS_'
 const getTypes = {
   ...actionTypes(getPrefix),
 }
-
-export const getStudyGuidanceGroups = () => {
-  const route = '/studyguidancegroups'
-  return callController(route, getPrefix)
-}
-
 const changeTagsPrefix = 'CHANGE_STUDY_GUIDANCE_GROUP_TAGS_'
 const changeTagsTypes = {
   ...actionTypes(changeTagsPrefix),
-}
-
-export const changeStudyGuidanceGroupTags = (groupId, tags) => {
-  const route = `/studyguidancegroups/${groupId}/tags`
-  return callController(route, changeTagsPrefix, tags, 'put')
 }
 const initialState = {
   pending: false,
@@ -25,56 +17,46 @@ const initialState = {
   data: [],
 }
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case getTypes.attempt:
-      return {
-        ...state,
-        error: false,
-        pending: true,
-      }
-    case getTypes.failure:
-      return {
-        ...state,
-        pending: false,
-        error: true,
-      }
-    case getTypes.success:
-      return {
-        ...state,
-        pending: false,
-        error: false,
-        data: action.response,
-      }
-    case changeTagsTypes.attempt:
-      return {
-        ...state,
-        error: false,
-        pending: false,
-      }
-    case changeTagsTypes.failure:
-      return {
-        ...state,
-        pending: false,
-        error: true,
-      }
-    case changeTagsTypes.success:
-      return {
-        ...state,
-        pending: false,
-        error: false,
-        data: state.data.map(group =>
-          group.id === action.response.studyGuidanceGroupId
-            ? {
-                ...state.data.find(group => group.id === action.response.studyGuidanceGroupId),
-                tags: (({ studyGuidanceGroupId, ...rest }) => rest)(action.response), // omit id
-              }
-            : group
-        ),
-      }
-    default:
-      return state
-  }
-}
+export const getStudyGuidanceGroups = () => callController(baseUrl, getPrefix)
+
+export const changeStudyGuidanceGroupTags = (groupId, tags) =>
+  callController(`${baseUrl}/${groupId}/tags`, changeTagsPrefix, tags, 'put')
+
+const reducer = createReducer(
+  initialState,
+  {
+    [getTypes.attempt]: state => {
+      state.error = false
+      state.pending = true
+    },
+    [getTypes.failure]: state => {
+      state.error = true
+      state.pending = false
+    },
+    [getTypes.success]: (state, action) => {
+      state.error = false
+      state.pending = false
+      state.data = action.response
+    },
+    [changeTagsTypes.attempt]: state => {
+      state.error = false
+      state.pending = true
+    },
+    [changeTagsTypes.failure]: state => {
+      state.error = true
+      state.pending = false
+    },
+    [changeTagsTypes.success]: (state, action) => {
+      state.error = false
+      state.pending = false
+      state.data.find(group => group.id === action.response.studyGuidanceGroupId).tags = (({
+        studyGuidanceGroupId,
+        ...rest
+      }) => rest)(action.response)
+    },
+  },
+  [],
+  () => {}
+)
 
 export default reducer
