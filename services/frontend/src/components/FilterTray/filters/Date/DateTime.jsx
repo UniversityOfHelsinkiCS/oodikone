fc875d30b6e0de0fb7d72406752d1c0db7acaed0
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2021-11-11 13:24:23 +0200

    Allow clearing of the credit date filter dates

--- services/frontend/src/components/FilterTray/filters/Date/DateTime.jsx
+++ services/frontend/src/components/FilterTray/filters/Date/DateTime.jsx
@@ -2,11 +2,38 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Datetime from 'react-datetime'
 import 'moment/locale/fi'
-import DateButton from './DateButton'
+import { Icon, Button } from 'semantic-ui-react'
+import moment from 'moment'
 
 const DateTime = ({ value, onChange }) => {
   return (
-    <Datetime value={value} onChange={onChange} timeFormat={false} locale="fi" closeOnSelect renderInput={DateButton} />
+    <Datetime
+      value={value}
+      onChange={onChange}
+      timeFormat={false}
+      locale="fi"
+      closeOnSelect
+      renderInput={(_, open) => (
+        <Button
+          icon={value !== null}
+          labelPosition={value !== null && 'right'}
+          onClick={open}
+          className="credit-date-filter-input"
+          style={{ whiteSpace: 'nowrap' }}
+        >
+          {value === null ? 'Select Date' : moment(value).format('DD.MM.YYYY')}
+          {value !== null && (
+            <Icon
+              name="x"
+              onClick={evt => {
+                evt.stopPropagation()
+                onChange(null)
+              }}
+            />
+          )}
+        </Button>
+      )}
+    />
   )
 }
 
