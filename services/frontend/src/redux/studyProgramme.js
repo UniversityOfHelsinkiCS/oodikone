4c096199d27bc7fabf650950bb29fda384118cd9
Saara Satokangas <saarasat@gmail.com>
2021-12-01 20:29:51 +0200

    Start using RTK Query for new studyprogramme overview

--- services/frontend/src/redux/studyProgramme.js
+++ services/frontend/src/redux/studyProgramme.js
@@ -1,42 +1,18 @@
-import { createReducer } from '@reduxjs/toolkit'
-import { callController, actionTypes } from '../apiConnection'
-import { initialState, matcherReducers, defaultReducer } from './common'
+import { RTKApi } from 'apiConnection'
 
-const baseUrl = '/v2/studyprogrammes'
-const getBasicsPrefix = 'GET_STUDY_PROGRAMME_BASIC_STATS_'
-const getBasics = {
-  ...actionTypes(getBasicsPrefix),
-}
+const studyprogrammeApi = RTKApi.injectEndpoints({
+  endpoints: builder => ({
+    getBasicStats: builder.query({
+      query: ({ id }) => `/v2/studyprogrammes/${id}/basicstats`,
+    }),
+    getCreditStats: builder.query({
+      query: ({ id }) => `/v2/studyprogrammes/${id}/creditstats`,
+    }),
+    getGraduationStats: builder.query({
+      query: ({ id }) => `/v2/studyprogrammes/${id}/graduationstats`,
+    }),
+  }),
+  overrideExisting: false,
+})
 
-const getCreditsPrefix = 'GET_STUDY_PROGRAMME_CREDIT_STATS_'
-const getCredits = {
-  ...actionTypes(getCreditsPrefix),
-}
-
-const getGraduationPrefix = 'GET_STUDY_PROGRAMME_GRADUATION_STATS_'
-const getGraduation = {
-  ...actionTypes(getGraduationPrefix),
-}
-
-export const getBasicStats = id => callController(`${baseUrl}/${id}/basicstats`, getBasicsPrefix)
-export const getCreditStats = id => callController(`${baseUrl}/${id}/creditstats`, getCreditsPrefix)
-export const getGraduationStats = id => callController(`${baseUrl}/${id}/graduationstats`, getGraduationPrefix)
-
-const reducer = createReducer(
-  initialState,
-  {
-    [getBasics.success]: (state, action) => {
-      state.basicStats = action.response
-    },
-    [getCredits.success]: (state, action) => {
-      state.creditStats = action.response
-    },
-    [getGraduation.success]: (state, action) => {
-      state.graduationStats = action.response
-    },
-  },
-  matcherReducers,
-  defaultReducer
-)
-
-export default reducer
+export const { useGetBasicStatsQuery, useGetCreditStatsQuery, useGetGraduationStatsQuery } = studyprogrammeApi
