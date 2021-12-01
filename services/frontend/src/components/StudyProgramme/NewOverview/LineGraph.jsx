5f5423971cdecb454f10f1e9bfd7c964908381a1
Saara Satokangas <saarasat@gmail.com>
2021-12-01 09:16:20 +0200

    Add colors to new overview charts

--- services/frontend/src/components/StudyProgramme/NewOverview/LineGraph.jsx
+++ services/frontend/src/components/StudyProgramme/NewOverview/LineGraph.jsx
@@ -4,12 +4,16 @@ import '../studyprogramme.css'
 
 const ReactHighcharts = require('react-highcharts')
 
+const colors = ['#003E65', '#1392c2', '#E68825', '#333737', '#036415']
+
 const LineGraph = ({ categories, data }) => {
+  const dataWithColors = data.map((series, index) => ({ ...series, color: colors[index] }))
+
   const defaultConfig = {
     title: {
       text: '',
     },
-    series: data,
+    series: dataWithColors,
     credits: {
       text: 'oodikone | TOSKA',
     },
