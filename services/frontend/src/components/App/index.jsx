8fa6ce72e66bc163ac7b79a56ca5dfe5e3a35eb5
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-29 14:47:02 +0200

    Remove error container

--- services/frontend/src/components/App/index.jsx
+++ services/frontend/src/components/App/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { initShibbolethPinger } from 'unfuck-spa-shibboleth-session'
 import Header from '../Header'
-import ErrorContainer from '../ErrorContainer'
 import Routes from '../Routes'
 import './app.css'
 import { isDev } from '../../conf'
@@ -17,7 +16,6 @@ const App = () => {
     <div className="appContainer">
       <main className="routeViewContainer">
         <Header />
-        <ErrorContainer />
         <Routes />
       </main>
     </div>
