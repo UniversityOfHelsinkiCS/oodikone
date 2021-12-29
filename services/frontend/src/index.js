3370c8e9f338776bf564c375a9fd6986de9219e0
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-29 14:42:45 +0200

    Unify index and app setup with toskaboiler

--- services/frontend/src/index.js
+++ services/frontend/src/index.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import TSA from './common/tsa'
 import 'semantic-ui-css/semantic.min.css'
 import 'react-datetime/css/react-datetime.css'
 import './styles/custom.css'
 import store from './redux'
 import ErrorBoundary from './components/ErrorBoundary'
-import Main from './components/Main'
+import App from './components/App'
 import CommonContext from './CommonContext'
 import initializeSentry from './util/sentry'
+import { basePath } from './conf'
 
 initializeSentry()
+
+const history = TSA.Matomo.connectToHistory(createBrowserHistory())
 
 ReactDOM.render(
   <Provider store={store}>
     <CommonContext>
-      <ErrorBoundary>
-        <Main />
-      </ErrorBoundary>
+      <Router basename={basePath} history={history}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Router>
     </CommonContext>
   </Provider>,
   document.getElementById('root')
