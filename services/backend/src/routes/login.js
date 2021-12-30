81688cef4c266470d49825b54261999db5e39eac
Otto Ahoniemi <otto@ottoahoniemi.fi>
2021-12-30 17:41:02 +0200

    Refactor sending mails to users

--- services/backend/src/routes/login.js
+++ services/backend/src/routes/login.js
@@ -12,15 +12,5 @@ router.get('/login', async (req, res) => {
     token: decodedToken,
     logoutUrl,
   })
-
-  // handle the isNew thingy and mail sending somewhere!
-  // let { token, isNew } = await userService.login(uid, full_name, hyGroups, affiliations, mail, hyPersonSisuId)
-  // if (isNew) {
-  //   const result = await sendNotificationAboutNewUser({ userId: uid, userFullName: full_name })
-  //   if (result.error) {
-  //     return res.status(400).json(result).end()
-  //   }
-  // }
-  // res.status(200).json({ token })
 })
 module.exports = router
