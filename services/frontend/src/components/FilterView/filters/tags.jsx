dd7027867b8543366e343c37d5b9da90090ab87d
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2022-01-02 23:06:49 +0200

    Display a message when opening the tags filter and no tags are available

--- services/frontend/src/components/FilterView/filters/tags.jsx
+++ services/frontend/src/components/FilterView/filters/tags.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, Dropdown } from 'semantic-ui-react'
+import { Form, Dropdown, Message } from 'semantic-ui-react'
 import fp from 'lodash/fp'
 import createFilter from './createFilter'
 
@@ -14,7 +14,13 @@ const TagsFilterCard = ({ options, onOptionsChange, withoutSelf }) => {
     value: tag,
   }))
 
-  if (Object.entries(countsByTag).length === 0) return null
+  if (Object.entries(countsByTag).length === 0) {
+    return (
+      <Message color="orange" size="tiny">
+        No tags have been defined for any of the selected students.
+      </Message>
+    );
+  }
 
   return (
     <div className="card-content">
