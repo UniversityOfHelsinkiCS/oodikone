3bed182e83be110051e3054b14414cb2b272d4cf
Petri Palmu <petrimus76@gmail.com>
2022-01-06 20:21:34 +0200

    added bscs regex in code sorting order

--- services/backend/src/util/utils.js
+++ services/backend/src/util/utils.js
@@ -12,13 +12,14 @@ const mapToProviders = elementDetails =>
   })
 
 // sort substitutions so that main code is first
-const newLetterBasedCode = /^[A-Za-z]{3,}/ // new letter based codes come first
+const newLetterBasedCode = /^[A-Za-z]/ // new letter based codes come first
 const oldNumericCode = /^\d/ // old numeric codes come second
 const openUniCode = /^AY?(.+?)(?:en|fi|sv)?$/ // open university codes come last
 const openUniCodeA = /A\d/ // open university with just A come last
 const digi = /DIGI-A?(.+?)(?:en|fi|sv)?$/ // digi-a goes on top courses goes third
+const bscsCode = /BSCS??/
 
-const codeRegexes = [openUniCodeA, openUniCode, oldNumericCode, newLetterBasedCode, digi]
+const codeRegexes = [openUniCodeA, openUniCode, bscsCode, oldNumericCode, newLetterBasedCode, digi]
 
 const getSortRank = code => {
   for (let i = 0; i < codeRegexes.length; i++) {
