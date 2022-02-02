a17af20323657111fbc27d2277671742ee6cb52f
Saara Satokangas <saarasat@gmail.com>
2022-02-02 07:49:06 +0200

    Increase sequelize acquire-timeout threshold closer to the default

--- services/backend/src/database/connection.js
+++ services/backend/src/database/connection.js
@@ -16,7 +16,7 @@ class DbConnection extends EventEmitter {
       pool: {
         max: 25,
         min: 0,
-        acquire: 20000,
+        acquire: 30000,
         idle: 300000000,
       },
       logging: false,
