1e940fde735ec7385cad0f8323e87a13810b86bb
Saara Satokangas <saarasat@gmail.com>
2022-02-14 08:31:55 +0200

    Remove old infotooltips

--- services/frontend/src/common/InfoToolTips/index.jsx
+++ services/frontend/src/common/InfoToolTips/index.jsx
@@ -1,5 +1,4 @@
 import PopulationStatistics from './populationStatistics'
-import PopulationOverview from './populationOverview'
 import CoolDataScience from './coolDataScience'
 import CourseStatistics from './courseStatistics'
 import CoursePopulation from './coursePopulation'
@@ -7,7 +6,6 @@ import Studyprogramme from './studyprogramme'
 
 export default {
   PopulationStatistics,
-  PopulationOverview,
   CoolDataScience,
   CourseStatistics,
   CoursePopulation,
