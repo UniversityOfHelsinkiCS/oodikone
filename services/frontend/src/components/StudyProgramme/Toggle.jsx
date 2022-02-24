0ece6f0172bfa7c8e2da6f7276919f4efe9aa52e
Saara Satokangas <saarasat@gmail.com>
2022-02-24 15:00:03 +0200

    Fix to toolTip-texts

--- services/frontend/src/components/StudyProgramme/Toggle.jsx
+++ services/frontend/src/components/StudyProgramme/Toggle.jsx
@@ -3,12 +3,12 @@ import { Radio } from 'semantic-ui-react'
 
 import WithHelpTooltip from '../Info/InfoWithHelpTooltip'
 
-const Toggle = ({ cypress, toolTip, firstLabel, secondLabel, value, setValue }) => (
+const Toggle = ({ cypress, toolTips, firstLabel, secondLabel, value, setValue }) => (
   <div className="radio-toggle">
     <label className={`toggle-label${value ? '-checked' : ''}`}>{firstLabel}</label>
     <Radio data-cy={cypress} toggle checked={value} onChange={() => setValue(!value)} />
-    {toolTip ? (
-      <WithHelpTooltip tooltip={{ short: toolTip }}>
+    {toolTips ? (
+      <WithHelpTooltip tooltip={{ short: toolTips }}>
         <label className={`toggle-label${value ? '' : '-checked'}`}>{secondLabel}</label>
       </WithHelpTooltip>
     ) : (
