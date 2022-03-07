dde7bfb5db4d83f610243ea0ffd88cc61f3a6768
Saara Satokangas <saarasat@gmail.com>
2022-03-07 15:43:45 +0200

    Remove thesisCourse-reducer and backend-logic
    These are redundant after the new studyprogramme overview

--- services/backend/src/routes/elementdetails.js
+++ services/backend/src/routes/elementdetails.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const MandatoryCourses = require('../services/mandatoryCourses')
-const { findProgrammeTheses, createThesisCourse, deleteThesisCourse } = require('../services/thesis')
 const { getAllProgrammes, getAllElementDetails } = require('../services/studyrights')
 
 router.get('/elementdetails/all', async (req, res) => {
@@ -17,37 +16,6 @@ router.get('/v2/studyprogrammes/:id/mandatory_courses', async (req, res) => {
   if (req.params.id) {
     const codes = await MandatoryCourses.byStudyprogramme(req.params.id)
     res.json(codes)
-  } else {
-    res.status(422).end()
-  }
-})
-
-router.get('/v2/studyprogrammes/:id/thesis', async (req, res) => {
-  const { id } = req.params
-  if (id) {
-    const thesis = await findProgrammeTheses(id)
-    res.json(thesis)
-  } else {
-    res.status(422).end()
-  }
-})
-
-router.post('/v2/studyprogrammes/:id/thesis', async (req, res) => {
-  const { id } = req.params
-  const { course, thesisType } = req.body
-  if (id && course && thesisType) {
-    const thesis = await createThesisCourse(id, course, thesisType)
-    res.status(201).json(thesis)
-  } else {
-    res.status(422).end()
-  }
-})
-
-router.delete('/v2/studyprogrammes/:id/thesis/:course', async (req, res) => {
-  const { id, course } = req.params
-  if (id && course) {
-    const deleted = await deleteThesisCourse(id, course)
-    res.status(204).json(deleted)
   } else {
     res.status(422).end()
   }
