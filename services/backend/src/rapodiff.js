b7a2d67bcb1d12da8618b01a8be43aea365dbb03
Matti Luukkainen <mluukkai@iki.fi>
2022-03-15 17:41:45 +0200

    rapo diff script betterments

--- services/backend/src/rapodiff.js
+++ services/backend/src/rapodiff.js
@@ -10,9 +10,35 @@ const { optimizedStatisticsOf } = require('./services/populations')
 
 const token = process.env.RAPO_NODEPROXY
 
-const programme_diff = async (programme, facultyCode, year) => {
+const getFromRapo = async (urlStart, urlEnd) => {
+  const { data: rapoStuffLasna } = await axios.get(urlStart)
+  const { data: rapoStuffLasnaMinus } = await axios.get(urlEnd)
+
+  const rapoStudents = _.difference(
+    rapoStuffLasna.map(s => s.opiskelijanumero),
+    rapoStuffLasnaMinus.map(s => s.opiskelijanumero)
+  )
+
+  return rapoStudents
+}
+
+const getFromOodikone = async (programme, year) => {
+  const theStudyright = student => {
+    const theOne = s => s.studyright_elements.find(e => e.code === programme)
+    return student.studyrights.find(theOne)
+  }
+
+  const notGraduated = student => {
+    const right = theStudyright(student)
+    return right.graduated !== 1
+  }
+
+  const notTransferred = student => {
+    const transfer = student.transfers.find(t => t.targetcode === programme)
+    return transfer === undefined
+  }
+
   const months = 12
-  const studyrightStartDate = `${year}-08-01`
 
   const oodikoneQuery = {
     semesters: ['FALL', 'SPRING'],
@@ -20,32 +46,70 @@ const programme_diff = async (programme, facultyCode, year) => {
     studyRights: { programme },
     year,
   }
-  const rapoUrl = presence =>
+
+  const oodikoneStuff = (await optimizedStatisticsOf(oodikoneQuery)).students
+
+  return oodikoneStuff
+    .filter(notGraduated)
+    .filter(notTransferred)
+    .map(s => s.studentNumber)
+}
+
+const programme_diff = async (programme, facultyCode, year) => {
+  const oodikoneStudents = await getFromOodikone(programme, year)
+
+  const studyrightStartDate = `${year}-08-01`
+  const studyrightEndDate = `${year + 1}-08-01`
+
+  const rapoUrlStart = presence =>
     encodeURI(
       `https://importer.cs.helsinki.fi/test/rapo/${facultyCode}/${programme}/${studyrightStartDate}/${presence}?token=${token}`
     )
+  const rapoUrlEnd = presence =>
+    encodeURI(
+      `https://importer.cs.helsinki.fi/test/rapo/${facultyCode}/${programme}/${studyrightEndDate}/${presence}?token=${token}`
+    )
 
-  const oodikoneStuff = await optimizedStatisticsOf(oodikoneQuery)
-  const oodikoneStudents = oodikoneStuff.students.map(s => s.studentNumber)
+  const rapoStudentsLasna = await getFromRapo(rapoUrlStart('Läsnä'), rapoUrlEnd('Läsnä'))
+  const rapoStudentsPoissa = await getFromRapo(rapoUrlStart('Poissa'), rapoUrlEnd('Poissa'))
+  const rapoStudentPoissaLaki = await getFromRapo(
+    rapoUrlStart('Poissa ei kuluta opintoaikaa'),
+    rapoUrlEnd('Poissa ei kuluta opintoaikaa')
+  )
 
-  const { data: rapoStuffLasna } = await axios.get(rapoUrl('Läsnä'))
-  const { data: rapoStuffPoissa } = await axios.get(rapoUrl('Poissa'))
-
-  console.log(rapoStuffLasna.length, rapoStuffPoissa.length)
-
-  const rapoStudents = rapoStuffLasna.map(s => s.opiskelijanumero).concat(rapoStuffPoissa.map(s => s.opiskelijanumero))
-
-  console.log(rapoStudents.length, oodikoneStudents.length)
+  const rapoStudents = rapoStudentsLasna.concat(rapoStudentsPoissa).concat(rapoStudentPoissaLaki)
 
   const onlyInOodikone = _.difference(oodikoneStudents, rapoStudents)
   const onlyInRapo = _.difference(rapoStudents, oodikoneStudents)
 
-  console.log('Only in Oodikone:', onlyInOodikone)
-  console.log('Only in Rapo:', onlyInRapo)
+  console.log('vuosi', year)
+
+  console.log('')
+  console.log('oodikone', oodikoneStudents.length)
+  console.log('rapo', rapoStudents.length)
+  console.log('only oodikone', onlyInOodikone.length)
+  console.log('only rapo', onlyInRapo.length)
+  console.log('')
+  /*
+  console.log('Only in Oodikone:')
+  console.log(onlyInOodikone.join('\n'))
+  console.log('Only in Rapo:')
+  console.log(onlyInRapo.join('\n'))
+  console.log('')
+  */
 }
 
 const main = async () => {
+  await programme_diff('KH50_005', 'H50', 2017)
+  console.log(' ')
+  await programme_diff('KH50_005', 'H50', 2018)
+  console.log(' ')
+  await programme_diff('KH50_005', 'H50', 2019)
+  console.log(' ')
+  await programme_diff('KH50_005', 'H50', 2020)
+  console.log(' ')
   await programme_diff('KH50_005', 'H50', 2021)
+  console.log(' ')
 }
 
 main().then(() => process.exit(0))
