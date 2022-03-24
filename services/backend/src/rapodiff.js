bf3b200a6c12824a890fa0401ae480c4f05dc904
Matti Luukkainen <mluukkai@iki.fi>
2022-03-24 17:12:45 +0200

    eslint igonre

--- services/backend/src/rapodiff.js
+++ services/backend/src/rapodiff.js
@@ -5,7 +5,7 @@
   needs also acces to importer db api, uses env IMPORTER_API
 
   known differences in APIs
-    - rapo does not show students that have graduated
+    - rapo does not show students that have graduated (it however takes some days for rapo to notice a graduation)
     - rapo does not handle the starting year of transfers properly (shows in old staring population)
     - rapo show some students incorrectly passive, see https://github.com/UniversityOfHelsinkiCS/oodikone/issues/3521
 */
@@ -15,7 +15,7 @@ const axios = require('axios').default
 const _ = require('lodash')
 
 const { optimizedStatisticsOf } = require('./services/populations')
-const { Student, Transfer } = require('./models')
+const { Student, Transfer, StudyrightElement, Studyright } = require('./models')
 
 const token = process.env.RAPO_NODEPROXY
 
@@ -31,15 +31,32 @@ const getFromRapo = async (urlStart, urlEnd) => {
   return rapoStudents
 }
 
+const known_problems_in_oodikone = {
+  KH30_001: ['013896191'],
+  KH40_003: ['013200457'],
+  KH60_001: ['014984109', '014855814', '014865260', '014967537'],
+  KH55_001: ['013195834', '015119708', '015138336'],
+}
+
+const known_problems_in_rapo = {
+  KH55_001: ['015160142', '015135575', '015119708', '015138336'],
+}
+
 const getFromOodikone = async (programme, year) => {
-  const theStudyright = student => {
+  const isNotAmongKnownProblems = student => {
+    const hasProblems = known_problems_in_oodikone[programme] ? known_problems_in_oodikone[programme] : []
+    return !hasProblems.includes(student.studentNumber)
+  }
+
+  const theStudyrights = student => {
     const theOne = s => s.studyright_elements.find(e => e.code === programme)
-    return student.studyrights.find(theOne)
+    // in some rare cases student may have more than one studyright for same programme
+    return student.studyrights.filter(theOne)
   }
 
   const notGraduated = student => {
-    const right = theStudyright(student)
-    return right.graduated !== 1
+    const graduation = theStudyrights(student).filter(r => r.graduated === 1)
+    return graduation.length === 0
   }
 
   const notTransferred = student => {
@@ -61,6 +78,7 @@ const getFromOodikone = async (programme, year) => {
   return oodikoneStuff
     .filter(notGraduated)
     .filter(notTransferred)
+    .filter(isNotAmongKnownProblems)
     .map(s => s.studentNumber)
 }
 
@@ -72,7 +90,8 @@ const isAcuallyActive = async student => {
   const { data } = await axios.get(url)
   const terms = data.reduce((all, s) => all.concat(s.terms), []).filter(t => t.semester_code === currentSemester)
   const active = terms.filter(t => t.type !== 'NEGLECTED')
-  return active.length > 0
+  const negl = terms.filter(t => t.type === 'NEGLECTED')
+  return active.length > 0 && negl.length > 0
 }
 
 const hasTransferred = async (student, programme) => {
@@ -88,7 +107,158 @@ const hasTransferred = async (student, programme) => {
   return transferTo !== undefined
 }
 
+const graduatedVeryRecently = async (student, programme) => {
+  const data = await Student.findByPk(student, {
+    include: [
+      {
+        model: Studyright,
+        include: [
+          {
+            model: StudyrightElement,
+            required: true,
+            where: {
+              code: programme,
+            },
+          },
+        ],
+        where: {
+          graduated: 1,
+        },
+      },
+    ],
+  })
+
+  if (!data || data.studyrights.length === 0) {
+    return false
+  }
+
+  const graduated = new Date(data.studyrights[0].enddate)
+  const weekAgo = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000)
+  return graduated > weekAgo
+}
+
 const programme_diff_year = async (programme, facultyCode, year, brief = false) => {
+  const oodikoneStudents = await getFromOodikone(programme, year)
+
+  const studyrightStartDate = `${year}-08-01`
+  const studyrightEndDate = `${year + 1}-08-01`
+
+  const rapoUrlStart = presence =>
+    encodeURI(
+      `https://importer.cs.helsinki.fi/test/rapo/${facultyCode}/${programme}/${studyrightStartDate}/${presence}?token=${token}`
+    )
+  const rapoUrlEnd = presence =>
+    encodeURI(
+      `https://importer.cs.helsinki.fi/test/rapo/${facultyCode}/${programme}/${studyrightEndDate}/${presence}?token=${token}`
+    )
+
+  const rapoStudentsLasna = await getFromRapo(rapoUrlStart('Läsnä'), rapoUrlEnd('Läsnä'))
+  const rapoStudentsPoissa = await getFromRapo(rapoUrlStart('Poissa'), rapoUrlEnd('Poissa'))
+  const rapoStudentPoissaLaki = await getFromRapo(
+    rapoUrlStart('Poissa ei kuluta opintoaikaa'),
+    rapoUrlEnd('Poissa ei kuluta opintoaikaa')
+  )
+
+  const isNotAmongKnownProblems = student => {
+    const hasProblems = known_problems_in_rapo[programme] ? known_problems_in_rapo[programme] : []
+    return !hasProblems.includes(student.studentNumber)
+  }
+
+  const rapoStudents = rapoStudentsLasna
+    .concat(rapoStudentsPoissa)
+    .concat(rapoStudentPoissaLaki)
+    .filter(isNotAmongKnownProblems)
+
+  const onlyInOodikone = _.difference(oodikoneStudents, rapoStudents)
+  const onlyInRapo = _.difference(rapoStudents, oodikoneStudents)
+
+  const wrongMissingInRapo = []
+  for (let student of onlyInOodikone) {
+    const status = await isAcuallyActive(student)
+    if (status) {
+      wrongMissingInRapo.push(student)
+    }
+  }
+
+  // these are in wrong RAPO-population
+  const transferred = []
+  for (let student of onlyInRapo) {
+    const isTransfer = await hasTransferred(student, programme)
+    if (isTransfer) {
+      transferred.push(student)
+    } else {
+      const isRecentGraduation = await graduatedVeryRecently(student, programme)
+      if (isRecentGraduation) {
+        transferred.push(student)
+      }
+    }
+  }
+
+  const checkOodikoneOnly = _.difference(onlyInOodikone, wrongMissingInRapo)
+  const checkRapoOnly = _.difference(onlyInRapo, transferred)
+
+  if (!brief) {
+    console.log('vuosi', year)
+    console.log('')
+    console.log('oodikone', oodikoneStudents.length, ' transferred', transferred.length)
+    console.log('rapo', rapoStudents.length, ' activeness wrong rapo', wrongMissingInRapo.length)
+    console.log('only rapo', checkRapoOnly.length)
+    console.log('only oodikone', checkOodikoneOnly.length)
+
+    console.log('')
+
+    if (checkRapoOnly.length > 0) {
+      console.log('Only in Rapo:')
+      console.log(checkRapoOnly.join('\n'))
+    }
+
+    if (checkOodikoneOnly.length > 0) {
+      console.log('Only in Oodikone:')
+      console.log(checkOodikoneOnly.join('\n'))
+    }
+  }
+
+  if (checkRapoOnly.length + checkOodikoneOnly.length) {
+    console.log('******************************', year)
+    if (checkRapoOnly.length > 0) {
+      console.log('Only in Rapo:')
+      console.log(checkRapoOnly.join('\n'))
+    }
+
+    if (checkOodikoneOnly.length > 0) {
+      console.log('Only in Oodikone:')
+      console.log(checkOodikoneOnly.join('\n'))
+    }
+  }
+
+  if (!brief) {
+    console.log('')
+  }
+}
+
+const programme_diff = async (programme, faculty, brief) => {
+  console.log()
+  console.log(programme)
+  console.log('--------', '\n')
+
+  await programme_diff_year(programme, faculty, 2017, brief)
+  await programme_diff_year(programme, faculty, 2018, brief)
+  await programme_diff_year(programme, faculty, 2019, brief)
+  await programme_diff_year(programme, faculty, 2020, brief)
+  await programme_diff_year(programme, faculty, 2021, brief)
+}
+
+const faculty_diff = async (faculty, programmes, brief) => {
+  console.log('\n')
+  console.log(faculty)
+  console.log('==')
+  for (let programme of programmes) {
+    await programme_diff(programme, faculty, brief)
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const msc_programme_diff_year = async (programme, facultyCode, year, brief = false) => {
   const oodikoneStudents = await getFromOodikone(programme, year)
 
   const studyrightStartDate = `${year}-08-01`
@@ -119,10 +289,11 @@ const programme_diff_year = async (programme, facultyCode, year, brief = false) 
   for (let student of onlyInOodikone) {
     const status = await isAcuallyActive(student)
     if (status) {
-      wrongInRapo.push(student)
+      //wrongInRapo.push(student)
     }
   }
 
+  // in wrong RAPO population
   const transferred = []
   for (let student of onlyInRapo) {
     const status = await hasTransferred(student, programme)
@@ -132,7 +303,8 @@ const programme_diff_year = async (programme, facultyCode, year, brief = false) 
   }
 
   const checkOodikoneOnly = _.difference(onlyInOodikone, wrongInRapo)
-  const checkRapoOnly = _.difference(onlyInOodikone, wrongInRapo)
+  const checkRapoOnly = _.difference(onlyInRapo, transferred)
+  const both = _.intersection(oodikoneStudents, rapoStudents)
 
   console.log('vuosi', year)
 
@@ -140,6 +312,7 @@ const programme_diff_year = async (programme, facultyCode, year, brief = false) 
     console.log('')
     console.log('oodikone', oodikoneStudents.length, ' transferred', transferred.length)
     console.log('rapo', rapoStudents.length, ' activeness wrong rapo', wrongInRapo.length)
+    console.log('both', both.length)
     console.log('only rapo', checkRapoOnly.length)
     console.log('only oodikone', checkOodikoneOnly.length)
 
@@ -165,26 +338,7 @@ const programme_diff_year = async (programme, facultyCode, year, brief = false) 
   }
 }
 
-const programme_diff = async (programme, faculty, brief) => {
-  console.log('\n', programme)
-  console.log('--------', '\n')
-
-  await programme_diff_year(programme, faculty, 2017, brief)
-  await programme_diff_year(programme, faculty, 2018, brief)
-  await programme_diff_year(programme, faculty, 2019, brief)
-  await programme_diff_year(programme, faculty, 2020, brief)
-  await programme_diff_year(programme, faculty, 2021, brief)
-}
-
-const faculty_diff = async (faculty, programmes, brief) => {
-  console.log('\n', faculty)
-  console.log('==')
-  for (let programme of programmes) {
-    await programme_diff(programme, faculty, brief)
-  }
-}
-
-const priogrammes_of_faculties = {
+const bsc_priogrammes_of_faculties = {
   H10: ['KH10_001'],
   H20: ['KH20_001'],
   H30: ['KH30_001', 'KH30_002'],
@@ -199,9 +353,12 @@ const priogrammes_of_faculties = {
 }
 
 const main = async () => {
-  for (let faculty of Object.keys(priogrammes_of_faculties)) {
-    await faculty_diff(faculty, priogrammes_of_faculties[faculty], true)
+  for (let faculty of Object.keys(bsc_priogrammes_of_faculties)) {
+    await faculty_diff(faculty, bsc_priogrammes_of_faculties[faculty], true)
   }
+
+  //await programme_diff_year('KH55_001', 'H55', 2021, true)
+  //await programme_diff('KH55_001', 'H55', true)
 }
 
 main().then(() => process.exit(0))
--- services/backend/src/services/populations.js
+++ services/backend/src/services/populations.js
@@ -454,7 +454,7 @@ const studentnumbersWithAllStudyrightElements = async (
   // - filter out all students who have enrolled for both semesters but whose studyright has ended
   // - don't use above filtering to graduated students
   // eslint-disable-next-line no-constant-condition
-  if (false && !cancelledStudents) {
+  if (!cancelledStudents) {
     const { semestercode } = await getCurrentSemester()
 
     const enrolments = await SemesterEnrollment.findAll({
