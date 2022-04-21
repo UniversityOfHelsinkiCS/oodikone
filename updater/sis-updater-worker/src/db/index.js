5dc725a9b9a3dd73dfefb8e1fa4727325405dd24
SNurmivaara <sami.nurmivaara@helsinki.fi>
2022-04-21 12:36:54 +0300

    Add granularity to updater sentry fingerprints

--- updater/sis-updater-worker/src/db/index.js
+++ updater/sis-updater-worker/src/db/index.js
@@ -60,7 +60,11 @@ const bulkCreate = async (model, entities, transaction = null, properties = ['id
     })
   } catch (e) {
     if (entities.length === 1) {
-      logger.error({ message: e.message, meta: { stack: e.stack, entity: entities[0] } })
+      logger.error({
+        message: e.message,
+        meta: { stack: e.stack, entity: entities[0] },
+        fingerprint: ['{{ default }}', e.message],
+      })
       return
     }
 
