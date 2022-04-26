4a2bfe83621c3ef628a667221da3fb77fd9fbef5
Petri Palmu <petrimus76@gmail.com>
2022-04-26 09:53:00 +0300

    study programme courses rtk-api connected to backend

--- services/frontend/src/components/StudyProgramme/programmeCoursesOverview/index.js
+++ services/frontend/src/components/StudyProgramme/programmeCoursesOverview/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useGetProgrammeCoursesStatsQuery } from 'redux/studyProgramme'
+import { Loader } from 'semantic-ui-react'
+import Toggle from '../Toggle'
 
-const ProgrammeCoursesOverview = () => {
+const ProgrammeCoursesOverview = ({ studyProgramme, academicYear, setAcademicYear }) => {
+  const { /* data */ error, isLoading } = useGetProgrammeCoursesStatsQuery({
+    id: studyProgramme,
+    academicyear: academicYear,
+  })
+
+  if (error) return <h3>Something went wrong, please try refreshing the page.</h3>
+
+  if (isLoading) {
+    return <Loader active style={{ marginTop: '10em' }} />
+  }
+
   return (
-    <div>
-      <strong>kissa</strong>
+    <div className="studyprogramme-courses">
+      <div className="toggle-container">
+        <Toggle
+          cypress="YearToggle"
+          // toolTips={toolTips.YearToggle}
+          firstLabel="Calendar year"
+          secondLabel="Academic year"
+          value={academicYear}
+          setValue={setAcademicYear}
+        />
+      </div>
     </div>
   )
 }
