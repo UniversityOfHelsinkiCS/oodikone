8c9bd4c3e0fcc11ea956112c7487f08b6e8af854
Petri Palmu <petrimus76@gmail.com>
2022-05-09 12:16:18 +0300

    study programme couses time management betterments

--- services/backend/src/services/studyprogrammeCourses.js
+++ services/backend/src/services/studyprogrammeCourses.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+
 const { getStudentsForProgrammeCourses, getCurrentStudyYearStartDate } = require('./studyprogramme')
 const { mapToProviders } = require('../util/utils')
 
@@ -6,12 +7,13 @@ const getCurrentYearStartDate = () => {
   return new Date(new Date().getFullYear(), 0, 1)
 }
 
-const makeYearlyCreditsPromises = (currentYear, years, getRange, providerCode) => {
+const makeYearlyCreditsPromises = (years, providerCode, academicYear) => {
   return years.map(
     year =>
       new Promise(async res => {
-        const diff = currentYear - year
-        const { from, to } = getRange(diff)
+        const from = academicYear == 'true' ? new Date(year, 7, 1, 0, 0, 0) : new Date(year, 0, 1, 0, 0, 0)
+        const to = academicYear == 'true' ? new Date(year + 1, 6, 31, 23, 59, 59) : new Date(year, 11, 31, 23, 59, 59)
+
         const studentsByCourse = await getStudentsForProgrammeCourses(from, to, providerCode)
 
         res(
@@ -24,29 +26,13 @@ const makeYearlyCreditsPromises = (currentYear, years, getRange, providerCode) =
   )
 }
 
-const getStudyprogrammeCoursesForStudytrack = async (unixMillis, studyprogramme, showByYear) => {
-  const YEAR_TO_MILLISECONDS = 31556952000
+const getStudyprogrammeCoursesForStudytrack = async (unixMillis, studyprogramme, academicYear) => {
   const providerCode = mapToProviders([studyprogramme])[0]
-  const startDate = showByYear === 'true' ? getCurrentYearStartDate() : await getCurrentStudyYearStartDate(unixMillis)
+  const startDate = academicYear == 'true' ? await getCurrentStudyYearStartDate(unixMillis) : getCurrentYearStartDate()
   const startYear = startDate.getFullYear()
-  const startTime = startDate.getTime()
   const yearRange = _.range(2017, startYear + 1)
 
-  const getRangeAcc = diff => ({
-    from: new Date(startTime - diff * YEAR_TO_MILLISECONDS),
-    to:
-      showByYear === 'true'
-        ? new Date(startTime - (diff - 1) * YEAR_TO_MILLISECONDS)
-        : new Date(unixMillis - diff * YEAR_TO_MILLISECONDS),
-  })
-
-  /* const getRangeTotal = diff => ({
-    from: new Date(startTime - diff * YEAR_TO_MILLISECONDS),
-    to: new Date(startTime - (diff - 1) * YEAR_TO_MILLISECONDS),
-  }) */
-
-  const yearlyStudentByCoursePromises = makeYearlyCreditsPromises(startYear, yearRange, getRangeAcc, providerCode)
-  // console.log('yearly student by course promises: ', yearlyStudentByCoursePromises)
+  const yearlyStudentByCoursePromises = makeYearlyCreditsPromises(yearRange, providerCode, academicYear)
 
   const [yearlyStudentByCourse] = await Promise.all([Promise.all(yearlyStudentByCoursePromises)])
 
