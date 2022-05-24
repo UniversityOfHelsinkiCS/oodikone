aabcd750607746e5a6ac286d3132ba13fc444d0b
Roni Lindholm <roni.lindholm@helsinki.fi>
2022-05-24 15:17:25 +0300

    Add more detailed info about semester enrollments to student page
    Display also in credit graph the status of semester enrollment.

--- services/frontend/src/components/StudentStatistics/StudentInfoCard/EnrollmentAccordion.jsx
+++ services/frontend/src/components/StudentStatistics/StudentInfoCard/EnrollmentAccordion.jsx
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
-import { Icon, Accordion, Table } from 'semantic-ui-react'
+import { Icon, Accordion, Table, Popup } from 'semantic-ui-react'
 import { array } from 'prop-types'
 import _ from 'lodash'
 
 import './studentInfoCard.css'
 
 const enrolmentTypes = {
-  1: 'Present',
-  2: 'Absent',
-  3: 'Passive',
+  1: { text: 'Present', className: 'label-present' },
+  2: { text: 'Absent', className: 'label-absent' },
+  3: { text: 'Passive', className: 'label-passive' },
+  STATUTORY: { text: 'Absent, statutory', className: 'label-absent-statutory' },
+}
+const helpTexts = {
+  1: '',
+  2: 'The registration has been done and the student is not attending to the study term.',
+  3: 'The registration has not been done and the student is counted as passive.',
+  STATUTORY: 'This absence is statutory e.g. parental leave or military service',
 }
 const curDate = new Date()
 
+const renderSemester = ({ enrollmenttype, statutoryAbsence }) => {
+  const { text, className } = statutoryAbsence ? enrolmentTypes.STATUTORY : enrolmentTypes[enrollmenttype]
+  const helpText = helpTexts[enrollmenttype] + (statutoryAbsence ? ` ${helpTexts.STATUTORY}` : '')
+  return (
+    <div className="enrollment-container">
+      {text}
+      <div className={`enrollment-label ${className}`}> </div>
+      {enrollmenttype > 1 ? (
+        <Popup content={helpText} trigger={<Icon className="help-icon" name="question circle outline" />} />
+      ) : null}
+    </div>
+  )
+}
+
 const getEnrollmentStatus = (semester, season) => {
   if (semester.name.en.includes('Spring') && season === 'SPRING') {
-    return enrolmentTypes[semester.enrollmenttype]
+    return renderSemester(semester)
   }
   if (semester.name.en.includes('Autumn') && season === 'FALL') {
-    return enrolmentTypes[semester.enrollmenttype]
+    return renderSemester(semester)
   }
   return 'Absent'
 }
