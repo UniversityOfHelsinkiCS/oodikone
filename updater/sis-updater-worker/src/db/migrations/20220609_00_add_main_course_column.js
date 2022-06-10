110ef2fe0b05fa87134b58c6db5a00d4a3e77fb9
Roni Lindholm <roni.lindholm@helsinki.fi>
2022-06-10 14:05:41 +0300

    Dummy commit to run migrations for :ano:

--- updater/sis-updater-worker/src/db/migrations/20220609_00_add_main_course_column.js
+++ updater/sis-updater-worker/src/db/migrations/20220609_00_add_main_course_column.js
@@ -4,7 +4,6 @@ module.exports = {
   up: async queryInterface => {
     await queryInterface.addColumn('course', 'main_course_code', TEXT)
   },
-
   down: async queryInterface => {
     await queryInterface.removeColumn('course', 'main_course_code')
   },
