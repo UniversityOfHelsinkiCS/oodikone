4cb57ffc5b9f2211dc049b2cb69e4bcca4db8d8a
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-07-04 18:49:24 +0300

    Show credits produced by faculty

--- services/frontend/src/components/FacultyStatistics/BasicOverview/index.jsx
+++ services/frontend/src/components/FacultyStatistics/BasicOverview/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-import { Divider } from 'semantic-ui-react'
+import { Divider, Loader } from 'semantic-ui-react'
+
+import { useGetCreditStatsQuery } from 'redux/facultyStats'
+import StackedBarChart from 'components/StudyProgramme/BasicOverview/StackedBarChart'
+import DataTable from 'components/StudyProgramme/BasicOverview/DataTable'
 import Toggle from '../../StudyProgramme/Toggle'
 import InfoBox from '../../Info/InfoBox'
 import InfotoolTips from '../../../common/InfoToolTips'
@@ -7,12 +11,9 @@ import '../faculty.css'
 
 const Overview = ({ faculty, academicYear, setAcademicYear, specialGroups, setSpecialGroups }) => {
   const toolTips = InfotoolTips.Studyprogramme
-  // const yearType = academicYear ? 'ACADEMIC_YEAR' : 'CALENDAR_YEAR'
-  // const special = specialGroups ? 'SPECIAL_EXCLUDED' : 'SPECIAL_INCLUDED'
-  if (!faculty) {
-    return null
-    // remove this
-  }
+  const yearType = academicYear ? 'ACADEMIC_YEAR' : 'CALENDAR_YEAR'
+  const special = specialGroups ? 'SPECIAL_EXCLUDED' : 'SPECIAL_INCLUDED'
+  const credits = useGetCreditStatsQuery({ id: faculty?.code, yearType, specialGroups: special })
 
   const getDivider = (title, toolTipText) => (
     <>
@@ -25,6 +26,11 @@ const Overview = ({ faculty, academicYear, setAcademicYear, specialGroups, setSp
       {/* <InfoBox content={toolTips[toolTipText]} /> */}
     </>
   )
+  const isFetchingOrLoading = credits.isLoading || credits.isFetching
+
+  const isError = credits.isError || (credits.isSuccess && !credits.data)
+
+  if (isError) return <h3>Something went wrong, please try refreshing the page.</h3>
 
   return (
     <div className="faculty-overview">
@@ -46,7 +52,30 @@ const Overview = ({ faculty, academicYear, setAcademicYear, specialGroups, setSp
           setValue={setSpecialGroups}
         />
       </div>
-      <>{getDivider('Credits produced by the faculty', 'CreditsProducedByTheFaculty')}</>
+
+      {isFetchingOrLoading ? (
+        <Loader active style={{ marginTop: '10em' }} />
+      ) : (
+        <>
+          {credits.isSuccess && credits.data && (
+            <>
+              {getDivider('Credits produced by the faculty', 'CreditsProducedByTheFaculty')}
+              <div className="section-container">
+                <StackedBarChart
+                  cypress="CreditsProducedByTheSFaculty"
+                  data={credits?.data?.graphStats}
+                  labels={credits?.data?.years}
+                />
+                <DataTable
+                  cypress="CreditsProducedByTheFaculty"
+                  data={credits?.data?.tableStats}
+                  titles={credits?.data?.titles}
+                />
+              </div>
+            </>
+          )}
+        </>
+      )}
     </div>
   )
 }
