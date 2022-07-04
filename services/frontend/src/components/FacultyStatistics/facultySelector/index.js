eb2d5277ed1fbf413ef18d331d4bbf8e79a62a6e
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-07-04 12:47:51 +0300

    Add faculty selector

---
+++ services/frontend/src/components/FacultyStatistics/facultySelector/index.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { debounce } from 'lodash'
+import { connect } from 'react-redux'
+import { Loader, Message, Header, Form } from 'semantic-ui-react'
+import { getTextIn } from '../../../common'
+import SortableTable from '../../SortableTable'
+
+const FacultySelector = ({ faculties, selected, language }) => {
+  const [filter, setFilter] = useState('')
+  const [filteredFaculties, setFilteredFaculties] = useState([])
+
+  const handleFilterChange = debounce(value => {
+    setFilter(value)
+  }, 500)
+
+  useEffect(() => {
+    if (faculties) {
+      const filteredFaculties = faculties.filter(faculty => {
+        if (faculty.name[language]) {
+          return (
+            faculty.code.toLowerCase().includes(filter.toLocaleLowerCase()) ||
+            faculty.name[language].toLowerCase().includes(filter.toLocaleLowerCase())
+          )
+        }
+        return (
+          faculty.code.toLowerCase().includes(filter.toLocaleLowerCase()) ||
+          faculty.name.fi.toLowerCase().includes(filter.toLocaleLowerCase())
+        )
+      })
+
+      setFilteredFaculties(filteredFaculties)
+    }
+  }, [filter, faculties])
+
+  if (selected) return null
+  if (!faculties) return <Loader active>Loading</Loader>
+
+  const headers = [
+    {
+      key: 'facultycode',
+      title: 'code',
+      getRowVal: faculty => faculty.code,
+      getRowContent: faculty => (
+        <Link
+          style={{
+            color: 'black',
+            display: 'inline-block',
+            width: '100%',
+            height: '100%',
+            padding: '.78571429em .78571429em',
+          }}
+          to={`/faculties/${faculty.code}`}
+        >
+          {faculty.code}
+        </Link>
+      ),
+      cellProps: {
+        style: {
+          padding: '0',
+        },
+      },
+    },
+    {
+      key: 'facultyname',
+      title: 'name',
+      getRowVal: faculty => getTextIn(faculty.name, language),
+      getRowContent: faculty => (
+        <Link
+          style={{
+            color: 'black',
+            display: 'inline-block',
+            width: '100%',
+            height: '100%',
+            padding: '.78571429em .78571429em',
+          }}
+          to={`/faculties/${faculty.code}`}
+        >
+          {getTextIn(faculty.name, language)}
+        </Link>
+      ),
+      cellProps: {
+        style: {
+          padding: '0',
+        },
+      },
+    },
+  ]
+
+  if (faculties == null) {
+    return <Message>You do not have access to any faculties</Message>
+  }
+
+  return (
+    <>
+      <Form>
+        Filter faculties:
+        <Form.Input onChange={e => handleFilterChange(e.target.value)} width="4" />
+      </Form>
+      {filteredFaculties.length > 0 ? (
+        <>
+          <Header>Faculties</Header>
+          <SortableTable
+            figure={false}
+            columns={headers}
+            getRowKey={faculty => faculty.code}
+            data={filteredFaculties}
+          />
+        </>
+      ) : null}
+    </>
+  )
+}
+
+const mapStateToProps = ({ settings }) => {
+  const { language } = settings
+
+  return {
+    language,
+  }
+}
+
+export default connect(mapStateToProps, null)(FacultySelector)
--- services/frontend/src/components/FacultyStatistics/index.js
+++ services/frontend/src/components/FacultyStatistics/index.js
@@ -1,20 +1,59 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { withRouter, useHistory } from 'react-router-dom'
 import { Segment, Header } from 'semantic-ui-react'
+import { getFaculties } from 'redux/faculties'
+import { getTextIn } from 'common'
 import { useTitle } from '../../common/hooks'
+import FacultySelector from './facultySelector'
+import useLanguage from '../LanguagePicker/useLanguage'
 
-const FacultyStatistics = () => {
+const FacultyStatistics = props => {
   useTitle('Faculties')
+  const history = useHistory()
+  const { language } = useLanguage()
+  const { match, faculties, getFaculties } = props
+  const { facultyCode } = match.params
+  const faculty = facultyCode && faculties.find(f => f.code === facultyCode)
+  const facultyName = faculty && getTextIn(faculty.name, language)
+
+  useEffect(() => {
+    if (faculties.length === 0) getFaculties()
+  }, [])
+
+  const handleSelect = useCallback(
+    faculty => {
+      history.push(`/faculties/${faculty}`, { selected: faculty })
+    },
+    [history]
+  )
+
+  if (!facultyCode)
+    return (
+      <div className="segmentContainer">
+        <Header className="segmentTitle" size="large">
+          Faculty statistics
+        </Header>
+        <Segment className="contentSegment">
+          <FacultySelector faculties={faculties} selected={facultyCode !== undefined} handleSelect={handleSelect} />
+        </Segment>
+      </div>
+    )
 
   return (
     <div className="segmentContainer">
-      <Header className="segmentTitle" size="large">
-        Faculty statistics
-      </Header>
-      <Segment className="contentSegment">Tosi hienot faculty statsit tähän</Segment>
+      <Segment className="contentSegment">
+        <div align="center" style={{ padding: '30px' }}>
+          <Header textAlign="center">{facultyName}</Header>
+          <span>{facultyCode}</span>
+        </div>
+      </Segment>
     </div>
   )
 }
 
-export default connect()(withRouter(FacultyStatistics))
+const mapStateToProps = state => ({
+  faculties: state.faculties.data,
+})
+
+export default connect(mapStateToProps, { getFaculties })(withRouter(FacultyStatistics))
