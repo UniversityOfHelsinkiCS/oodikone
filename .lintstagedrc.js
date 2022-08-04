6e74bb7a06e8d9137e5d85f4a5fd5e8afe5ca015
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-08-04 13:42:02 +0300

    Use actionlint npm package instead of broken docker version

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -9,7 +9,7 @@ module.exports = {
   '*.css': files => `stylelint --fix ${files.join(' ')}`,
   Dockerfile: files => `${dockerCmdBase} hadolint/hadolint:v2.7.0 hadolint ${relativeFilePaths(files)}`,
   '*.sh': files => `${dockerCmdBase} koalaman/shellcheck:v0.7.2 ${relativeFilePaths(files)} -x`,
-  '.github/workflows/*': files => `${dockerCmdBase} rhysd/actionlint:1.6.2 ${relativeFilePaths(files)}`,
+  '.github/workflows/*': files => `npm run actionlint ${relativeFilePaths(files)}`,
   'docker-compose*': files => {
     const composeFiles = file => {
       if (['docker-compose.ci.yml', 'docker-compose.test.yml'].some(f => file.includes(f))) {
