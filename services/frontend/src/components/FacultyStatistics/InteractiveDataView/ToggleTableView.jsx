7c132820e830e650736064ce7587fd09b70a5274
outisa <savolainen.outi@gmail.com>
2022-08-09 14:50:21 +0300

    Faculty: Change button to 'array'

--- services/frontend/src/components/FacultyStatistics/InteractiveDataView/ToggleTableView.jsx
+++ services/frontend/src/components/FacultyStatistics/InteractiveDataView/ToggleTableView.jsx
@@ -1,5 +1,34 @@
 import React, { useState, useImperativeHandle } from 'react'
-import { Button, Table } from 'semantic-ui-react'
+import { Button, Icon, Table, Label } from 'semantic-ui-react'
+
+const BasicRow = ({ icon, yearArray, cypress, yearIndex, toggleVisibility, styles }) => {
+  return (
+    <Table.Row style={styles} key={`random-year-key-${Math.random()}`}>
+      {yearArray?.map((value, idx) => (
+        <Table.Cell key={`random-button-cell-key-${Math.random()}`}>
+          {idx === 0 ? (
+            <Button
+              as="div"
+              onClick={toggleVisibility}
+              labelPosition="right"
+              style={{ backgroundColor: 'white', borderRadius: 0 }}
+              data-cy={`Button-${cypress}-${yearIndex}`}
+            >
+              <Button icon style={{ backgroundColor: 'white', borderRadius: 0 }}>
+                <Icon name={icon} />
+              </Button>
+              <Label as="a" style={{ backgroundColor: 'white', borderRadius: 0 }}>
+                {value}
+              </Label>
+            </Button>
+          ) : (
+            value
+          )}
+        </Table.Cell>
+      ))}
+    </Table.Row>
+  )
+}
 
 const ToggleTableView = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
@@ -14,25 +43,27 @@ const ToggleTableView = React.forwardRef((props, ref) => {
     rowSpan: 100,
     display: visible ? '' : 'none',
   }
+  const basicRowStyles = {
+    display: visible ? 'none' : '',
+  }
   return (
     <React.Fragment key={`random-fragment-key-${Math.random()}`}>
-      <Table.Row key={`random-year-key-${Math.random()}`}>
-        {props.yearArray?.map((value, idx) => (
-          <Table.Cell key={`random-button-cell-key-${Math.random()}`}>
-            {idx === 0 ? (
-              <Button
-                data-cy={`Button-${props.cypress}-${props.yearIndex}`}
-                className="ui tiny basic button"
-                onClick={toggleVisibility}
-              >
-                {value}
-              </Button>
-            ) : (
-              value
-            )}
-          </Table.Cell>
-        ))}
-      </Table.Row>
+      <BasicRow
+        icon="chevron right"
+        yearArray={props.yearArray}
+        cypress={`Show-${props.cypress}`}
+        yearIndex={props.yearIndex}
+        toggleVisibility={toggleVisibility}
+        styles={basicRowStyles}
+      />
+      <BasicRow
+        icon="chevron down"
+        yearArray={props.yearArray}
+        cypress={`Hide-${props.cypress}`}
+        yearIndex={props.yearIndex}
+        toggleVisibility={toggleVisibility}
+        styles={chartRowStyles}
+      />
       <Table.Row key={`stack-row-key-${Math.random()}`} style={chartRowStyles}>
         {props.children}
       </Table.Row>
