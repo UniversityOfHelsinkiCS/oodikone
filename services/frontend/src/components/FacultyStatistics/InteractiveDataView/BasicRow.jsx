3530723bd5f49cca3237bd5b91e5d19bee9da40f
outisa <savolainen.outi@gmail.com>
2022-08-26 16:41:31 +0300

    [Faculty] Change sort by selection

--- services/frontend/src/components/FacultyStatistics/InteractiveDataView/BasicRow.jsx
+++ services/frontend/src/components/FacultyStatistics/InteractiveDataView/BasicRow.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useImperativeHandle } from 'react'
+import React from 'react'
 import { Button, Icon, Table, Label } from 'semantic-ui-react'
 
 const BasicRow = ({ icon, yearArray, cypress, yearIndex, toggleVisibility, styles }) => {
@@ -30,35 +30,4 @@ const BasicRow = ({ icon, yearArray, cypress, yearIndex, toggleVisibility, style
   )
 }
 
-const ToggleTableView = React.forwardRef((props, ref) => {
-  const [visible, setVisible] = useState(false)
-
-  const toggleVisibility = () => {
-    setVisible(!visible)
-  }
-  useImperativeHandle(ref, () => {
-    return toggleVisibility
-  })
-  const chartRowStyles = {
-    display: visible ? '' : 'none',
-  }
-
-  return (
-    <React.Fragment key={`random-fragment-key-${Math.random()}`}>
-      <BasicRow
-        icon={visible ? 'chevron down' : 'chevron right'}
-        yearArray={props.yearArray}
-        cypress={visible ? `Hide-${props.cypress}` : `Show-${props.cypress}`}
-        yearIndex={props.yearIndex}
-        toggleVisibility={toggleVisibility}
-      />
-      <Table.Row key={`stack-row-key-${Math.random()}`} style={chartRowStyles}>
-        {props.children}
-      </Table.Row>
-    </React.Fragment>
-  )
-})
-
-ToggleTableView.displayName = 'ToggleTableView'
-
-export default ToggleTableView
+export default BasicRow
