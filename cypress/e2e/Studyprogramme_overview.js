5324d69063b35f77c2a945fd765348ac046dd871
outisa <savolainen.outi@gmail.com>
2022-09-20 13:59:36 +0300

    Fix cypress test values

--- cypress/e2e/Studyprogramme_overview.js
+++ cypress/e2e/Studyprogramme_overview.js
@@ -332,11 +332,11 @@ describe('Studyprogramme overview', () => {
       cy.contains('a', 'Tietojenkäsittelytieteen kandiohjelma').click()
       cy.get('.attached').contains('Studytracks and student populations').click()
       const tableContents = [
-        ['2020 - 2021', 12, 12, 0, 0, 0, 0, 0, 0],
-        ['2019 - 2020', 86, 26, 34, 21, 5, 0, 0, 0],
-        ['2018 - 2019', 162, 10, 27, 47, 36, 26, 12, 4],
-        ['2017 - 2018', 171, 20, 21, 26, 22, 22, 33, 27],
-        ['Total', 431, 51, 64, 97, 79, 55, 51, 34],
+        ['2020 - 2021', 12, 12, 0, 0, 0, 0, 0, 0, 0],
+        ['2019 - 2020', 86, 10, 16, 34, 21, 5, 0, 0, 0],
+        ['2018 - 2019', 162, 2, 8, 27, 47, 36, 26, 12, 4],
+        ['2017 - 2018', 171, 5, 15, 21, 26, 22, 22, 33, 27],
+        ['Total', 431, 14, 37, 64, 97, 79, 55, 51, 34],
       ]
 
       cy.checkTableStats(tableContents, 'StudytrackProgress')
@@ -347,12 +347,12 @@ describe('Studyprogramme overview', () => {
       cy.get('.attached').contains('Studytracks and student populations').click()
 
       cy.get('[data-cy=Graph-StudytrackProgress]')
-        .should('contain', 'Less than 30 credits')
+        .should('contain', 'Less than 15 credits')
         .should('contain', '30-59 credits')
         .should('contain', 'More than 180 credits')
-        .should('contain', '11.7%') // The percentage for less than 30 credits in 2017-2018, to check that the graph renders
-        .should('contain', '6.2%') // The percentage for less than 30 credits in 2018-2019
-        .should('contain', '30.2%') // The percentage for less than 30 credits in 2019-2020
+        .should('contain', '2.9%') // The percentage for less than 15 credits in 2017-2018, to check that the graph renders
+        .should('contain', '4.9%') // The percentage for 15-29 credits in 2018-2019
+        .should('contain', '11.6%') // The percentage for less than 15 credits in 2019-2020
 
       cy.get('[data-cy=Graph-2018-AverageGraduationTimes]')
         .should('contain', '2018 - 2019')
