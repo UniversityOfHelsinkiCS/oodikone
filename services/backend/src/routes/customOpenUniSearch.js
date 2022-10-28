cdf912db9b5d2d1f0e678c8e758c7bcf276a7538
outisa <savolainen.outi@gmail.com>
2022-10-28 13:11:30 +0300

    [Open uni] Add small modifications

--- services/backend/src/routes/customOpenUniSearch.js
+++ services/backend/src/routes/customOpenUniSearch.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { getCustomOpenUniCourses } = require('../services/openUni/openUniStats')
 
-// post used insead of get because of the possible huge amount of the courses
+// Method POST used insead of GET because of the possible huge amount of the course_codes
 router.post('/', async (req, res) => {
   const courseCodes = req.body?.courselist || []
-  if (!courseCodes) return res.status(400).json({ error: 'Courses missing' })
-  if (!Array.isArray(courseCodes)) res.status(400).json({ error: 'Courses must be of type array' })
+  if (!courseCodes) return res.status(400).json({ error: 'Courses missing' }).end()
+  if (!Array.isArray(courseCodes)) res.status(400).json({ error: 'Courses must be of type array' }).end()
   const customOpenUniSearches = await getCustomOpenUniCourses(courseCodes)
   res.json(customOpenUniSearches)
 })
--- services/backend/src/services/openUni/openUniStats.js
+++ services/backend/src/services/openUni/openUniStats.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 const { getCredits, getStudyRights, getEnrollments, getStudentInfo } = require('./openUniSearches')
-/*
+/* IF NOT NEEDED REMOVE THIS
 const checkStudyrights = (studyrights, date) => {
   const within = (right, date) => {
     const first = moment(right.startdate)
@@ -23,7 +23,10 @@ const getCustomOpenUniCourses = async courseCodes => {
   const studentInfo = await getStudentInfo(students)
   const studentsWithCurrentStudyRight = allStudyrights
     .filter(
-      right => moment(right.startdate).isSameOrBefore(moment()) && !moment(right.enddate).isSameOrBefore(moment())
+      right =>
+        (moment(right.startdate).isBetween('2020-08-01', moment()) &&
+          !moment(right.enddate).isBetween('2020-08-01', moment())) ||
+        (moment(right.startdate).isSameOrBefore('2020-08-01') && moment(right.enddate).isSameOrAfter(moment()))
     )
     .map(right2 => right2.studentnumber)
 
@@ -31,7 +34,7 @@ const getCustomOpenUniCourses = async courseCodes => {
   for (const { studentnumber, email, secondary_email } of studentInfo) {
     // StudyRight checking: student has some studyright now? if yes, then stop here
     if (studentnumber in studentsWithCurrentStudyRight) return
-    // const studentStudyrights = allStudyrights.filter(studyRight => studyRight.studyrightStudentnumber === studentnumber)
+
     if (!(studentnumber in studentStats)) {
       studentStats[studentnumber] = {
         courseInfo: {},
