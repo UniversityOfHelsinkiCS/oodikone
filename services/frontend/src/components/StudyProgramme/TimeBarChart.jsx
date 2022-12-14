84cfde0c392b4573ce2bab3a481b882fe367fe65
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2022-12-14 14:44:26 +0200

    Switch studytrack graduation times to bar chart

--- services/frontend/src/components/StudyProgramme/TimeBarChart.jsx
+++ services/frontend/src/components/StudyProgramme/TimeBarChart.jsx
@@ -2,15 +2,20 @@
 import React from 'react'
 import ReactHighcharts from 'react-highcharts'
 
-const TimeBarChart = ({ data, goal, showMeanTime, title }) => {
+const TimeBarChart = ({ data, goal, showMeanTime, title, byStartYear }) => {
   const maxValue = data.reduce((max, { y }) => {
     return y > max ? y : max
   }, goal * 2)
 
-  const getDataLabel = amount => {
-    // if (label === 'Start year') {
-    //   return `${amount} graduated (${getPercentage(amount, category)} % of class)`
-    // }
+  const getPercentage = (amount, classSize) => {
+    const percent = Math.round((amount / classSize) * 100 * 10) / 10
+    return Number.isNaN(percent) ? 0 : percent
+  }
+
+  const getDataLabel = (amount, classSize = undefined) => {
+    if (byStartYear) {
+      return `${amount} graduated (${getPercentage(amount, classSize)} % of class)`
+    }
     return `${amount} graduated`
   }
 
@@ -19,11 +24,15 @@ const TimeBarChart = ({ data, goal, showMeanTime, title }) => {
     return data.length * t + 100
   }
 
-  const getTooltipText = (amount, y, year, statistics) => {
-    const sortingText = `<b>${amount} students graduated in year ${year}</b>`
+  const getTooltipText = (amount, y, year, statistics, classSize = undefined) => {
+    const sortingText = byStartYear
+      ? `<b>From class of ${year}, ${amount}/${classSize} students have graduated</b>`
+      : `<b>${amount} students graduated in year ${year}</b>`
+
     const timeText = `<br /><p>${sortingText}, <br /><b>${
       showMeanTime ? 'mean' : 'median'
     } study time: ${y} months</p></b>`
+
     const statisticsText = `<br /><p>${statistics.onTime} graduated on time</p><br />
         <p>${statistics.yearOver} graduated max year overtime</p>
         <br /><p>${statistics.wayOver} graduated over year late</p>`
@@ -44,7 +53,7 @@ const TimeBarChart = ({ data, goal, showMeanTime, title }) => {
       fontSize: '25px',
       // eslint-disable-next-line
       formatter: function() {
-        return getTooltipText(this.point.amount, this.y, this.point.name, this.point.statistics)
+        return getTooltipText(this.point.amount, this.y, this.point.name, this.point.statistics, this.point?.classSize)
       },
     },
     plotOptions: {
@@ -69,7 +78,7 @@ const TimeBarChart = ({ data, goal, showMeanTime, title }) => {
             },
             // eslint-disable-next-line
             formatter: function () {
-              return getDataLabel(this.point.amount)
+              return getDataLabel(this.point.amount, this.point?.classSize)
             },
           },
         ],
@@ -92,7 +101,7 @@ const TimeBarChart = ({ data, goal, showMeanTime, title }) => {
     xAxis: {
       type: 'category',
       title: {
-        text: 'Graduation year',
+        text: byStartYear ? 'Start year' : 'Graduation year',
         align: 'high',
         offset: 0,
         rotation: 0,
