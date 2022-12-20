ab76ee4349593f8e85afc0fd514bd3e6ea849767
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-12-20 10:23:49 +0200

    Use jami microservice for getting iam rights

---
+++ services/backend/src/util/jamiClient.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+const { importerToken } = require('../conf-backend')
+const logger = require('./logger')
+
+const jamiClient = axios.create({
+  params: {
+    token: importerToken,
+  },
+  baseURL: 'https://importer.cs.helsinki.fi/api/auth',
+})
+
+const getJamiClient = () => {
+  if (!importerToken) {
+    logger.error("Importer token not set, can't return client!")
+    return null
+  }
+  return jamiClient
+}
+
+module.exports = {
+  getJamiClient,
+}
