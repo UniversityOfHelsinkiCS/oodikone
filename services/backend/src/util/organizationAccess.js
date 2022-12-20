8d389f275fd6051b8c32c2f61e8465e19c5781e7
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-12-20 10:23:49 +0200

    Use jami microservice for getting iam rights

--- services/backend/src/util/organizationAccess.js
+++ services/backend/src/util/organizationAccess.js
@@ -1,92 +1,54 @@
 /* eslint-disable no-unused-vars */
 const _ = require('lodash')
-const Organization = require('../models/organization')
-const { getIAMRights } = require('./IAMRights')
+const Sentry = require('@sentry/node')
 
-const isNumber = value => !Number.isNaN(parseInt(value, 10))
+const { runningInCI, isProduction } = require('../conf-backend')
+const logger = require('./logger')
+const { getJamiClient } = require('../util/jamiClient')
+const jamiClient = getJamiClient()
 
-const normalizeOrganizationCode = r => {
-  if (!r.includes('_')) {
-    return r
+const getIAMAccessFromJami = async (user, attempt = 1) => {
+  const { userId, iamGroups } = user
+
+  try {
+    const { data: iamAccess } = await jamiClient.post('/', {
+      userId,
+      iamGroups,
+      noLogging: !isProduction,
+    })
+
+    return iamAccess
+  } catch (error) {
+    if (attempt > 3) {
+      logger.error(error)
+      Sentry.captureException(error)
+
+      return {}
+    }
+
+    return getIAMAccessFromJami(user, attempt + 1)
   }
-
-  const [left, right] = r.split('_')
-  const prefix = [...left].filter(isNumber).join('')
-  const suffix = `${left[0]}${right}`
-  const providercode = `${prefix}0-${suffix}`
-  return providercode
 }
 
-const RELEVANT_ORGANIZATION_CODES = [
-  'H906', // Kielikeskus
-  'H930', // Avoin yliopisto
-]
+const getAccessFromIAMs = async user => {
+  if (runningInCI || user.iamGroups.length === 0) return {}
 
-const ORGANIZATION_ACCESS_BY_IAM_GROUP = {
-  'grp-kielikeskus-esihenkilot': {
-    // Kielikeskus
-    H906: {
-      read: true,
-      write: true,
-      admin: true,
-    },
-  },
-  'grp-avoin-johto': {
-    // Avoin yliopisto
-    H930: {
-      read: true,
-    },
-  },
-}
-
-const getOrganizationAccessFromIamGroups = user => {
   const access = {}
 
-  const iamAccess = (user.iamGroups ?? []).reduce(
-    (access, group) => _.merge(access, ORGANIZATION_ACCESS_BY_IAM_GROUP[group] ?? {}),
-    {}
-  )
-
-  Object.keys(iamAccess).forEach(code => {
-    access[code] = iamAccess[code]
-  })
-
-  return access
-}
-
-const organizationIsRelevant = organization => {
-  const { code } = organization
-
-  return code.includes('-') || RELEVANT_ORGANIZATION_CODES.includes(code)
-}
-
-const getAccessToAll = async accessLevel => {
-  const access = {}
-  const allOrganizations = await Organization.findAll({ attributes: ['code'] })
-  allOrganizations.filter(organizationIsRelevant).forEach(({ code }) => {
-    access[code] = accessLevel
-  })
-  return access
-}
-
-const getLomakeAccess = async user => {
-  if (!user.iamGroups) user.iamGroups = user.iam_groups
-
-  const access = {}
-  const { access: iamAccess } = getIAMRights(user.iamGroups)
+  const iamAccess = await getIAMAccessFromJami(user)
 
   if (!_.isObject(iamAccess)) return access
   Object.keys(iamAccess).forEach(code => {
     access[code] = iamAccess[code]
   })
+
   return access
 }
 
 const getOrganizationAccess = async user => {
-  const access = {
-    ...(await getLomakeAccess(user)),
-    ...getOrganizationAccessFromIamGroups(user),
-  }
+  if (!user.iamGroups) user.iamGroups = user.iam_groups
+
+  const access = await getAccessFromIAMs(user)
 
   return access
 }
