08d578d0740d26ee299959d0af37431c4117091e
outisa <savolainen.outi@gmail.com>
2023-02-03 17:53:39 +0200

    Add frontend functionality to change criteria for studyprogramme

---
+++ services/frontend/src/redux/programmeProgressCriteria.js
@@ -0,0 +1,37 @@
+import { RTKApi } from 'apiConnection'
+
+const programmeProgressCriteria = RTKApi.injectEndpoints({
+  endpoints: builder => ({
+    getProgressCriteria: builder.query({
+      query: ({ programmeCode }) => `/programmecriteria?programmecode=${programmeCode}`,
+    }),
+    addProgressCriteriaCourse: builder.mutation({
+      query: ({ programmeCode, courses, year }) => ({
+        url: '/programmecriteria/courses',
+        method: 'POST',
+        body: {
+          code: programmeCode,
+          courses,
+          year,
+        },
+      }),
+    }),
+    addProgressCriteriaCredits: builder.mutation({
+      query: ({ programmeCode, credits }) => ({
+        url: '/programmecriteria/credits',
+        method: 'POST',
+        body: {
+          code: programmeCode,
+          credits,
+        },
+      }),
+    }),
+  }),
+  overrideExisting: false,
+})
+
+export const {
+  useGetProgressCriteriaQuery,
+  useAddProgressCriteriaCourseMutation,
+  useAddProgressCriteriaCreditsMutation,
+} = programmeProgressCriteria
