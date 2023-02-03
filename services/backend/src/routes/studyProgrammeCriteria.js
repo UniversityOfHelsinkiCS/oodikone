9db14a839f4aedafdde8e3509fea8964e27e7c0a
outisa <savolainen.outi@gmail.com>
2023-02-03 17:51:32 +0200

    Add backend functionality to change criteria for studyprogrammes

---
+++ services/backend/src/routes/studyProgrammeCriteria.js
@@ -0,0 +1,30 @@
+const {
+  getCriteria,
+  saveYearlyCourseCriteria,
+  saveYearlyCreditCriteria,
+} = require('../services/studyProgrammeCriteria')
+
+const router = require('express').Router()
+
+router.get('/', async (req, res) => {
+  const studyProgramme = req.query?.programmecode
+  if (!studyProgramme) return res.status(422).end()
+  const studyProgrammeCriteria = await getCriteria(studyProgramme)
+  return res.json(studyProgrammeCriteria)
+})
+
+router.post('/courses', async (req, res) => {
+  const { code, courses, year } = req.body
+  if (!code || !courses || !year) return res.status(400).end()
+  const studyProgrammeCriteria = await saveYearlyCourseCriteria(code, courses, year)
+  return res.json(studyProgrammeCriteria)
+})
+
+router.post('/credits', async (req, res) => {
+  const { code, credits } = req.body
+  if (!code || !credits) return res.status(400).end()
+  const studyProgrammeCriteria = await saveYearlyCreditCriteria(code, credits)
+  return res.json(studyProgrammeCriteria)
+})
+
+module.exports = router
