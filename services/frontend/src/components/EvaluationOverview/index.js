b0a0be7969b8b685a27158caa4c502fca4282a86
Laura Sinikallio <46318664+sinikala@users.noreply.github.com>
2023-03-17 18:51:37 +0200

    Add faculty view for tilannekuvalomake

--- services/frontend/src/components/EvaluationOverview/index.js
+++ services/frontend/src/components/EvaluationOverview/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Redirect, withRouter } from 'react-router-dom'
-import { Header, Segment } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
 
 import FacultyView from './FacultyView'
 import ProgrammeView from './ProgrammeView'
@@ -15,12 +15,6 @@ const EvaluationOverview = props => {
   return (
     <div className="segmentContainer">
       <Segment className="contentSegment">
-        <div align="center" style={{ padding: '30px' }}>
-          <Header textAlign="center">{id}</Header>
-          {/* <span>
-            {programmeLetterId ? `${programmeLetterId} - ` : ''} {studyProgrammeId}
-          </span> */}
-        </div>
         {level === 'programme' ? <ProgrammeView studyprogramme={id} /> : <FacultyView faculty={id} />}
       </Segment>
     </div>
