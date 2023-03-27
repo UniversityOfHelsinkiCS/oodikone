eba296f5fdf235c262ee56937ad30c84c927b21c
outisa <savolainen.outi@gmail.com>
2023-03-27 14:39:03 +0300

    Fix typo

--- services/frontend/src/components/FacultyStatistics/FacultyProgrammeOverview/index.jsx
+++ services/frontend/src/components/FacultyStatistics/FacultyProgrammeOverview/index.jsx
@@ -180,9 +180,9 @@ const FacultyProgrammeOverview = ({
               </div>
               {getDivider('Bachelor + Master', 'ProgressOfBachelorMaster', 'no-infobox')}
               <Message data-cy="FacultyProgrammesShownInfo">
-                The starting year is the studyright start in the bachelor programme. The credits are comouted by the
-                start date of the bachelor programme and at the moment, they do not include any transferred credits.
-                Thus, in these statistics some students have less credits than in reality.
+                Please note: The starting year is the studyright start in the bachelor programme. The credits are
+                computed by the start date of the bachelor programme and at the moment, they do not include any
+                transferred credits. Thus, in these statistics some students have less credits than in reality.
               </Message>
               <div className="section-container">
                 <div className="graph-container">
