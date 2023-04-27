1f67cf577009a6f15d062dccb279356d6e6ed9b9
ShootingStar91 <ttu91@hotmail.com>
2023-04-27 12:17:38 +0300

    [CustomPopulation] Invalid or forbidden studentnumbers explicitly mentioned

--- services/frontend/src/components/CompletedCoursesSearch/CompletedCoursesSearch.jsx
+++ services/frontend/src/components/CompletedCoursesSearch/CompletedCoursesSearch.jsx
@@ -123,7 +123,7 @@ const CompletedCoursesSearch = ({ setValues, history, location }) => {
       .filter(c => c !== '')
     const studentList = studentInput
       .split(/[\s,]+/)
-      .map(code => code.trim().toUpperCase())
+      .map(code => code.trim())
       .filter(s => s !== '')
       .map(s => (s.length === 8 ? `0${s}` : s))
 
