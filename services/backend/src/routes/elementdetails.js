caafdb3139ade6137b03cac96eac945af5457a96
outisa <savolainen.outi@gmail.com>
2023-05-30 09:48:45 +0300

    Remove old unused mandatory courses related code

--- services/backend/src/routes/elementdetails.js
+++ services/backend/src/routes/elementdetails.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const MandatoryCourses = require('../services/mandatoryCourses')
 const { getAllProgrammes, getAllElementDetails } = require('../services/studyrights')
 
 router.get('/elementdetails/all', async (req, res) => {
@@ -10,15 +9,6 @@ router.get('/elementdetails/all', async (req, res) => {
 router.get('/studyprogrammes', async (req, res) => {
   const studyrights = await getAllProgrammes()
   res.json(studyrights)
-})
-
-router.get('/v2/studyprogrammes/:id/mandatory_courses', async (req, res) => {
-  if (req.params.id) {
-    const codes = await MandatoryCourses.byStudyprogramme(req.params.id)
-    res.json(codes)
-  } else {
-    res.status(422).end()
-  }
 })
 
 module.exports = router
