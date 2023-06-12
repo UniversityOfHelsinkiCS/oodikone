38b2ad38b4a7423f79affdeb3dbc3e0223a19b55
outisa <savolainen.outi@gmail.com>
2023-06-12 20:18:27 +0300

    [date time range selector] Add keys and adjust possible start date

--- services/frontend/src/components/DateTimeSelector/index.jsx
+++ services/frontend/src/components/DateTimeSelector/index.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react'
 import Datetime from 'react-datetime'
-import 'moment/locale/fi'
 import { Icon, Button } from 'semantic-ui-react'
 import moment from 'moment'
+import 'moment/locale/fi'
 import { useGetSemestersQuery } from 'redux/semesters'
 import { getTextIn } from '../../common'
 
@@ -12,7 +12,7 @@ const semesterListStyles = {
   maxHeight: '10em',
   overflowY: 'auto',
   padding: 0,
-  margin: '0 -4px -4px -4px',
+  margin: '0px -4px -4px -4px',
   borderTop: '1px solid #f9f9f9',
 }
 
@@ -20,7 +20,9 @@ const DateTime = ({ value, onChange, before, after, showSemesters }) => {
   const datetimeRef = useRef()
   const semesterRequest = useGetSemestersQuery()
   const allSemesters = semesterRequest.data?.semesters ?? []
-
+  const today = moment().endOf('day')
+  // Do not allow to select dates after today. At least some cases program just crashed.
+  const startdate = before || today
   return (
     <Datetime
       ref={datetimeRef}
@@ -29,18 +31,21 @@ const DateTime = ({ value, onChange, before, after, showSemesters }) => {
       timeFormat={false}
       locale="fi"
       closeOnSelect
-      isValidDate={date => (!before || date.isBefore(before)) && (!after || date.isAfter(after))}
+      isValidDate={date => {
+        return date.isBefore(moment(startdate)) && (!after || date.isAfter(moment(after)))
+      }}
       renderView={(mode, renderDefault) => {
         const createDateButton = (date, label) => (
           <button
             type="button"
+            key={label}
             onClick={() => {
               datetimeRef.current.setViewDate(moment(date))
               onChange(moment(date))
             }}
             style={{
               height: '1.75em',
-              background: value && value.isSame(date, 'day') ? '#428bca' : undefined,
+              background: value && value.isSame(date, 'day') ? '#428bca' : 'white',
               color: value && value.isSame(date, 'day') ? 'white' : 'inherit',
             }}
             className="date-picker-semester-button"
@@ -77,9 +82,14 @@ const DateTime = ({ value, onChange, before, after, showSemesters }) => {
                       })
                       .sort((a, b) => new Date(b.startdate) - new Date(a.startdate))
                       .map(({ name, startdate, enddate }) => (
-                        <li style={{ padding: '0.2em 0.3em', display: 'flex', alignItems: 'center' }}>
-                          <span style={{ flexGrow: 1 }}>{getTextIn(name)}</span>
-                          {createDateButton(startdate, 'Start')}
+                        <li
+                          key={`${getTextIn(name)}-${Math.random()}`}
+                          style={{ padding: '0.2em 0.3em 0.2em', display: 'flex', alignItems: 'center' }}
+                        >
+                          <span key={getTextIn(name)} style={{ flexGrow: 1 }}>
+                            {getTextIn(name)}
+                          </span>
+                          {createDateButton(moment(startdate), 'Start')}
                           {createDateButton(moment(enddate).subtract(1, 'days'), 'End')}
                         </li>
                       ))}
@@ -92,7 +102,6 @@ const DateTime = ({ value, onChange, before, after, showSemesters }) => {
       renderInput={(_, open) => (
         <Button
           icon={value !== null}
-          labelPosition={value !== null && 'right'}
           onClick={open}
           className="credit-date-filter-input"
           size="mini"
