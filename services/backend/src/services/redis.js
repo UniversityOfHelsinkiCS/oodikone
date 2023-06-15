6fb329684b7d3b6f5013a1f7fcd345f20514c2d4
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-06-15 09:23:26 +0300

    Redis error logging

--- services/backend/src/services/redis.js
+++ services/backend/src/services/redis.js
@@ -1,10 +1,13 @@
 const redis = require('redis')
 const bluebird = require('bluebird')
 const conf = require('../conf-backend.js')
+const logger = require('../util/logger')
 
 bluebird.promisifyAll(redis.RedisClient.prototype)
 
 const redisClient = redis.createClient(6379, conf.redis)
+
+redisClient.on('error', err => logger.error('Redis Client Error', err))
 
 module.exports = {
   redisClient,
