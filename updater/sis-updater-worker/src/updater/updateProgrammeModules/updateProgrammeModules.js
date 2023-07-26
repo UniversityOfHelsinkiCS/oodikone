b7769503d3cfec73c1952a366f37128f384da079
ShootingStar91 <ttu91@hotmail.com>
2023-07-26 14:18:50 +0300

    [Updater] Fix and improve programmemodules logic
    Preserves whole degree structure & imports all time versions separately,
    saving validity_periods to sis-db. Improved code readability,
    performance, reliability.

---
+++ updater/sis-updater-worker/src/updater/updateProgrammeModules/updateProgrammeModules.js
@@ -0,0 +1,77 @@
+const { dbConnections } = require('../../db/connection')
+const { chunk } = require('lodash')
+const { selectFromByIds, bulkCreate } = require('../../db')
+const { ProgrammeModule, ProgrammeModuleChild } = require('../../db/models')
+const ModuleResolver = require('./resolver')
+
+const updateProgrammeModules = async programmeIds => {
+  const programmeChunks = chunk(programmeIds, 20)
+  for (const chunk of programmeChunks) {
+    await updateProgrammeModulesChunk(chunk)
+  }
+}
+
+const updateProgrammeModulesChunk = async programmeIds => {
+  const moduleMap = {}
+  const joinMap = {}
+  const programmes = await selectFromByIds('modules', programmeIds)
+
+  for (const programme of programmes) {
+    const resolvedProgramme = await resolveProgramme(programme)
+    recursiveWrite(resolvedProgramme, programme.id, moduleMap, joinMap)
+  }
+
+  await bulkCreate(ProgrammeModule, Object.values(moduleMap))
+  await ProgrammeModuleChild.bulkCreate(Object.values(joinMap), { ignoreDuplicates: true })
+}
+
+const resolveProgramme = async programme => {
+  const responsibleOrg = programme.organisations
+    ? programme.organisations.find(o => o.roleUrn === 'urn:code:organisation-role:responsible-organisation')
+    : null
+
+  const resolver = new ModuleResolver(dbConnections.knex)
+
+  return {
+    id: programme.id,
+    group_id: programme.group_id,
+    code: programme.code,
+    name: programme.name,
+    type: 'module',
+    studyLevel: programme.study_level,
+    organization_id: responsibleOrg?.organisationId ?? null,
+    valid_from: programme?.validity_period?.startDate ?? null,
+    valid_to: programme?.validity_period?.endDate ?? null,
+    order: 0,
+    children: await resolver.resolve(programme.rule),
+  }
+}
+
+const recursiveWrite = (modArg, parentId, programmeMap, joinMap) => {
+  const mod = (!modArg.type || !modArg.id) && !Array.isArray(modArg) && modArg.children ? modArg.children : modArg
+
+  if (Array.isArray(mod)) {
+    mod.filter(Boolean).forEach(m => recursiveWrite(m, parentId, programmeMap, joinMap))
+    return
+  }
+
+  if (!mod.id) {
+    return
+  }
+
+  let join = {
+    composite: `${parentId}-${mod.id}`,
+    parentId: parentId,
+    childId: mod.id,
+  }
+
+  const { children, ...newModule } = mod
+
+  programmeMap[mod.id] = newModule
+  joinMap[join.composite] = join
+
+  if (!children) return
+  children.forEach(child => recursiveWrite(child, mod.id, programmeMap, joinMap))
+}
+
+module.exports = { updateProgrammeModules }
