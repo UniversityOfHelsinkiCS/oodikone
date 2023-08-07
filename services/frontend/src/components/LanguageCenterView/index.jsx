1ad900b3dea6c5d94d28095bc388ac9c1cea1e79
ShootingStar91 <ttu91@hotmail.com>
2023-08-07 16:59:00 +0300

    Changed basic idea

--- services/frontend/src/components/LanguageCenterView/index.jsx
+++ services/frontend/src/components/LanguageCenterView/index.jsx
@@ -1,81 +1,90 @@
 import React, { useState } from 'react'
-import { Divider, Loader } from 'semantic-ui-react'
+import { Divider, Loader, Radio } from 'semantic-ui-react'
 import SortableTable from 'components/SortableTable'
 import useLanguageCenterData from 'redux/languageCenterView'
 import './index.css'
 import useLanguage from 'components/LanguagePicker/useLanguage'
 import Toggle from 'components/StudyProgramme/Toggle'
-import _ from 'lodash'
 
-// field either 'courseCode' or 'faculty'
-const getStats = (data, field) => {
-  const stats = data.attempts.reduce((obj, cur) => {
-    const old = obj[cur[field]]
-    if (old) {
-      const newStats = {
-        total: old.total + 1,
-        incomplete: cur.completed ? old.incomplete : old.incomplete + 1,
-        completed: cur.completed ? old.completed + 1 : old.completed,
-      }
-      obj[cur[field]] = newStats
-      return obj
-    }
-    obj[cur[field]] = { total: 1, incomplete: cur.completed ? 0 : 1, completed: cur.completed ? 1 : 0 }
-    return obj
-  }, {})
-  console.log({ stats })
-  // console.log({ courseStats })
-  if (field === 'courseCode')
-    return data.courses.map(course => ({ code: course.code, name: course.name, stats: stats[course.code] }))
-  return Object.keys(stats).map(faculty => ({ code: faculty, name: '', stats: stats[faculty] }))
-}
+const shorten = (text, maxLength) => (text.length > maxLength ? `${text.substring(0, maxLength)} ... ` : text)
 
-const shorten = (text, maxLength) => {
-  return text.length > maxLength ? `${text.substring(0, maxLength)} ... ` : text
-}
-
-const getColumns = (byCourses, getTextIn) => {
+const getColumns = (getTextIn, faculties, mode) => {
   const columns = [
     {
       key: 'course-name',
-      title: byCourses ? 'Course' : 'Faculty',
+      title: 'Course',
       getRowVal: row => row.code ?? 'No faculty found because enrollment date missing.',
       getRowContent: row => (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           <b>{row.code ?? 'No faculty'}</b>
-          {byCourses && (
-            <i style={{ color: 'gray', fontWeight: 'normal' }}>{row.name && shorten(getTextIn(row.name), 90)}</i>
-          )}
+          <i style={{ color: 'gray', fontWeight: 'normal' }}>{row.name && shorten(getTextIn(row.name), 90)}</i>
         </div>
       ),
     },
+    ...faculties.map(facultyCode => ({
+      key: facultyCode,
+      title: facultyCode,
+      getRowVal: row => {
+        const stats = row.facultyStats[facultyCode]
+        if (!stats) return 0
+        if (mode === 'total') {
+          const result = (stats.completed ?? 0) + (stats.notCompleted ?? 0)
+          return result
+        }
+        return stats[mode] ?? 0
+      },
+      filterable: false,
+    })),
     {
-      key: 'course-incomplete',
-      title: 'Not completed',
-      getRowVal: row => (row.stats ? row.stats.incomplete : 0),
-    },
-    {
-      key: 'course-completed',
-      title: 'Passed',
-      getRowVal: row => (row.stats ? row.stats.completed : 0),
-    },
-    {
-      key: 'course-total',
-      title: 'Total',
-      getRowVal: row => (row.stats ? row.stats.total : 0),
+      key: 'all',
+      title: 'All faculties',
+      getRowVal: row => {
+        const objVals = Object.values(row.facultyStats)
+        const arr = objVals.map(stat => {
+          if (mode === 'total') {
+            const notCompleted = stat.notCompleted ?? 0
+            const completed = stat.completed ?? 0
+            return notCompleted + completed
+          }
+          return stat[mode]
+        })
+        return arr.reduce((sum, cur) => {
+          if (cur === undefined) return sum
+          return cur + sum
+        }, 0)
+      },
     },
   ]
-
   return columns
 }
 
+const getCourseFaculties = attempts => {
+  const map = attempts.reduce((obj, cur) => {
+    if (!obj[cur.courseCode]) {
+      obj[cur.courseCode] = {}
+    }
+    if (!obj[cur.courseCode][cur.faculty]) {
+      obj[cur.courseCode][cur.faculty] = {}
+    }
+    const stats = obj[cur.courseCode][cur.faculty]
+    const field = cur.completed ? 'completed' : 'notCompleted'
+    if (!stats[field]) {
+      stats[field] = 1
+    } else {
+      stats[field] += 1
+    }
+    return obj
+  }, {})
+  return map
+}
+
 const LanguageCenterView = () => {
-  const { data, isFetchingOrLoading, isError } = useLanguageCenterData()
+  const { data: rawData, isFetchingOrLoading, isError } = useLanguageCenterData()
   const { getTextIn } = useLanguage()
-  const [byCourses, setByCourses] = useState(true)
   const [filterOld, setFilterOld] = useState(true)
+  const [mode, setMode] = useState('total')
   if (isError) return <h3>Something went wrong, please try refreshing the page.</h3>
-  if (isFetchingOrLoading || !data) return <Loader active style={{ marginTop: '15em' }} />
+  if (isFetchingOrLoading || !rawData) return <Loader active style={{ marginTop: '15em' }} />
 
   const filterFaculties = data => {
     if (!filterOld) return data
@@ -84,22 +93,39 @@ const LanguageCenterView = () => {
       attempts: data.attempts.filter(attempt => attempt.faculty?.substring(0, 3).match(`^H\\d`)),
     }
   }
-  console.log("kkesp-101 count ", data.attempts.filter(attempt => attempt.courseCode === 'KK-ESP101' && attempt.completed).length)
-  console.log({ data })
-  console.log(data.attempts.length)
 
-  const rows = getStats(filterFaculties(data), byCourses ? 'courseCode' : 'faculty')
-  console.log(rows.filter(row => !row.stats))
-  console.log(rows.reduce((prev, cur) => (cur.stats ? cur.stats.total + prev : prev), 0))
-  console.log(
-    'sum of totals ',
-    rows.reduce((previous, { stats }) => previous + (stats ? stats.total : 0), 0)
-  )
+  const data = filterFaculties(rawData)
+  const courseFaculties = getCourseFaculties(data.attempts)
+  const coursesWithFaculties = data.courses
+    .map(c => ({ ...c, facultyStats: courseFaculties[c.code] }))
+    .filter(course => course.facultyStats)
+  const faculties = [...new Set(data.attempts.map(({ faculty }) => faculty))].sort()
+  console.log({ attempts: data.attempts, courseFaculties, coursesWithFaculties })
   return (
     <div className="languagecenterview">
       <Divider horizontal>Language center overview</Divider>
       <div className="toggle-container">
-        <Toggle firstLabel="By faculties" secondLabel="By courses" value={byCourses} setValue={setByCourses} />
+        <Radio
+          name="modeRadioGroup"
+          value="total"
+          label="Total"
+          onChange={() => setMode('total')}
+          checked={mode === 'total'}
+        />
+        <Radio
+          name="modeRadioGroup"
+          value="notCompleted"
+          label="Not completed"
+          onChange={() => setMode('notCompleted')}
+          checked={mode === 'notCompleted'}
+        />
+        <Radio
+          name="modeRadioGroup"
+          value="completed"
+          label="Completed"
+          onChange={() => setMode('completed')}
+          checked={mode === 'completed'}
+        />
         <Toggle
           firstLabel="All faculties"
           secondLabel="Filter out old faculties"
@@ -108,7 +134,7 @@ const LanguageCenterView = () => {
         />
       </div>
       <div className="languagecenter-table">
-        <SortableTable columns={getColumns(byCourses, getTextIn)} data={rows} stretch />
+        <SortableTable columns={getColumns(getTextIn, faculties, mode)} data={coursesWithFaculties} stretch />
       </div>
     </div>
   )
