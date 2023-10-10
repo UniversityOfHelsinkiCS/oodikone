2dcf460a6061d502b1ef0427484129c50a586937
ShootingStar91 <ttu91@hotmail.com>
2023-10-10 16:21:22 +0300

    Refactoring: programmemodules-querys renamed, returns null on error

--- services/frontend/src/redux/populationCourses.js
+++ services/frontend/src/redux/populationCourses.js
@@ -58,11 +58,11 @@ export const { useGetStudentListCourseStatisticsQuery } = courseStatisticsApi
 export const curriculumsApi = RTKApi.injectEndpoints({
   endpoints: builder => ({
     getCurriculumOptions: builder.query({
-      query: ({ code }) => `/v3/programme_modules/get_curriculum_options/${code}`,
+      query: ({ code }) => `/v3/get_curriculum_options/${code}`,
     }),
     getCurriculums: builder.query({
       // eslint-disable-next-line camelcase
-      query: ({ code, period_ids }) => `/v3/programme_modules/get_curriculum/${code}/${period_ids.join(',')}`,
+      query: ({ code, period_ids }) => `/v3/get_curriculum/${code}/${period_ids.join(',')}`,
     }),
   }),
 })
