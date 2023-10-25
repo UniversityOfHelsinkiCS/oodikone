26e153ed0536f7c15b7905900a12a43c2d3d5d0f
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2023-10-25 11:17:06 +0300

    Update front page layout

--- services/frontend/src/components/Frontpage/Changelog.jsx
+++ services/frontend/src/components/Frontpage/Changelog.jsx
@@ -2,46 +2,50 @@
 import React, { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import changelogApi from 'redux/changelog'
-import { Button, Divider, Header, Loader } from 'semantic-ui-react'
+import { Divider, Header, Loader } from 'semantic-ui-react'
+import { builtAt } from '../../conf'
 
 const { useGetChangelogQuery } = changelogApi
 
-const Changelog = () => {
-  const [showAll, setShowAll] = useState()
+export const Changelog = ({ showFullChangelog }) => {
   const [itemsToShow, setItemsToShow] = useState([])
   const { data, isLoading } = useGetChangelogQuery()
 
   useEffect(() => {
     if (!data) return
-    setItemsToShow(showAll ? [...data.slice(0, 20)] : [...data.slice(0, 2)])
-  }, [data, showAll])
+    setItemsToShow(showFullChangelog ? [...data.slice(0, 20)] : [...data.slice(0, 2)])
+  }, [data, showFullChangelog])
+
+  const formatDate = dateString => {
+    const date = new Date(dateString)
+    const dateFormatter = new Intl.DateTimeFormat(undefined, { dateStyle: 'medium', timeStyle: 'medium' })
+    return dateFormatter.format(date)
+  }
 
   const getReleaseString = release => {
-    const date = new Date(release.time).toLocaleString()
+    const date = formatDate(release.time)
     const { title } = release
     const description = release.description.split('### Internal')[0]
-    return `## ${title}\n${date}\n\n${description}`
+    const releaseString = showFullChangelog ? `## ${title}\n${date}\n\n${description}` : `#### ${title}\n${description}`
+    return releaseString
   }
-  if (isLoading) {
-    return <Loader />
-  }
-  if (!data) {
-    return null
-  }
+
+  if (isLoading || itemsToShow.length === 0) return <Loader />
+
   return (
     <div>
-      <Header as="h1">Changelog</Header>
+      {!showFullChangelog && (
+        <>
+          <Header as="h3">Updates</Header>
+          <p>Last update on: {builtAt ? formatDate(builtAt) : formatDate(itemsToShow[0].time)}</p>
+        </>
+      )}
       {itemsToShow.map(release => (
         <div key={release.time}>
           <Divider section />
           <ReactMarkdown children={getReleaseString(release)} />
         </div>
       ))}
-      <Button style={{ marginTop: '15px' }} onClick={() => setShowAll(!showAll)}>
-        {showAll ? 'Show less' : 'Show more'}
-      </Button>
     </div>
   )
 }
-
-export default Changelog
--- services/frontend/src/components/Frontpage/index.jsx
+++ services/frontend/src/components/Frontpage/index.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Container, Header, Image, Divider, List } from 'semantic-ui-react'
-import moment from 'moment'
+import React, { useState } from 'react'
+import { Container, Header, Image, Divider, List, Grid, Button, Icon } from 'semantic-ui-react'
 import { useGetAuthorizedUserQuery } from 'redux/auth'
 import { images, checkUserAccess } from '../../common'
 import { useTitle } from '../../common/hooks'
-import { builtAt } from '../../conf'
-import Changelog from './Changelog'
+import { Changelog } from './Changelog'
 
 const FrontPage = () => {
   const { rights, roles } = useGetAuthorizedUserQuery()
+  const [showFullChangelog, setShowFullChangelog] = useState(false)
 
   useTitle()
 
@@ -24,59 +23,87 @@ const FrontPage = () => {
 
   return (
     <div>
-      <Container text style={{ paddingTop: 50 }} textAlign="justified">
-        <Header as="h1" textAlign="center">
-          Oodikone
-        </Header>
-        <Header as="h3" style={{ textAlign: 'center' }}>
-          Exploratory Research on Study Data
-        </Header>
-
-        {showItems.populations && (
+      <Container style={{ paddingTop: 50, marginBottom: '20px' }} textAlign="justified">
+        {showFullChangelog ? (
           <>
-            <Divider section />
-            <Header as="h4">Study Programme</Header>
-            <List bulleted>
-              <List.Item>
-                <i>Search by Class:</i> Query a student population specified by a starting year and a study right.
-                Oodikone will show you interactive statistics and visualizations for the population to be explored.
-              </List.Item>
-              <List.Item>
-                <i>Overview:</i> View student progress and annual productivity for a given study programme.
-              </List.Item>
-            </List>
+            <Header as="h1" style={{ textAlign: 'center' }}>
+              Latest updates
+            </Header>
+            <Button fluid style={{ margin: '30px 0' }} onClick={() => setShowFullChangelog(false)}>
+              <Icon name="arrow left" />
+              Back to front page
+            </Button>
+            <Changelog showFullChangelog />
+            <Button fluid style={{ margin: '30px 0' }} onClick={() => setShowFullChangelog(false)}>
+              <Icon name="arrow left" />
+              Back to front page
+            </Button>
+          </>
+        ) : (
+          <>
+            <Header as="h1" textAlign="center">
+              Oodikone
+            </Header>
+            <Header as="h3" style={{ textAlign: 'center' }}>
+              Exploratory Research on Study Data
+            </Header>
+            <Grid columns="two" style={{ marginTop: '40px' }} divided>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as="h3">Trends</Header>
+                  <p>View many kinds visualizations of study progress and study programme status.</p>
+                  <Divider section />
+                  {showItems.populations && (
+                    <>
+                      <Header as="h3">Study programme</Header>
+                      <List bulleted>
+                        <List.Item>
+                          <i>Search by Class:</i> Query a student population specified by a starting year and a study
+                          right. Oodikone will show you interactive statistics and visualizations for the population to
+                          be explored.
+                        </List.Item>
+                        <List.Item>
+                          <i>Overview:</i> View student progress and annual productivity for a given study programme.
+                        </List.Item>
+                      </List>
+                    </>
+                  )}
+                  {showItems.courseStatistics && (
+                    <>
+                      <Divider section />
+                      <Header as="h3">Course statistics</Header>
+                      <p>View statistics by course and year.</p>
+                    </>
+                  )}
+                  {showItems.students && (
+                    <>
+                      <Divider section />
+                      <Header as="h3">Student statistics</Header>
+                      <p>View detailed information for a given student.</p>
+                    </>
+                  )}
+                  <Divider section />
+                  <Header as="h3">Feedback</Header>
+                  <p>
+                    For questions and suggestions, please use the{' '}
+                    <a href="https://oodikone.helsinki.fi/feedback">feedback form</a> or shoot an email to{' '}
+                    <a href="mailto:grp-toska@helsinki.fi">grp-toska@helsinki.fi</a>.
+                  </p>
+                </Grid.Column>
+                <Grid.Column style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <Changelog showFullChangelog={false} />
+                  <Button style={{ marginTop: '40px' }} onClick={() => setShowFullChangelog(true)}>
+                    Show more
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </>
         )}
-        {showItems.students && (
-          <>
-            <Divider section />
-            <Header as="h4">Student Statistics</Header>
-            <p>View detailed information for a given student.</p>
-          </>
-        )}
-        {showItems.courseStatistics && (
-          <>
-            <Divider section />
-            <Header as="h4">Course Statistics</Header>
-            <p>View statistics by course and year.</p>
-          </>
-        )}
-        <Divider section />
-        <Header as="h4">Trends</Header>
-        <p>View many kinds visualizations of study progress and study programme status.</p>
-
-        <Divider section />
-        <Header as="h4">Feedback</Header>
-        <p>
-          For questions and suggestions, use either the feedback form or shoot an e-mail to{' '}
-          <a href="mailto:grp-toska@helsinki.fi">grp-toska@helsinki.fi</a>.
-        </p>
-
-        <Divider section />
-        <Changelog />
-        {builtAt ? <p>Oodikone was last updated on: {moment(builtAt).toDate().toLocaleString()}</p> : null}
       </Container>
-      <Image src={images.toskaLogo} size="medium" centered style={{ bottom: 0 }} />
+      <a href="https://toska.dev/">
+        <Image src={images.toskaLogo} size="medium" centered style={{ bottom: 0 }} />
+      </a>
     </div>
   )
 }
