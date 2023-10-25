b41ab17d877b5eda477324fecc447aac4a4986cf
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2023-10-25 13:41:03 +0300

    [Feedback form] Use RTK Query for sending feedback

--- services/frontend/src/redux/feedback.js
+++ services/frontend/src/redux/feedback.js
@@ -1,40 +1,16 @@
-import { callController } from '../apiConnection'
+import { RTKApi } from '../apiConnection'
 
-export const sendFeedbackAction = content => {
-  const route = '/feedback/email'
-  const prefix = 'SEND_FEEDBACK_'
-  const data = { content }
-  const method = 'post'
+const feedbackApi = RTKApi.injectEndpoints({
+  endpoints: builder => ({
+    sendFeedback: builder.mutation({
+      query: ({ content }) => ({
+        url: '/feedback/email',
+        method: 'POST',
+        body: { content },
+      }),
+    }),
+  }),
+  overrideExisting: false,
+})
 
-  return callController(route, prefix, data, method)
-}
-
-const reducer = (state = { data: [], enabledOnly: true, success: false, pending: false, error: false }, action) => {
-  switch (action.type) {
-    case 'SEND_FEEDBACK_ATTEMPT':
-      return {
-        ...state,
-        success: false,
-        pending: true,
-        error: false,
-      }
-    case 'SEND_FEEDBACK_SUCCESS':
-      return {
-        ...state,
-        success: true,
-        pending: false,
-        error: false,
-      }
-    case 'SEND_FEEDBACK_FAILURE':
-      return {
-        ...state,
-        success: false,
-        pending: false,
-        error: true,
-      }
-    default:
-      return state
-  }
-}
-
-export default reducer
+export const { useSendFeedbackMutation } = feedbackApi
