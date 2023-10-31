bf29468f432efd2a598aa0585b80b653fa4ca182
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2023-10-31 12:31:43 +0200

    [Changelog] Save changelog data on the server to avoid unnecessary refetching

--- services/backend/src/routes/changelog.js
+++ services/backend/src/routes/changelog.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const axios = require('axios')
+const changelog = {}
 
 router.get('/', async (req, res) => {
+  if (changelog?.data) return res.status(200).send(changelog.data)
   const result = await axios.get('https://api.github.com/repos/UniversityOfHelsinkiCS/oodikone/releases')
   const formattedResult = result.data.map(r => ({ description: r.body, time: r.created_at, title: r.name }))
+  changelog.data = formattedResult
   res.status(200).send(formattedResult)
 })
 
