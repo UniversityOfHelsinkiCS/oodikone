9da8ccf2dde4701ed9c06e089d4a58a42fabaf69
ShootingStar91 <ttu91@hotmail.com>
2023-11-10 12:58:20 +0200

    [Language center] Add info message with guidance

--- services/frontend/src/components/LanguageCenterView/index.jsx
+++ services/frontend/src/components/LanguageCenterView/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { Divider, Loader, Message, Tab } from 'semantic-ui-react'
+import { Divider, Icon, Loader, Message, Tab } from 'semantic-ui-react'
 import './index.css'
 import { useGetSemestersQuery } from 'redux/semesters'
 import { useHistory } from 'react-router-dom'
@@ -95,14 +95,51 @@ const LanguageCenterView = () => {
     <LanguageCenterContext.Provider value={settingsContext}>
       <div className="languagecenterview">
         <Divider horizontal>Language center statistics</Divider>
-        <Message>
+        <Message style={{ maxWidth: '60em' }}>
           <p>This view displays amounts of enrollments and completions of courses organized by language center.</p>
           <p>You can view the numbers by faculties or by semesters.</p>
           Explanations of settings:
           <ul>
-            <li>Completion: Amount of passed completions of course</li>
-            <li>Enrollments: Total amount of enrollments on course</li>
+            <li>
+              <b>Show number of:</b>
+            </li>
+
+            <ul>
+              <li>Completion: Amount of passed completions of course</li>
+              <li>Enrollments: Amount of accepted enrollments on course</li>
+              <li>
+                Ratio: Percentage of credits per enrollments. 0 % means there are zero credits and at least one
+                enrollment. 100 % means there are at least as many credits as enrollments. A dash "-" indicates there
+                are no credits or enrollments. Hover mouse over a cell to view the amount of enrollments and credits.
+              </li>
+            </ul>
+            <li>
+              <b>Hide empty courses</b>: Hides courses where the total of selected number is zero. In ratio-mode, the
+              courses where both enrollments and completions are zero, are hidden.
+            </li>
           </ul>
+          <p>Tips:</p>
+          <ul>
+            <li>Hover your mouse over the faculty column header to see the name of the faculty</li>
+            <li>
+              When in ratio mode, hover your mouse over the cells to see the numbers of completions and enrollments.
+            </li>
+            <li>
+              Click the column headers to sort by the column, or click the filter icon{' '}
+              <Icon name="filter" style={{ color: '#bbb' }} /> (appears when hovering mouse on column header) to set a
+              filter on that column.
+            </li>
+            <li>
+              <b>Example of viewing all courses of a language</b>: Click the filter-icon
+              <Icon name="filter" style={{ color: '#bbb' }} />. Type "KK-ESP" and press enter. Now only the courses
+              whose code contains "KK-ESP" will be shown. Notice that the total-row on top of the table still shows
+              numbers from all courses.
+            </li>
+          </ul>
+          <p>
+            This is a new feature. Suggestions for improvement or questions are welcomed to grp-toska@helsinki.fi or via
+            the <a href="https://oodikone.helsinki.fi/feedback">feedback form</a>.
+          </p>
         </Message>
         <div className="languagecenter-table">
           <Tab panes={getPanes()} activeIndex={tab} onTabChange={setTab} />
