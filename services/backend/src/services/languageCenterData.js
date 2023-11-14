fefdf8c6c83fc9d9d1719f8a8576c63a1b8188fd
ShootingStar91 <ttu91@hotmail.com>
2023-11-14 12:45:33 +0200

    [Language center] Switch to using semestercode than enrollment/credit time

--- services/backend/src/services/languageCenterData.js
+++ services/backend/src/services/languageCenterData.js
@@ -1,12 +1,8 @@
 const { Course, Credit, Enrollment, StudyrightElement, Studyright } = require('../models')
 const { Op } = require('sequelize')
 const { redisClient } = require('./redis')
-const { getSemestersAndYears } = require('./semesters')
 const { orderBy } = require('lodash')
 const LANGUAGE_CENTER_REDIS_KEY = 'LANGUAGE_CENTER_DATA'
-
-const isBetween = (start, date, end) =>
-  new Date(start).getTime() <= new Date(date).getTime() && new Date(date).getTime() <= new Date(end).getTime()
 
 const getLanguageCenterCourses = async () => {
   const courses = await Course.findAll({
@@ -88,6 +84,7 @@ const computeLanguageCenterData = async () => {
       completed: true,
       date: c.createdate,
       faculty: studyrightToFacultyMap[c.studyright_id],
+      semestercode: c.semestercode,
     })
   })
 
@@ -103,6 +100,7 @@ const computeLanguageCenterData = async () => {
       completed: false,
       date: e.enrollment_date_time,
       faculty: studyrightToFacultyMap[e.studyright_id],
+      semestercode: e.semestercode,
     })
   })
 
@@ -149,10 +147,8 @@ const computeLanguageCenterData = async () => {
 
 const createArrayOfCourses = async (attempts, courses) => {
   const fields = { completed: 0, notCompleted: 0, ratio: 0 }
-  const semestersAndYears = await getSemestersAndYears()
-  const semesters = Object.values(semestersAndYears.semesters)
   const semesterStatsMap = attempts.reduce((obj, cur) => {
-    const semester = semesters.find(s => isBetween(s.startdate, cur.date, s.enddate)).semestercode
+    const semester = cur.semestercode
     if (!obj[cur.courseCode]) {
       obj[cur.courseCode] = { ...fields }
     }
