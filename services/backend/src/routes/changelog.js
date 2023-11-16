5c72eb89d4e2de9d9477f9d8d8a48c1d34781042
ShootingStar91 <ttu91@hotmail.com>
2023-11-16 14:32:03 +0200

    [Changelog] Don't use github api in development

--- services/backend/src/routes/changelog.js
+++ services/backend/src/routes/changelog.js
@@ -1,9 +1,18 @@
 const router = require('express').Router()
 const axios = require('axios')
+const { isDev } = require('../conf-backend')
 const changelog = {}
 
 router.get('/', async (req, res) => {
   if (changelog?.data) return res.status(200).send(changelog.data)
+  if (isDev)
+    return res.status(200).json([
+      {
+        description: "### Fake release\nLet's not spam github api in development!",
+        title: 'Fake title for fake release',
+        time: new Date(),
+      },
+    ])
   const result = await axios.get('https://api.github.com/repos/UniversityOfHelsinkiCS/oodikone/releases')
   const formattedResult = result.data.map(r => ({ description: r.body, time: r.created_at, title: r.name }))
   changelog.data = formattedResult
