a9104cba3f6a0a7cac1054b5937eee01b1ba8455
ShootingStar91 <ttu91@hotmail.com>
2023-12-12 12:26:06 +0200

    Added job that schedulers update of students whose studyplans were not updated recently.

--- services/backend/src/services/sisUpdaterService.js
+++ services/backend/src/services/sisUpdaterService.js
@@ -1,42 +1,59 @@
 const axios = require('axios')
 const { SIS_UPDATER_URL, SECRET_TOKEN } = require('../conf-backend')
+const { Studyplan } = require('../models')
+const { Op } = require('sequelize')
 
 const client = axios.create({ baseURL: SIS_UPDATER_URL })
 
+const params = { params: { token: SECRET_TOKEN } }
+
 const updateSISStudents = async () => {
-  const response = await client.get('/v1/students', { params: { token: SECRET_TOKEN } })
+  const response = await client.get('/v1/students', params)
   return response.data
 }
 
 const updateStudentsByStudentNumber = async studentnumbers => {
   const data = { studentnumbers }
-  const response = await client.post('/v1/students', data, { params: { token: SECRET_TOKEN } })
+  const response = await client.post('/v1/students', data, params)
   return response.data
 }
 
 const updateCoursesByCourseCode = async coursecodes => {
   const data = { coursecodes }
-  const response = await client.post('/v1/courses', data, { params: { token: SECRET_TOKEN } })
+  const response = await client.post('/v1/courses', data, params)
   return response.data
 }
 
 const updateSISMetadata = async () => {
-  const response = await client.get('/v1/meta', { params: { token: SECRET_TOKEN } })
+  const response = await client.get('/v1/meta', params)
   return response.data
 }
 
 const updateSISProgrammes = async () => {
-  const response = await client.get('/v1/programmes', { params: { token: SECRET_TOKEN } })
+  const response = await client.get('/v1/programmes', params)
   return response.data
 }
 
 const updateSISRedisCache = async () => {
-  const response = await client.get('/v1/rediscache', { params: { token: SECRET_TOKEN } })
+  const response = await client.get('/v1/rediscache', params)
+  return response.data
+}
+
+const studyplansUpdate = async days => {
+  const limitDate = new Date()
+  limitDate.setDate(limitDate.getDate() - days)
+  const studentnumbers = await Studyplan.findAll({
+    where: { updatedAt: { [Op.lte]: limitDate } },
+    attributes: ['studentnumber'],
+    raw: true,
+    limit: 1000,
+  })
+  const response = await client.post('v1/studyplans', { studentnumbers }, params)
   return response.data
 }
 
 const abort = async () => {
-  const response = await client.get('/v1/abort', { params: { token: SECRET_TOKEN } })
+  const response = await client.get('/v1/abort', params)
   return response.data
 }
 
@@ -48,4 +65,5 @@ module.exports = {
   updateSISRedisCache,
   abort,
   updateCoursesByCourseCode,
+  studyplansUpdate,
 }
