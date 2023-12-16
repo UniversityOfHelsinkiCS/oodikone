492d6ce29f76d1eb50c11f7b4db07c1c0c63685a
arttu <ttu91@hotmail.com>
2023-12-16 11:01:28 +0200

    [Course stats] Add export to multi-course summary tab

---
+++ services/frontend/src/components/CourseStatistics/SummaryTab/DataExport.jsx
@@ -0,0 +1,40 @@
+import xlsx from 'xlsx'
+import React from 'react'
+import { Button, Icon } from 'semantic-ui-react'
+
+export const DataExport = ({ data }) => {
+  const jsonItems = data.reduce((arr, cur) => {
+    const { passed: Passed, failed: Failed, passrate: Passrate, category: Title } = cur
+    const years = cur.realisations.map(r => ({
+      Passed: r.passed,
+      Failed: r.failed,
+      Passrate: r.passrate,
+      Title: r.realisation,
+      obfuscated: r.obfuscated,
+    }))
+    const rows = [{ Title, Passed, Failed, Passrate }, ...years]
+    const obfuscatedRows = rows.map(row => {
+      if (row.obfuscated) {
+        const obf = '5 or less students'
+        return { Title: row.Title, Passed: obf, Failed: obf, Passrate: obf }
+      }
+      const { obfuscated, ...rest } = row
+      return rest
+    })
+    arr.push(...obfuscatedRows)
+    return arr
+  }, [])
+  const worksheet = xlsx.utils.json_to_sheet(jsonItems)
+  const workbook = xlsx.utils.book_new()
+  xlsx.utils.book_append_sheet(workbook, worksheet)
+  return (
+    <Button
+      onClick={() => {
+        xlsx.writeFile(workbook, 'coursestatistics.xlsx')
+      }}
+    >
+      <Icon name="save" />
+      Export to Excel
+    </Button>
+  )
+}
