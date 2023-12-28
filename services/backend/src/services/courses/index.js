41e00067cc64ea0ef412967c83a49050a3aba81c
arttu <ttu91@hotmail.com>
2023-12-28 15:00:55 +0200

    [Course stats] Refactored backend to smaller files

--- services/backend/src/services/courses/index.js
+++ services/backend/src/services/courses/index.js
@@ -1,215 +1,17 @@
 const Sequelize = require('sequelize')
-const {
-  Student,
-  Credit,
-  Course,
-  ElementDetail,
-  StudyrightElement,
-  Studyright,
-  Semester,
-  Organization,
-  Enrollment,
-} = require('../models')
+const { Credit, Course, Organization, Enrollment } = require('../../models')
 
-const { parseCredit } = require('./parseCredits')
-const { parseEnrollment } = require('./parseEnrollments')
+const { parseCredit } = require('../parseCredits')
+const { parseEnrollment } = require('../parseEnrollments')
 const Op = Sequelize.Op
 const { CourseYearlyStatsCounter } = require('./course_yearly_stats_counter')
-const { sortMainCode, getSortRank } = require('../util/utils')
-
-const byNameOrCode = (searchTerm, language) =>
-  Course.findAll({
-    where: {
-      [Op.or]: [
-        {
-          name: {
-            [language]: {
-              [Op.iLike]: searchTerm,
-            },
-          },
-        },
-        {
-          code: {
-            [Op.like]: searchTerm,
-          },
-        },
-      ],
-    },
-  })
-
-const byName = (name, language) =>
-  Course.findAll({
-    where: {
-      name: {
-        [language]: {
-          [Op.eq]: name,
-        },
-      },
-    },
-    order: [['latest_instance_date', 'DESC']],
-    limit: 1,
-  })
-
-const byCode = code => Course.findByPk(code)
-
-const findOneByCode = code => {
-  return Course.findOne({
-    attributes: ['id', 'code', 'name'],
-    where: {
-      code: code,
-    },
-  })
-}
-
-const creditsForCourses = async (codes, anonymizationSalt, unification) => {
-  let is_open = false
-
-  if (unification === 'open') is_open = true
-
-  if (unification === 'unify') {
-    is_open = {
-      [Op.in]: [false, true],
-    }
-  }
-
-  return await Credit.findAll({
-    include: [
-      {
-        model: Student,
-        attributes: ['studentnumber'],
-      },
-      {
-        model: Semester,
-        attributes: ['semestercode', 'name', 'yearcode', 'yearname'],
-        where: {
-          startdate: {
-            [Op.lte]: new Date(),
-          },
-        },
-      },
-    ],
-    where: {
-      course_code: {
-        [Op.in]: codes,
-      },
-      student_studentnumber: {
-        [Op.ne]: null,
-      },
-      [Op.or]: [{ is_open }, { is_open: null }],
-    },
-    order: [['attainment_date', 'ASC']],
-  })
-}
-
-const getStudentNumberToSrElementsMap = async studentNumbers => {
-  const studyrights = await Studyright.findAll({
-    attributes: ['prioritycode', 'faculty_code', 'student_studentnumber', 'studyrightid'],
-    where: {
-      prioritycode: {
-        [Op.eq]: 1,
-      },
-      student_studentnumber: { [Op.in]: studentNumbers },
-    },
-    include: {
-      model: Organization,
-    },
-  })
-
-  const studyrightMap = studyrights.reduce((obj, cur) => {
-    obj[cur.studyrightid] = cur
-    return obj
-  }, {})
-
-  const studyrightIds = Object.keys(studyrightMap)
-
-  const studyrightElements = await StudyrightElement.findAll({
-    attributes: ['code', 'startdate', 'studentnumber', 'studyrightid'],
-    include: [
-      {
-        model: ElementDetail,
-        attributes: ['name', 'type'],
-        where: {
-          type: {
-            [Op.eq]: 20,
-          },
-        },
-      },
-    ],
-    where: { studyrightid: { [Op.in]: studyrightIds } },
-  })
-
-  return studyrightElements.reduce((obj, cur) => {
-    if (!obj[cur.studentnumber]) obj[cur.studentnumber] = []
-    cur.studyright = studyrightMap[cur.studyrightid]
-    obj[cur.studentnumber].push(cur)
-    return obj
-  }, {})
-}
-
-const enrollmentsForCourses = async (codes, anonymizationSalt, unification) => {
-  let is_open = false
-
-  if (unification === 'open') is_open = true
-
-  if (unification === 'unify') {
-    is_open = {
-      [Op.in]: [false, true],
-    }
-  }
-
-  return await Enrollment.findAll({
-    include: [
-      {
-        model: Student,
-        attributes: ['studentnumber'],
-      },
-      {
-        model: Semester,
-        attributes: ['semestercode', 'name', 'yearcode', 'yearname'],
-        where: {
-          startdate: {
-            [Op.lte]: new Date(),
-          },
-        },
-      },
-    ],
-    where: {
-      course_code: {
-        [Op.in]: codes,
-      },
-      studentnumber: {
-        [Op.ne]: null,
-      },
-      enrollment_date_time: { [Op.gte]: new Date('2021-05-31') },
-      state: ['ENROLLED', 'CONFIRMED'],
-      [Op.or]: [{ is_open }, { is_open: null }],
-    },
-  })
-}
-
-const bySearchTerm = async (term, language) => {
-  const formatCourse = course => ({
-    name: course.name[language],
-    code: course.code,
-    date: course.latest_instance_date,
-  })
-
-  try {
-    const result = await byNameOrCode(`%${term}%`, language)
-    return result.map(formatCourse)
-  } catch (e) {
-    return {
-      error: e,
-    }
-  }
-}
-
-const createCourse = async (code, name, latest_instance_date) =>
-  Course.create({
-    code,
-    name,
-    latest_instance_date,
-  })
+const { sortMainCode } = require('../../util/utils')
+const {
+  enrollmentsForCourses,
+  creditsForCourses,
+  getStudentNumberToSrElementsMap,
+} = require('./creditsAndEnrollmentsOfCourse')
+const { byNameAndOrCodeLike, byCodes } = require('./courseFinders')
 
 const isOpenUniCourseCode = code => code.match(/^AY?(.+?)(?:en|fi|sv)?$/)
 
@@ -250,7 +52,7 @@ const yearlyStatsOfNew = async (
 
   const [credits, enrollments, course] = await Promise.all([
     creditsForCourses(codes, anonymizationSalt, unification),
-    enrollmentsForCourses(codes, anonymizationSalt, unification),
+    enrollmentsForCourses(codes, unification),
     Course.findOne({
       where: {
         code: coursecode,
@@ -451,90 +253,6 @@ const courseYearlyStats = async (coursecodes, separate, anonymizationSalt, combi
   return statsRegular
 }
 
-const nameLikeTerm = name => {
-  if (!name) {
-    return undefined
-  }
-  const term = `%${name.trim()}%`
-  return {
-    name: {
-      [Op.or]: {
-        fi: {
-          [Op.iLike]: term,
-        },
-        sv: {
-          [Op.iLike]: term,
-        },
-        en: {
-          [Op.iLike]: term,
-        },
-      },
-    },
-  }
-}
-
-const byCodes = codes => {
-  return Course.findAll({
-    where: {
-      code: {
-        [Op.in]: codes,
-      },
-    },
-  })
-}
-
-const codeLikeTerm = code =>
-  !code
-    ? undefined
-    : {
-        code: {
-          // Starts with code or has AY/A in front of the code
-          [Op.iRegexp]: `^(AY|ay|A|a)?${code}`,
-        },
-      }
-
-const byNameAndOrCodeLike = async (name, code) => {
-  let rawCourses = await Course.findAll({
-    where: {
-      ...nameLikeTerm(name),
-      ...codeLikeTerm(code),
-    },
-  })
-
-  const courses = rawCourses
-    .map(course => {
-      return { ...course.dataValues }
-    })
-    .sort((x, y) => getSortRank(y.code) - getSortRank(x.code))
-
-  let substitutionGroupIndex = 0
-  const visited = []
-
-  const organizeSubgroups = course => {
-    if (visited.includes(course.code)) return
-
-    let temp = []
-    if (course.substitutions !== null) {
-      temp = courses.filter(c => course.substitutions.includes(c.code))
-    }
-
-    temp.unshift(course)
-    temp.forEach(cu => {
-      if (visited.includes(course.code)) return
-      visited.push(cu.id)
-      cu.subsId = substitutionGroupIndex
-    })
-  }
-
-  courses.forEach(course => {
-    if (!visited.includes(course.id)) {
-      substitutionGroupIndex++
-      organizeSubgroups(course)
-    }
-  })
-  return { courses }
-}
-
 const getCourseProvidersForCourses = async codes => {
   return (
     await Organization.findAll({
@@ -553,16 +271,11 @@ const getCourseProvidersForCourses = async codes => {
 }
 
 module.exports = {
-  byCode,
-  byName,
-  bySearchTerm,
-  createCourse,
   courseYearlyStats,
-  byNameAndOrCodeLike,
-  byCodes,
   maxYearsToCreatePopulationFrom,
   unifyOpenUniversity,
   allCodeAlternatives,
-  findOneByCode,
   getCourseProvidersForCourses,
+  byCodes,
+  byNameAndOrCodeLike,
 }
