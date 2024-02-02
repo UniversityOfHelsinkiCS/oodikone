2e003bcc4b28a8b2ffdf6c2c83356916f215ef12
arttu <ttu91@hotmail.com>
2024-02-02 14:10:46 +0200

    [General tab] Use new sre finder to find more correct studyright for dates.

--- services/frontend/src/components/PopulationStudents/StudentTable/GeneralTab/columnHelpers/createMaps.js
+++ services/frontend/src/components/PopulationStudents/StudentTable/GeneralTab/columnHelpers/createMaps.js
@@ -1,9 +1,9 @@
-import _ from 'lodash'
+import { findStudyrightElementForClass } from 'common'
 
-export const createMaps = ({ selectedStudents, students, programmeCode, combinedProgrammeCode }) => {
+export const createMaps = ({ selectedStudents, students, programmeCode, combinedProgrammeCode, year }) => {
   const studentToStudyrightStartMap = selectedStudents.reduce((res, sn) => {
     const currentStudyright = students[sn].studyrights.find(studyright =>
-      studyright.studyright_elements.some(e => e.code === programmeCode)
+      findStudyrightElementForClass([studyright], programmeCode, year)
     )
     if (currentStudyright?.studyrightid && currentStudyright.studyrightid.slice(-2) === '-2') {
       const bachelorId = currentStudyright.studyrightid.replace(/-2$/, '-1')
@@ -16,20 +16,15 @@ export const createMaps = ({ selectedStudents, students, programmeCode, combined
   }, {})
 
   const studentToProgrammeStartMap = selectedStudents.reduce((res, sn) => {
-    const targetStudyright = _.flatten(
-      students[sn].studyrights.reduce((acc, curr) => {
-        acc.push(curr.studyright_elements)
-        return acc
-      }, [])
-    ).filter(e => e.code === programmeCode)
+    const targetStudyright = findStudyrightElementForClass(students[sn].studyrights, programmeCode, year)
     // clean up odd bachelor start dates, (givendate)
-    res[sn] = new Date(Math.max(new Date(targetStudyright[0]?.startdate), new Date(studentToStudyrightStartMap[sn])))
+    res[sn] = new Date(Math.max(new Date(targetStudyright.startdate), new Date(studentToStudyrightStartMap[sn])))
     return res
   }, {})
 
   const studentToStudyrightEndMap = selectedStudents.reduce((res, sn) => {
     const targetStudyright = students[sn].studyrights.find(studyright =>
-      studyright.studyright_elements.some(e => e.code === programmeCode)
+      findStudyrightElementForClass([studyright], programmeCode, year)
     )
     res[sn] = targetStudyright && targetStudyright.graduated === 1 ? targetStudyright.enddate : null
     return res
