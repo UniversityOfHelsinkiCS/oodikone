4b5787464c358d5ad785fb16aa9de6d1e3d82e22
arttu <ttu91@hotmail.com>
2024-02-03 13:21:59 +0200

    Population search by tag -route: Fix function not being exported in correct place

--- services/backend/src/services/populations/index.js
+++ services/backend/src/services/populations/index.js
@@ -2,10 +2,12 @@ const { bottlenecksOf } = require('./bottlenecksOf')
 const { getStudentsIncludeCoursesBetween } = require('./getStudentsIncludeCoursesBetween')
 const { optimizedStatisticsOf } = require('./optimizedStatisticsOf')
 const { studentnumbersWithAllStudyrightElements } = require('./studentnumbersWithAllStudyrightElements')
+const { getEarliestYear } = require('./shared')
 
 module.exports = {
   bottlenecksOf,
   getStudentsIncludeCoursesBetween,
   optimizedStatisticsOf,
   studentnumbersWithAllStudyrightElements,
+  getEarliestYear,
 }
