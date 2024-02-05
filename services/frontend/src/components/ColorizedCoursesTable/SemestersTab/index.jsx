428a540dfa4fccf4532c240f2fa99d8b4d26d01b
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-02-05 15:28:45 +0200

    [ColorizedCoursesTable] Have the first column stick to the left

--- services/frontend/src/components/ColorizedCoursesTable/SemestersTab/index.jsx
+++ services/frontend/src/components/ColorizedCoursesTable/SemestersTab/index.jsx
@@ -37,7 +37,7 @@ export const SemestersTab = () => {
         )}
         data={tableData}
         striped={colorMode === 'none'}
-        stretch
+        firstColumnSticky
       />
     </div>
   )
