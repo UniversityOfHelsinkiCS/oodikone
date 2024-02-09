387df4a0e0d72ea8d96d0cf7bab0aed7cbd69e48
Riku Rauhala <riku@rauhala.io>
2024-02-09 19:11:31 +0200

    [Course statistics] Fix broken scaling for the grade distribution chart

--- services/frontend/src/components/CourseStatistics/ResultTabs/Panes/gradeDistribution.jsx
+++ services/frontend/src/components/CourseStatistics/ResultTabs/Panes/gradeDistribution.jsx
@@ -8,6 +8,8 @@ export const GradeDistributionPane = ({ datasets, userHasAccessToAllStats }) => 
   const [isRelative, setIsRelative] = useState(false)
   const [splitDirection, setSplitDirection] = useState('row')
 
+  const halfWidth = datasets.filter(dataset => dataset).length > 1 && splitDirection === 'row'
+
   return (
     <PaneContent>
       <div style={{ display: 'flex', marginBottom: '2em' }}>
@@ -31,7 +33,7 @@ export const GradeDistributionPane = ({ datasets, userHasAccessToAllStats }) => 
         {datasets
           .filter(i => i)
           .map(data => (
-            <div key={data.name} style={{ flexGrow: 1, flexBasis: 1, width: '100%' }}>
+            <div key={data.name} style={{ flexGrow: 1, flexBasis: 1, maxWidth: halfWidth ? '50%' : '100%' }}>
               <h3>{data.name}</h3>
               <GradeDistributionChart
                 data={data}
