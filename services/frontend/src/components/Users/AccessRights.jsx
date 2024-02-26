d02dff573c8a57d1da71dcb681fd8484e7c3f50a
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-02-26 10:58:41 +0200

    [Users] Display IAM group based study programme access rights on user page

--- services/frontend/src/components/Users/AccessRights.jsx
+++ services/frontend/src/components/Users/AccessRights.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Form, Header, List, Message } from 'semantic-ui-react'
+import { Button, Form, Header, Icon, List, Message, Popup, Radio } from 'semantic-ui-react'
 
 import { useAddUserUnitsMutation, useRemoveUserUnitsMutation } from 'redux/users'
 import { useGetUnfilteredProgrammesQuery } from 'redux/populations'
@@ -12,6 +12,7 @@ export const AccessRights = ({ user }) => {
   const { getTextIn } = useLanguage()
   const [accessRightsToBeAdded, setAccessRightsToBeAdded] = useState([])
   const [accessRightsToBeRemoved, setAccessRightsToBeRemoved] = useState([])
+  const [filterOldProgrammes, setFilterOldProgrammes] = useState(true)
   const { data: elementdetails = [] } = useGetAllElementDetailsQuery()
   const { data: allProgrammes } = useGetUnfilteredProgrammesQuery()
   const programmes = Object.values(allProgrammes?.programmes || {})
@@ -31,7 +32,7 @@ export const AccessRights = ({ user }) => {
     }
   }
 
-  const options = programmes
+  let options = programmes
     .map(({ code, name }) => ({
       key: code,
       value: code,
@@ -40,10 +41,22 @@ export const AccessRights = ({ user }) => {
     }))
     .sort(createLocaleComparator('text'))
 
+  if (filterOldProgrammes) {
+    options = options.filter(({ value }) => ['MH', 'KH'].includes(value.slice(0, 2)))
+  }
+
   const currentAccessRights = regularRights
     .reduce((acc, { elementDetailCode }) => {
       const elementInfo = elementdetails.find(e => e.code === elementDetailCode)
       if (elementInfo) acc.push({ code: elementInfo.code, name: getTextIn(elementInfo.name) })
+      return acc
+    }, [])
+    .sort(createLocaleComparator('name'))
+
+  const currentIamAccessRights = Object.entries(user.iam_groups)
+    .reduce((acc, [code, rights]) => {
+      const elementInfo = elementdetails.find(e => e.code === code)
+      acc.push({ code, name: getTextIn(elementInfo?.name), rights: { read: rights.read, admin: rights.admin } })
       return acc
     }, [])
     .sort(createLocaleComparator('name'))
@@ -61,21 +74,31 @@ export const AccessRights = ({ user }) => {
       <Header size="small" content="Select new study programme access rights" style={{ marginBottom: '8px' }} />
       <Message error content="Modifying access rights failed." />
       <Message success content="The access rights were updated successfully." />
-      <Form.Dropdown
-        name="programme"
-        placeholder="Select study programmes to add"
-        data-cy="access-rights-form"
-        options={options}
-        multiple
-        value={accessRightsToBeAdded}
-        onChange={(_, { value }) => setAccessRightsToBeAdded(value)}
-        fluid
-        search={textAndDescriptionSearch}
-        selection
-        clearable
-        selectOnBlur={false}
-        selectOnNavigation={false}
-      />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+        <div style={{ flexGrow: 1 }}>
+          <Form.Dropdown
+            name="programme"
+            placeholder="Select study programmes to add"
+            data-cy="access-rights-form"
+            options={options}
+            multiple
+            value={accessRightsToBeAdded}
+            onChange={(_, { value }) => setAccessRightsToBeAdded(value)}
+            fluid
+            search={textAndDescriptionSearch}
+            selection
+            clearable
+            selectOnBlur={false}
+            selectOnNavigation={false}
+          />
+        </div>
+        <Radio
+          toggle
+          label="Filter out old and specialized programmes"
+          checked={filterOldProgrammes}
+          onChange={() => setFilterOldProgrammes(!filterOldProgrammes)}
+        />
+      </div>
       <Header size="small" content={`Current study programme access rights (${currentAccessRights.length})`} />
       <List divided>
         {currentAccessRights.map(({ code, name }) => (
@@ -98,6 +121,47 @@ export const AccessRights = ({ user }) => {
               content={`${name} (${code})`}
               style={{ color: accessRightsToBeRemoved.includes(code) ? 'grey' : '' }}
             />
+          </List.Item>
+        ))}
+      </List>
+      <Header
+        size="small"
+        content={`Current IAM group based study programme access rights (${Object.keys(user.iam_groups).length})`}
+      />
+      <List divided>
+        {currentIamAccessRights.map(({ code, name, rights }) => (
+          <List.Item key={code}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ flexGrow: 1 }}>
+                <List.Content content={`${name} (${code})`} />
+              </div>
+              <List.Content
+                content={
+                  <Popup
+                    trigger={
+                      <Icon
+                        name="exclamation triangle"
+                        color={!rights.read || rights.admin ? 'grey' : 'green'}
+                        disabled={!rights.read || rights.admin}
+                      />
+                    }
+                    content="Limited rights"
+                    position="top center"
+                  />
+                }
+              />
+              <List.Content
+                content={
+                  <Popup
+                    trigger={
+                      <Icon name="check circle" color={!rights.admin ? 'grey' : 'green'} disabled={!rights.admin} />
+                    }
+                    content="Full rights"
+                    position="top center"
+                  />
+                }
+              />
+            </div>
           </List.Item>
         ))}
       </List>
