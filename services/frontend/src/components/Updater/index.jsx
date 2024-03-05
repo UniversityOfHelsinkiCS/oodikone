eeeeb1c67197fb7d9731e5d2be538d021df0bed9
arttu <ttu91@hotmail.com>
2024-03-05 16:11:09 +0200

    Revert "Add explode backend button & route"
    This reverts commit b2b2d87ff31f5614b6aff1fe0683235c2efde6b1.

--- services/frontend/src/components/Updater/index.jsx
+++ services/frontend/src/components/Updater/index.jsx
@@ -104,15 +104,6 @@ export const Updater = () => {
           >
             Cause frontend crash
           </button>
-          <button
-            style={{ marginLeft: '5px' }}
-            type="button"
-            onClick={() => {
-              callApi('explode', 'get')
-            }}
-          >
-            Cause backend error
-          </button>
         </p>
       </Message>
       <Form>
