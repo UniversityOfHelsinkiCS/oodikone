1dc2c942f318acfcefc9a69a5f1acb5937f0fbf3
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-03-08 13:06:58 +0200

    Revert "[Users] Move language-related stuff to userRouter"
    This reverts commit f5ae6d42e9a8de9a424533e24ef43bf72d861e80.

--- services/frontend/src/components/LanguagePicker/useLanguage.jsx
+++ services/frontend/src/components/LanguagePicker/useLanguage.jsx
@@ -48,7 +48,7 @@ export const useLanguage = () => {
     }
 
     setState(newLanguage)
-    callApi('/users/language', 'post', { language: newLanguage })
+    callApi('/language', 'post', { language: newLanguage })
   }
 
   return {
