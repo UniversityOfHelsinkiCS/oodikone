04408fb3f2855ff3e485c0cc1ad89d6ef2ad81b9
Riku Rauhala <riku@rauhala.io>
2024-04-02 14:30:16 +0300

    Remove unused exported declaration

--- services/frontend/src/constants/types.js
+++ services/frontend/src/constants/types.js
@@ -1,10 +1,5 @@
 import { arrayOf, number, oneOfType, shape, string } from 'prop-types'
 
-export const graphDataType = shape({
-  name: string.isRequired,
-  data: arrayOf(number),
-})
-
 const courseDataType = shape({
   id: oneOfType([number, string]),
   category: oneOfType([number, string]),
