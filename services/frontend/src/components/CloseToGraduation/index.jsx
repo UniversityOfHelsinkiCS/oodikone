fbabd4ff3585f18d68d493c682f5c3dafaa9081e
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-04-02 17:23:53 +0300

    [Close to graduation] Add a programme filter

--- services/frontend/src/components/CloseToGraduation/index.jsx
+++ services/frontend/src/components/CloseToGraduation/index.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { Divider, Dropdown, Form, Icon, Loader, Message } from 'semantic-ui-react'
 
-import { reformatDate } from '@/common'
+import { createLocaleComparator, reformatDate } from '@/common'
 import { useTitle } from '@/common/hooks'
 import { StudentInfoItem } from '@/components/common/StudentInfoItem'
 import { useLanguage } from '@/components/LanguagePicker/useLanguage'
 import { SortableTable } from '@/components/SortableTable'
 import { StudentNameVisibilityToggle, useStudentNameVisibility } from '@/components/StudentNameVisibilityToggle'
 import { useGetStudentsCloseToGraduationQuery } from '@/redux/closeToGraduation'
+import { useFilteredAndFormattedElementDetails } from '@/redux/elementdetails'
 import { useGetFacultiesQuery } from '@/redux/facultyStats'
 
 const getColumns = (getTextIn, namesVisible, studyTrackVisible) => [
@@ -106,9 +107,14 @@ export const CloseToGraduation = () => {
 
   const { data: students, isError, isLoading } = useGetStudentsCloseToGraduationQuery()
   const { data: faculties = [] } = useGetFacultiesQuery()
+  const programmes = useFilteredAndFormattedElementDetails()
   const { getTextIn } = useLanguage()
   const { visible: namesVisible } = useStudentNameVisibility()
   const [chosenFaculties, setChosenFaculties] = useState([])
+  const [chosenProgrammes, setChosenProgrammes] = useState([])
+  const [rowCount, setRowCount] = useState(0)
+
+  const handleRowCountChange = count => setRowCount(count)
 
   const renderContent = () => {
     if (isError) {
@@ -119,28 +125,63 @@ export const CloseToGraduation = () => {
 
     if (isLoading) return <Loader active inline="centered" />
 
+    const facultyOptions = faculties.map(f => ({
+      key: f.code,
+      text: getTextIn(f.name),
+      value: f.code,
+      description: f.code,
+    }))
+    const programmeOptions = programmes
+      .filter(p => !p.value.startsWith('MH') && !p.value.includes('+'))
+      .filter(p => (chosenFaculties.length > 0 ? chosenFaculties.includes(p.value.slice(1, 4)) : true))
+      .sort(createLocaleComparator('text'))
+    const filteredStudents = students.filter(s => {
+      if (chosenProgrammes.length > 0) return chosenProgrammes.includes(s.programme.code)
+      return chosenFaculties.length === 0 || chosenFaculties.includes(s.programme.code.slice(1, 4))
+    })
+
     return (
       <>
         <StudentNameVisibilityToggle style={{ marginBottom: '2em' }} />
         <Form style={{ width: '100%', marginBottom: '1em' }}>
+          <Message info>
+            You can filter students by choosing a degree programme and/or a faculty. You can also select multiple
+            programmes or faculties. Please note that if you select a faculty, only the degree programmes belonging to
+            that faculty will be shown.
+          </Message>
           <Form.Field>
-            <label>Choose faculties to filter by:</label>
+            <label>Faculties</label>
             <Dropdown
               multiple
-              onChange={(_, { value }) => setChosenFaculties([...value])}
-              options={faculties.map(f => ({ key: f.code, text: getTextIn(f.name), value: f.code }))}
-              placeholder="Faculty"
+              onChange={(_, { value }) => {
+                setChosenFaculties([...value])
+                setChosenProgrammes(chosenProgrammes.filter(p => value.includes(p.slice(1, 4))))
+              }}
+              options={facultyOptions}
+              placeholder="Choose faculties..."
               selection
               value={chosenFaculties}
             />
           </Form.Field>
+          <Form.Field>
+            <label>Degree programmes</label>
+            <Dropdown
+              multiple
+              onChange={(_, { value }) => setChosenProgrammes([...value])}
+              options={programmeOptions}
+              placeholder="Choose degree programmes..."
+              selection
+              value={chosenProgrammes}
+            />
+          </Form.Field>
         </Form>
         <SortableTable
-          columns={getColumns(getTextIn, namesVisible)}
-          data={students.filter(
-            s => chosenFaculties.length === 0 || chosenFaculties.includes(s.programme.code.slice(1, 4))
-          )}
+          columns={getColumns(getTextIn, namesVisible, chosenProgrammes.length === 1)}
+          data={filteredStudents}
           featureName="students_close_to_graduation"
+          handleRowCountChange={handleRowCountChange}
+          title={`Students close to graduation (${rowCount} out of ${students.length} students shown)`}
+          useFilteredDataOnExport
         />
       </>
     )
