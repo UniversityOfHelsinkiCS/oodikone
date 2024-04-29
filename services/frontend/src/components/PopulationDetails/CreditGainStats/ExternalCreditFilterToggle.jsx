252f4a8592b796684f7d7f576370499d07903ba0
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-04-29 10:02:06 +0300

    [Class statistics] Fix resetting credit filter

--- services/frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.jsx
+++ services/frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 
 import { getMonths } from '@/common/query'
@@ -8,8 +7,8 @@ import { FilterToggle } from '@/components/FilterView/FilterToggle'
 import { useFilters } from '@/components/FilterView/useFilters'
 
 export const ExternalCreditFilterToggle = ({ min, max }) => {
-  const { filterDispatch } = useFilters()
-  const { min: currentMin, max: currentMax } = useSelector(creditFilter.selectors.selectOptions)
+  const { filterDispatch, useFilterSelector } = useFilters()
+  const { min: currentMin, max: currentMax } = useFilterSelector(creditFilter.selectors.selectOptions)
 
   const months = getMonths(useLocation())
   const limitedMax = max === 0 ? 1 : max
@@ -19,7 +18,7 @@ export const ExternalCreditFilterToggle = ({ min, max }) => {
     <FilterToggle
       active={active}
       applyFilter={() => filterDispatch(creditFilter.actions.setOptions({ min, max: limitedMax }))}
-      clearFilter={() => filterDispatch(creditFilter.actions.clear())}
+      clearFilter={() => filterDispatch(creditFilter.actions.reset())}
       filter={creditFilter}
       filterName="Credit Filter"
       popupContent={`Rajaa opiskelijat ensimmäisen ${months} kuukauden aikana saatujen opintopisteiden perusteella`}
