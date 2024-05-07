4506005e582dc7013603c14a8562e82aced91b4b
Riku Rauhala <riku@rauhala.io>
2024-05-07 10:51:58 +0300

    Add the actual comparator function...

--- services/frontend/src/common/index.js
+++ services/frontend/src/common/index.js
@@ -411,8 +411,25 @@ export const getCurrentSemester = allSemesters => {
  * @param {string} field - The field to sort by (optional: if not given, the function will sort by the strings themselves)
  */
 export const createLocaleComparator = (field = null) => {
-  if (!field) return (val1, val2) => val1.localeCompare(val2, 'fi', { sensitivity: 'accent' })
+  if (!field) {
+    return (val1, val2) => val1.localeCompare(val2, 'fi', { sensitivity: 'accent' })
+  }
   return (val1, val2) => val1[field]?.localeCompare(val2[field], 'fi', { sensitivity: 'accent' })
+}
+
+export const createPinnedFirstComparator = pinnedProgrammes => {
+  const localeComparator = createLocaleComparator('code')
+  return (programmeA, programmeB) => {
+    const pinnedA = pinnedProgrammes.includes(programmeA.code)
+    const pinnedB = pinnedProgrammes.includes(programmeB.code)
+    if (pinnedA && !pinnedB) {
+      return -1
+    }
+    if (!pinnedA && pinnedB) {
+      return 1
+    }
+    return localeComparator(programmeA, programmeB)
+  }
 }
 
 const interpolateColor = (color1, color2, factor) =>
