98cd4df0eb2cdce1f582726ffd6b0eec574d7eff
ShootingStar91 <ttu91@hotmail.com>
2024-06-10 15:06:07 +0300

    Add types to utils.ts

--- services/backend/src/util/utils.ts
+++ services/backend/src/util/utils.ts
@@ -36,7 +36,7 @@ const bscsCode = /BSCS??/
 
 const codeRegexes = [openUniCodeA, openUniCode, bscsCode, oldNumericCode, newLetterBasedCode, digi]
 
-const getSortRank = code => {
+const getSortRank = (code: string) => {
   for (let i = 0; i < codeRegexes.length; i++) {
     if (codeRegexes[i].test(code)) {
       return i
@@ -45,23 +45,24 @@ const getSortRank = code => {
   return 3 // if no hit, put before open uni courses
 }
 
-const sortMainCode = codeArray => {
-  if (!codeArray) return []
-  return codeArray.sort((x, y) => getSortRank(y) - getSortRank(x))
+const sortMainCode = (codes: string[] | undefined) => {
+  if (!codes) return []
+  return codes.sort((x, y) => getSortRank(y) - getSortRank(x))
 }
 
 /** Returns a sorting function that can be used to sort strings so that Finnish alphabetical order is respected.
  * @param {string} field - The field to sort by (optional: if not given, the function will sort by the strings themselves)
  */
-const createLocaleComparator = (field = null) => {
-  if (!field) return (val1, val2) => val1.localeCompare(val2, 'fi', { sensitivity: 'accent' })
-  return (val1, val2) => val1[field].localeCompare(val2[field], 'fi', { sensitivity: 'accent' })
+const createLocaleComparator = (field: string | null = null) => {
+  if (!field) return (val1: string, val2: string) => val1.localeCompare(val2, 'fi', { sensitivity: 'accent' })
+  return (val1: string, val2: string) => val1[field].localeCompare(val2[field], 'fi', { sensitivity: 'accent' })
 }
 
 const getFullStudyProgrammeRights = programmeRights =>
   programmeRights.filter(({ limited }) => !limited).map(({ code }) => code)
 
-const hasFullAccessToStudentData = roles => roles?.some(role => ['admin', 'fullSisuAccess'].includes(role))
+// TODO better type for roles
+const hasFullAccessToStudentData = (roles: string[]) => roles?.some(role => ['admin', 'fullSisuAccess'].includes(role))
 
 module.exports = {
   mapToProviders,
