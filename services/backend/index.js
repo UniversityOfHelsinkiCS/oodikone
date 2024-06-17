a7a739a29af4491f461d3c311f21c69354acafb3
ShootingStar91 <ttu91@hotmail.com>
2024-06-17 19:53:05 +0300

    Handle sigterm and sigint to hot reload with tsx better

--- services/backend/index.js
+++ services/backend/index.js
@@ -4,3 +4,6 @@ const logger = require('./src/util/logger')
 process.on('unhandledRejection', reason => {
   logger.error({ message: 'unhandledRejection: ', meta: reason })
 })
+
+process.on('SIGTERM', process.exit)
+process.on('SIGINT', process.exit)
