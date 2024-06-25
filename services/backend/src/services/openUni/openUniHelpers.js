98b7ebe5d5acfa5f890057ab176bb45306add18a
Riku Rauhala <riku@rauhala.io>
2024-06-25 09:38:19 +0300

    [Backend] Refactor openUniHelpers

--- services/backend/src/services/openUni/openUniHelpers.js
+++ services/backend/src/services/openUni/openUniHelpers.js
@@ -1,38 +1,37 @@
-const mapCourseInfo = course => {
-  const { code, name } = course
-  return {
-    label: code,
-    name,
-  }
+const mapObject = (obj, schema) => {
+  return Object.keys(schema).reduce((acc, key) => {
+    acc[key] = obj[schema[key]]
+    return acc
+  }, {})
 }
 
-const mapOpenCredits = credit => {
-  const { attainment_date, course_code, grade, student_studentnumber } = credit
-  return {
-    attainmentCourseCode: course_code,
-    attainmentDate: attainment_date,
-    attainmentGrade: grade,
-    attainmentStudentNumber: student_studentnumber,
-  }
-}
+const mapCourseInfo = course =>
+  mapObject(course, {
+    label: 'code',
+    name: 'name',
+  })
 
-const mapOpenEnrollments = enrollment => {
-  const { course_code, enrollment_date_time, studentnumber } = enrollment
-  return {
-    enrollmentCourseCode: course_code,
-    enrollmentDateTime: enrollment_date_time,
-    enrollmentStudentNumber: studentnumber,
-  }
-}
+const mapOpenCredits = credit =>
+  mapObject(credit, {
+    attainmentCourseCode: 'course_code',
+    attainmentDate: 'attainment_date',
+    attainmentGrade: 'grade',
+    attainmentStudentNumber: 'student_studentnumber',
+  })
 
-const mapStudentInfo = student => {
-  const { studentnumber, email, secondary_email } = student
-  return {
-    studentNumber: studentnumber,
-    email,
-    secondaryEmail: secondary_email,
-  }
-}
+const mapOpenEnrollments = enrollment =>
+  mapObject(enrollment, {
+    enrollmentCourseCode: 'course_code',
+    enrollmentDateTime: 'enrollment_date_time',
+    enrollmentStudentNumber: 'studentnumber',
+  })
+
+const mapStudentInfo = student =>
+  mapObject(student, {
+    studentNumber: 'studentnumber',
+    email: 'email',
+    secondaryEmail: 'secondary_email',
+  })
 
 module.exports = {
   mapCourseInfo,
