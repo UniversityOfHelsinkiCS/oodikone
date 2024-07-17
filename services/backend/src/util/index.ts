ddac6ea36cf277b529b1e3a32a06f53f7bfa9fed
Riku Rauhala <riku@rauhala.io>
2024-07-17 13:42:04 +0300

    [Backend] Improve util typing

--- services/backend/src/util/index.ts
+++ services/backend/src/util/index.ts
@@ -1,13 +1,21 @@
 import { Role } from '../types'
 
-/** Returns a sorting function that can be used to sort strings so that Finnish alphabetical order is respected.
- * @param {string} field - The field to sort by (optional: if not given, the function will sort by the strings themselves)
+type Comparator = (val1: string, val2: string) => number
+type FieldComparator = (val1: Record<string, any>, val2: Record<string, any>) => number
+
+/**
+ * Returns a sorting function that can be used to sort strings
+ * so that Finnish alphabetical order is respected.
+ *
+ * @param {string} [field] - The field to sort by, defaults to null.
  */
-export const createLocaleComparator = (field: string | null = null) => {
+export const createLocaleComparator = (field: string | null = null): Comparator | FieldComparator => {
   if (!field) {
     return (val1: string, val2: string) => val1.localeCompare(val2, 'fi', { sensitivity: 'accent' })
   }
-  return (val1: string, val2: string) => val1[field].localeCompare(val2[field], 'fi', { sensitivity: 'accent' })
+  return (val1: Record<string, any>, val2: Record<string, any>) => {
+    return val1[field].localeCompare(val2[field], 'fi', { sensitivity: 'accent' })
+  }
 }
 
 export const getFullStudyProgrammeRights = programmeRights => {
@@ -15,7 +23,7 @@ export const getFullStudyProgrammeRights = programmeRights => {
 }
 
 export const hasFullAccessToStudentData = (roles: Role[]): boolean => {
-  const rolesWithFullAccess = ['admin', 'fullSisuAccess']
+  const rolesWithFullAccess: Role[] = ['admin', 'fullSisuAccess']
   return roles?.some(role => rolesWithFullAccess.includes(role))
 }
 
@@ -25,7 +33,7 @@ export const validateParamLength = (param: string, minLength: number): boolean =
   return param && param.trim().length >= minLength
 }
 
-export const sortByProgrammeCode = (a: string, b: string) => {
+export const sortByProgrammeCode = (a: string, b: string): number => {
   const getPrefixPriority = (code: string): 1 | 2 | 3 => {
     if (code.startsWith('KH')) return 1
     if (code.startsWith('MH')) return 2
