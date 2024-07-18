60ff7a210d386ba78b9b164af0354b0f7e03eeca
Riku Rauhala <riku@rauhala.io>
2024-07-18 14:20:11 +0300

    [Backend] Type and refactor userService

--- services/backend/src/services/userService.ts
+++ services/backend/src/services/userService.ts
@@ -1,48 +1,47 @@
-const { isEqual, keyBy, omit, uniq, uniqBy } = require('lodash')
-const { LRUCache } = require('lru-cache')
+import { isEqual, keyBy, omit, uniq } from 'lodash'
+import { LRUCache } from 'lru-cache'
 
-const { serviceProvider } = require('../config')
-const { sequelizeUser } = require('../database/connection')
-const { User } = require('../models/user')
-const { createLocaleComparator, getFullStudyProgrammeRights, hasFullAccessToStudentData } = require('../util')
-const { sendNotificationAboutNewUser } = require('./mailservice')
-const { getSisuAuthData, personSearchQuery, getGraphqlData } = require('./oriProvider')
-const { getStudentnumbersByElementdetails } = require('./students')
-const { checkStudyGuidanceGroupsAccess, getAllStudentsUserHasInGroups } = require('./studyGuidanceGroups')
-const { getAllUserAccess, getUserIams, getUserIamAccess } =
-  serviceProvider === 'Toska' ? require('../util/jami') : require('../util/mami')
+import { serviceProvider } from '../config'
+import { roles } from '../config/roles'
+import { sequelizeUser } from '../database/connection'
+import { User } from '../models/user'
+import { Role } from '../types'
+import { createLocaleComparator, getFullStudyProgrammeRights, hasFullAccessToStudentData } from '../util'
+import jami from '../util/jami'
+import mami from '../util/mami'
+import { sendNotificationAboutNewUser } from './mailservice'
+import { getSisuAuthData, personSearchQuery, getGraphqlData } from './oriProvider'
+import { getStudentnumbersByElementdetails } from './students'
+import { checkStudyGuidanceGroupsAccess, getAllStudentsUserHasInGroups } from './studyGuidanceGroups'
+
+const userAccessUtils = serviceProvider === 'Toska' ? jami : mami
+const { getAllUserAccess, getUserIams, getUserIamAccess } = userAccessUtils
 
 const courseStatisticsGroup = 'grp-oodikone-basic-users'
 const facultyStatisticsGroup = 'grp-oodikone-users'
-
-const roles = [
-  'teachers',
-  'admin',
-  'courseStatistics',
-  'studyGuidanceGroups',
-  'facultyStatistics',
-  'openUniSearch',
-  'katselmusViewer',
-  'fullSisuAccess',
-]
 
 // Max 25 users can be stored in the cache, and the data is valid for 1 hour
 const userDataCache = new LRUCache({ max: 25, ttl: 1000 * 60 * 60 })
 
 const findUser = async where => User.findOne({ where })
 
-const deleteOutdatedUsers = async () =>
-  sequelizeUser.query("DELETE FROM users WHERE last_login < CURRENT_DATE - INTERVAL '18 months'")
+export const deleteOutdatedUsers = async () => {
+  return sequelizeUser.query("DELETE FROM users WHERE last_login < CURRENT_DATE - INTERVAL '18 months'")
+}
 
-const modifyAccess = async (username, roles) => {
+const isRole = (role: string): role is Role => {
+  return (roles as readonly string[]).includes(role)
+}
+
+export const modifyAccess = async (username: string, rolesOfUser: Record<string, boolean>) => {
   const user = await findUser({ username })
-  const rolesToAdd = Object.keys(roles).filter(code => roles[code])
-  user.roles = rolesToAdd
+  const newRoles = Object.keys(rolesOfUser).filter(role => isRole(role) && rolesOfUser[role]) as Role[]
+  user.roles = newRoles
   await user.save()
   userDataCache.delete(username)
 }
 
-const modifyElementDetails = async (id, codes, enable) => {
+export const modifyElementDetails = async (id: bigint, codes: string[], enable: boolean) => {
   const user = await findUser({ id })
   if (enable === true) {
     user.programmeRights = uniq([...user.programmeRights, ...codes])
@@ -53,66 +52,107 @@ const modifyElementDetails = async (id, codes, enable) => {
   userDataCache.delete(user.username)
 }
 
-const updateUser = async (username, fields) => {
+export const updateUser = async (username: string, fields: Array<Record<string, any>>) => {
   const user = await findUser({ username })
-  if (!user) throw new Error(`User ${username} not found`)
+  if (!user) {
+    throw new Error(`User ${username} not found`)
+  }
   await user.update(fields)
   userDataCache.delete(username)
 }
 
-const getStudyProgrammeRights = (iamAccess, specialGroup, userProgrammes) => [
-  ...(specialGroup?.fullSisuAccess
-    ? []
-    : Object.entries(iamAccess || {}).map(([code, rights]) => ({
-        code,
-        limited: !rights.admin,
-        isIamBased: true,
-      }))),
-  ...userProgrammes.map(code => ({ code, limited: false, isIamBased: false })),
-]
+type IamAccess = Record<string, Record<'read' | 'write' | 'admin', boolean>>
 
-const formatUser = async (user, getStudentAccess = true) => {
-  const { roles } = user
+type ProgrammeRights = {
+  code: string
+  limited: boolean
+  isIamBased: boolean
+}
+
+const getIamBasedRights = (iamAccess: IamAccess): ProgrammeRights[] => {
+  if (!iamAccess) {
+    return []
+  }
+
+  return Object.entries(iamAccess).map(([code, rights]) => ({
+    code,
+    limited: !rights.admin,
+    isIamBased: true,
+  }))
+}
+
+const getUserProgrammesRights = (userProgrammes: string[]): ProgrammeRights[] => {
+  return userProgrammes.map(code => ({
+    code,
+    limited: false,
+    isIamBased: false,
+  }))
+}
+
+const getStudyProgrammeRights = (
+  iamAccess: IamAccess,
+  specialGroup: Record<string, boolean>,
+  userProgrammes: string[]
+): ProgrammeRights[] => {
+  const hasFullSisuAccess = specialGroup?.fullSisuAccess
+
+  const iamBasedRights = hasFullSisuAccess ? [] : getIamBasedRights(iamAccess)
+  const userProgrammesRights = getUserProgrammesRights(userProgrammes)
+
+  const studyProgrammeRights = [...iamBasedRights, ...userProgrammesRights]
+  return studyProgrammeRights
+}
+
+type ExpandedUser = User & { iamGroups: string[]; mockedBy?: string; programmeRights: ProgrammeRights[] }
+
+const formatUser = async (user: ExpandedUser, getStudentAccess: boolean = true) => {
   const fullStudyProgrammeRights = getFullStudyProgrammeRights(user.programmeRights)
+  const shouldFetchStudentAccess = getStudentAccess && !hasFullAccessToStudentData(user.roles)
 
-  // No need to fetch student numbers if the user already has full access to student data
-  const studentsUserCanAccess =
-    !getStudentAccess || hasFullAccessToStudentData(roles)
-      ? []
-      : uniqBy(
-          (
-            await Promise.all([
-              getStudentnumbersByElementdetails(fullStudyProgrammeRights),
-              getAllStudentsUserHasInGroups(user.sisuPersonId),
-            ])
-          ).flat()
-        )
+  let studentsUserCanAccess = []
+  if (shouldFetchStudentAccess) {
+    const promises = [
+      getStudentnumbersByElementdetails(fullStudyProgrammeRights),
+      getAllStudentsUserHasInGroups(user.sisuPersonId),
+    ]
 
-  // attribute naming is a bit confusing, but it's used so widely in oodikone, that it's not probably worth changing
-  return {
-    id: user.id, // our internal id, not the one from sis, (e.g. 101)
-    userId: user.username, // This is only used for logging purposes (Grafana), please use either id or username for everything else
+    const studentLists = await Promise.all(promises)
+    const flatStudentNumbers = studentLists.flat()
+
+    studentsUserCanAccess = uniq(flatStudentNumbers)
+  }
+
+  const formattedUser = {
+    id: user.id,
+    userId: user.username,
     username: user.username,
     name: user.fullName,
     language: user.language,
     sisPersonId: user.sisuPersonId,
     email: user.email,
-    roles,
-    studentsUserCanAccess, // studentnumbers used in various parts of backend. For admin this is usually empty, since no programmes / faculties are set.
-    isAdmin: roles.includes('admin'),
+    roles: user.roles,
+    studentsUserCanAccess,
+    isAdmin: user.roles.includes('admin'),
     programmeRights: user.programmeRights,
     iamGroups: user.iamGroups || [],
     mockedBy: user.mockedBy,
     lastLogin: user.lastLogin,
   }
+
+  return formattedUser
 }
 
-const formatUserForFrontend = async user => {
+const formatUserForFrontend = async (user: ExpandedUser) => {
   const formattedUser = await formatUser(user, false)
   return omit(formattedUser, ['studentsUserCanAccess', 'isAdmin', 'mockedBy', 'userId'])
 }
 
-const updateAccessGroups = async (username, iamGroups, specialGroup, sisId) => {
+const updateAccessGroups = async (
+  username: string,
+  iamGroups: string[],
+  specialGroup: Record<string, boolean>,
+  sisId: string
+) => {
   const { jory, hyOne, superAdmin, openUni, katselmusViewer, fullSisuAccess } = specialGroup
   const userFromDb = await findUser({ username })
   const currentAccessGroups = userFromDb.roles
@@ -128,7 +168,7 @@ const updateAccessGroups = async (username, iamGroups, specialGroup, sisId) => {
     ...(openUni ? ['openUniSearch'] : []),
     ...(katselmusViewer ? ['katselmusViewer'] : []),
     ...(fullSisuAccess ? ['fullSisuAccess'] : []),
-  ]
+  ] as Role[]
 
   if (!isEqual(newAccessGroups.sort(), currentAccessGroups.sort())) {
     userFromDb.roles = newAccessGroups
@@ -136,8 +176,8 @@ const updateAccessGroups = async (username, iamGroups, specialGroup, sisId) => {
   }
 }
 
-const findAll = async () => {
-  const users = (await User.findAll()).map(user => user.toJSON())
+export const findAll = async () => {
+  const users = (await User.findAll()).map(user => user.toJSON()) as User[]
   const userAccess = await getAllUserAccess(users.map(user => user.sisuPersonId))
   const userAccessMap = keyBy(userAccess, 'id')
 
@@ -152,10 +192,11 @@ const findAll = async () => {
   return formattedUsers.sort(createLocaleComparator('name'))
 }
 
-const findOne = async id => {
+export const findOne = async (id: bigint) => {
   const user = (await findUser({ id })).toJSON()
-  if (!user) throw new Error(`User with id ${id} not found`)
-
+  if (!user) {
+    throw new Error(`User with id ${id} not found`)
+  }
   const iamGroups = await getUserIams(user.sisuPersonId)
   const { iamAccess, specialGroup } = await getUserIamAccess(user.sisuPersonId, iamGroups)
   const programmeRights = getStudyProgrammeRights(iamAccess, specialGroup, user.programmeRights)
@@ -163,18 +204,20 @@ const findOne = async id => {
   return formattedUser
 }
 
-const getOrganizationAccess = async (sisPersonId, iamGroups) => {
-  if (!iamGroups.length) return {}
+export const getOrganizationAccess = async (sisPersonId: string, iamGroups: string[]) => {
+  if (!iamGroups.length) {
+    return {}
+  }
   const { iamAccess, specialGroup } = await getUserIamAccess(sisPersonId, iamGroups)
   return { access: iamAccess || {}, specialGroup }
 }
 
-const basicGetMockedUser = async ({ userToMock, mockedBy }) => {
+const basicGetMockedUser = async ({ userToMock, mockedBy }: { userToMock: string; mockedBy: string }) => {
   // Using different keys for users being mocked to prevent users from seeing themselves as mocked. Also, if the user
   // is already logged in, we don't want the regular data from the cache because that doesn't have the mockedBy field
   const cacheKey = `mocking-as-${userToMock}`
   if (userDataCache.has(cacheKey)) {
-    const cachedUser = userDataCache.get(cacheKey)
+    const cachedUser = userDataCache.get(cacheKey) as User & { mockedBy: string }
     if (cachedUser.mockedBy !== mockedBy) {
       cachedUser.mockedBy = mockedBy
       userDataCache.set(cacheKey, cachedUser)
@@ -182,26 +225,43 @@ const basicGetMockedUser = async ({ userToMock, mockedBy }) => {
     return cachedUser
   }
 
-  const userFromDb = (await findUser({ username: userToMock })).toJSON()
+  const userFromDb = (await findUser({ username: userToMock })).toJSON() as User
   const iamGroups = await getUserIams(userFromDb.sisuPersonId)
   const { access, specialGroup } = await getOrganizationAccess(userFromDb.sisuPersonId, iamGroups)
   const programmeRights = getStudyProgrammeRights(access, specialGroup, userFromDb.programmeRights)
-
   const mockedUser = await formatUser({ ...userFromDb, iamGroups, programmeRights, mockedBy })
   userDataCache.set(cacheKey, mockedUser)
   return mockedUser
 }
 
-const fdGetMockedUser = async ({ userToMock, mockedBy }) => {
+const fdGetMockedUser = async ({ userToMock, mockedBy }: { userToMock: string; mockedBy: string }) => {
   const mockedByFromDb = (await findUser({ username: mockedBy })).toJSON()
-
-  if (!mockedByFromDb) return null
-
+  if (!mockedByFromDb) {
+    return null
+  }
   return await basicGetMockedUser({ userToMock, mockedBy })
 }
 
-const toskaGetUser = async ({ username, name, email, iamGroups, specialGroup, sisId, access }) => {
-  if (userDataCache.has(username)) return userDataCache.get(username)
+const toskaGetUser = async ({
+  username,
+  name,
+  email,
+  iamGroups,
+  specialGroup,
+  sisId,
+  access,
+}: {
+  username: string
+  name: string
+  email: string
+  iamGroups: string[]
+  specialGroup: Record<string, boolean>
+  sisId: string
+  access: IamAccess
+}) => {
+  if (userDataCache.has(username)) {
+    return userDataCache.get(username)
+  }
 
   const isNewUser = !(await User.findOne({ where: { username } }))
   await User.upsert({ fullName: name, username, email, sisuPersonId: sisId, lastLogin: new Date() })
@@ -210,16 +270,22 @@ const toskaGetUser = async ({ username, name, email, iamGroups, specialGroup, si
 
   const programmeRights = getStudyProgrammeRights(access, specialGroup, userFromDb.programmeRights)
   const user = await formatUser({ ...userFromDb, iamGroups, programmeRights })
-  if (isNewUser) await sendNotificationAboutNewUser({ userId: username, userFullName: name })
+  if (isNewUser) {
+    await sendNotificationAboutNewUser({ userId: username, userFullName: name })
+  }
   userDataCache.set(username, user)
   return user
 }
 
-const fdGetUser = async ({ username }) => {
-  if (userDataCache.has(username)) return userDataCache.get(username)
+const fdGetUser = async ({ username }: { username: string }) => {
+  if (userDataCache.has(username)) {
+    return userDataCache.get(username)
+  }
 
   const userFromDbOrm = await findUser({ username })
-  if (!userFromDbOrm) return
+  if (!userFromDbOrm) {
+    return
+  }
 
   userFromDbOrm.lastLogin = new Date()
   userFromDbOrm.save()
@@ -233,7 +299,7 @@ const fdGetUser = async ({ username }) => {
   return user
 }
 
-const addNewUser = async user => {
+export const addNewUser = async user => {
   const name = user.first_name.concat(' ', user.last_name)
   await User.upsert({
     fullName: name,
@@ -244,7 +310,7 @@ const addNewUser = async user => {
   })
 }
 
-const getUserFromSisuByEppn = async (requesterEppn, newUserEppn) => {
+export const getUserFromSisuByEppn = async (requesterEppn: string, newUserEppn: string) => {
   const { accessToken: requesterAccessToken } = await getSisuAuthData(requesterEppn)
   const { tokenData: newUserTokenData } = await getSisuAuthData(newUserEppn)
   const personData = await getGraphqlData(requesterAccessToken, {
@@ -254,20 +320,5 @@ const getUserFromSisuByEppn = async (requesterEppn, newUserEppn) => {
   return personData
 }
 
-const getMockedUser = serviceProvider === 'Toska' ? basicGetMockedUser : fdGetMockedUser
-const getUser = serviceProvider === 'Toska' ? toskaGetUser : fdGetUser
-
-module.exports = {
-  updateUser,
-  modifyElementDetails,
-  findAll,
-  findOne,
-  modifyAccess,
-  getUser,
-  getMockedUser,
-  getOrganizationAccess,
-  deleteOutdatedUsers,
-  roles,
-  getUserFromSisuByEppn,
-  addNewUser,
-}
+export const getUser = serviceProvider === 'Toska' ? toskaGetUser : fdGetUser
+export const getMockedUser = serviceProvider === 'Toska' ? basicGetMockedUser : fdGetMockedUser
