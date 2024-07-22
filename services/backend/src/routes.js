1118d2b2da5ee69dcb8cbed87aa8b4910d4a75b4
ShootingStar91 <ttu91@hotmail.com>
2024-07-22 15:08:42 +0300

    Remove deprecated route

--- services/backend/src/routes.js
+++ services/backend/src/routes.js
@@ -31,7 +31,6 @@ const studyProgrammeCriteria = require('./routes/studyProgrammeCriteria')
 const studyProgrammePins = require('./routes/studyProgrammePins')
 const tags = require('./routes/tags')
 const teachers = require('./routes/teachers')
-const testRoute = require('./routes/testRoute')
 const university = require('./routes/university')
 const updater = require('./routes/updater')
 const usersToska = require('./routes/users')
@@ -39,7 +38,6 @@ const usersFd = require('./routes/usersFd')
 const initializeSentry = require('./util/sentry')
 
 module.exports = (app, url) => {
-  app.use(`${url}/testroute`, testRoute)
   initializeSentry(app)
 
   app.use(Sentry.Handlers.requestHandler())
