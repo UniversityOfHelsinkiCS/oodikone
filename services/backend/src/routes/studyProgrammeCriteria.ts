f65621f02842eff471e4c2398c93071815bb5676
Riku Rauhala <riku@rauhala.io>
2024-07-22 19:13:56 +0300

    [Backend] Type routes/studyProgrammeCriteria

--- services/backend/src/routes/studyProgrammeCriteria.ts
+++ services/backend/src/routes/studyProgrammeCriteria.ts
@@ -1,30 +1,38 @@
-const router = require('express').Router()
+import { Router } from 'express'
 
-const {
+import {
   getCriteria,
   saveYearlyCourseCriteria,
   saveYearlyCreditCriteria,
-} = require('../services/studyProgramme/studyProgrammeCriteria')
+} from '../services/studyProgramme/studyProgrammeCriteria'
+
+const router = Router()
 
 router.get('/', async (req, res) => {
   const studyProgramme = req.query?.programmecode
-  if (studyProgramme !== '' && !studyProgramme) return res.status(422).end()
+  if (studyProgramme !== '' && !studyProgramme) {
+    return res.status(422).end()
+  }
   const studyProgrammeCriteria = await getCriteria(studyProgramme)
   return res.json(studyProgrammeCriteria)
 })
 
 router.post('/courses', async (req, res) => {
   const { code, courses, year } = req.body
-  if (!code || !courses || !year) return res.status(400).end()
+  if (!code || !courses || !year) {
+    return res.status(400).end()
+  }
   const studyProgrammeCriteria = await saveYearlyCourseCriteria(code, courses, year)
   return res.json(studyProgrammeCriteria)
 })
 
 router.post('/credits', async (req, res) => {
   const { code, credits } = req.body
-  if (!code || !credits) return res.status(400).end()
+  if (!code || !credits) {
+    return res.status(400).end()
+  }
   const studyProgrammeCriteria = await saveYearlyCreditCriteria(code, credits)
   return res.json(studyProgrammeCriteria)
 })
 
-module.exports = router
+export default router
