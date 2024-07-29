fcc9881dd72385fad0cf3f5c97be03db92c97f75
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-07-29 10:12:46 +0300

    [Faculties] Clean up `facultyUpdates.js`

--- services/backend/src/services/faculty/facultyUpdates.js
+++ services/backend/src/services/faculty/facultyUpdates.js
@@ -15,102 +15,53 @@ const { combineFacultyStudentProgress } = require('./facultyStudentProgress')
 const { combineFacultyStudents } = require('./facultyStudents')
 const { combineFacultyThesisWriters } = require('./facultyThesisWriters')
 
+const yearOptions = ['CALENDAR_YEAR', 'ACADEMIC_YEAR']
+const specialGroupOptions = ['SPECIAL_INCLUDED', 'SPECIAL_EXCLUDED']
+const programmeFilterOptions = ['ALL_PROGRAMMES', 'NEW_STUDY_PROGRAMMES']
+const graduatedOptions = ['GRADUATED_INCLUDED', 'GRADUATED_EXCLUDED']
+
 const updateFacultyOverview = async (faculty, statsType) => {
-  const calendarNewSpecial = {
-    yearType: 'CALENDAR_YEAR',
-    programmeFilter: 'NEW_STUDY_PROGRAMMES',
-    specialGroups: 'SPECIAL_INCLUDED',
-  }
-  const calendarNewSpecialOut = {
-    yearType: 'CALENDAR_YEAR',
-    programmeFilter: 'NEW_STUDY_PROGRAMMES',
-    specialGroups: 'SPECIAL_EXCLUDED',
-  }
-
-  const calendarAllSpecial = {
-    yearType: 'CALENDAR_YEAR',
-    programmeFilter: 'ALL_PROGRAMMES',
-    specialGroups: 'SPECIAL_INCLUDED',
-  }
-  const calendarAllSpecialOut = {
-    yearType: 'CALENDAR_YEAR',
-    programmeFilter: 'ALL_PROGRAMMES',
-    specialGroups: 'SPECIAL_EXCLUDED',
-  }
-  const academicNewSpecial = {
-    yearType: 'ACADEMIC_YEAR',
-    programmeFilter: 'NEW_STUDY_PROGRAMMES',
-    specialGroups: 'SPECIAL_INCLUDED',
-  }
-
-  const academicAllSpecial = {
-    yearType: 'ACADEMIC_YEAR',
-    programmeFilter: 'ALL_PROGRAMMES',
-    specialGroups: 'SPECIAL_INCLUDED',
-  }
-
-  const academicNewSpecialOut = {
-    yearType: 'ACADEMIC_YEAR',
-    programmeFilter: 'NEW_STUDY_PROGRAMMES',
-    specialGroups: 'SPECIAL_EXCLUDED',
-  }
-
-  const academicAllSpecialOut = {
-    yearType: 'ACADEMIC_YEAR',
-    programmeFilter: 'ALL_PROGRAMMES',
-    specialGroups: 'SPECIAL_EXCLUDED',
-  }
-
-  const options = [
-    calendarNewSpecial,
-    calendarAllSpecial,
-    academicNewSpecial,
-    academicAllSpecial,
-    calendarNewSpecialOut,
-    calendarAllSpecialOut,
-    academicNewSpecialOut,
-    academicAllSpecialOut,
-  ]
-  let allProgrammes = []
-  let newProgrammes = []
   const allProgrammeCodes = []
 
   const all = await getDegreeProgrammesOfFaculty(faculty, false)
   const onlyNew = await getDegreeProgrammesOfFaculty(faculty, true)
 
-  allProgrammes = await setFacultyProgrammes(faculty, all, 'ALL_PROGRAMMES')
-  newProgrammes = await setFacultyProgrammes(faculty, onlyNew, 'NEW_STUDY_PROGRAMMES')
+  const allProgrammes = await setFacultyProgrammes(faculty, all, 'ALL_PROGRAMMES')
+  const newProgrammes = await setFacultyProgrammes(faculty, onlyNew, 'NEW_STUDY_PROGRAMMES')
   allProgrammes?.data.forEach(prog => allProgrammeCodes.push(prog.code))
 
-  for (const option of options) {
-    const { yearType, programmeFilter, specialGroups } = option
-    if (statsType === 'ALL' || statsType === 'STUDENT') {
-      const updatedStudentInfo = await combineFacultyBasics(
-        faculty,
-        programmeFilter === 'ALL_PROGRAMMES' ? allProgrammes.data : newProgrammes.data,
-        yearType,
-        allProgrammeCodes,
-        programmeFilter,
-        specialGroups
-      )
-      await setBasicStats(updatedStudentInfo, option.yearType, option.programmeFilter, option.specialGroups)
-    }
-    if ((statsType === 'ALL' || statsType === 'CREDITS') && specialGroups !== 'SPECIAL_EXCLUDED') {
-      const updatedCredits = await computeCreditsProduced(
-        faculty,
-        yearType === 'ACADEMIC_YEAR',
-        specialGroups === 'SPECIAL_INCLUDED'
-      )
-      await setCreditStats(updatedCredits, yearType === 'ACADEMIC_YEAR', specialGroups === 'SPECIAL_INCLUDED')
-    }
-    if (statsType === 'ALL' || statsType === 'THESIS') {
-      const updateThesisWriters = await combineFacultyThesisWriters(
-        faculty,
-        programmeFilter === 'ALL_PROGRAMMES' ? allProgrammes.data : newProgrammes.data,
-        yearType,
-        specialGroups
-      )
-      await setThesisWritersStats(updateThesisWriters, yearType, programmeFilter, specialGroups)
+  for (const yearType of yearOptions) {
+    for (const specialGroups of specialGroupOptions) {
+      for (const programmeFilter of programmeFilterOptions) {
+        if (statsType === 'ALL' || statsType === 'STUDENT') {
+          const updatedStudentInfo = await combineFacultyBasics(
+            faculty,
+            programmeFilter === 'ALL_PROGRAMMES' ? allProgrammes.data : newProgrammes.data,
+            yearType,
+            allProgrammeCodes,
+            programmeFilter,
+            specialGroups
+          )
+          await setBasicStats(updatedStudentInfo, yearType, programmeFilter, specialGroups)
+        }
+        if ((statsType === 'ALL' || statsType === 'CREDITS') && specialGroups !== 'SPECIAL_EXCLUDED') {
+          const updatedCredits = await computeCreditsProduced(
+            faculty,
+            yearType === 'ACADEMIC_YEAR',
+            specialGroups === 'SPECIAL_INCLUDED'
+          )
+          await setCreditStats(updatedCredits, yearType === 'ACADEMIC_YEAR', specialGroups === 'SPECIAL_INCLUDED')
+        }
+        if (statsType === 'ALL' || statsType === 'THESIS') {
+          const updateThesisWriters = await combineFacultyThesisWriters(
+            faculty,
+            programmeFilter === 'ALL_PROGRAMMES' ? allProgrammes.data : newProgrammes.data,
+            yearType,
+            specialGroups
+          )
+          await setThesisWritersStats(updateThesisWriters, yearType, programmeFilter, specialGroups)
+        }
+      }
     }
   }
 
@@ -122,40 +73,23 @@ const updateFacultyOverview = async (faculty, statsType) => {
 }
 
 const updateFacultyProgressOverview = async faculty => {
-  const specialGraduated = {
-    specialGroups: 'SPECIAL_INCLUDED',
-    graduated: 'GRADUATED_INCLUDED',
-  }
-  const specialNotGraduated = {
-    specialGroups: 'SPECIAL_INCLUDED',
-    graduated: 'GRADUATED_EXCLUDED',
-  }
-  const notSpecialGraduated = {
-    specialGroups: 'SPECIAL_EXCLUDED',
-    graduated: 'GRADUATED_INCLUDED',
-  }
-  const notSpecialNotGraduated = {
-    specialGroups: 'SPECIAL_EXCLUDED',
-    graduated: 'GRADUATED_EXCLUDED',
-  }
-  const options = [specialGraduated, specialNotGraduated, notSpecialGraduated, notSpecialNotGraduated]
-
-  let newProgrammes = []
   const onlyNew = await getDegreeProgrammesOfFaculty(faculty, true)
-  newProgrammes = await setFacultyProgrammes(faculty, onlyNew, 'NEW_STUDY_PROGRAMMES')
+  const newProgrammes = await setFacultyProgrammes(faculty, onlyNew, 'NEW_STUDY_PROGRAMMES')
 
-  for (const option of options) {
-    const { specialGroups, graduated } = option
-    const updateFacultyStudentStats = await combineFacultyStudents(faculty, onlyNew, specialGroups, graduated)
-    await setFacultyStudentStats(updateFacultyStudentStats, specialGroups, graduated)
-    const updateFacultyProgressStats = await combineFacultyStudentProgress(
-      faculty,
-      newProgrammes.data,
-      specialGroups,
-      graduated
-    )
-    await setFacultyProgressStats(updateFacultyProgressStats, specialGroups, graduated)
+  for (const graduated of graduatedOptions) {
+    for (const specialGroups of specialGroupOptions) {
+      const updateFacultyStudentStats = await combineFacultyStudents(faculty, onlyNew, specialGroups, graduated)
+      await setFacultyStudentStats(updateFacultyStudentStats, specialGroups, graduated)
+      const updateFacultyProgressStats = await combineFacultyStudentProgress(
+        faculty,
+        newProgrammes.data,
+        specialGroups,
+        graduated
+      )
+      await setFacultyProgressStats(updateFacultyProgressStats, specialGroups, graduated)
+    }
   }
+
   return 'OK'
 }
 
