cf328a77144463bb63b190f41ee3e00b05019219
Jan Rundt <jan.rundt@funidata.fi>
2024-08-02 16:00:04 +0300

    OOD-43: basic auth for importerclient optional

--- services/backend/src/util/importerClient.ts
+++ services/backend/src/util/importerClient.ts
@@ -1,18 +1,40 @@
 import axios from 'axios'
 
-import { importerUrl, importerToken } from '../config'
+import { importerUrl, importerToken, serviceProvider, importerDbApiUser, importerDbApiPassword } from '../config'
 import logger from './logger'
 
+const createEnvAuthHeader = () : { Authorization : string } | { token : string } => {
+  if (serviceProvider === 'toska') {
+    return createTokenHeader()
+  } else {
+    return createBasicAuthHeader()
+  }
+}
+
+const createTokenHeader = ()  => { 
+  return { token: importerToken }
+}
+
+const createBasicAuthHeader = () => {
+  const auth = `${importerDbApiUser}:${importerDbApiPassword}`
+  const token = Buffer.from(auth).toString('base64')
+  return {
+    Authorization: 'Basic ' + token
+  }
+}
+
 const importerClient = axios.create({
-  headers: {
-    token: importerToken,
-  },
+  headers: createEnvAuthHeader(),
   baseURL: importerUrl,
 })
 
 export const getImporterClient = () => {
-  if (!importerToken) {
+  if (serviceProvider === 'toska' && !importerToken) {
     logger.error("Importer token not set, can't return client!")
+    return null
+  }
+  else if (serviceProvider === 'fd' && (!importerDbApiUser || !importerDbApiPassword)) {
+    logger.error("Basic auth credentials not set, can't return client!")
     return null
   }
   return importerClient
