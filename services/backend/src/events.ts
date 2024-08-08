a36eb4e010db58064b51faf8a6d4042c3e2528d5
Riku Rauhala <riku@rauhala.io>
2024-08-08 11:00:13 +0300

    [Backend] Type events

--- services/backend/src/events.ts
+++ services/backend/src/events.ts
@@ -1,32 +1,41 @@
-const { CronJob } = require('cron')
+import { CronJob } from 'cron'
 
-const { isProduction, runningInCI } = require('./config')
-const { getDegreeProgrammesOfFaculty } = require('./services/faculty/faculty')
-const { getFaculties } = require('./services/faculty/facultyHelpers')
-const { updateFacultyOverview, updateFacultyProgressOverview } = require('./services/faculty/facultyUpdates')
-const { computeLanguageCenterData, LANGUAGE_CENTER_REDIS_KEY } = require('./services/languageCenterData')
-const {
-  findStudentsCloseToGraduation,
-  CLOSE_TO_GRADUATION_REDIS_KEY,
-} = require('./services/populations/closeToGraduation')
-const { redisClient } = require('./services/redis')
-const { getCurrentSemester } = require('./services/semesters')
-const { combinedStudyprogrammes, isRelevantProgramme } = require('./services/studyProgramme/studyProgrammeHelpers')
-const { updateBasicView, updateStudytrackView } = require('./services/studyProgramme/studyProgrammeUpdates')
-const { getProgrammesFromStudyRights, refreshAssociationsInRedis } = require('./services/studyrights')
-const { findAndSaveTeachers } = require('./services/teachers/top')
-const { deleteOutdatedUsers } = require('./services/userService')
-const logger = require('./util/logger')
-const { jobMaker, addToFlow } = require('./worker/queue')
+import { isProduction, runningInCI } from './config'
+import { getDegreeProgrammesOfFaculty } from './services/faculty/faculty'
+import { getFaculties } from './services/faculty/facultyHelpers'
+import { updateFacultyOverview, updateFacultyProgressOverview } from './services/faculty/facultyUpdates'
+import { computeLanguageCenterData, LANGUAGE_CENTER_REDIS_KEY } from './services/languageCenterData'
+import { findStudentsCloseToGraduation, CLOSE_TO_GRADUATION_REDIS_KEY } from './services/populations/closeToGraduation'
+import { redisClient } from './services/redis'
+import { getCurrentSemester } from './services/semesters'
+import { combinedStudyprogrammes, isRelevantProgramme } from './services/studyProgramme/studyProgrammeHelpers'
+import { updateBasicView, updateStudytrackView } from './services/studyProgramme/studyProgrammeUpdates'
+import { getProgrammesFromStudyRights, refreshAssociationsInRedis } from './services/studyrights'
+import { findAndSaveTeachers } from './services/teachers/top'
+import { deleteOutdatedUsers } from './services/userService'
+import logger from './util/logger'
+import { jobMaker, addToFlow } from './worker/queue'
 
-const schedule = (cronTime, func) => new CronJob({ cronTime, onTick: func, start: true, timeZone: 'Europe/Helsinki' })
-
-const refreshStudyrightAssociations = async () => {
-  await refreshAssociationsInRedis()
-  logger.info('Studyright associations refreshed!')
+const schedule = (cronTime: string, onTick: () => void) => {
+  const onComplete = null
+  const start = true
+  const timeZone = 'Europe/Helsinki'
+  return new CronJob(cronTime, onTick, onComplete, start, timeZone)
 }
 
-const refreshFaculties = async () => {
+export const refreshCloseToGraduating = async () => {
+  logger.info('Refreshing students close to graduating')
+  const updatedData = await findStudentsCloseToGraduation()
+  await redisClient.setAsync(CLOSE_TO_GRADUATION_REDIS_KEY, JSON.stringify(updatedData))
+  logger.info('Students close to graduating updated!')
+}
+
+export const refreshFaculty = async (code: string) => {
+  await updateFacultyOverview(code, 'ALL')
+  await updateFacultyProgressOverview(code)
+}
+
+export const refreshFaculties = async () => {
   logger.info('Adding jobs to refresh all faculties')
   const faculties = await getFaculties()
   for (const faculty of faculties) {
@@ -34,69 +43,58 @@ const refreshFaculties = async () => {
   }
 }
 
+export const refreshLanguageCenterData = async () => {
+  logger.info('Refreshing language center data')
+  const freshData = await computeLanguageCenterData()
+  await redisClient.setAsync(LANGUAGE_CENTER_REDIS_KEY, JSON.stringify(freshData))
+  logger.info('Language center data refreshed!')
+}
+
 const refreshProgrammesAndFaculties = async () => {
   const facultyCodes = (await getFaculties()).map(faculty => faculty.code)
   for (const faculty of facultyCodes) {
     const programmeCodes = (await getDegreeProgrammesOfFaculty(faculty, true))
-      .map(prog => prog.code)
+      .map(programme => programme.code)
       .filter(code => isRelevantProgramme(code))
     await addToFlow(faculty, programmeCodes)
   }
 }
 
-const refreshFaculty = async code => {
-  await updateFacultyOverview(code, 'ALL')
-  await updateFacultyProgressOverview(code)
+export const refreshProgramme = async (code: string) => {
+  await updateBasicView(code, '')
+  await updateStudytrackView(code, '')
+  const combinedProgramme = combinedStudyprogrammes[code] || ''
+  await updateBasicView(code, combinedProgramme)
+  await updateStudytrackView(code, combinedProgramme)
 }
 
-const refreshProgrammes = async () => {
-  logger.info('Refreshing studyprogramme and studytrack overview statistics for all programmes')
-
+export const refreshProgrammes = async () => {
+  logger.info('Refreshing study programme and study track overview statistics for all programmes')
   const programmes = await getProgrammesFromStudyRights()
   const codes = programmes.map(programme => programme.code).filter(code => isRelevantProgramme(code))
-
   for (const code of codes) {
     // If combined programme is given, this updates only the bachelor programme
     jobMaker.programme(code)
   }
 }
 
-const refreshProgramme = async code => {
-  await updateBasicView(code, '')
-  await updateStudytrackView(code, '')
-
-  const combinedProgramme = combinedStudyprogrammes[code] || ''
-  await updateBasicView(code, combinedProgramme)
-  await updateStudytrackView(code, combinedProgramme)
+const refreshStudyRightAssociations = async () => {
+  await refreshAssociationsInRedis()
+  logger.info('Studyright associations refreshed!')
 }
 
 const refreshTeacherLeaderboard = async () => {
-  // refresh this and previous year
   const currentSemestersYearCode = (await getCurrentSemester()).getDataValue('yearcode')
   await findAndSaveTeachers(currentSemestersYearCode, currentSemestersYearCode - 1)
 }
 
-const refreshStatistics = async () => {
-  const statfuncs = [refreshStudyrightAssociations, refreshTeacherLeaderboard]
+export const refreshStatistics = async () => {
+  const statfuncs = [refreshStudyRightAssociations, refreshTeacherLeaderboard]
   logger.info('Refreshing statistics')
   for (const func of statfuncs) {
     await func()
   }
   logger.info('Statistics refreshed!')
-}
-
-const refreshLanguageCenterData = async () => {
-  logger.info('Refreshing language center data...')
-  const freshData = await computeLanguageCenterData()
-  await redisClient.setAsync(LANGUAGE_CENTER_REDIS_KEY, JSON.stringify(freshData))
-  logger.info('Language center data refreshed!')
-}
-
-const refreshCloseToGraduating = async () => {
-  logger.info('Updating students close to graduating...')
-  const updatedData = await findStudentsCloseToGraduation()
-  await redisClient.setAsync(CLOSE_TO_GRADUATION_REDIS_KEY, JSON.stringify(updatedData))
-  logger.info('Students close to graduating updated!')
 }
 
 const dailyJobs = async () => {
@@ -110,17 +108,17 @@ const dailyJobs = async () => {
   }
 }
 
-const startCron = () => {
+export const startCron = () => {
   if (isProduction && !runningInCI) {
-    logger.info('Cronjob for refreshing stats started: runs daily at 23:00.')
+    logger.info('Cronjob for refreshing stats started: runs daily at 23:00')
     schedule('0 23 * * *', async () => {
       logger.info('Running daily jobs from cron')
       await dailyJobs()
     })
     schedule('0 4 * * 3', async () => {
       logger.info("Deleting users who haven't logged in for 18 months")
-      const [, result] = await deleteOutdatedUsers()
-      logger.info(`Deleted ${result.rowCount} users.`)
+      const [, result] = (await deleteOutdatedUsers()) as { rowCount: number }[]
+      logger.info(`Deleted ${result.rowCount} users`)
     })
     schedule('0 19 * * 1', () => {
       logger.info('Updating students whose studyplans have not been updated recently')
@@ -131,15 +129,4 @@ const startCron = () => {
       jobMaker.studyplansUpdate(5)
     })
   }
-}
-
-module.exports = {
-  startCron,
-  refreshStatistics,
-  refreshFaculties,
-  refreshFaculty,
-  refreshProgramme,
-  refreshProgrammes,
-  refreshLanguageCenterData,
-  refreshCloseToGraduating,
 }
