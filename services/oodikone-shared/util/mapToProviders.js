bf0e7bb05058d237ce237fc312d14b43f4b37c65
Riku Rauhala <riku@rauhala.io>
2024-08-15 13:10:24 +0300

    [Shared] Convert to js

--- services/oodikone-shared/util/mapToProviders.js
+++ services/oodikone-shared/util/mapToProviders.js
@@ -1,11 +1,11 @@
-const handleUnderscoreProgrammeCode = (programmeCode: string) => {
+const handleUnderscoreProgrammeCode = programmeCode => {
   const [left, right] = programmeCode.split('_')
-  const prefix = [...left!].filter(char => !Number.isNaN(Number(char))).join('')
-  const suffix = `${left![0]}${right}`
+  const prefix = [...left].filter(char => !Number.isNaN(Number(char))).join('')
+  const suffix = `${left[0]}${right}`
   return `${prefix}0-${suffix}`
 }
 
-const handleDoctoralProgrammeCode = (programmeCode: string) => {
+const handleDoctoralProgrammeCode = programmeCode => {
   const numbers = programmeCode.substring(1)
   const courseProvider = Number(`7${numbers}`)
   if (courseProvider < 7920111 && courseProvider > 7920102) {
@@ -17,7 +17,7 @@ const handleDoctoralProgrammeCode = (programmeCode: string) => {
   return `${courseProvider}`
 }
 
-export const programmeCodeToProviderCode = (programmeCode: string) => {
+const programmeCodeToProviderCode = programmeCode => {
   if (programmeCode.includes('_')) {
     return handleUnderscoreProgrammeCode(programmeCode)
   }
@@ -27,6 +27,8 @@ export const programmeCodeToProviderCode = (programmeCode: string) => {
   return programmeCode
 }
 
-export const mapToProviders = (programmeCodes: string[]) => {
+const mapToProviders = programmeCodes => {
   return programmeCodes.map(programmeCodeToProviderCode)
 }
+
+module.exports = { mapToProviders, programmeCodeToProviderCode }
