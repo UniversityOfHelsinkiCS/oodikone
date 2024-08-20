8819cc838251339a717002711bb33a932ba70a89
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-08-20 11:26:35 +0300

    [Evaluation overview] Hide section for study level if there are no stats for it

--- services/frontend/src/components/EvaluationOverview/FacultyProgress.jsx
+++ services/frontend/src/components/EvaluationOverview/FacultyProgress.jsx
@@ -14,65 +14,72 @@ export const FacultyProgress = ({ faculty, progressStats, getDivider }) => {
     7
   )
   const masterStats = calculateStats(progressStats?.data?.creditCounts?.master, 120)
-  const licentiateStats = calculateStats(progressStats?.data?.creditCounts?.licentiate, 360)
   const doctorStats = calculateStats(progressStats?.data?.creditCounts?.doctor, 40, 0, 5)
 
   return (
     <>
-      {getDivider('Bachelor', 'BachelorStudentsOfTheFacultyByStartingYear', 'no-infobox')}
-      <FacultyBarChart
-        cypress="FacultyBachelorsProgress"
-        data={{
-          id: faculty,
-          stats: bachelorStats.chartStats,
-          years: progressStats?.data.years,
-        }}
-      />
-      <FacultyProgressTable
-        cypress="FacultyBachelorsProgressTable"
-        data={bachelorStats.tableStats}
-        programmeNames={progressStats?.data.programmeNames}
-        programmeStats={progressStats?.data.bachelorsProgStats}
-        progressTitles={progressStats?.data.yearlyBachelorTitles}
-        sortedKeys={sortProgrammeKeys(
-          Object.keys(progressStats?.data.bachelorsProgStats).map(obj => [
-            obj,
-            progressStats?.data?.programmeNames[obj].code,
-          ]),
-          faculty
-        ).map(listObj => listObj[0])}
-        titles={bachelorStats.tableTitles}
-      />
-      {getDivider('Bachelor + Master', 'ProgressOfBachelorMaster', 'no-infobox')}
-      <Message data-cy="FacultyProgrammesShownInfo">
-        The starting year is the studyright start in the bachelor programme. The credits are computed by the start date
-        of the bachelor programme and at the moment, they do not include any transferred credits. Thus, in these
-        statistics some students have less credits than in reality.
-      </Message>
-      <FacultyBarChart
-        cypress="FacultyBachelorMastersProgress"
-        data={{
-          id: faculty,
-          stats: bachelorMasterStats.chartStats,
-          years: progressStats?.data.years,
-        }}
-      />
-      <FacultyProgressTable
-        cypress="FacultyBachelorMasterProgressTable"
-        data={bachelorMasterStats.tableStats}
-        programmeNames={progressStats?.data.programmeNames}
-        programmeStats={progressStats?.data.bcMsProgStats}
-        progressTitles={progressStats?.data.yearlyBcMsTitles}
-        sortedKeys={sortProgrammeKeys(
-          Object.keys(progressStats?.data.bcMsProgStats).map(obj => [
-            obj,
-            progressStats?.data?.programmeNames[obj].code,
-          ]),
-          faculty
-        ).map(listObj => listObj[0])}
-        titles={bachelorMasterStats.tableTitles}
-      />
-      {faculty !== 'H90' && (
+      {bachelorStats != null && (
+        <>
+          {getDivider('Bachelor', 'BachelorStudentsOfTheFacultyByStartingYear', 'no-infobox')}
+          <FacultyBarChart
+            cypress="FacultyBachelorsProgress"
+            data={{
+              id: faculty,
+              stats: bachelorStats.chartStats,
+              years: progressStats?.data.years,
+            }}
+          />
+          <FacultyProgressTable
+            cypress="Table-FacultyBachelorsProgress"
+            data={bachelorStats.tableStats}
+            programmeNames={progressStats?.data.programmeNames}
+            programmeStats={progressStats?.data.bachelorsProgStats}
+            progressTitles={progressStats?.data.yearlyBachelorTitles}
+            sortedKeys={sortProgrammeKeys(
+              Object.keys(progressStats?.data.bachelorsProgStats).map(obj => [
+                obj,
+                progressStats?.data?.programmeNames[obj].code,
+              ]),
+              faculty
+            ).map(listObj => listObj[0])}
+            titles={bachelorStats.tableTitles}
+          />
+        </>
+      )}
+      {bachelorMasterStats != null && (
+        <>
+          {getDivider('Bachelor + Master', 'ProgressOfBachelorMaster', 'no-infobox')}
+          <Message data-cy="FacultyProgrammesShownInfo">
+            The starting year is the studyright start in the bachelor programme. The credits are computed by the start
+            date of the bachelor programme and at the moment, they do not include any transferred credits. Thus, in
+            these statistics some students have less credits than in reality.
+          </Message>
+          <FacultyBarChart
+            cypress="FacultyBachelorMastersProgress"
+            data={{
+              id: faculty,
+              stats: bachelorMasterStats.chartStats,
+              years: progressStats?.data.years,
+            }}
+          />
+          <FacultyProgressTable
+            cypress="Table-FacultyBachelorMastersProgress"
+            data={bachelorMasterStats.tableStats}
+            programmeNames={progressStats?.data.programmeNames}
+            programmeStats={progressStats?.data.bcMsProgStats}
+            progressTitles={progressStats?.data.yearlyBcMsTitles}
+            sortedKeys={sortProgrammeKeys(
+              Object.keys(progressStats?.data.bcMsProgStats).map(obj => [
+                obj,
+                progressStats?.data?.programmeNames[obj].code,
+              ]),
+              faculty
+            ).map(listObj => listObj[0])}
+            titles={bachelorMasterStats.tableTitles}
+          />
+        </>
+      )}
+      {masterStats != null && faculty !== 'H90' && (
         <>
           {getDivider('Master', 'MasterStudentsOfTheFacultyByStartingYear', 'no-infobox')}
           <FacultyBarChart
@@ -84,7 +91,7 @@ export const FacultyProgress = ({ faculty, progressStats, getDivider }) => {
             }}
           />
           <FacultyProgressTable
-            cypress="FacultyMastersProgressTable"
+            cypress="Table-FacultyMastersProgress"
             data={masterStats.tableStats}
             programmeNames={progressStats?.data.programmeNames}
             programmeStats={progressStats?.data.mastersProgStats}
@@ -100,57 +107,33 @@ export const FacultyProgress = ({ faculty, progressStats, getDivider }) => {
           />
         </>
       )}
-      {(faculty === 'H30' || faculty === 'ALL') && (
+      {doctorStats != null && (
         <>
-          {getDivider('Licentiate', 'LicentiateStudentsOfTheFacultyByStartingYear', 'no-infobox')}
+          {getDivider('Doctor', 'DoctoralStudentsOfTheFacultyByStartingYear', 'no-infobox')}
           <FacultyBarChart
-            cypress="FacultyLicentiateProgress"
+            cypress="FacultyDoctoralProgress"
             data={{
               id: faculty,
-              stats: licentiateStats.chartStats,
+              stats: doctorStats.chartStats,
               years: progressStats?.data.years,
             }}
           />
           <FacultyProgressTable
-            cypress="FacultyLicentiateProgressTable"
-            data={licentiateStats.tableStats}
+            cypress="Table-FacultyDoctoralProgress"
+            data={doctorStats.tableStats}
             programmeNames={progressStats?.data.programmeNames}
-            programmeStats={progressStats?.data.licentiateProgStats}
-            progressTitles={progressStats?.data.yearlyLicentiateTitles}
+            programmeStats={progressStats?.data.doctoralProgStats}
             sortedKeys={sortProgrammeKeys(
-              Object.keys(progressStats?.data.licentiateProgStats).map(obj => [
+              Object.keys(progressStats?.data.doctoralProgStats).map(obj => [
                 obj,
                 progressStats?.data?.programmeNames[obj].code,
               ]),
               faculty
             ).map(listObj => listObj[0])}
-            titles={licentiateStats.tableTitles}
+            titles={doctorStats.tableTitles}
           />
         </>
       )}
-      {getDivider('Doctor', 'DoctoralStudentsOfTheFacultyByStartingYear', 'no-infobox')}
-      <FacultyBarChart
-        cypress="FacultyDoctoralProgress"
-        data={{
-          id: faculty,
-          stats: doctorStats.chartStats,
-          years: progressStats?.data.years,
-        }}
-      />
-      <FacultyProgressTable
-        cypress="FacultyDoctoralProgressTable"
-        data={doctorStats.tableStats}
-        programmeNames={progressStats?.data.programmeNames}
-        programmeStats={progressStats?.data.doctoralProgStats}
-        sortedKeys={sortProgrammeKeys(
-          Object.keys(progressStats?.data.doctoralProgStats).map(obj => [
-            obj,
-            progressStats?.data?.programmeNames[obj].code,
-          ]),
-          faculty
-        ).map(listObj => listObj[0])}
-        titles={doctorStats.tableTitles}
-      />
     </>
   )
 }
