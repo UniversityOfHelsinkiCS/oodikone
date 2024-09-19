41551182a41d43ba11569b764b1d53f3e79a920e
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-09-19 08:44:44 +0300

    [Close to graduation] Update new credit limits to infobox

--- services/frontend/src/common/InfoToolTips/closeToGraduation.js
+++ services/frontend/src/common/InfoToolTips/closeToGraduation.js
@@ -3,12 +3,12 @@ export const closeToGraduationToolTips = `
 
   Students are included in this list based on the number of credits they have completed in their primary study plan. The limits for each study programme are as follows:  
 
-  - All bachelor’s programmes: 160 credits
+  - All bachelor’s programmes: 140 credits
   - Degree Programme in Veterinary Medicine (eläinlääketieteen lisensiaatin koulutusohjelma): 150 credits
   - Degree Programme in Medicine (lääketieteen koulutusohjelma): 330 credits
   - Degree Programme in Dentistry (hammaslääketieteen koulutusohjelma): 300 credits
   - Master’s Programme in Psychology (psykologian maisteriohjelma): 115 credits
-  - Other master’s programmes: 85 credits
+  - Other master’s programmes: 70 credits
 
   If you have access to all student data in Oodikone, you can also see all students here. Otherwise you will only see the students in your study guidance groups.
   `
