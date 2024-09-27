43e746de6fe99d7d2cd4bb7d66d8c7aabfaa9abd
Riku Rauhala <riku@rauhala.io>
2024-09-27 10:47:39 +0300

    [Course statistics] Refactor countTotalStats

--- services/frontend/src/components/CourseStatistics/SingleCourseStats/countTotalStats.ts
+++ services/frontend/src/components/CourseStatistics/SingleCourseStats/countTotalStats.ts
@@ -1,142 +1,146 @@
-const parseGrade = grade => {
+const parseGrade = (grade: string) => {
   const parsedGrade = Number(grade) ? Math.round(Number(grade)).toString() : grade
-  if (parsedGrade === 'LA') return 'LUB' // merge LA and LUB grades
+  if (parsedGrade === 'LA') {
+    return 'LUB'
+  }
   return parsedGrade
 }
 
-export const countTotalStats = (formattedStats, userHasAccessToAllStats) => {
-  const totals = formattedStats.reduce(
-    (acc, curr) => {
-      if (curr.rowObfuscated) {
-        return acc
-      }
-
-      let { passed } = acc.attempts.categories
-      let { failed } = acc.attempts.categories
-      const cgrades = acc.attempts.grades
-      const bestEffortGrades = acc.students.grades
-
-      Object.keys(curr.attempts.grades).forEach(grade => {
-        const parsedGrade = parseGrade(grade)
-        if (!cgrades[parsedGrade]) cgrades[parsedGrade] = 0
-        cgrades[parsedGrade] += curr.attempts.grades[grade]
-
-        if (['eisa', 'hyl.', 'hyl', '0', 'luop'].includes(parsedGrade.toLowerCase())) {
-          failed += curr.attempts.grades[grade]
-        } else {
-          passed += curr.attempts.grades[grade]
-        }
-      })
-
-      Object.keys(curr.students.grades).forEach(grade => {
-        const parsedGrade = parseGrade(grade)
-        if (!bestEffortGrades[parsedGrade]) bestEffortGrades[parsedGrade] = 0
-        bestEffortGrades[parsedGrade] += curr.students.grades[grade]
-      })
-
-      const { passedFirst, passedEventually, neverPassed } = curr.students.categories
-
-      const newPassedFirst = passedFirst
-        ? acc.students.categories.passedFirst + passedFirst
-        : acc.students.categories.passedFirst
-
-      const newPassedEventually = passedEventually
-        ? acc.students.categories.passedEventually + passedEventually
-        : acc.students.categories.passedEventually
-
-      const newNeverPassed = neverPassed
-        ? acc.students.categories.neverPassed + neverPassed
-        : acc.students.categories.neverPassed
-
-      Object.keys(curr.students.enrollmentsByState).forEach(k => {
-        if (acc.students.enrollmentsByState[k] === undefined) acc.students.enrollmentsByState[k] = 0
-        acc.students.enrollmentsByState[k] += curr.students.enrollmentsByState[k] || 0
-      })
-
-      Object.keys(curr.attempts.enrollmentsByState).forEach(k => {
-        if (acc.attempts.enrollmentsByState[k] === undefined) acc.attempts.enrollmentsByState[k] = 0
-        acc.attempts.enrollmentsByState[k] += curr.attempts.enrollmentsByState[k] || 0
-      })
-
-      return {
-        ...acc,
-        coursecode: curr.coursecode,
-        attempts: {
-          categories: { passed, failed },
-          grades: cgrades,
-          totalAttempts: acc.attempts.totalAttempts + curr.attempts.totalEnrollments || passed + failed,
-          totalEnrollments: acc.attempts.totalEnrollments + (curr.attempts.totalEnrollments || 0),
-          enrollmentsByState: { ...acc.attempts.enrollmentsByState },
-        },
-        students: {
-          totalEnrollments: acc.students.totalEnrollments + (curr.students.totalEnrollments || 0),
-          totalPassed: acc.students.totalPassed + curr.students.totalPassed,
-          totalFailed: acc.students.totalFailed + curr.students.totalFailed,
-          enrolledStudentsWithNoGrade:
-            acc.students.enrolledStudentsWithNoGrade + (curr.students.enrolledStudentsWithNoGrade || 0),
-          categories: {
-            passedFirst: newPassedFirst,
-            passedEventually: newPassedEventually,
-            neverPassed: newNeverPassed,
-          },
-          grades: bestEffortGrades,
-          enrollmentsByState: { ...acc.students.enrollmentsByState },
-          withEnrollments: {
-            total: acc.students.withEnrollments.total + curr.students.withEnrollments.total,
-            totalFailed: acc.students.withEnrollments.totalFailed + curr.students.withEnrollments.totalFailed,
-          },
-        },
-      }
+export const countTotalStats = (formattedStats, userHasAccessToAllStats: boolean) => {
+  const initialStats = {
+    code: 9999,
+    name: 'Total',
+    coursecode: '000',
+    userHasAccessToAllStats,
+    attempts: {
+      categories: {
+        passed: 0,
+        failed: 0,
+      },
+      passRate: 0,
+      grades: {},
+      totalAttempts: 0,
+      totalEnrollments: 0,
+      enrollmentsByState: {
+        ENROLLED: 0,
+        REJECTED: 0,
+        ABORTED: 0,
+      },
     },
-    {
-      code: 9999,
-      name: 'Total',
-      coursecode: '000',
-      userHasAccessToAllStats,
+    students: {
+      categories: {
+        passedFirst: 0,
+        passedEventually: 0,
+        neverPassed: 0,
+      },
+      grades: {},
+      passRate: 0,
+      failRate: 0,
+      total: 0,
+      totalPassed: 0,
+      totalFailed: 0,
+      totalEnrollments: 0,
+      enrolledStudentsWithNoGrade: 0,
+      enrollmentsByState: {
+        ENROLLED: 0,
+        REJECTED: 0,
+        ABORTED: 0,
+      },
+      withEnrollments: {
+        total: 0,
+        totalFailed: 0,
+      },
+    },
+    studentnumbers: [],
+  }
+
+  const totals = formattedStats.reduce((acc, curr) => {
+    if (curr.rowObfuscated) {
+      return acc
+    }
+
+    let { passed } = acc.attempts.categories
+    let { failed } = acc.attempts.categories
+    const cgrades = acc.attempts.grades
+    const bestEffortGrades = acc.students.grades
+
+    Object.keys(curr.attempts.grades).forEach(grade => {
+      const parsedGrade = parseGrade(grade)
+      if (!cgrades[parsedGrade]) {
+        cgrades[parsedGrade] = 0
+      }
+      cgrades[parsedGrade] += curr.attempts.grades[grade]
+
+      if (['eisa', 'hyl.', 'hyl', '0', 'luop'].includes(parsedGrade.toLowerCase())) {
+        failed += curr.attempts.grades[grade]
+      } else {
+        passed += curr.attempts.grades[grade]
+      }
+    })
+
+    Object.keys(curr.students.grades).forEach(grade => {
+      const parsedGrade = parseGrade(grade)
+      if (!bestEffortGrades[parsedGrade]) {
+        bestEffortGrades[parsedGrade] = 0
+      }
+      bestEffortGrades[parsedGrade] += curr.students.grades[grade]
+    })
+
+    const { passedFirst, passedEventually, neverPassed } = curr.students.categories
+
+    const newPassedFirst = passedFirst
+      ? acc.students.categories.passedFirst + passedFirst
+      : acc.students.categories.passedFirst
+
+    const newPassedEventually = passedEventually
+      ? acc.students.categories.passedEventually + passedEventually
+      : acc.students.categories.passedEventually
+
+    const newNeverPassed = neverPassed
+      ? acc.students.categories.neverPassed + neverPassed
+      : acc.students.categories.neverPassed
+
+    Object.keys(curr.students.enrollmentsByState).forEach(k => {
+      if (acc.students.enrollmentsByState[k] === undefined) acc.students.enrollmentsByState[k] = 0
+      acc.students.enrollmentsByState[k] += curr.students.enrollmentsByState[k] || 0
+    })
+
+    Object.keys(curr.attempts.enrollmentsByState).forEach(k => {
+      if (acc.attempts.enrollmentsByState[k] === undefined) acc.attempts.enrollmentsByState[k] = 0
+      acc.attempts.enrollmentsByState[k] += curr.attempts.enrollmentsByState[k] || 0
+    })
+
+    return {
+      ...acc,
+      coursecode: curr.coursecode,
       attempts: {
-        categories: {
-          passed: 0,
-          failed: 0,
-        },
-        passRate: 0,
-        grades: {},
-        totalAttempts: 0,
-        totalEnrollments: 0,
-        enrollmentsByState: {
-          ENROLLED: 0,
-          REJECTED: 0,
-          ABORTED: 0,
-        },
+        categories: { passed, failed },
+        grades: cgrades,
+        totalAttempts: acc.attempts.totalAttempts + curr.attempts.totalEnrollments || passed + failed,
+        totalEnrollments: acc.attempts.totalEnrollments + (curr.attempts.totalEnrollments || 0),
+        enrollmentsByState: { ...acc.attempts.enrollmentsByState },
       },
       students: {
+        totalEnrollments: acc.students.totalEnrollments + (curr.students.totalEnrollments || 0),
+        totalPassed: acc.students.totalPassed + curr.students.totalPassed,
+        totalFailed: acc.students.totalFailed + curr.students.totalFailed,
+        enrolledStudentsWithNoGrade:
+          acc.students.enrolledStudentsWithNoGrade + (curr.students.enrolledStudentsWithNoGrade || 0),
         categories: {
-          passedFirst: 0,
-          passedEventually: 0,
-          neverPassed: 0,
+          passedFirst: newPassedFirst,
+          passedEventually: newPassedEventually,
+          neverPassed: newNeverPassed,
         },
-        grades: {},
-        passRate: 0,
-        failRate: 0,
-        total: 0,
-        totalPassed: 0,
-        totalFailed: 0,
-        totalEnrollments: 0,
-        enrolledStudentsWithNoGrade: 0,
-        enrollmentsByState: {
-          ENROLLED: 0,
-          REJECTED: 0,
-          ABORTED: 0,
-        },
+        grades: bestEffortGrades,
+        enrollmentsByState: { ...acc.students.enrollmentsByState },
         withEnrollments: {
-          total: 0,
-          totalFailed: 0,
+          total: acc.students.withEnrollments.total + curr.students.withEnrollments.total,
+          totalFailed: acc.students.withEnrollments.totalFailed + curr.students.withEnrollments.totalFailed,
         },
       },
-      studentnumbers: [],
     }
-  )
+  }, initialStats)
 
-  // Count pass- and failrates also for "Total"-lines
   const { passedFirst = 0, passedEventually = 0, neverPassed = 0 } = totals.students.categories
   const { enrolledStudentsWithNoGrade = 0 } = totals.students
   const total = passedFirst + passedEventually + neverPassed
