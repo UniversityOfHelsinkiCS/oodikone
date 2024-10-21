ed0bcd7fee1137d3b4f57e53a86fc011782b5742
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-10-21 18:42:30 +0300

    Remove unused constant

--- services/frontend/src/constants/date.js
+++ services/frontend/src/constants/date.js
@@ -1,6 +1,5 @@
 // Replace with an enum when TypeScript is introduced
 
-export const API_DATE_FORMAT = 'YYYY.MM.DD'
 export const DISPLAY_DATE_FORMAT = 'DD.MM.YYYY'
 export const DISPLAY_DATE_FORMAT_DEV = 'DD.MM.YYYY HH:mm:ss'
 export const ISO_DATE_FORMAT = 'YYYY-MM-DD'
