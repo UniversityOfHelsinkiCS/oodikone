5277f71dd97503d3d4bf77950b6364dd202eb67a
Riku Rauhala <riku@rauhala.io>
2024-10-31 11:46:21 +0200

    [InfoBox] Convert to TypeScript

--- services/frontend/src/components/InfoBox/index.tsx
+++ services/frontend/src/components/InfoBox/index.tsx
@@ -4,23 +4,15 @@ import { Button, Icon, Message, Popup } from 'semantic-ui-react'
 import { InfoBoxButton } from './InfoBoxButton'
 import './infoBox.css'
 
-/**
- * Removes leading spaces so that the content can be properly formatted with Markdown.
- *
- * @param {string} content - The content to display in the InfoBox.
- * @returns {string} The content with leading spaces removed.
- */
-const formatContent = content => content.replace(/\n +/g, '\n')
+const formatContent = (content: string) => content.replace(/\n +/g, '\n')
 
-/**
- * A toggleable component for displaying information on how to use a feature or how to interpret data.
- * The InfoBox can be displayed as a popup or as a message box. The default is a message box.
- *
- * @param {string} content - The content to display in the InfoBox. Supports Markdown for formatting.
- * @param {string} [cypress] - A unique, descriptive tag to use for end-to-end testing with Cypress.
- * @param {boolean} [popup] - If true, the InfoBox will be displayed as a popup. False by default.
- */
-export const InfoBox = ({ content, cypress, popup = false }) => {
+interface InfoBoxProps {
+  content: string
+  cypress?: string
+  popup?: boolean
+}
+
+export const InfoBox = ({ content, cypress = '', popup = false }: InfoBoxProps) => {
   const [open, setOpen] = useState(false)
   const toggleOpen = () => setOpen(prev => !prev)
 
