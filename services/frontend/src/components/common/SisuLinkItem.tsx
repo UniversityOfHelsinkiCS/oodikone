27c071dca43688195c002c8bee006f12a4f5c9fc
Riku Rauhala <riku@rauhala.io>
2024-11-04 14:51:07 +0200

    [SisuLinkItem] Convert to TypeScript

--- services/frontend/src/components/common/SisuLinkItem.tsx
+++ services/frontend/src/components/common/SisuLinkItem.tsx
@@ -1,6 +1,10 @@
-import { Item, Icon } from 'semantic-ui-react'
+import { Icon, Item } from 'semantic-ui-react'
 
-export const SisuLinkItem = ({ id }) => (
+interface SisuLinkItemProps {
+  id: string
+}
+
+export const SisuLinkItem = ({ id }: SisuLinkItemProps) => (
   <div data-cy="sisulink">
     <Item as="a" href={`https://sisu.helsinki.fi/tutor/role/staff/student/${id}/basic/basic-info`} target="_blank">
       <Icon name="external alternate" />
