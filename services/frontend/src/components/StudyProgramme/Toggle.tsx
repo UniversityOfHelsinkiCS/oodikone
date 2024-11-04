81d4921bf00d682dde28c06d9fe6fe9aa325d262
Riku Rauhala <riku@rauhala.io>
2024-11-04 16:45:58 +0200

    [Toggle] Convert to TypeScript

--- services/frontend/src/components/StudyProgramme/Toggle.tsx
+++ services/frontend/src/components/StudyProgramme/Toggle.tsx
@@ -1,19 +1,21 @@
-import ReactMarkdown from 'react-markdown'
 import { Radio } from 'semantic-ui-react'
 
-import { formatContent } from '@/common'
 import { HoverableHelpPopup } from '@/components/common/HoverableHelpPopup'
 
-export const Toggle = ({ cypress, firstLabel, secondLabel, setValue, toolTips, value }) => (
+interface ToggleProps {
+  cypress?: string
+  firstLabel: string
+  secondLabel: string
+  setValue: (value: boolean) => void
+  toolTips?: string
+  value: boolean
+}
+
+export const Toggle = ({ cypress, firstLabel, secondLabel, setValue, toolTips, value }: ToggleProps) => (
   <div className="radio-toggle">
     <label className={`toggle-label${value ? '-checked' : ''}`}>{firstLabel}</label>
     <Radio checked={value} data-cy={cypress} onChange={() => setValue(!value)} toggle />
     <label className={`toggle-label${value ? '' : '-checked'}`}>{secondLabel}</label>
-    {toolTips && (
-      <HoverableHelpPopup
-        content={<ReactMarkdown>{formatContent(toolTips)}</ReactMarkdown>}
-        style={{ marginLeft: '0.4em', color: '#888' }}
-      />
-    )}
+    {toolTips && <HoverableHelpPopup content={toolTips} style={{ marginLeft: '0.4em', color: '#888' }} />}
   </div>
 )
