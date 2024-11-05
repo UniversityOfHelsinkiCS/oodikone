44d15b5ed8909638c3e6513c8d7d894abf4c355a
Riku Rauhala <riku@rauhala.io>
2024-11-05 10:01:47 +0200

    [facultyHelpers] Convert to TypeScript

--- services/frontend/src/components/FacultyStatistics/facultyHelpers.ts
+++ services/frontend/src/components/FacultyStatistics/facultyHelpers.ts
@@ -18,9 +18,9 @@ const regexValuesAll = [
   /^Y/,
   /\d$/,
   /^\d.*e$/,
-]
+] as const
 
-const testKey = value => {
+const testKey = (value: string) => {
   for (let i = 0; i < regexValuesAll.length; i++) {
     if (regexValuesAll[i].test(value)) {
       return i
@@ -29,7 +29,7 @@ const testKey = value => {
   return 6
 }
 
-export const sortProgrammeKeys = (programmeKeys, faculty) => {
+export const sortProgrammeKeys = (programmeKeys: string[][], faculty: string) => {
   try {
     return programmeKeys.sort((a, b) => {
       if (a[1].includes(faculty) && !b[1].includes(faculty)) return -1
@@ -38,9 +38,7 @@ export const sortProgrammeKeys = (programmeKeys, faculty) => {
       if (!a[1].includes(faculty) && b[1].startsWith('T') && !a[1].includes('T')) return 1
       if (a[1].startsWith('LIS') && !b[1].includes(faculty) && !b[1].includes('LIS')) return -1
       if (!a[1].includes(faculty) && b[1].startsWith('LIS') && !a[1].includes('LIS')) return 1
-      if (testKey(a[1]) - testKey(b[1]) === 0) {
-        return a[0].localeCompare(b[0])
-      }
+      if (testKey(a[1]) - testKey(b[1]) === 0) return a[0].localeCompare(b[0])
       return testKey(a[1]) - testKey(b[1])
     })
   } catch (error) {
