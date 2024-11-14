1903f747641d6a9f2eb28e6d9cff3744438bd862
Riku Rauhala <riku@rauhala.io>
2024-11-14 13:31:12 +0200

    [PageTitle] Center by default and add optional subtitle

--- services/frontend/src/components/material/PageTitle/index.tsx
+++ services/frontend/src/components/material/PageTitle/index.tsx
@@ -1,16 +1,22 @@
-import { Box, Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 
 /**
  * A title text displayed at the top of the page.
  *
+ * @param {string} [subtitle] - The subtitle of the page.
  * @param {string} title - The main title of the page.
  */
-export const PageTitle = ({ title }: { title: string }) => {
+export const PageTitle = ({ subtitle, title }: { subtitle?: string; title: string }) => {
   return (
-    <Box my={3}>
+    <Stack sx={{ gap: 1, my: 3, textAlign: 'center' }}>
       <Typography component="h1" variant="h4">
         {title}
       </Typography>
-    </Box>
+      {subtitle && (
+        <Typography color="textSecondary" component="h2" variant="h5">
+          {subtitle}
+        </Typography>
+      )}
+    </Stack>
   )
 }
