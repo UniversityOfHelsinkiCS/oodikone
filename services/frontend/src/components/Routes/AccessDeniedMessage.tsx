80cf4acf0193ba02260a6491ff90afe47d92742c
Riku Rauhala <riku@rauhala.io>
2024-11-18 21:59:14 +0200

    [AccessDeniedMessage] Use the Alert component instead

--- services/frontend/src/components/Routes/AccessDeniedMessage.tsx
+++ services/frontend/src/components/Routes/AccessDeniedMessage.tsx
@@ -1,36 +1,36 @@
 import { DoNotDisturb } from '@mui/icons-material'
-import { Container, Box, Link, Paper, Typography } from '@mui/material'
+import { Alert, AlertTitle, Box, Container, Link } from '@mui/material'
 
 import { useTitle } from '@/common/hooks'
+import { PageTitle } from '../material/PageTitle'
 
 export const AccessDeniedMessage = () => {
   useTitle('Access denied')
 
   return (
-    <Container maxWidth="md" sx={{ paddingTop: 2 }}>
-      <Paper
-        sx={{
-          alignItems: 'center',
-          bgcolor: 'error.light',
-          color: 'common.white',
-          display: 'flex',
-          padding: 2,
-        }}
+    <Container maxWidth="md">
+      <PageTitle title="Access denied" />
+      <Alert
+        icon={
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <DoNotDisturb />
+          </Box>
+        }
+        severity="error"
+        variant="outlined"
       >
-        <DoNotDisturb fontSize="large" sx={{ marginRight: 2 }} />
-        <Box>
-          <Typography component="h2" variant="h6">
-            Access denied
-          </Typography>
-          <Typography variant="body1">
-            You don't currently have permission to view this page. If you believe this is a mistake, please contact{' '}
-            <Link color="inherit" href="mailto:oodikone@helsinki.fi" underline="hover">
-              oodikone@helsinki.fi
-            </Link>
-            .
-          </Typography>
-        </Box>
-      </Paper>
+        <AlertTitle>Access denied</AlertTitle>
+        You don't currently have permission to view this page. If you believe this is a mistake, please contact{' '}
+        <Link href="mailto:oodikone@helsinki.fi" underline="hover">
+          oodikone@helsinki.fi
+        </Link>
+        .
+      </Alert>
     </Container>
   )
 }
