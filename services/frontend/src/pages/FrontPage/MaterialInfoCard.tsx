301208cf99c97bfcb3a74109f26f945612bfb8b5
Riku Rauhala <riku@rauhala.io>
2024-11-19 10:40:42 +0200

    [Front page] Only display feedback link for HY

--- services/frontend/src/pages/FrontPage/MaterialInfoCard.tsx
+++ services/frontend/src/pages/FrontPage/MaterialInfoCard.tsx
@@ -2,6 +2,8 @@ import { InfoOutlined } from '@mui/icons-material'
 import { Alert, AlertTitle, Box, Link } from '@mui/material'
 import { NavLink } from 'react-router-dom'
 
+import { isDefaultServiceProvider } from '@/common'
+
 /**
  * A temporary notification card about the new UI.
  * Can be removed when most of the work is done.
@@ -23,11 +25,16 @@ export const MaterialInfoCard = () => {
       variant="outlined"
     >
       <AlertTitle>New user interface</AlertTitle>
-      Oodikone is getting a new look! We are gradually updating each view to the new design.{' '}
-      <Link component={NavLink} to="/feedback">
-        Let us know
-      </Link>{' '}
-      if you have any feedback or suggestions.
+      Oodikone is getting a new look! We are gradually updating each view to the new design.
+      {isDefaultServiceProvider() && (
+        <>
+          {' '}
+          <Link component={NavLink} to="/feedback">
+            Let us know
+          </Link>{' '}
+          if you have any feedback or suggestions.
+        </>
+      )}
     </Alert>
   )
 }
