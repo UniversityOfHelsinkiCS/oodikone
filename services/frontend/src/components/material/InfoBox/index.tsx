2db12dcaac7f902dd4538c91330be2a83304b1f3
Riku Rauhala <riku@rauhala.io>
2024-11-21 11:40:08 +0200

    [InfoBox] Add a mini variant with smaller button

--- services/frontend/src/components/material/InfoBox/index.tsx
+++ services/frontend/src/components/material/InfoBox/index.tsx
@@ -1,10 +1,18 @@
 import { HelpOutline } from '@mui/icons-material'
-import { Box, Button, Tooltip, Typography } from '@mui/material'
+import { Box, Button, IconButton, Tooltip, Typography } from '@mui/material'
 import ReactMarkdown from 'react-markdown'
 
 import { formatContent } from '@/common'
 
-export const InfoBox = ({ content, cypress = '' }: { content: string; cypress?: string }) => {
+export const InfoBox = ({
+  content,
+  cypress = '',
+  mini = false,
+}: {
+  content: string
+  cypress?: string
+  mini?: boolean
+}) => {
   return (
     <Tooltip
       arrow
@@ -16,9 +24,15 @@ export const InfoBox = ({ content, cypress = '' }: { content: string; cypress?: 
         </Box>
       }
     >
-      <Button color="info" startIcon={<HelpOutline />} variant="outlined">
-        Info
-      </Button>
+      {mini ? (
+        <IconButton>
+          <HelpOutline fontSize="small" />
+        </IconButton>
+      ) : (
+        <Button color="info" startIcon={<HelpOutline />} variant="outlined">
+          Info
+        </Button>
+      )}
     </Tooltip>
   )
 }
