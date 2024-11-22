230d07cbde74c325230df62e298c4b8b08400e1c
Riku Rauhala <riku@rauhala.io>
2024-11-22 13:45:29 +0200

    [University] Separate the main sections into tabs

--- services/frontend/src/components/material/Section/index.tsx
+++ services/frontend/src/components/material/Section/index.tsx
@@ -1,15 +1,31 @@
-import { Box, Paper, Stack, Typography } from '@mui/material'
+import { Alert, Box, Paper, Skeleton, Stack, Typography } from '@mui/material'
 
 import { InfoBox } from '@/components/material/InfoBox'
+
+const ErrorMessage = () => {
+  return (
+    <Alert severity="error">
+      <Typography variant="body1">Something went wrong, please try refreshing the page.</Typography>
+    </Alert>
+  )
+}
+
+const LoadingSkeleton = () => {
+  return <Skeleton height={200} variant="rectangular" />
+}
 
 export const Section = ({
   children,
   cypress,
+  isLoading,
+  isError,
   title,
   infoBoxContent,
 }: {
   children: React.ReactNode
   cypress?: string
+  isLoading?: boolean
+  isError?: boolean
   title: string
   infoBoxContent?: string
 }) => {
@@ -21,7 +37,7 @@ export const Section = ({
         </Typography>
         {infoBoxContent && <InfoBox content={infoBoxContent} cypress={cypress} />}
       </Stack>
-      <Box sx={{ marginTop: 2 }}>{children}</Box>
+      <Box sx={{ marginTop: 2 }}>{isLoading ? <LoadingSkeleton /> : isError ? <ErrorMessage /> : children}</Box>
     </Paper>
   )
 }
--- services/frontend/src/pages/University/index.tsx
+++ services/frontend/src/pages/University/index.tsx
@@ -1,6 +1,6 @@
-import { Box, Container, Stack } from '@mui/material'
-import { useState } from 'react'
-import { Loader } from 'semantic-ui-react'
+import { Box, Container, Stack, Tab, Tabs } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
 import { useTitle } from '@/common/hooks'
 import { facultyToolTips } from '@/common/InfoToolTips'
@@ -9,65 +9,73 @@ import { FacultyProgress } from '@/components/material/FacultyProgress'
 import { PageTitle } from '@/components/material/PageTitle'
 import { Section } from '@/components/material/Section'
 import { Toggle } from '@/components/material/Toggle'
-import { useGetAllFacultiesGraduationStatsQuery, useGetAllFacultiesProgressStatsQuery } from '@/redux/facultyStats'
+import { useGetAllFacultiesGraduationStatsQuery } from '@/redux/facultyStats'
 import { ExcludedMessage } from './ExcludedMessage'
 
 export const University = () => {
   useTitle('University')
 
-  const [graduatedGroup, setGraduatedGroup] = useState(false)
+  const history = useHistory()
+  const location = useLocation()
+
+  const query = new URLSearchParams(location.search)
+  const initialTab = parseInt(query.get('tab') ?? '0')
+  const [activeTab, setActiveTab] = useState(initialTab)
+
+  const [excludeGraduated, setExcludeGraduated] = useState(false)
   const [medianMode, setMedianMode] = useState(false)
   const [excludeSpecials, setIncludeSpecials] = useState(false)
 
-  const progressStats = useGetAllFacultiesProgressStatsQuery({
-    graduated: graduatedGroup ? 'GRADUATED_EXCLUDED' : 'GRADUATED_INCLUDED',
-    includeSpecials: !excludeSpecials,
-  })
   const graduationStats = useGetAllFacultiesGraduationStatsQuery({})
 
-  if (graduationStats.isLoading || graduationStats.isFetching || progressStats.isFetching || progressStats.isLoading) {
-    return <Loader active style={{ marginTop: '10em' }} />
+  const handleTabChange = (_event, newValue: number) => {
+    setActiveTab(newValue)
+    query.set('tab', newValue.toString())
+    history.push({ search: query.toString() })
   }
 
-  const isError =
-    progressStats.isError ||
-    (progressStats.isSuccess && !progressStats.data) ||
-    graduationStats.isError ||
-    !graduationStats.data
-  if (isError) {
-    return <h3>Something went wrong, please try refreshing the page.</h3>
-  }
+  useEffect(() => {
+    setActiveTab(initialTab)
+  }, [initialTab])
 
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="lg">
       <PageTitle title="University" />
       <ExcludedMessage />
-      <Stack>
-        <Section
-          cypress="InfoFacultyProgress"
-          infoBoxContent={facultyToolTips.studentProgress}
-          title="Progress of students of the university"
-        >
-          <Stack direction="row" justifyContent="space-around">
-            <Toggle
-              cypress="GraduatedToggle"
-              firstLabel="Graduated included"
-              infoBoxContent={facultyToolTips.graduatedToggle}
-              secondLabel="Graduated excluded"
-              setValue={setGraduatedGroup}
-              value={graduatedGroup}
-            />
-            <Toggle
-              cypress="StudentToggle"
-              firstLabel="All study rights"
-              infoBoxContent={facultyToolTips.studentToggle}
-              secondLabel="Special study rights excluded"
-              setValue={() => setIncludeSpecials(!excludeSpecials)}
-              value={excludeSpecials}
-            />
-          </Stack>
-        </Section>
-        <FacultyProgress faculty="ALL" progressStats={progressStats} />
+      <Tabs onChange={handleTabChange} sx={{ marginBottom: 2 }} value={activeTab}>
+        <Tab label="Faculty progress" />
+        <Tab label="Faculty graduations" />
+      </Tabs>
+      {activeTab === 0 && (
+        <Box>
+          <Section
+            cypress="InfoFacultyProgress"
+            infoBoxContent={facultyToolTips.studentProgress}
+            title="Progress of students of the university"
+          >
+            <Stack direction="row" justifyContent="space-around">
+              <Toggle
+                cypress="GraduatedToggle"
+                firstLabel="Graduated included"
+                infoBoxContent={facultyToolTips.graduatedToggle}
+                secondLabel="Graduated excluded"
+                setValue={setExcludeGraduated}
+                value={excludeGraduated}
+              />
+              <Toggle
+                cypress="StudentToggle"
+                firstLabel="All study rights"
+                infoBoxContent={facultyToolTips.studentToggle}
+                secondLabel="Special study rights excluded"
+                setValue={() => setIncludeSpecials(!excludeSpecials)}
+                value={excludeSpecials}
+              />
+            </Stack>
+          </Section>
+          <FacultyProgress excludeGraduated={excludeGraduated} excludeSpecials={excludeSpecials} faculty="ALL" />
+        </Box>
+      )}
+      {activeTab === 1 && (
         <Section infoBoxContent={facultyToolTips.averageGraduationTimes} title="Average graduation times">
           <Box display="flex" justifyContent="center">
             <Toggle
@@ -85,7 +93,7 @@ export const University = () => {
             universityMode
           />
         </Section>
-      </Stack>
+      )}
     </Container>
   )
 }
