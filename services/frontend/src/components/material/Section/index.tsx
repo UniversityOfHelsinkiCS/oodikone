356ffc953eeb525d0536143d9dd389ae2063fe35
Riku Rauhala <riku@rauhala.io>
2024-12-12 11:57:18 +0200

    [Faculties] Wrap progress sections into accordions

--- services/frontend/src/components/material/Section/index.tsx
+++ services/frontend/src/components/material/Section/index.tsx
@@ -24,19 +24,15 @@ export const Section = ({
 }) => {
   return (
     <Paper data-cy={cypress} sx={{ padding: 2 }} variant="outlined">
-      {title && (
-        <Stack alignItems="center" direction="row" justifyContent="space-between">
-          <Typography component="h2" variant="h5">
-            {title}
-          </Typography>
-          <Stack direction="row" gap={1}>
-            {exportOnClick && (
-              <ExportButton cypress={cypress} disabled={isError || isLoading} onClick={exportOnClick} />
-            )}
-            {infoBoxContent && <InfoBox content={infoBoxContent} cypress={cypress} />}
-          </Stack>
+      <Stack alignItems="center" direction="row" justifyContent="space-between" sx={{ paddingBottom: 1 }}>
+        <Typography component="h2" variant="h5">
+          {title}
+        </Typography>
+        <Stack direction="row" gap={1}>
+          {exportOnClick && <ExportButton cypress={cypress} disabled={isError || isLoading} onClick={exportOnClick} />}
+          {infoBoxContent && <InfoBox content={infoBoxContent} cypress={cypress} />}
         </Stack>
-      )}
+      </Stack>
       <Box sx={{ marginTop: title ? 2 : 0 }}>
         {isError ? <ErrorMessage /> : isLoading ? <LoadingSkeleton /> : children}
       </Box>
