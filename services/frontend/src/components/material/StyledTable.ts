71dc06728f65a7c0cdab828a11a7bc59b3d88b2c
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-12-16 11:36:55 +0200

    Create StyledTable

---
+++ services/frontend/src/components/material/StyledTable.ts
@@ -0,0 +1,22 @@
+import { Table } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+export const StyledTable = styled(Table, {
+  shouldForwardProp: prop => prop !== 'showCellBorders',
+})<{ showCellBorders?: boolean }>(({ theme, showCellBorders }) => {
+  return {
+    '& tr': {
+      border: `1px solid ${theme.palette.grey[300]}`,
+      '&:hover': {
+        backgroundColor: theme.palette.grey[100],
+      },
+    },
+    '& th': {
+      backgroundColor: theme.palette.grey[50],
+    },
+    '& th, & td': {
+      border: showCellBorders ? `1px solid ${theme.palette.grey[300]}` : 'none',
+      padding: theme.spacing(1.5),
+    },
+  }
+})
