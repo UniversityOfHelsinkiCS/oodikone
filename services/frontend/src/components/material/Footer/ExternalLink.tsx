59a9854ed5a95bd1e2925318e4bdf6b67335f6dd
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-12-16 13:54:58 +0200

    Add optional `cypress` prop to ExternalLink

--- services/frontend/src/components/material/Footer/ExternalLink.tsx
+++ services/frontend/src/components/material/Footer/ExternalLink.tsx
@@ -6,19 +6,23 @@ import { Link, LinkOwnProps } from '@mui/material'
  *
  * @param href - URL of the external website
  * @param text - Text to display in the link
+ * @param cypress - Cypress data-cy attribute
  * @param variant - Typography variant for the link text
  */
 export const ExternalLink = ({
   href,
   text,
+  cypress,
   variant = 'body2',
 }: {
   href: string
   text: string
+  cypress?: string
   variant?: LinkOwnProps['variant']
 }) => {
   return (
     <Link
+      data-cy={cypress}
       href={href}
       rel="noopener noreferrer"
       sx={{ alignItems: 'center', display: 'flex', gap: 0.5 }}
