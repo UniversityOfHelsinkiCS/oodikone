23cb8c8ee54979faaa9166a9fd8592941df63753
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-12-16 15:27:20 +0200

    [Class statistics] Fix credit accumulation graph

--- services/frontend/src/components/PopulationDetails/index.jsx
+++ services/frontend/src/components/PopulationDetails/index.jsx
@@ -63,7 +63,7 @@ export const PopulationDetails = ({
           {filteredStudents.length > 0 && (
             <CreditAccumulationGraphHighCharts
               programmeCodes={programmeCodes}
-              showFullStudyPath={query?.showFullStudyPath}
+              showFullStudyPath={query?.showFullStudyPath === 'true'}
               students={filteredStudents}
               studyPlanFilterIsActive={studyPlanFilterIsActive}
             />
