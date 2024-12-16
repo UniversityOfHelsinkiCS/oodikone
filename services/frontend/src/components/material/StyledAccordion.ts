9484ecb456830a63f19a94206f9923a6c7d11483
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2024-12-16 11:36:25 +0200

    Create StyledAccordion

---
+++ services/frontend/src/components/material/StyledAccordion.ts
@@ -0,0 +1,29 @@
+import { Accordion } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+export const StyledAccordion = styled(Accordion)(() => ({
+  boxShadow: 'none',
+  width: '100%',
+  '&::before': {
+    display: 'none',
+  },
+  '& .MuiAccordionSummary-root': {
+    flexDirection: 'row-reverse',
+    padding: 0,
+    marginLeft: '-8px',
+    '& .MuiAccordionSummary-expandIconWrapper': {
+      transform: 'rotate(-90deg)',
+      transition: 'transform 0.3s ease',
+    },
+    '&.Mui-expanded .MuiAccordionSummary-expandIconWrapper': {
+      transform: 'rotate(0deg)',
+    },
+  },
+  '& .MuiAccordionDetails-root': {
+    padding: 0,
+  },
+}))
+
+StyledAccordion.defaultProps = {
+  disableGutters: true,
+}
