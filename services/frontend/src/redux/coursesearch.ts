f1a63029284d3d0b7e25d741ca93be4229604b79
Valtteri Kantanen <valtteri.kantanen@iki.fi>
2025-01-10 12:59:25 +0200

    [Course statistics] Use RTK Query for course search

---
+++ services/frontend/src/redux/coursesearch.ts
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { RTKApi } from '@/apiConnection'
+
+const courseSearchApi = RTKApi.injectEndpoints({
+  endpoints: builder => ({
+    getCourseSearchResult: builder.query({
+      query: ({ name, code, combineSubstitutions }) => ({
+        url: '/v2/coursesmulti',
+        params: { name, code, combineSubstitutions },
+      }),
+    }),
+  }),
+})
+
+export const { useGetCourseSearchResultQuery } = courseSearchApi
+
+const initialState: { openOrRegular: 'openStats' | 'regularStats' | 'unifyStats' } = { openOrRegular: 'unifyStats' }
+
+const courseSearchSlice = createSlice({
+  name: 'courseSearch',
+  initialState,
+  reducers: {
+    toggleOpenAndRegularCourses: (state, { payload }) => {
+      if (!['openStats', 'regularStats', 'unifyStats'].includes(payload)) {
+        throw new Error(`Invalid payload ${payload} for toggleOpenAndRegularCourses`)
+      }
+      state.openOrRegular = payload
+    },
+  },
+})
+
+export const { toggleOpenAndRegularCourses } = courseSearchSlice.actions
+
+export const { reducer } = courseSearchSlice
