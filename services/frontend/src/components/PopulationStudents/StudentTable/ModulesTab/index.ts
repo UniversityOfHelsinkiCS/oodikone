96b181d3af287d8aecee509d25fb6a82265c31a1
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2025-02-19 17:50:20 +0200

    [Modules Tab] Fix use of wrong studyplan in case of multiple studyplans or active studyrights

--- services/frontend/src/components/PopulationStudents/StudentTable/ModulesTab/index.ts
+++ services/frontend/src/components/PopulationStudents/StudentTable/ModulesTab/index.ts
@@ -11,12 +11,31 @@ export type FormattedStudent = {
 
 export type FormattedModules = Record<string, Record<string, string>>
 
-const formatStudent = (student: any): FormattedStudent => {
+const getModulesFromRelevantStudyPlan = (student: any, degreeProgrammeCodes: string[]): string[] => {
+  if (!student.studyRights || !Array.isArray(student.studyRights)) {
+    return []
+  }
+
+  const relevantStudyRight =
+    student.studyRights?.find((studyRight: any) =>
+      studyRight?.studyRightElements?.some((element: any) => degreeProgrammeCodes.includes(element.code))
+    ) ?? false
+
+  if (relevantStudyRight) {
+    return (
+      student.studyplans?.find((plan: any) => plan.sis_study_right_id === relevantStudyRight.id)?.includedModules ?? []
+    )
+  }
+
+  return []
+}
+
+const formatStudent = (student: any, degreeProgrammeCodes: string[]): FormattedStudent => {
   return {
     firstNames: student.firstnames,
     lastName: student.lastname,
     studentNumber: student.studentNumber,
-    modulesInHOPS: student.studyplans?.[0]?.includedModules,
+    modulesInHOPS: getModulesFromRelevantStudyPlan(student, degreeProgrammeCodes),
     sis_person_id: student.sis_person_id,
   }
 }
@@ -50,7 +69,10 @@ export const ModulesTabContainer = ({ curriculum, students }) => {
     [curriculumCombined, degreeProgrammeCodes]
   )
 
-  const formattedStudents = useMemo(() => students.map(formatStudent), [students])
+  const formattedStudents = useMemo(
+    () => students.map(student => formatStudent(student, degreeProgrammeCodes)),
+    [students, degreeProgrammeCodes]
+  )
 
   return ModulesTab({ formattedModules, formattedStudents })
 }
