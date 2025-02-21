44a7f8bcee5a5d7a227dcbedece436d271d02719
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2025-02-21 14:47:44 +0200

    [Class statistics] Fix page crash when opening courses tab before data has finished loading

--- services/frontend/src/components/PopulationStudents/StudentTable/CoursesTab/index.jsx
+++ services/frontend/src/components/PopulationStudents/StudentTable/CoursesTab/index.jsx
@@ -6,35 +6,41 @@ import { useGetStudyGuidanceGroupPopulationCoursesQuery } from '@/redux/studyGui
 import { CoursesTable } from './CoursesTable'
 
 export const CoursesTabContainer = ({ curriculum, includeSubstitutions, students, studyGuidanceGroup, variant }) => {
-  const { data: studyGuidanceGroupPopulationsCourses, isLoading: coursesAreLoading } =
-    useGetStudyGuidanceGroupPopulationCoursesQuery({
-      studentnumberlist: students.map(student => student.studentNumber).sort(),
-      year: studyGuidanceGroup?.tags?.year,
-    })
+  const { data: studyGuidanceGroupPopulationsCourses, isLoading } = useGetStudyGuidanceGroupPopulationCoursesQuery({
+    studentnumberlist: students.map(student => student.studentNumber).sort(),
+    year: studyGuidanceGroup?.tags?.year,
+  })
 
-  const { data: populationCourses, pending } = useSelector(state => state?.populationSelectedStudentCourses)
+  const { data: populationCourses } = useSelector(state => state?.populationSelectedStudentCourses)
+
+  const studyGuidanceGroupLoading = isLoading || !curriculum || !students
+  const loading = !curriculum || !students || !populationCourses
 
   if (variant === 'studyGuidanceGroupPopulation') {
     return (
-      <Tab.Pane loading={coursesAreLoading || !curriculum}>
-        <CoursesTable
-          curriculum={curriculum}
-          includeSubstitutions={includeSubstitutions}
-          populationCourses={studyGuidanceGroupPopulationsCourses}
-          students={students}
-        />
+      <Tab.Pane loading={studyGuidanceGroupLoading}>
+        {!studyGuidanceGroupLoading && (
+          <CoursesTable
+            curriculum={curriculum}
+            includeSubstitutions={includeSubstitutions}
+            populationCourses={studyGuidanceGroupPopulationsCourses}
+            students={students}
+          />
+        )}
       </Tab.Pane>
     )
   }
 
   return (
-    <Tab.Pane loading={pending || !curriculum}>
-      <CoursesTable
-        curriculum={curriculum}
-        includeSubstitutions={includeSubstitutions}
-        populationCourses={populationCourses}
-        students={students}
-      />
+    <Tab.Pane loading={loading}>
+      {!loading && (
+        <CoursesTable
+          curriculum={curriculum}
+          includeSubstitutions={includeSubstitutions}
+          populationCourses={populationCourses}
+          students={students}
+        />
+      )}
     </Tab.Pane>
   )
 }
