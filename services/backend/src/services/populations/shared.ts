0b86f82eb7cf4e10105c0ad8c34e574e8cd0aeb3
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-03-13 17:19:00 +0200

    [Backend] Making code climate less unhappy with populations/shared

--- services/backend/src/services/populations/shared.ts
+++ services/backend/src/services/populations/shared.ts
@@ -424,31 +424,23 @@ export const getOptionsForStudents = async (studentNumbers: string[], code: stri
     ],
   })
 
-  return studyRightElementsForStudyRight.reduce<Record<string, { name: Name }>>((acc, { studyRight }) => {
-    let programme: SISStudyRightElement | null = null
+  return Object.fromEntries(
+    studyRightElementsForStudyRight
+      .map(({ studyRight }) => {
+        const levelIsMasters = level === DegreeProgrammeType.MASTER
+        const filter = levelIsMasters ? DegreeProgrammeType.BACHELOR : DegreeProgrammeType.MASTER
 
-    if (level === DegreeProgrammeType.MASTER) {
-      const [latestBachelorsProgramme] = orderBy(
-        studyRight.studyRightElements.filter(element => element.degreeProgrammeType === DegreeProgrammeType.BACHELOR),
-        ['endDate'],
-        ['desc']
-      )
-      programme = latestBachelorsProgramme
-    } else {
-      const [firstMastersProgramme] = orderBy(
-        studyRight.studyRightElements.filter(element => element.degreeProgrammeType === DegreeProgrammeType.MASTER),
-        ['startDate'],
-        ['asc']
-      )
-      programme = firstMastersProgramme
-    }
+        // NOTE: If in masters, then select latest finished bachlor studyRight otherwise select first started masters studyRight
+        const [latestProgramme] = orderBy(
+          studyRight.studyRightElements.filter(element => element.degreeProgrammeType === filter),
+          [levelIsMasters ? 'endDate' : 'startDate'],
+          [levelIsMasters ? 'desc' : 'asc']
+        )
 
-    if (programme) {
-      acc[studyRight.studentNumber] = { name: programme.name }
-    }
-
-    return acc
-  }, {})
+        return [studyRight.studentNumber, { name: latestProgramme?.name }] as [string, { name: Name }]
+      })
+      .filter(([_, { name }]) => !!name)
+  )
 }
 
 export const formatStudentsForApi = (
@@ -512,108 +504,107 @@ export type CoursesQueryResult = Array<
   }
 >
 
+const findDefinedCourseStatsForStudents = `
+  SELECT
+    course.code,
+    course.name,
+    course.substitutions,
+    course.main_course_code,
+    enrollment.data AS enrollments,
+    credit.data AS credits
+  FROM course
+  LEFT JOIN (
+    SELECT
+      course_code,
+      ARRAY_AGG(JSONB_BUILD_OBJECT(
+        'studentnumber', studentnumber,
+        'state', state,
+        'enrollment_date_time', enrollment_date_time
+      )) AS data
+    FROM enrollment
+    WHERE studentnumber IN (:studentnumbers)
+      AND state = :enrollmentState
+    GROUP BY course_code
+  ) AS enrollment
+    ON enrollment.course_code = course.code
+  LEFT JOIN (
+    SELECT
+      course_code,
+      ARRAY_AGG(JSONB_BUILD_OBJECT(
+        'grade', grade,
+        'student_studentnumber', student_studentnumber,
+        'attainment_date', attainment_date,
+        'credittypecode', credittypecode,
+        'course_code', course_code
+      )) AS data
+    FROM credit
+    WHERE student_studentnumber IN (:studentnumbers)
+    GROUP BY course_code
+  ) AS credit
+    ON credit.course_code = course.code
+  WHERE
+    (
+      course.code IN (:courseCodes)
+      OR (
+        SELECT
+          JSONB_AGG(DISTINCT alt_code)
+        FROM course, LATERAL JSONB_ARRAY_ELEMENTS(substitutions) as alt_code
+        WHERE code IN (:courseCodes)
+      ) ? course.code
+    ) AND (enrollment.data IS NOT NULL OR credit.data IS NOT NULL)
+`
+
+const findAllCourseStatsForStudents = `
+  SELECT
+    course.code,
+    course.name,
+    course.substitutions,
+    course.main_course_code,
+    enrollment.data AS enrollments,
+    credit.data AS credits
+  FROM course
+  LEFT JOIN (
+    SELECT
+      course_code,
+      ARRAY_AGG(JSONB_BUILD_OBJECT(
+        'studentnumber', studentnumber,
+        'state', state,
+        'enrollment_date_time', enrollment_date_time
+      )) AS data
+    FROM enrollment
+    WHERE studentnumber IN (:studentnumbers)
+      AND state = :enrollmentState
+    GROUP BY course_code
+  ) AS enrollment
+    ON enrollment.course_code = course.code
+  LEFT JOIN (
+    SELECT
+      course_code,
+      ARRAY_AGG(JSONB_BUILD_OBJECT(
+        'grade', grade,
+        'student_studentnumber', student_studentnumber,
+        'attainment_date', attainment_date,
+        'credittypecode', credittypecode,
+        'course_code', course_code
+      )) AS data
+    FROM credit
+    WHERE student_studentnumber IN (:studentnumbers)
+    GROUP BY course_code
+  ) AS credit
+    ON credit.course_code = course.code
+  WHERE
+    enrollment.data IS NOT NULL OR credit.data IS NOT NULL
+`
+
 export const findCourses = async (studentNumbers: string[], courses: string[] = []) => {
-  return sequelize.query(
-    courses.length
-      ? `
-      SELECT
-        course.code,
-        course.name,
-        course.substitutions,
-        course.main_course_code,
-        enrollment.data AS enrollments,
-        credit.data AS credits
-      FROM course
-      LEFT JOIN (
-        SELECT
-          course_code,
-          ARRAY_AGG(JSONB_BUILD_OBJECT(
-            'studentnumber', studentnumber,
-            'state', state,
-            'enrollment_date_time', enrollment_date_time
-          )) AS data
-        FROM enrollment
-        WHERE studentnumber IN (:studentnumbers)
-          AND state = :enrollmentState
-        GROUP BY course_code
-      ) AS enrollment
-        ON enrollment.course_code = course.code
-      LEFT JOIN (
-        SELECT
-          course_code,
-          ARRAY_AGG(JSONB_BUILD_OBJECT(
-            'grade', grade,
-            'student_studentnumber', student_studentnumber,
-            'attainment_date', attainment_date,
-            'credittypecode', credittypecode,
-            'course_code', course_code
-          )) AS data
-        FROM credit
-        WHERE student_studentnumber IN (:studentnumbers)
-        GROUP BY course_code
-      ) AS credit
-        ON credit.course_code = course.code
-      WHERE
-        (
-          course.code IN (:courseCodes)
-          OR (
-            SELECT
-              JSONB_AGG(DISTINCT alt_code)
-            FROM course, LATERAL JSONB_ARRAY_ELEMENTS(substitutions) as alt_code
-            WHERE code IN (:courseCodes)
-          ) ? course.code
-        ) AND (enrollment.data IS NOT NULL OR credit.data IS NOT NULL)
-    `
-      : `
-      SELECT
-        course.code,
-        course.name,
-        course.substitutions,
-        course.main_course_code,
-        enrollment.data AS enrollments,
-        credit.data AS credits
-      FROM course
-      LEFT JOIN (
-        SELECT
-          course_code,
-          ARRAY_AGG(JSONB_BUILD_OBJECT(
-            'studentnumber', studentnumber,
-            'state', state,
-            'enrollment_date_time', enrollment_date_time
-          )) AS data
-        FROM enrollment
-        WHERE studentnumber IN (:studentnumbers)
-          AND state = :enrollmentState
-        GROUP BY course_code
-      ) AS enrollment
-        ON enrollment.course_code = course.code
-      LEFT JOIN (
-        SELECT
-          course_code,
-          ARRAY_AGG(JSONB_BUILD_OBJECT(
-            'grade', grade,
-            'student_studentnumber', student_studentnumber,
-            'attainment_date', attainment_date,
-            'credittypecode', credittypecode,
-            'course_code', course_code
-          )) AS data
-        FROM credit
-        WHERE student_studentnumber IN (:studentnumbers)
-        GROUP BY course_code
-      ) AS credit
-        ON credit.course_code = course.code
-      WHERE
-        enrollment.data IS NOT NULL OR credit.data IS NOT NULL
-    `,
-    {
-      replacements: {
-        studentnumbers: studentNumbers.length > 0 ? studentNumbers : ['DUMMY'],
-        courseCodes: courses.length ? courses : ['DUMMY'],
-        enrollmentState: EnrollmentState.ENROLLED,
-      },
-      type: QueryTypes.SELECT,
-    }
-  ) as Promise<CoursesQueryResult>
+  return sequelize.query(courses.length ? findDefinedCourseStatsForStudents : findAllCourseStatsForStudents, {
+    replacements: {
+      studentnumbers: studentNumbers.length > 0 ? studentNumbers : ['DUMMY'],
+      courseCodes: courses.length ? courses : ['DUMMY'],
+      enrollmentState: EnrollmentState.ENROLLED,
+    },
+    type: QueryTypes.SELECT,
+  }) as Promise<CoursesQueryResult>
 }
 
 export const parseCreditInfo = (credit: CreditPick) => {
