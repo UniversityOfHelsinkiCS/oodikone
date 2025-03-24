6a649b5f0088f760f8088414b9ac69f28350496c
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-03-24 12:53:53 +0200

    [Backend] Refactor customOpenUniSearch route

--- services/backend/src/routes/customOpenUniSearch.ts
+++ services/backend/src/routes/customOpenUniSearch.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express'
+import { Router } from 'express'
 
 import {
   getOpenUniSearches,
@@ -7,59 +7,75 @@ import {
   updateSearch,
 } from '../services/openUni/openUniManageSearches'
 import { getCustomOpenUniCourses } from '../services/openUni/openUniStats'
+import { CanError } from '../types'
+import { safeJSONParse } from '../util'
 
 const router = Router()
 
-interface GetSearchRequest extends Request {
-  query: {
-    courselist: string
-    startdate: string
-    enddate: string
-  }
+type SearchResBody = CanError<Awaited<ReturnType<typeof getCustomOpenUniCourses>>>
+type SearchReqBody = never
+type SearchQuery = {
+  courselist: string
+  startdate: string
+  enddate: string
 }
 
-router.get('/', async (req: GetSearchRequest, res: Response) => {
-  const courseCodes = JSON.parse(req.query?.courselist) || []
-  const startDate = req.query?.startdate !== 'null' ? new Date(req.query.startdate) : new Date(2017, 7, 1, 0, 0, 0)
-  const endDate = req.query?.enddate !== 'null' ? new Date(req.query.enddate) : new Date()
-  if (req.query?.enddate === 'null') {
-    endDate.setHours(23, 59, 59, 999)
-  }
+router.get<never, SearchResBody, SearchReqBody, SearchQuery>('/', async (req, res) => {
+  const { courselist, startdate, enddate } = req.query
 
+  const courseCodes = (await safeJSONParse(courselist)) || []
   if (!Array.isArray(courseCodes)) {
     return res.status(400).json({ error: 'Courses must be of type array' })
   }
+
+  const startDate = startdate !== 'null' ? new Date(startdate) : new Date(2017, 7, 1, 0, 0, 0)
+  const endDate = enddate !== 'null' ? new Date(enddate) : new Date()
+  if (enddate === 'null') {
+    endDate.setHours(23, 59, 59, 999)
+  }
+
   const customOpenUniSearches = await getCustomOpenUniCourses(courseCodes, startDate, endDate)
   return res.json(customOpenUniSearches)
 })
 
-router.get('/searches', async (req: Request, res: Response) => {
-  const userId = req.user.id
+type GetSearchResBody = Awaited<ReturnType<typeof getOpenUniSearches>>
+router.get<never, GetSearchResBody>('/searches', async (req, res) => {
+  const { id: userId } = req.user
   const foundSearches = await getOpenUniSearches(userId)
   return res.json(foundSearches)
 })
 
-interface CreateSearchRequest extends Request {
-  body: {
-    courselist: string[]
-    name: string
-  }
+type CreateSearchResBody = CanError<{
+  id: string
+  userId: string
+  name: string
+  courseList: string[]
+  updatedAt: string
+}>
+type CreateSearchReqBody = {
+  courselist: string[]
+  name: string
 }
 
-router.post('/searches', async (req: CreateSearchRequest, res: Response) => {
-  const courseCodes = req.body?.courselist || []
-  const name = req.body?.name
-  const userId = req.user.id
+router.post<never, CreateSearchResBody, CreateSearchReqBody>('/searches', async (req, res) => {
+  const { courselist, name } = req.body
+  const { id: userId } = req.user
+
+  const courseCodes = courselist || []
+
   if (!name) {
     return res.status(400).json({ error: 'Name missing' })
   }
+
   if (courseCodes && !Array.isArray(courseCodes)) {
     return res.status(400).json({ error: 'Course codes must be type of array' })
   }
+
   const createdSearch = await createNewSearch(userId, name, courseCodes)
   if (!createdSearch) {
     return res.status(400).json({ error: 'Failed to create search' })
   }
+
   return res.status(201).json({
     id: createdSearch.id,
     userId: createdSearch.userId,
@@ -69,26 +85,36 @@ router.post('/searches', async (req: CreateSearchRequest, res: Response) => {
   })
 })
 
-interface UpdateSearchRequest extends Request {
-  body: {
-    courselist: string[]
-  }
-  params: {
-    id: string
-  }
+type UpdateSearchParams = {
+  id: string
+}
+type UpdateSearchResBody = CanError<{
+  id: string
+  userId: string
+  name: string
+  courseList: string[]
+  updatedAt: string
+}>
+type UpdateSearchReqBody = {
+  courselist: string[]
 }
 
-router.put('/searches/:id', async (req: UpdateSearchRequest, res: Response) => {
-  const id = req.params?.id
-  const courseCodes = req.body?.courselist || []
-  const userId = req.user.id
+router.put<UpdateSearchParams, UpdateSearchResBody, UpdateSearchReqBody>('/searches/:id', async (req, res) => {
+  const { id } = req.params
+  const { courselist } = req.body
+  const { id: userId } = req.user
+
+  const courseCodes = courselist || []
+
   if (!id || !userId) {
     return res.status(422).end()
   }
+
   const updatedSearch = await updateSearch(userId, id, courseCodes)
   if (!updatedSearch) {
     return res.status(404).json({ error: 'Open uni search could not be found' })
   }
+
   return res.json({
     id: updatedSearch.id,
     userId: updatedSearch.userId,
@@ -98,22 +124,24 @@ router.put('/searches/:id', async (req: UpdateSearchRequest, res: Response) => {
   })
 })
 
-interface DeleteSearchRequest extends Request {
-  params: {
-    id: string
-  }
+type DeleteSearchParams = {
+  id: string
 }
+type DeleteSearchResBody = CanError<string>
 
-router.delete('/searches/:id', async (req: DeleteSearchRequest, res: Response) => {
-  const id = req.params?.id
-  const userId = req.user.id
+router.delete<DeleteSearchParams, DeleteSearchResBody>('/searches/:id', async (req, res) => {
+  const { id } = req.params
+  const { id: userId } = req.user
+
   if (!id || !userId) {
     return res.status(422).end()
   }
+
   const deletedSearch = await deleteSearch(userId, id)
   if (!deletedSearch) {
     return res.status(404).json({ error: 'Open uni search could not be found' })
   }
+
   return res.json(id)
 })
 
