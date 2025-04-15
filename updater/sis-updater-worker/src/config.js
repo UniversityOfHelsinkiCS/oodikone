31787e6df003cf4fc7fa0273b055d98c2f6eeb17
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2025-04-15 16:14:32 +0300

    [Updater] Add default for REDIS_HOST

--- updater/sis-updater-worker/src/config.js
+++ updater/sis-updater-worker/src/config.js
@@ -3,7 +3,7 @@ module.exports.isStaging = process.env.STAGING || false
 module.exports.isProduction = process.env.NODE_ENV === 'production'
 module.exports.MIGRATIONS_LOCK = 'MIGRATIONS_LOCK'
 module.exports.PURGE_LOCK = 'PURGE_LOCK'
-module.exports.REDIS_HOST = process.env.REDIS_HOST
+module.exports.REDIS_HOST = process.env.REDIS_HOST || 'redis-updater'
 module.exports.REDIS_PORT = process.env.REDIS_PORT || 6379
 module.exports.rootOrgId = process.env.ROOT_ORG_ID || 'hy-university-root-id'
 module.exports.serviceProvider = process.env.SERVICE_PROVIDER ? process.env.SERVICE_PROVIDER.toLowerCase() : ''
