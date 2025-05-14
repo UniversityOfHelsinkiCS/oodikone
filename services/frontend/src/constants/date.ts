be45b0939fdc26d048eed7d2c7357b248aa2526b
Joonas Ahovalli <joonas.ahovalli@gmail.com>
2025-05-14 22:21:52 +0300

    Chore: delete unused components

--- services/frontend/src/constants/date.ts
+++ services/frontend/src/constants/date.ts
@@ -4,7 +4,7 @@ export const DISPLAY_DATETIME_FORMAT = 'DD.MM.YYYY HH:mm'
 export const DISPLAY_DATE_FORMAT_DEV = 'DD.MM.YYYY HH:mm:ss'
 export const LONG_DATE_TIME_FORMAT = 'D MMMM YYYY [at] HH:mm:ss'
 export const ISO_DATE_FORMAT = 'YYYY-MM-DD'
-export const ISO_DATE_FORMAT_DEV = 'YYYY-MM-DD HH:mm:ss'
+// export const ISO_DATE_FORMAT_DEV = 'YYYY-MM-DD HH:mm:ss' // uncomment if needed
 export const YEAR_DATE_FORMAT = 'YYYY'
 
 export enum DateFormat {
