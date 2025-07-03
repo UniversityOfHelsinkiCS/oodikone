e2774bfdc4b58c859d9dff57f52272302c748aa2
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-07-03 17:47:39 +0300

    [Backend] Add route types to routes/login

--- services/backend/src/routes/login.ts
+++ services/backend/src/routes/login.ts
@@ -1,11 +1,19 @@
-import { Request, Response, Router } from 'express'
-import { omit } from 'lodash'
+import { Router } from 'express'
 
+import { CanError } from '@oodikone/shared/routes'
+import { omitKeys } from '@oodikone/shared/util'
+import { FormattedUser } from '../types/user'
 import { ApplicationError } from '../util/customErrors'
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
+type LoginReqBody = never
+type LoginResBody = {
+  user: Omit<FormattedUser, 'studentsUserCanAccess'>
+  logoutUrl?: string
+}
+
+router.get<never, CanError<LoginResBody>, LoginReqBody>('/', (req, res) => {
   const { user, logoutUrl } = req
 
   if (!user) {
@@ -13,7 +21,7 @@ router.get('/', (req: Request, res: Response) => {
   }
 
   res.send({
-    user: omit(user, ['studentsUserCanAccess']),
+    user: omitKeys(user, ['studentsUserCanAccess']),
     logoutUrl,
   })
 })
