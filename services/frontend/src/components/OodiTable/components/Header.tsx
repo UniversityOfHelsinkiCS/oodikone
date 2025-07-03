2e8c9c2a7e5eddd3db2049eeb9833d49928027be
joonas-a <joonas.ahovalli@gmail.com>
2025-07-03 16:46:08 +0300

    OT: two-story columns

--- services/frontend/src/components/OodiTable/components/Header.tsx
+++ services/frontend/src/components/OodiTable/components/Header.tsx
@@ -2,16 +2,15 @@ import type { FC, ReactNode } from 'react'
 
 import TableCell, { type TableCellProps } from '@mui/material/TableCell'
 
-const OodiTableHeader: FC<TableCellProps & { children?: ReactNode }> = ({
+export const OodiTableHeader: FC<TableCellProps & { children?: ReactNode }> = ({
   children,
   ...props
 }) => {
   return (
-    <TableCell {...props} sx={{ background: "white", border: '1px solid rgba(34,36,38,.1)', fontWeight: 'bold' }}>
+    <TableCell {...props} sx={{ border: '1px solid #e9e9e9', fontWeight: 'bold' }}>
       {children}
     </TableCell>
   )
 
 }
 
-export default OodiTableHeader
