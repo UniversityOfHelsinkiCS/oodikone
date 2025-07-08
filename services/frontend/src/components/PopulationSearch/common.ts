a0ee324fe7006a508821fc9eb310962027ce5e9b
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-07-08 10:08:02 +0300

    [Frontend] Remove unused common file

--- services/frontend/src/components/PopulationSearch/common.ts
+++
@@ -1,5 +0,0 @@
-export const getMonths = (year: string, term: 'FALL' | 'SPRING') => {
-  const now = new Date()
-  const start = term === 'FALL' ? new Date(`${year}-08-01`) : new Date(`${Number(year) + 1}-01-01`)
-  return (now.getFullYear() - start.getFullYear()) * 12 + now.getMonth() - start.getMonth() + 1
-}
