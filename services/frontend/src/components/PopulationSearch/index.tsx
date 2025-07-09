d1c41972f79886491ca951cce8913f217034289b
joonas-a <joonas.ahovalli@gmail.com>
2025-07-09 16:33:12 +0300

    [Population search] styling

--- services/frontend/src/components/PopulationSearch/index.tsx
+++ services/frontend/src/components/PopulationSearch/index.tsx
@@ -1,9 +1,18 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import { populationStatisticsToolTips } from '@/common/InfoToolTips'
 import { Section } from '../material/Section'
 import { PopulationSearchForm } from './PopulationSearchForm'
 
 export const PopulationSearch = () => (
-  <Section cypress="PopulationSearch" infoBoxContent={populationStatisticsToolTips.search} title="Search for class">
-    <PopulationSearchForm />
-  </Section>
+  <Box sx={{ mx: '1em' }}>
+    <Box sx={{ maxWidth: '1200px', mx: 'auto', my: 2.5, flex: 1 }}>
+      <Typography sx={{ textAlign: 'center', m: 2 }} variant="h4">
+        Class statistics
+      </Typography>
+      <Section cypress="PopulationSearch" infoBoxContent={populationStatisticsToolTips.search} title="Search for class">
+        <PopulationSearchForm />
+      </Section>
+    </Box>
+  </Box>
 )
