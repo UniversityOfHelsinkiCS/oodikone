191b8b4c3a84565d543f7063ecdd593b89574509
joonas-a <joonas.ahovalli@gmail.com>
2025-07-17 16:01:19 +0300

    [Population search form] split into parts

--- services/frontend/src/components/PopulationSearch/PopulationSearchForm.tsx
+++ services/frontend/src/components/PopulationSearch/PopulationSearchForm.tsx
@@ -1,13 +1,8 @@
-import AddIcon from '@mui/icons-material/Add'
-import RemoveIcon from '@mui/icons-material/Remove'
 import Alert from '@mui/material/Alert'
 import Autocomplete from '@mui/material/Autocomplete'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import FormControlLabel from '@mui/material/FormControlLabel'
-import IconButton from '@mui/material/IconButton'
-import MenuItem from '@mui/material/MenuItem'
-import Select, { type SelectChangeEvent } from '@mui/material/Select'
 import Stack from '@mui/material/Stack'
 import Switch from '@mui/material/Switch'
 import TextField from '@mui/material/TextField'
@@ -17,7 +12,6 @@ import { useState } from 'react'
 import { useNavigate, useLocation } from 'react-router'
 
 import { isNewStudyProgramme } from '@/common'
-import { FilterOldProgrammesToggle } from '@/components/common/FilterOldProgrammesToggle'
 import { useLanguage } from '@/components/LanguagePicker/useLanguage'
 import { InfoBox } from '@/components/material/InfoBox'
 import { SearchHistory } from '@/components/material/SearchHistory'
@@ -38,6 +32,7 @@ import type {
 } from '@/types/populationSearch'
 import { createPinnedFirstComparator } from '@/util/comparator'
 import { queryParamsToString } from '@/util/queryparams'
+import { EnrollmentDateSelector } from './EnrollmentDateSelector'
 
 const bachelorAndMasterInfoTooltip = `If you choose a Bachelor's programme, toggling 'Show
     Bachelor + Master' on will also show information about the students' master's studies. If you
@@ -57,7 +52,7 @@ export const PopulationSearchForm = () => {
   const [studentStatuses, _setStudentStatuses] = useState<StudentStatus[]>([])
   const [showBachelorAndMaster, setShowBachelorAndMaster] = useState(false)
   const [searchHistory, addItemToSearchHistory, updateItemInSearchHistory] = useSearchHistory('populationSearch', 8)
-  const [filterProgrammes, setFilterProgrammes] = useState(fullAccessToStudentData)
+  const [filterProgrammes, setFilterProgrammes] = useState<boolean>(fullAccessToStudentData)
   const { data: programmes = {}, isLoading: programmesAreLoading } = useGetProgrammesQuery()
 
   const studyProgrammes =
@@ -139,66 +134,6 @@ export const PopulationSearchForm = () => {
     })
 
     pushQueryToUrl(query)
-  }
-
-  const handleYearSelection = (event: SelectChangeEvent) => {
-    setYear(parseInt(event.target.value, 10))
-  }
-
-  const RenderEnrollmentDateSelector = () => {
-    const currentYear = new Date().getFullYear()
-    const lowestYear = 1970
-    const options = [...Array(currentYear + 1 - lowestYear).keys()].map(value => lowestYear + value)
-    return (
-      <Box>
-        <Typography fontWeight="bold" sx={{ mb: 1 }} variant="subtitle1">
-          Class of
-        </Typography>
-        <Stack direction="row" spacing={1} sx={{ mt: 0 }}>
-          <IconButton
-            data-cy="population-year-decrement"
-            disabled={year <= lowestYear}
-            onClick={() => setYear(year => (year > lowestYear ? year - 1 : year))}
-          >
-            <RemoveIcon />
-          </IconButton>
-          <Select
-            MenuProps={{
-              PaperProps: {
-                sx: {
-                  maxHeight: 300,
-                },
-              },
-            }}
-            data-cy="population-year-selector"
-            onChange={handleYearSelection}
-            sx={{ width: 'fit-content', pl: 1, pr: 1 }}
-            value={year.toString()}
-          >
-            {options.map(option => (
-              <MenuItem
-                key={option}
-                sx={{ justifyContent: 'center' }}
-                value={option}
-              >{`${option} - ${option + 1}`}</MenuItem>
-            ))}
-          </Select>
-          <IconButton
-            data-cy="population-year-increment"
-            disabled={year >= currentYear}
-            onClick={() => setYear(year => year + 1)}
-          >
-            <AddIcon />
-          </IconButton>
-          {fullAccessToStudentData && (
-            <FilterOldProgrammesToggle
-              checked={filterProgrammes}
-              onChange={() => setFilterProgrammes(!filterProgrammes)}
-            />
-          )}
-        </Stack>
-      </Box>
-    )
   }
 
   const StudyProgrammeSelector = () => {
@@ -332,7 +267,13 @@ export const PopulationSearchForm = () => {
 
   return (
     <Stack spacing={3} sx={{ maxWidth: '1200px', width: '100%' }}>
-      <RenderEnrollmentDateSelector />
+      <EnrollmentDateSelector
+        filterProgrammes={filterProgrammes}
+        fullAccessToStudentData={fullAccessToStudentData}
+        setFilterProgrammes={setFilterProgrammes}
+        setYear={setYear}
+        year={year}
+      />
       <StudyProgrammeSelector />
       <StudyTrackSelector />
       <Button disabled={!programme} onClick={handleSubmit} size="large" sx={{ maxWidth: '12rem' }} variant="contained">
