bc26f1cf7b81ad23be189fcccee92a0104286799
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-07-18 17:04:52 +0300

    [Frontend] Convert studentNumber filter to predefined MUI components

--- services/frontend/src/components/FilterView/filters/studentNumber.tsx
+++ services/frontend/src/components/FilterView/filters/studentNumber.tsx
@@ -1,157 +1,54 @@
-import { difference, includes, isEqual, union } from 'lodash'
-import { useState } from 'react'
-import { Button, Icon } from 'semantic-ui-react'
+import SwapVertIcon from '@mui/icons-material/SwapVert'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import { includes, isEqual, union } from 'lodash'
 
 import { filterToolTips } from '@/common/InfoToolTips'
 import { formatToArray } from '@oodikone/shared/util'
 import { FilterTrayProps } from '../FilterTray'
+import { FilterSelect } from './common/FilterSelect'
 import { createFilter } from './createFilter'
 
-const IconButton = ({ onClick, ...props }) => (
-  <Button icon onClick={onClick} style={{ background: 'none', padding: '0 0.15em' }} type="button">
-    <Icon {...props} style={{ color: '#5a5a5a', marginRight: 0 }} />
-  </Button>
-)
-
-const EditableList = ({ value, onChange, renderLabel }) => {
-  const [inputValue, setInput] = useState('')
-
-  return (
-    <div style={{ border: '1px solid #dbdbdb', borderRadius: '3pt' }}>
-      <ul
-        style={{
-          listStyle: 'none',
-          margin: 0,
-          maxHeight: '10em',
-          overflowY: 'auto',
-          padding: 0,
-          scrollbarWidth: 'thin',
-        }}
-      >
-        {value.map((studentNumber, index: number) => (
-          <li
-            className={'asdasd'}
-            key={studentNumber}
-            style={{
-              alignItems: 'center',
-              borderTop: index ? '1px solid #e6e6e7' : undefined,
-              display: 'flex',
-              height: '1.75em',
-            }}
-          >
-            <span style={{ fontSize: '0.9em', marginLeft: '0.3em' }}>{studentNumber}</span>
-            {renderLabel?.(studentNumber)}
-            <span style={{ flexGrow: 1 }} />
-            <IconButton
-              name="x"
-              onClick={() => {
-                onChange(difference(value, [studentNumber]))
-              }}
-            />
-          </li>
-        ))}
-        {value.length === 0 && (
-          <li
-            style={{
-              color: 'gray',
-              fontSize: '0.9em',
-              fontStyle: 'italic',
-              height: '1.75em',
-              lineHeight: '1.75em',
-              paddingLeft: '0.3em',
-            }}
-          >
-            Empty
-          </li>
-        )}
-      </ul>
-      <div style={{ borderTop: '1px solid #e6e6e7', display: 'flex', height: '1.75em' }}>
-        <input
-          onChange={event => setInput(event.target.value)}
-          placeholder="Student number(s)"
-          style={{
-            border: 'none',
-            outline: 'none',
-            flexGrow: 1,
-            fontSize: '0.9em',
-            minWidth: 0,
-            paddingLeft: '0.3em',
-          }}
-          type="text"
-          value={inputValue}
-        />
-        <div style={{ backgroundColor: '#e6e6e7', width: '1px' }} />
-        <IconButton
-          name="plus"
-          onClick={() => {
-            const parts = inputValue.split(/[^0-9]+/)
-            onChange(union(value, parts))
-            setInput('')
-          }}
-          title="Add to list"
-        />
-        <IconButton
-          name="minus"
-          onClick={() => {
-            const parts = inputValue.split(/[^0-9]+/)
-            onChange(difference(value, parts))
-            setInput('')
-          }}
-          title="Remove from list"
-        />
-        <IconButton name="trash alternate outline" onClick={() => onChange([])} title="Clear list" />
-      </div>
-    </div>
-  )
-}
-
 const StudentNumberFilterCard = ({ options, onOptionsChange, students }: FilterTrayProps) => {
-  const swapLists = () => {
-    onOptionsChange({
-      allowlist: options.blocklist,
-      blocklist: options.allowlist,
-    })
-  }
-
-  const labelRenderer = studentNumber => {
-    const studentIsPresent = students.some(student => student.studentNumber === studentNumber)
-    return (
-      <Icon
-        name={studentIsPresent ? 'check' : 'x'}
-        style={{
-          color: studentIsPresent ? '#31ac31' : '#df4747',
-          cursor: 'help',
-          fontSize: '0.8em',
-          marginLeft: '0.3em',
-          position: 'relative',
-          top: '-3px',
-        }}
-        title={
-          studentIsPresent ? 'Filter was applied to this student' : 'This student was not present in the population'
-        }
-      />
-    )
-  }
+  const dropdownOptions = students.map(({ studentNumber }) => ({
+    key: studentNumber,
+    text: studentNumber,
+    value: studentNumber,
+  }))
 
   return (
-    <div>
-      <h3 style={{ fontSize: '0.8em', marginBottom: '0.5em' }}>Allowlist</h3>
-      <EditableList
-        onChange={allowlist => onOptionsChange({ ...options, allowlist })}
-        renderLabel={labelRenderer}
+    <Stack gap={1}>
+      <FilterSelect
+        filterKey="studentNumberFilter-allowlist"
+        multiple
+        onChange={({ target }) => onOptionsChange({ ...options, allowlist: target.value })}
+        options={dropdownOptions}
         value={options.allowlist}
       />
-      <Button icon labelPosition="right" onClick={swapLists} size="mini" style={{ marginTop: '0.5em' }}>
-        <Icon name="exchange" rotated="clockwise" />
-        Swap lists
+      <Button
+        onClick={() =>
+          onOptionsChange({
+            allowlist: options.blocklist,
+            blocklist: options.allowlist,
+          })
+        }
+        sx={{ width: '80%', m: 'auto' }}
+        variant="outlined"
+      >
+        <Stack direction="row">
+          <SwapVertIcon />
+          <Typography>Swap lists</Typography>
+        </Stack>
       </Button>
-      <h3 style={{ fontSize: '0.8em', marginTop: '0.5em', marginBottom: '0.5em' }}>Blocklist</h3>
-      <EditableList
-        onChange={blocklist => onOptionsChange({ ...options, blocklist })}
-        renderLabel={labelRenderer}
+      <FilterSelect
+        filterKey="studentNumberFilter-blocklist"
+        multiple
+        onChange={({ target }) => onOptionsChange({ ...options, blocklist: target.value })}
+        options={dropdownOptions}
         value={options.blocklist}
       />
-    </div>
+    </Stack>
   )
 }
 
