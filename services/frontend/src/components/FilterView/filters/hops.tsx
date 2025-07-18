a68d1f13c19d7e5c03a0d5e3e366bbd06cf4df8b
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-07-18 11:11:38 +0300

    [Frontend] Convert hops filter to predefined MUI components

--- services/frontend/src/components/FilterView/filters/hops.tsx
+++ services/frontend/src/components/FilterView/filters/hops.tsx
@@ -1,148 +1,85 @@
-import dayjs from 'dayjs'
-import { Radio, Button, Form } from 'semantic-ui-react'
-
-import { useFilters } from '@/components/FilterView/useFilters'
 import { FilterTrayProps } from '../FilterTray'
+import { FilterRadio } from './common/FilterRadio'
+import { FilterSwitch } from './common/FilterSwitch'
 import { createFilter } from './createFilter'
-import { creditDateFilter } from './date'
 
-const getCutStudyStart = ({ options, filterDispatch }) => {
+const CombinedProgramme = ({ options, onOptionsChange, combinedProgramme }) => {
+  // For combined programme, we show radiobuttons due to many possible options
+  const typeOfCombined = combinedProgramme === 'MH90_001' ? 'Licentiate' : 'Master'
+
+  const modeObject = {
+    None: () =>
+      onOptionsChange({
+        ...options,
+        activeProgramme: false,
+        activeCombinedProgramme: false,
+        combinedIsSelected: 'default',
+      }),
+    'Bachelor studyright': () =>
+      onOptionsChange({
+        ...options,
+        activeProgramme: true,
+        activeCombinedProgramme: false,
+        combinedIsSelected: 'default',
+      }),
+    [`${typeOfCombined} studyright`]: () =>
+      onOptionsChange({
+        ...options,
+        activeProgramme: false,
+        activeCombinedProgramme: true,
+        combinedIsSelected: combinedProgramme,
+      }),
+    'Both studyrights': () =>
+      onOptionsChange({
+        ...options,
+        activeProgramme: true,
+        activeCombinedProgramme: true,
+        combinedIsSelected: combinedProgramme,
+      }),
+  }
+
+  const modeOptions = Object.keys(modeObject).map(key => ({
+    key,
+    text: key,
+    value: key,
+  }))
+
+  const defaultOption = modeOptions.shift()!
+
   return (
-    <Button
-      content="Cut credits to study start"
-      disabled={!options.activeProgramme && !options.activeCombinedProgramme}
-      onClick={() =>
-        filterDispatch(
-          creditDateFilter.actions.setOptions({
-            startDate: dayjs(options.studyStart),
-            endDate: null,
-          })
-        )
-      }
-      primary
-      size="mini"
-      style={{
-        margin: '0.5rem',
-        whiteSpace: 'nowrap',
-      }}
+    <FilterRadio
+      defaultOption={defaultOption}
+      filterKey="hopsFilter"
+      onChange={({ target }) => modeObject[target.value]()}
+      options={modeOptions}
     />
   )
 }
 
+const SingleProgramme = ({ options, onOptionsChange }) => {
+  const modeOptions = [
+    {
+      key: 'Default studyright',
+      text: 'Show only credits included in study plan',
+      checked: options.activeProgramme,
+      onClick: () =>
+        onOptionsChange({
+          ...options,
+          activeProgramme: !options.activeProgramme,
+          combinedIsSelected: 'default',
+        }),
+    },
+  ]
+
+  return <FilterSwitch filterKey="hopsFilter" options={modeOptions} />
+}
+
 const HopsFilterCard = ({ options, onOptionsChange, precomputed: combinedProgramme }: FilterTrayProps) => {
-  const { selectedStartDate } = creditDateFilter.selectors
-  const { filterDispatch, useFilterSelector } = useFilters()
-  const selectedCreditStartDate = useFilterSelector(selectedStartDate(''))
-
-  if (combinedProgramme) {
-    // For combined programme, we show radiobuttons due to many possible options
-    const typeOfCombined = combinedProgramme === 'MH90_001' ? 'Licentiate' : 'Master'
+  if (combinedProgramme)
     return (
-      <Form>
-        <div className="card-content">
-          <Form.Field style={{ display: 'flex', flexDirection: 'column' }}>
-            <Radio
-              checked={
-                !options.activeProgramme ||
-                (options.activeProgramme === false && options.activeCombinedProgramme === false)
-              }
-              data-cy="option-hops-bachelor"
-              label="None"
-              name="radioGroup"
-              onChange={() =>
-                onOptionsChange({
-                  ...options,
-                  activeProgramme: false,
-                  activeCombinedProgramme: false,
-                  combinedIsSelected: 'default',
-                })
-              }
-              style={{ margin: '0.5rem 0' }}
-            />
-            <Radio
-              checked={options.activeProgramme === true && options.activeCombinedProgramme === false}
-              data-cy="option-hops-bachelor"
-              label="Bachelor studyright"
-              name="radioGroup"
-              onChange={() =>
-                onOptionsChange({
-                  ...options,
-                  activeProgramme: true,
-                  activeCombinedProgramme: false,
-                  combinedIsSelected: 'default',
-                })
-              }
-              style={{ margin: '0.5rem 0' }}
-            />
-            <Radio
-              checked={options.activeProgramme === false && options.activeCombinedProgramme === true}
-              data-cy="option-hops-combined"
-              label={`${typeOfCombined} studyright`}
-              name="radioGroup"
-              onChange={() =>
-                onOptionsChange({
-                  ...options,
-                  activeProgramme: false,
-                  activeCombinedProgramme: true,
-                  combinedIsSelected: combinedProgramme,
-                })
-              }
-              style={{ margin: '0.5rem 0' }}
-            />
-            <Radio
-              checked={options.activeProgramme === true && options.activeCombinedProgramme === true}
-              data-cy="option-hops-both"
-              label="Both studyrights"
-              name="radioGroup"
-              onChange={() =>
-                onOptionsChange({
-                  ...options,
-                  activeProgramme: true,
-                  activeCombinedProgramme: true,
-                  combinedIsSelected: combinedProgramme,
-                })
-              }
-              style={{ margin: '0.5rem 0' }}
-            />
-            {options.studyStart ? getCutStudyStart({ options, filterDispatch }) : null}
-          </Form.Field>
-        </div>
-      </Form>
+      <CombinedProgramme combinedProgramme={combinedProgramme} onOptionsChange={onOptionsChange} options={options} />
     )
-  }
-
-  // For a single programme
-  return (
-    <div>
-      <div
-        onClick={() => {
-          if (
-            selectedCreditStartDate &&
-            options.studyStart &&
-            options.clearCreditDate &&
-            new Date(selectedCreditStartDate) > new Date(options.studyStart) &&
-            !options.activeProgramme
-          )
-            filterDispatch(
-              creditDateFilter.actions.setOptions({
-                startDate: null,
-                endDate: null,
-              })
-            )
-          onOptionsChange({
-            ...options,
-            activeProgramme: !options.activeProgramme,
-            combinedIsSelected: 'default',
-          })
-        }}
-        style={{ display: 'flex', alignItems: 'center', gap: '1em', cursor: 'pointer' }}
-      >
-        <Radio checked={options.activeProgramme} style={{ width: '3.5rem', flexShrink: 0 }} toggle />
-        <div>Show only credits included in study plan</div>
-      </div>
-      {options.studyStart ? getCutStudyStart({ options, filterDispatch }) : null}
-    </div>
-  )
+  return <SingleProgramme onOptionsChange={onOptionsChange} options={options} />
 }
 
 export const hopsFilter = createFilter({
@@ -156,9 +93,7 @@ export const hopsFilter = createFilter({
     combinedIsSelected: 'default',
   },
 
-  precompute: ({ args }) => {
-    return args.combinedProgrammeCode
-  },
+  precompute: ({ args }) => args.combinedProgrammeCode,
 
   isActive: arg => !!arg?.activeProgramme || !!arg?.activeCombinedProgramme,
 
@@ -170,11 +105,10 @@ export const hopsFilter = createFilter({
       ({ programme_code, sis_study_right_id }) =>
         programme_code === args.programmeCode && studyRights.includes(sis_study_right_id)
     )
-    const secondHops = args.combinedProgrammeCode
-      ? student.studyplans.find(
-          plan => plan.programme_code === args.combinedProgrammeCode && studyRights.includes(plan.sis_study_right_id)
-        )
-      : null
+    const secondHops = student.studyplans.find(
+      ({ programme_code, sis_study_right_id }) =>
+        programme_code === args.combinedProgrammeCode && studyRights.includes(sis_study_right_id)
+    )
 
     if (activeProgramme || activeCombinedProgramme) {
       if (!hops && !secondHops) {
