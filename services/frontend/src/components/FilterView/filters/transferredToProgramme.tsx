2212af8d7ec00c1d5ed771e8483025407f03794d
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-07-18 20:51:02 +0300

    [Frontend] Convert transferredToProgramme filter to predefined MUI components

--- services/frontend/src/components/FilterView/filters/transferredToProgramme.tsx
+++ services/frontend/src/components/FilterView/filters/transferredToProgramme.tsx
@@ -1,43 +1,37 @@
-import { Form, Radio } from 'semantic-ui-react'
-
 import { filterToolTips } from '@/common/InfoToolTips'
 import { FilterTrayProps } from '../FilterTray'
+import { FilterRadio } from './common/FilterRadio'
 import { createFilter } from './createFilter'
 
 const TransferredToProgrammeFilterCard = ({ options, onOptionsChange }: FilterTrayProps) => {
   const { transferred } = options
 
-  const toggle = buttonValue => () =>
+  const toggle = buttonValue =>
     onOptionsChange({
       transferred: transferred === buttonValue ? null : buttonValue,
     })
 
+  const modeObject = {
+    All: () => toggle(null),
+    Transferred: () => toggle(true),
+    'Not transferred': () => toggle(false),
+  }
+
+  const modeOptions = Object.keys(modeObject).map(key => ({
+    key,
+    text: key,
+    value: key,
+  }))
+
+  const defaultOption = modeOptions.shift()!
+
   return (
-    <Form>
-      <Form.Field style={{ display: 'flex', flexDirection: 'column', gap: '0.5em' }}>
-        <Radio
-          checked={transferred === null}
-          data-cy="option-all"
-          label="All"
-          name="radioGroup"
-          onChange={toggle(null)}
-        />
-        <Radio
-          checked={transferred === true}
-          data-cy="option-have"
-          label="Transferred"
-          name="radioGroup"
-          onChange={toggle(true)}
-        />
-        <Radio
-          checked={transferred === false}
-          data-cy="option-havenot"
-          label="Not transferred"
-          name="radioGroup"
-          onChange={toggle(false)}
-        />
-      </Form.Field>
-    </Form>
+    <FilterRadio
+      defaultOption={defaultOption}
+      filterKey="transferredToProgrammeFilter"
+      onChange={({ target }) => modeObject[target.value]()}
+      options={modeOptions}
+    />
   )
 }
 
