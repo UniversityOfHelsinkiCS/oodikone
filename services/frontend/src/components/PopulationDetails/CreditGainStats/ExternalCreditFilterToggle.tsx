9bc087818a375785a4ba19b88647bec37ec350ef
joonas-a <joonas.ahovalli@gmail.com>
2025-07-22 16:38:39 +0300

    [Credit statistics] convert filter toggle to tsx

--- services/frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.tsx
+++ services/frontend/src/components/PopulationDetails/CreditGainStats/ExternalCreditFilterToggle.tsx
@@ -2,7 +2,7 @@ import { studentNumberFilter } from '@/components/FilterView/filters'
 import { FilterToggle } from '@/components/FilterView/FilterToggle'
 import { useFilters } from '@/components/FilterView/useFilters'
 
-export const ExternalCreditFilterToggle = ({ students, helpText }) => {
+export const ExternalCreditFilterToggle = ({ students, helpText }: { students: string[]; helpText: string }) => {
   const { filterDispatch, useFilterSelector } = useFilters()
   const currentFilterIsActive = useFilterSelector(studentNumberFilter.selectors.studentListIsEqualToAllowlist(students))
   const someFilterIsActive = useFilterSelector(studentNumberFilter.selectors.isActive())
