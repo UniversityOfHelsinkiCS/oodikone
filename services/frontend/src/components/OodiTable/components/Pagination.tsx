8ad4be048d86e44425609d884cd2fe6848a8cae6
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-08-13 16:16:06 +0300

    [Ooditable] Make pagination sizes consistent

--- services/frontend/src/components/OodiTable/components/Pagination.tsx
+++ services/frontend/src/components/OodiTable/components/Pagination.tsx
@@ -1,17 +1,22 @@
 import TablePagination from '@mui/material/TablePagination'
 import type { Table } from '@tanstack/react-table'
 
-export const OodiTablePagination = <TData,>({ table }: { table: Table<TData> }) => (
-  <TablePagination
-    component="div"
-    count={table.getFilteredRowModel().rows.length}
-    onPageChange={(_, newPage) => table.setPageIndex(newPage)}
-    onRowsPerPageChange={event => {
-      table.setPageSize(Number(event.target.value))
-      table.setPageIndex(0)
-    }}
-    page={table.getState().pagination.pageIndex}
-    rowsPerPage={table.getState().pagination.pageSize}
-    rowsPerPageOptions={[100, 200, 500, 1000, table.getRowCount()]}
-  />
-)
+export const OodiTablePagination = <TData,>({ table }: { table: Table<TData> }) => {
+  const tableRows = table.getRowCount()
+  const pageRowOptions = [100, 200, 500, 1000, tableRows].filter(count => count <= tableRows)
+
+  return (
+    <TablePagination
+      component="div"
+      count={table.getFilteredRowModel().rows.length}
+      onPageChange={(_, newPage) => table.setPageIndex(newPage)}
+      onRowsPerPageChange={event => {
+        table.setPageSize(Number(event.target.value))
+        table.setPageIndex(0)
+      }}
+      page={table.getState().pagination.pageIndex}
+      rowsPerPage={table.getState().pagination.pageSize}
+      rowsPerPageOptions={pageRowOptions}
+    />
+  )
+}
--- services/frontend/src/components/OodiTable/index.tsx
+++ services/frontend/src/components/OodiTable/index.tsx
@@ -28,7 +28,7 @@ export const OodiTable = <TData,>({
 }) => {
   const [pagination, setPagination] = useState({
     pageIndex: 0,
-    pageSize: 200,
+    pageSize: Math.min(200, data.length),
   })
 
   // should maybe use a deep merging tool or write own
