8fc2019358555cd897da083c3ef924560eb14b29
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-08-21 15:08:15 +0300

    [Ooditable] Simplify cell function

--- services/frontend/src/components/OodiTable/components/Cell.tsx
+++ services/frontend/src/components/OodiTable/components/Cell.tsx
@@ -3,39 +3,36 @@ import TableCell, { type TableCellProps } from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
 import { flexRender } from '@tanstack/react-table'
 import type { Row } from '@tanstack/react-table'
-import type { FC, ReactNode } from 'react'
+import type { FC } from 'react'
 
 import { getCommonPinningStyles } from '../styles'
 
-const OodiTableCell: FC<TableCellProps & { children?: ReactNode }> = ({ children, ...props }) => {
-  return (
-    <TableCell
-      {...props}
+const OodiTableCell: FC<TableCellProps> = ({ children, ...props }) => (
+  <TableCell
+    {...props}
+    sx={{
+      borderWidth: '0 1px 1px 0',
+      borderStyle: 'solid',
+      borderColor: 'grey.300',
+      padding: '0 1em',
+      height: '3em',
+      ...props?.sx,
+    }}
+  >
+    <Box
       sx={{
-        borderWidth: '0 1px 1px 0',
-        borderStyle: 'solid',
-        borderColor: 'grey.300',
-        paddingTop: 0,
-        paddingBottom: 0,
-        height: '3em',
-        minHeight: '3em',
-        ...props?.sx,
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        maxWidth: '20em',
+        whiteSpace: 'nowrap',
       }}
     >
-      <Box
-        sx={{
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          maxWidth: '20em',
-          whiteSpace: 'nowrap',
-        }}
-      >
-        {children}
-      </Box>
-    </TableCell>
-  )
-}
+      {children}
+    </Box>
+  </TableCell>
+)
 
 export const OodiTableDataRow = <OTData,>(row: Row<OTData>) => (
   <TableRow key={row.id}>
