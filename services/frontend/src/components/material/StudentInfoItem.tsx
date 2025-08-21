d3d2c2aaf28490cac8d5cd324ca78b5636a70d27
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-08-21 15:21:38 +0300

    Rework StudentInfoItem

--- services/frontend/src/components/material/StudentInfoItem.tsx
+++ services/frontend/src/components/material/StudentInfoItem.tsx
@@ -1,6 +1,7 @@
 import PersonIcon from '@mui/icons-material/Person'
 import IconButton from '@mui/material/IconButton'
 import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 
 import { Link } from 'react-router'
 
@@ -9,12 +10,12 @@ import { ExternalLink } from './ExternalLink'
 
 export const StudentInfoItem = ({ sisPersonId, studentNumber }: { sisPersonId: string; studentNumber: string }) => (
   <Stack direction="row" justifyContent="space-between">
-    {studentNumber}
-    <Stack direction="row" gap={1}>
-      <IconButton component={Link} sx={{ padding: 0 }} target="_blank" to={`/students/${studentNumber}`}>
-        <PersonIcon color="primary" fontSize="small" />
-      </IconButton>
-      <ExternalLink href={`${sisUrl}/tutor/role/staff/student/${sisPersonId}/basic/basic-info`} text="Sisu" />
-    </Stack>
+    <Typography component="span" sx={{ alignContent: 'center', padding: '0 0.25em' }} variant="body2">
+      {studentNumber}
+    </Typography>
+    <IconButton component={Link} target="_blank" to={`/students/${studentNumber}`}>
+      <PersonIcon color="primary" fontSize="small" />
+    </IconButton>
+    <ExternalLink href={`${sisUrl}/tutor/role/staff/student/${sisPersonId}/basic/basic-info`} text="Sisu" />
   </Stack>
 )
