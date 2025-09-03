7040d78de8060cebf268b848c35db665cf4fb5e8
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-09-03 13:31:17 +0300

    [Filters] Outline filter contents

--- services/frontend/src/components/FilterView/filters/common/FilterCard.tsx
+++ services/frontend/src/components/FilterView/filters/common/FilterCard.tsx
@@ -1,42 +1,56 @@
 import ClearIcon from '@mui/icons-material/Clear'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
 import Box from '@mui/material/Box'
 import Collapse from '@mui/material/Collapse'
 import IconButton from '@mui/material/IconButton'
+import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import { FC, useState } from 'react'
 
+import { useState } from 'react'
 import { InfoBox } from '@/components/material/InfoBox'
 
 import type { Filter } from '../createFilter'
 
-export const FilterCard: FC<{
+const ConditionalInfoBox = ({ info }) => {
+  return info ? <InfoBox content={info} mini /> : null
+}
+
+export const FilterCard = ({
+  active,
+  filter,
+  children,
+  onClear,
+}: {
   active: boolean
   filter: Filter
   children: React.ReactNode
   onClear: () => void
-}> = ({ active, filter, children, onClear }) => {
+}) => {
   const [opened, setOpened] = useState<boolean>(active)
   const { info, key, title } = filter
 
   const handleOnClick = () => setOpened(state => !state)
 
+  const iconStyle = {
+    transition: 'transform 300ms',
+    ...(opened ? { transform: 'rotate(90deg)' } : {}),
+  }
+
   return (
-    <Stack data-active={active} data-cy={`${key}-filter-card`} spacing={1.2} sx={{ width: '100%' }}>
+    <Stack data-active={active} data-cy={`${key}-filter-card`} padding={0.5} spacing={1.2} sx={{ width: '100%' }}>
       <Box
         data-cy={`${key}-header`}
         sx={{ alignItems: 'center', justifyContent: 'center', cursor: 'pointer', display: 'inline-flex' }}
       >
-        <IconButton onClick={handleOnClick} size="small">
-          {opened ? <KeyboardArrowDownIcon /> : <KeyboardArrowRightIcon />}
+        <IconButton onClick={handleOnClick} size="small" sx={iconStyle}>
+          <KeyboardArrowRightIcon />
         </IconButton>
         <Typography
           component="span"
-          fontWeight={500}
+          fontWeight="medium"
           onClick={handleOnClick}
-          sx={{ mr: 1, flex: 1, whiteSpace: 'wrap' }}
+          sx={{ flex: 1, whiteSpace: 'wrap' }}
           variant="subtitle1"
         >
           {title}
@@ -47,17 +61,19 @@ export const FilterCard: FC<{
             onClick={() => onClear()}
             sx={{
               color: theme => theme.palette.error.dark,
-              mr: info ? 0.5 : 0,
+              m: '0 0.5',
               '&:hover': {
                 color: theme => theme.palette.error.light,
               },
             }}
           />
         ) : null}
-        {info ? <InfoBox content={info} mini /> : null}
+        <ConditionalInfoBox info={info} />
       </Box>
       <Collapse in={opened}>
-        <Box sx={{ px: 1, mb: 1.5 }}>{children}</Box>
+        <Paper sx={{ p: 1.5 }} variant="outlined">
+          {children}
+        </Paper>
       </Collapse>
     </Stack>
   )
