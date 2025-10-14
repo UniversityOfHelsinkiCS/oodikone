2149cad79ca5fe8fb993033f0463f84a3cfe8c0b
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-14 17:49:58 +0300

    [Teachers] Upgrade frontpage tab feature to MUI

--- services/frontend/src/components/Teachers/index.jsx
+++ services/frontend/src/components/Teachers/index.jsx
@@ -1,8 +1,12 @@
-import { useCallback } from 'react'
-import { useParams, useLocation, useNavigate } from 'react-router'
-import { Header, Segment, Tab } from 'semantic-ui-react'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Tab from '@mui/material/Tab'
+import Tabs from '@mui/material/Tabs'
+import Typography from '@mui/material/Typography'
+import { useParams } from 'react-router'
 
-import { useSemanticTabs } from '@/hooks/tabs'
+import { useTabs } from '@/hooks/tabs'
+
 import { useTitle } from '@/hooks/title'
 import { useGetAuthorizedUserQuery } from '@/redux/auth'
 import { hasFullAccessToTeacherData } from '@/util/access'
@@ -11,45 +15,45 @@ import { TeacherLeaderBoard } from './TeacherLeaderBoard'
 import { TeacherSearchTab } from './TeacherSearchTab'
 import { TeacherStatistics } from './TeacherStatistics'
 
-const pane = (title, Content, icon) => ({
-  menuItem: { key: title, content: title, icon },
-  render: () => (
-    <Tab.Pane style={{ borderWidth: '0' }}>
-      <Content />
-    </Tab.Pane>
-  ),
+const pane = (label, Content, icon) => ({
+  label,
+  icon,
+  render: () => <Content />,
 })
 
 const TeachersTabs = () => {
-  const navigate = useNavigate()
-  const location = useLocation()
   const { roles, iamGroups } = useGetAuthorizedUserQuery()
-  const replace = useCallback(options => navigate(options, { replace: true }), [navigate])
-  const [tab, setTab] = useSemanticTabs('t_tab', 0, { location, replace })
+  const [tab, setTab] = useTabs(3, 't')
+
   const panes = [pane('Statistics', TeacherStatistics, 'table')]
   if (hasFullAccessToTeacherData(roles, iamGroups)) {
     panes.push(pane('Leaderboard', TeacherLeaderBoard, 'trophy'), pane('Search', TeacherSearchTab, 'user'))
   }
 
   return (
-    <Tab
-      activeIndex={tab}
-      menu={{ attached: false, borderless: true, tabular: true }}
-      onTabChange={setTab}
-      panes={panes}
-    />
+    <Box>
+      <Tabs onChange={(_, newTab) => setTab(newTab)} value={tab}>
+        {panes.map(({ label }) => (
+          <Tab key={label} label={label} />
+        ))}
+      </Tabs>
+      {panes.at(tab)?.render()}
+    </Box>
   )
 }
 
 export const Teachers = () => {
   useTitle('Teachers')
   const { teacherid } = useParams()
+
   return (
     <div className="segmentContainer">
-      <Header className="segmentTitle" content="Teacher statistics" size="large" />
-      <Segment className="contentSegment">
+      <Typography className="segmentTitle" variant="h4">
+        Teacher statistics
+      </Typography>
+      <Paper className="contentSegment">
         {teacherid ? <TeacherDetails teacherId={teacherid} /> : <TeachersTabs />}
-      </Segment>
+      </Paper>
     </div>
   )
 }
