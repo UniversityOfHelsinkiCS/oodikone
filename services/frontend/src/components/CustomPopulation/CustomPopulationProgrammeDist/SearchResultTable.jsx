bfad75d03e0e176728ffafaeb6b293b9f4592558
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-20 21:46:26 +0300

    [CustomPopulation] Upgraded search result table to MUI

--- services/frontend/src/components/CustomPopulation/CustomPopulationProgrammeDist/SearchResultTable.jsx
+++ services/frontend/src/components/CustomPopulation/CustomPopulationProgrammeDist/SearchResultTable.jsx
@@ -1,56 +1,37 @@
-import { array, arrayOf, bool, func, string } from 'prop-types'
-import { Segment, Table } from 'semantic-ui-react'
-
-import './searchResultsTable.css'
-
-const getHeaderRow = headers => (
-  <Table.Header>
-    <Table.Row>
-      {headers.map(header => (
-        <Table.HeaderCell key={`header-${header}`}>{header}</Table.HeaderCell>
-      ))}
-    </Table.Row>
-  </Table.Header>
-)
-
-const getTableBody = (rows, selectable, actionTrigger) => (
-  <Table.Body>
-    {rows.map((row, i) => (
-      <Table.Row
-        className={selectable ? 'selectableRow' : ''}
-        // eslint-disable-next-line react/no-array-index-key
-        key={`row-${i}`}
-      >
-        {Object.values(row).map((value, index) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <Table.Cell key={`cell-${index}`}>
-            {!!actionTrigger && index === 0 && actionTrigger(row)}
-            {value}
-          </Table.Cell>
-        ))}
-      </Table.Row>
-    ))}
-  </Table.Body>
-)
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import { StyledTable } from '@/components/material/StyledTable'
 
 export const SearchResultTable = ({ actionTrigger, headers, rows, noResultText, selectable }) => {
-  if (rows.length > 0) {
-    return (
-      <Segment style={{ maxHeight: '80vh', overflowY: 'auto', padding: 0 }}>
-        <Table className="fixed-header" definition={false} selectable={selectable} unstackable>
-          {getHeaderRow(headers)}
-          {getTableBody(rows, selectable, actionTrigger)}
-        </Table>
-      </Segment>
-    )
-  }
-  return <div>{noResultText}</div>
-}
+  if (!rows.length) return <div>{noResultText}</div>
 
-SearchResultTable.propTypes = {
-  headers: arrayOf(string).isRequired,
-  rows: arrayOf(array).isRequired,
-  noResultText: string.isRequired,
-  selectable: bool.isRequired,
-  actionTrigger: func.isRequired,
+  return (
+    <StyledTable showCellBorders>
+      <TableHead>
+        <TableRow>
+          {headers.map(header => (
+            <TableCell key={`header-${header}`}>{header}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map(row => (
+          <TableRow
+            className={selectable ? 'selectableRow' : ''}
+            key={`row-${row[1]}`} // code
+          >
+            {Object.values(row).map((value, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <TableCell key={`row-${row[1]}-cell-${index}`}>
+                {!!actionTrigger && index === 0 && actionTrigger(row)}
+                {value}
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </StyledTable>
+  )
 }
