bf4c8fa2fb4f48368bae7e5b105efc61e791bc66
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-20 13:09:53 +0300

    [Frontend] Apply ForwardRef to Link component
    Use https://mui.com/material-ui/integrations/routing/ as guidance.

--- services/frontend/src/components/material/Link.tsx
+++ services/frontend/src/components/material/Link.tsx
@@ -1,4 +1,12 @@
-import LinkContainer from '@mui/material/Link'
-import { Link as RouterLink } from 'react-router'
+// For information about this implementation, see.
+// https://mui.com/material-ui/integrations/routing/
+//
+import LinkContainer, { LinkProps } from '@mui/material/Link'
+import { forwardRef } from 'react'
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router'
 
-export const Link = props => <LinkContainer component={RouterLink} {...props} />
+export const Link = forwardRef<HTMLAnchorElement, LinkProps & RouterLinkProps>((props, ref) => (
+  <LinkContainer component={RouterLink} ref={ref} {...props} />
+))
+
+Link.displayName = 'OKLink'
