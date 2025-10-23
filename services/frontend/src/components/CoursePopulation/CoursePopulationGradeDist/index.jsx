3337192b8a9bca071cd2e643df476a2b89193e72
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-23 13:10:17 +0300

    [CoursePopulation] Upgrade course grade distribution table to MUI

--- services/frontend/src/components/CoursePopulation/CoursePopulationGradeDist/index.jsx
+++ services/frontend/src/components/CoursePopulation/CoursePopulationGradeDist/index.jsx
@@ -1,7 +1,12 @@
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
 import { intersection, orderBy } from 'lodash'
-import { Progress, Table } from 'semantic-ui-react'
 
 import { getHighestGradeOfCourseBetweenRange } from '@/common'
+import { PercentageBar } from '@/components/material/PercentageBar'
+import { StyledTable } from '@/components/material/StyledTable'
 import { ExternalGradeFilterToggle } from './ExternalGradeFilterToggle'
 
 export const CoursePopulationGradeDist = ({ singleCourseStats, students, courseCodes, from, to }) => {
@@ -38,39 +43,33 @@ export const CoursePopulationGradeDist = ({ singleCourseStats, students, courseC
   )
 
   return (
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell collapsing />
-          <Table.HeaderCell>Grades</Table.HeaderCell>
-          <Table.HeaderCell>
+    <StyledTable showCellBorders>
+      <TableHead>
+        <TableRow>
+          <TableCell width={1} /* width: 1, forces the cell to collapse */ />
+          <TableCell>Grades</TableCell>
+          <TableCell>
             Number of students
             <div style={{ fontWeight: 100 }}>(n={students.length})</div>
-          </Table.HeaderCell>
-          <Table.HeaderCell>Percentage of population</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+          </TableCell>
+          <TableCell>Percentage of population</TableCell>
+        </TableRow>
+      </TableHead>
 
-      <Table.Body>
+      <TableBody>
         {sortedCourseGrades.map(grade => (
-          <Table.Row key={`grade-table-row-${grade.grade}`}>
-            <Table.Cell collapsing>
+          <TableRow key={`grade-table-row-${grade.grade}`}>
+            <TableCell collapsing>
               <ExternalGradeFilterToggle grade={grade.grade} />
-            </Table.Cell>
-            <Table.Cell>{grade.grade}</Table.Cell>
-            <Table.Cell>{grade.amount}</Table.Cell>
-            <Table.Cell>
-              <Progress
-                precision={0}
-                progress="percent"
-                style={{ margin: 0 }}
-                total={students.length}
-                value={grade.amount}
-              />
-            </Table.Cell>
-          </Table.Row>
+            </TableCell>
+            <TableCell>{grade.grade}</TableCell>
+            <TableCell>{grade.amount}</TableCell>
+            <TableCell>
+              <PercentageBar denominator={students.length} numerator={grade.amount} />
+            </TableCell>
+          </TableRow>
         ))}
-      </Table.Body>
-    </Table>
+      </TableBody>
+    </StyledTable>
   )
 }
