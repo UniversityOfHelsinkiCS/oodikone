61ee4475e7d181e5be7cdacbe6d83a0c909482fa
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-23 12:57:34 +0300

    [Frontend] Add loading section wrapper

--- services/frontend/src/components/material/Loading.tsx
+++ services/frontend/src/components/material/Loading.tsx
@@ -1,10 +1,19 @@
 import Box from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
+import Paper, { PaperProps } from '@mui/material/Paper'
 
 export const Loading = () => {
   return (
     <Box display="flex" justifyContent="center" width="100%">
       <CircularProgress />
     </Box>
+  )
+}
+
+export const LoadingSection = ({ ...sectionProps }: PaperProps) => {
+  return (
+    <Paper variant="outlined" {...sectionProps}>
+      <Loading />
+    </Paper>
   )
 }
