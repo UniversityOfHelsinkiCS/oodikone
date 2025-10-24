554920229944a119c0fcb23a41594b712badebda
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-10-24 16:10:42 +0300

    [OpenUni] Upgrade custom open uni population wrapper to MUI

--- services/frontend/src/components/CustomOpenUniPopulation/index.jsx
+++ services/frontend/src/components/CustomOpenUniPopulation/index.jsx
@@ -1,6 +1,11 @@
+import CheckIcon from '@mui/icons-material/Check'
+import CloseIcon from '@mui/icons-material/Close'
+import MinusIcon from '@mui/icons-material/Remove'
+import Alert from '@mui/material/Alert'
+import Typography from '@mui/material/Typography'
 import { useState } from 'react'
-import { Message, Icon, Loader } from 'semantic-ui-react'
 
+import { Loading } from '@/components/material/Loading'
 import { DateFormat } from '@/constants/date'
 import { useTitle } from '@/hooks/title'
 import { useGetSavedSearchesQuery } from '@/redux/openUniPopulations'
@@ -16,21 +21,27 @@ export const CustomOpenUniPopulation = () => {
   const isError = savedSearches.isError || (savedSearches.isSuccess && !savedSearches.data)
 
   if (isError) return <h3>Something went wrong, please try refreshing the page.</h3>
-  if (isFetchingOrLoading) return <Loader active style={{ marginTop: '15em' }} />
+  if (isFetchingOrLoading) return <Loading active style={{ marginTop: '15em' }} />
   return (
     <div className="segmentContainer">
-      <Message style={{ maxWidth: '800px', fontSize: '16px' }}>
-        <Message.Header>Open uni student population</Message.Header>
+      <Alert icon={false} severity="info" style={{ maxWidth: '800px' }} variant="outlined">
+        <Typography variant="h6">Open uni student population</Typography>
         <p>
           Here you can create custom population using a list of courses. Clicking the blue button will open a modal
           where you can enter a list of courses. <br />
-          <Icon color="green" fitted name="check" />: Student has passed the course. <br />
-          <Icon color="red" fitted name="times" />: Student has failed the course. <br />
-          <Icon color="grey" fitted name="minus" />: Student has enrolled, but has not received any grade from the
+          <Typography sx={{ display: 'flex' }}>
+            <CheckIcon color="success" />: Student has passed the course. <br />
+          </Typography>
+          <Typography sx={{ display: 'flex' }}>
+            <CloseIcon color="error" />: Student has failed the course. <br />
+          </Typography>
+          <Typography sx={{ display: 'flex' }}>
+            <MinusIcon color="disabled" />: Student has enrolled, but has not received any grade from the
+          </Typography>
           course. <br />
           <b>Empty cell</b>: Student has no enrollments for the course. <br />
         </p>
-      </Message>
+      </Alert>
       <CustomOpenUniSearch savedSearches={savedSearches.data} setValues={setValues} />
       <div style={{ paddingTop: '25px', paddingBottom: '10px', fontSize: '20px' }}>
         {fieldValues && fieldValues.courseList?.length > 0 ? (
