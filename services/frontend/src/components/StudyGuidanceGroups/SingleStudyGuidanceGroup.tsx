fbc92b6949fcb05a8ef4c0d38e23d80c6c9eaba7
joonas-a <joonas.ahovalli@gmail.com>
2025-10-30 17:16:35 +0200

    [SGG] Split large file

--- services/frontend/src/components/StudyGuidanceGroups/SingleStudyGuidanceGroup.tsx
+++ services/frontend/src/components/StudyGuidanceGroups/SingleStudyGuidanceGroup.tsx
@@ -9,173 +9,19 @@ import Grid from '@mui/material/Grid2'
 import Stack from '@mui/material/Stack'
 import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
-import dayjs, { extend as dayjsExtend } from 'dayjs'
-import isBetween from 'dayjs/plugin/isBetween'
 import { useNavigate } from 'react-router'
 
-import { populationStatisticsToolTips } from '@/common/InfoToolTips'
-import { PanelView } from '@/components/common/PanelView'
-import { CreditAccumulationGraphHighCharts } from '@/components/CreditAccumulationGraphHighCharts'
 import { FilterView } from '@/components/FilterView'
-import { creditDateFilter, hopsFilter as studyPlanFilter } from '@/components/FilterView/filters'
-import { useFilters } from '@/components/FilterView/useFilters'
-import { InfoBox } from '@/components/InfoBox'
 import { useLanguage } from '@/components/LanguagePicker/useLanguage'
 import { PageLoading } from '@/components/material/Loading'
-import { AgeStats } from '@/components/PopulationDetails/AgeStats'
-import { CreditStatistics } from '@/components/PopulationDetails/CreditGainStats'
-import { PopulationStudents } from '@/components/PopulationStudents'
-import { useFormat as formatGeneralTab } from '@/components/PopulationStudents/StudentTable/GeneralTab/format/index'
 import { useGetCustomPopulationQuery } from '@/redux/populations'
 
 import { useFilteredAndFormattedStudyProgrammes } from '@/redux/studyProgramme'
-import { FormattedStudent, FormattedCourse } from '@oodikone/shared/types'
 import { GroupsWithTags } from '@oodikone/shared/types/studyGuidanceGroup'
-import { useCurriculumState } from '../../hooks/useCurriculums'
 import { StyledMessage } from '../common/StyledMessage'
 import { startYearToAcademicYear } from './common'
-import { useColumns as columnsGeneralTab } from './studentColumns'
-import { StudyGuidanceGroupPopulationCourses } from './StudyGuidanceGroupPopulationCourses'
-import { useGetFilters, createAcademicYearStartDate } from './utils'
-
-dayjsExtend(isBetween)
-
-const SingleStudyGroupContent = ({
-  filteredStudents,
-  filteredCourses,
-  group,
-}: {
-  filteredStudents: FormattedStudent[]
-  filteredCourses: FormattedCourse[]
-  group: GroupsWithTags
-}) => {
-  const { useFilterSelector, filterDispatch } = useFilters()
-
-  const groupYear = group.tags?.year
-  const groupProgramme = group.tags?.studyProgramme
-
-  const [programme, combinedProgramme] = groupProgramme?.split('+') ?? []
-
-  const query = {
-    programme,
-    combinedProgramme,
-    years: groupYear ? [Number(groupYear)] : [],
-  }
-
-  const [curriculum, curriculumList, setCurriculum] = useCurriculumState(programme, groupYear!) // TODO: fix year
-
-  const creditDateFilterActive = useFilterSelector(creditDateFilter.selectors.isActive())
-  const studyPlanFilterIsActive = useFilterSelector(studyPlanFilter.selectors.isActive())
-
-  const toggleCreditDateFilter = () => {
-    if (creditDateFilterActive) {
-      filterDispatch(creditDateFilter.actions.reset(undefined))
-    } else {
-      filterDispatch(
-        creditDateFilter.actions.setOptions({
-          startDate: dayjs(createAcademicYearStartDate(Number(groupYear))),
-          endDate: null,
-        })
-      )
-    }
-  }
-
-  const panels = [
-    {
-      title: `Credit accumulation (for ${filteredStudents.length} students)`,
-      content: (
-        <Box>
-          <InfoBox content={populationStatisticsToolTips.creditAccumulation} />
-          {group.tags?.year ? (
-            <Button color="primary" onClick={() => toggleCreditDateFilter()} variant="outlined">
-              {creditDateFilterActive ? 'Show all credits' : 'Show starting from associated year'}
-            </Button>
-          ) : null}
-          <CreditAccumulationGraphHighCharts
-            absences={null}
-            endDate={null}
-            programmeCodes={group?.tags?.studyProgramme ? [programme, combinedProgramme] : []}
-            selectedStudyPlan={null}
-            showBachelorAndMaster={null}
-            singleStudent={false}
-            startDate={null}
-            students={filteredStudents}
-            studyPlanFilterIsActive={studyPlanFilterIsActive}
-            studyRightId={null}
-          />
-        </Box>
-      ),
-    },
-    (programme || group?.tags?.studyProgramme) && groupYear
-      ? {
-          title: 'Credit statistics',
-          content: (
-            <div>
-              <CreditStatistics filteredStudents={filteredStudents} query={query} />
-            </div>
-          ),
-        }
-      : null,
-    {
-      title: 'Age distribution',
-      content: <AgeStats filteredStudents={filteredStudents} query={query} />,
-    },
-    {
-      title: 'Courses of population',
-      content: (
-        <div>
-          <StudyGuidanceGroupPopulationCourses
-            curriculum={curriculum}
-            curriculumList={curriculumList}
-            filteredCourses={filteredCourses}
-            filteredStudents={filteredStudents}
-            setCurriculum={setCurriculum}
-            studyProgramme={group.tags?.studyProgramme ? programme : null}
-            year={groupYear}
-          />
-        </div>
-      ),
-    },
-    {
-      title: `Students (${filteredStudents.length})`,
-      content: (
-        <div>
-          {/* @ts-expect-error FIX typing */}
-          <PopulationStudents
-            curriculum={curriculum}
-            filteredCourses={filteredCourses}
-            filteredStudents={filteredStudents}
-            generalTabColumnFunction={() => columnsGeneralTab({ group })}
-            generalTabFormattingFunction={() =>
-              formatGeneralTab({
-                variant: 'studyGuidanceGroupPopulation',
-                filteredStudents,
-
-                years: groupYear ? [Number(groupYear)] : [],
-
-                programme: group.tags?.studyProgramme?.split('+')[0],
-                combinedProgramme: group.tags?.studyProgramme?.split('+')[1],
-
-                showBachelorAndMaster: false,
-                includePrimaryProgramme: true,
-
-                coursecodes: [],
-                from: undefined,
-                to: undefined,
-              })
-            }
-            programme={group.tags?.studyProgramme?.split('+').at(0)}
-            studyGuidanceGroup={group}
-            variant="studyGuidanceGroupPopulation"
-            year={groupYear ?? undefined}
-          />
-        </div>
-      ),
-    },
-  ]
-
-  return <PanelView panels={panels} />
-}
+import { SingleStudyGuidanceGroupPanels } from './SingleStudyGuidanceGroupPanels'
+import { useGetFilters } from './utils'
 
 export const SingleStudyGuidanceGroupContainer = ({ group }: { group: GroupsWithTags | undefined }) => {
   // Sorting is needed for RTK query cache to work properly
@@ -290,7 +136,7 @@ export const SingleStudyGuidanceGroupContainer = ({ group }: { group: GroupsWith
               </Grid>
               <Grid size={2} /> {/* for alignment */}
             </Grid>
-            <SingleStudyGroupContent
+            <SingleStudyGuidanceGroupPanels
               filteredCourses={filteredCourses}
               filteredStudents={filteredStudents}
               group={group}
---
+++ services/frontend/src/components/StudyGuidanceGroups/SingleStudyGuidanceGroupPanels.tsx
@@ -0,0 +1,161 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import dayjs, { extend as dayjsExtend } from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween'
+
+import { populationStatisticsToolTips } from '@/common/InfoToolTips'
+import { PanelView } from '@/components/common/PanelView'
+import { CreditAccumulationGraphHighCharts } from '@/components/CreditAccumulationGraphHighCharts'
+import { creditDateFilter, hopsFilter as studyPlanFilter } from '@/components/FilterView/filters'
+import { useFilters } from '@/components/FilterView/useFilters'
+import { InfoBox } from '@/components/InfoBox'
+import { AgeStats } from '@/components/PopulationDetails/AgeStats'
+import { CreditStatistics } from '@/components/PopulationDetails/CreditGainStats'
+import { PopulationStudents } from '@/components/PopulationStudents'
+import { useFormat as formatGeneralTab } from '@/components/PopulationStudents/StudentTable/GeneralTab/format/index'
+import { FormattedStudent, FormattedCourse } from '@oodikone/shared/types'
+import { GroupsWithTags } from '@oodikone/shared/types/studyGuidanceGroup'
+
+import { useCurriculumState } from '../../hooks/useCurriculums'
+import { useColumns as columnsGeneralTab } from './studentColumns'
+import { StudyGuidanceGroupPopulationCourses } from './StudyGuidanceGroupPopulationCourses'
+import { createAcademicYearStartDate } from './utils'
+
+dayjsExtend(isBetween)
+
+export const SingleStudyGuidanceGroupPanels = ({
+  filteredStudents,
+  filteredCourses,
+  group,
+}: {
+  filteredStudents: FormattedStudent[]
+  filteredCourses: FormattedCourse[]
+  group: GroupsWithTags
+}) => {
+  const { useFilterSelector, filterDispatch } = useFilters()
+
+  const groupYear = group.tags?.year
+  const groupProgramme = group.tags?.studyProgramme
+
+  const [programme, combinedProgramme] = groupProgramme?.split('+') ?? []
+
+  const query = {
+    programme,
+    combinedProgramme,
+    years: groupYear ? [Number(groupYear)] : [],
+  }
+
+  const [curriculum, curriculumList, setCurriculum] = useCurriculumState(programme, groupYear!) // TODO: fix year
+
+  const creditDateFilterActive = useFilterSelector(creditDateFilter.selectors.isActive())
+  const studyPlanFilterIsActive = useFilterSelector(studyPlanFilter.selectors.isActive())
+
+  const toggleCreditDateFilter = () => {
+    if (creditDateFilterActive) {
+      filterDispatch(creditDateFilter.actions.reset(undefined))
+    } else {
+      filterDispatch(
+        creditDateFilter.actions.setOptions({
+          startDate: dayjs(createAcademicYearStartDate(Number(groupYear))),
+          endDate: null,
+        })
+      )
+    }
+  }
+
+  const panels = [
+    {
+      title: `Credit accumulation (for ${filteredStudents.length} students)`,
+      content: (
+        <Box>
+          <InfoBox content={populationStatisticsToolTips.creditAccumulation} />
+          {group.tags?.year ? (
+            <Button color="primary" onClick={() => toggleCreditDateFilter()} variant="outlined">
+              {creditDateFilterActive ? 'Show all credits' : 'Show starting from associated year'}
+            </Button>
+          ) : null}
+          <CreditAccumulationGraphHighCharts
+            absences={null}
+            endDate={null}
+            programmeCodes={group?.tags?.studyProgramme ? [programme, combinedProgramme] : []}
+            selectedStudyPlan={null}
+            showBachelorAndMaster={null}
+            singleStudent={false}
+            startDate={null}
+            students={filteredStudents}
+            studyPlanFilterIsActive={studyPlanFilterIsActive}
+            studyRightId={null}
+          />
+        </Box>
+      ),
+    },
+    (programme || group?.tags?.studyProgramme) && groupYear
+      ? {
+          title: 'Credit statistics',
+          content: (
+            <div>
+              <CreditStatistics filteredStudents={filteredStudents} query={query} />
+            </div>
+          ),
+        }
+      : null,
+    {
+      title: 'Age distribution',
+      content: <AgeStats filteredStudents={filteredStudents} query={query} />,
+    },
+    {
+      title: 'Courses of population',
+      content: (
+        <div>
+          <StudyGuidanceGroupPopulationCourses
+            curriculum={curriculum}
+            curriculumList={curriculumList}
+            filteredCourses={filteredCourses}
+            filteredStudents={filteredStudents}
+            setCurriculum={setCurriculum}
+            studyProgramme={group.tags?.studyProgramme ? programme : null}
+            year={groupYear}
+          />
+        </div>
+      ),
+    },
+    {
+      title: `Students (${filteredStudents.length})`,
+      content: (
+        <div>
+          {/* @ts-expect-error FIX typing */}
+          <PopulationStudents
+            curriculum={curriculum}
+            filteredCourses={filteredCourses}
+            filteredStudents={filteredStudents}
+            generalTabColumnFunction={() => columnsGeneralTab({ group })}
+            generalTabFormattingFunction={() =>
+              formatGeneralTab({
+                variant: 'studyGuidanceGroupPopulation',
+                filteredStudents,
+
+                years: groupYear ? [Number(groupYear)] : [],
+
+                programme: group.tags?.studyProgramme?.split('+')[0],
+                combinedProgramme: group.tags?.studyProgramme?.split('+')[1],
+
+                showBachelorAndMaster: false,
+                includePrimaryProgramme: true,
+
+                coursecodes: [],
+                from: undefined,
+                to: undefined,
+              })
+            }
+            programme={group.tags?.studyProgramme?.split('+').at(0)}
+            studyGuidanceGroup={group}
+            variant="studyGuidanceGroupPopulation"
+            year={groupYear ?? undefined}
+          />
+        </div>
+      ),
+    },
+  ]
+
+  return <PanelView panels={panels} />
+}
