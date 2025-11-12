340d46c73975dd08653468d4b573990776430fef
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-11-12 04:21:27 +0200

    [Frontend] Remove Semantic UI

--- services/frontend/src/main.tsx
+++ services/frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router'
 
-import 'semantic-ui-css/semantic.min.css'
 import './styles/custom.css'
 import { App } from './components/App'
 import { ErrorBoundary } from './components/ErrorBoundary'
