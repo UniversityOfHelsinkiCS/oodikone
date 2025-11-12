52f77935fcd3a9473f1400076b5266225c2cbf2e
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-11-12 04:20:00 +0200

    [Frontend] Remove Semantic UI icon from Highcharts charts

--- services/frontend/src/components/CreditAccumulationGraphHighCharts/CreditGraphTooltip.jsx
+++ services/frontend/src/components/CreditAccumulationGraphHighCharts/CreditGraphTooltip.jsx
@@ -2,25 +2,12 @@ import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
 
-// FIXME: For some reason Semantic UI manages to build a working HTML -string, but MUI doesn't.
-import { Icon } from 'semantic-ui-react'
-
-const getCorrectIcon = (passed, isStudyModuleCredit) => {
-  if (isStudyModuleCredit) {
-    return <Icon color="purple" name="certificate" style={{ margin: 0 }} />
-  }
-  if (passed) {
-    return <Icon color="green" name="check circle" style={{ margin: 0 }} />
-  }
-  return null
-}
-
 const Tile = ({ children }) => (
   <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontWeight: 'bold' }}>{children}</div>
 )
 const TileValue = ({ children }) => <div style={{ fontSize: '1.25em' }}>{children}</div>
 
-export const CreditGraphTooltip = ({ credits, date, grade, passed, isStudyModuleCredit, courseCode, courseName }) => {
+export const CreditGraphTooltip = ({ credits, date, grade, isStudyModuleCredit, courseCode, courseName }) => {
   const title = `${courseName} (${courseCode})`
 
   return (
@@ -29,7 +16,7 @@ export const CreditGraphTooltip = ({ credits, date, grade, passed, isStudyModule
         content={`${title}${isStudyModuleCredit ? ' [Study Module]' : ''}`}
         style={{ whiteSpace: 'normal' }}
       />
-      <CardContent style={{ display: 'grid', grid: '1fr 1fr 1fr 1fr', gridRowGap: '0.25rem' }}>
+      <CardContent style={{ display: 'grid', grid: '1fr 1fr 1fr', gridRowGap: '0.25rem' }}>
         <Tile>
           Credits <TileValue>{credits}</TileValue>
         </Tile>
@@ -38,10 +25,6 @@ export const CreditGraphTooltip = ({ credits, date, grade, passed, isStudyModule
         </Tile>
         <Tile>
           Date <TileValue>{date}</TileValue>
-        </Tile>
-        <Tile>
-          {isStudyModuleCredit ? 'Module' : 'Passed'}{' '}
-          <TileValue>{getCorrectIcon(passed, isStudyModuleCredit)}</TileValue>
         </Tile>
       </CardContent>
     </Card>
