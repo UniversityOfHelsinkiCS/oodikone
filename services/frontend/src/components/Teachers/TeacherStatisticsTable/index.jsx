31c45581e045eab4ed0d88a0987c9ffaeb3424de
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-11-12 02:43:31 +0200

    [Teachers] Convert SortableTable to OodiTable

--- services/frontend/src/components/Teachers/TeacherStatisticsTable/index.jsx
+++ services/frontend/src/components/Teachers/TeacherStatisticsTable/index.jsx
@@ -1,72 +1,76 @@
 import ArrowIcon from '@mui/icons-material/NorthEast'
 import Typography from '@mui/material/Typography'
+import { createColumnHelper } from '@tanstack/react-table'
 import { useMemo } from 'react'
 
 import { calculatePercentage, isDefaultServiceProvider } from '@/common'
 import { Link } from '@/components/material/Link'
-import { SortableTable } from '@/components/SortableTable'
+import { OodiTable } from '@/components/OodiTable'
+import { OodiTableExcelExport } from '@/components/OodiTable/excelExport'
 
-const createColumnWithTitle = title => ({
-  key: title,
-  title: title[0].toUpperCase() + title.slice(1),
-  getRowVal: row => row.name,
-})
+const columnHelper = createColumnHelper()
+
+const createColumnWithTitle = title =>
+  columnHelper.display({
+    key: title,
+    header: title[0].toUpperCase() + title.slice(1),
+    cell: ({ row }) => row.original.name,
+  })
 
 export const TeacherStatisticsTable = ({ statistics, variant }) => {
   const columns = [
-    {
-      key: 'credits',
-      title: 'Credits',
-      getRowVal: row => row.credits,
-    },
-    {
-      key: 'credits-transferred',
-      title: 'Credits transferred',
-      getRowVal: row => row.transferred,
-    },
-    {
-      key: 'passrate',
-      title: 'Passed',
-      getRowVal: row => row.passrate,
-    },
+    columnHelper.accessor('credits', {
+      header: 'Credits',
+      cell: ({ row }) => row.original.credits,
+    }),
+    columnHelper.accessor('transferred', {
+      header: 'Credits transferred',
+      cell: ({ row }) => row.original.transferred,
+    }),
+    columnHelper.accessor('passrate', {
+      header: 'Passed',
+      cell: ({ row }) => row.original.passrate,
+    }),
   ]
 
   switch (variant) {
     case 'leaderboard':
-      columns.unshift({
-        key: 'name',
-        title: 'Name',
-        getRowVal: row => row.name,
-        getRowContent: row => (
-          <div style={{ display: 'flex', justifyContent: 'space-between', gap: '0.5em' }}>
-            {row.name}
-            {isDefaultServiceProvider() && (
-              <Link target="_blank" to={`/teachers/${row.id}`}>
-                <ArrowIcon />
-              </Link>
-            )}
-          </div>
-        ),
-      })
+      columns.unshift(
+        columnHelper.display({
+          key: 'name',
+          header: 'Name',
+          cell: ({ row }) => (
+            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '0.5em' }}>
+              {row.original.name}
+              {isDefaultServiceProvider() && (
+                <Link target="_blank" to={`/teachers/${row.original.id}`}>
+                  <ArrowIcon />
+                </Link>
+              )}
+            </div>
+          ),
+        })
+      )
       break
     case 'course':
       columns.unshift(createColumnWithTitle('course name'))
-      columns.unshift({
-        key: 'code',
-        title: 'Course code',
-        getRowVal: row => row.id,
-        formatValue: code => (
-          <div style={{ display: 'flex', justifyContent: 'space-between', gap: '0.5em' }}>
-            {code}
-            <Link
-              target="_blank"
-              to={`/coursestatistics?combineSubstitutions=true&courseCodes=["${code}"]&separate=false`}
-            >
-              <ArrowIcon />
-            </Link>
-          </div>
-        ),
-      })
+      columns.unshift(
+        columnHelper.display({
+          key: 'code',
+          header: 'Course code',
+          cell: ({ row }) => (
+            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '0.5em' }}>
+              {row.original.id}
+              <Link
+                target="_blank"
+                to={`/coursestatistics?combineSubstitutions=true&courseCodes=["${row.original.id}"]&separate=false`}
+              >
+                <ArrowIcon />
+              </Link>
+            </div>
+          ),
+        })
+      )
       break
     case 'semester':
       columns.unshift(createColumnWithTitle('semester'))
@@ -74,28 +78,39 @@ export const TeacherStatisticsTable = ({ statistics, variant }) => {
     case 'year':
       columns.unshift(createColumnWithTitle('year'))
       break
-    default:
-      break
   }
 
-  const data = useMemo(
-    () =>
-      statistics.map(stat => ({
-        ...stat,
-        passrate: calculatePercentage(stat.passed, stat.passed + stat.failed),
-      })),
-    [statistics]
-  )
+  const [data, excelData] = useMemo(() => {
+    const data = statistics.map(stat => ({
+      ...stat,
+      passrate: calculatePercentage(stat.passed, stat.passed + stat.failed),
+    }))
+
+    const excelData = data.map(stat => ({
+      code: stat?.id, // if stats provides code, do not override it
+      year: stat?.name,
+      semester: stat?.name,
+      'course name': stat?.name,
+      ...stat,
+    }))
+
+    return [data, excelData]
+  }, [statistics])
+
+  const accessorKeys = useMemo(() => columns.map(col => col.accessorKey ?? col.key), [columns])
 
   if (!statistics.length) return <Typography>No statistics found for the given query.</Typography>
 
+  const tableOptions = {
+    initialState: {
+      sorting: [variant === 'leaderboard' ? { id: 'credits', desc: true } : {}],
+    },
+  }
+
   return (
-    <SortableTable
-      columns={columns}
-      data={data}
-      defaultSort={variant === 'leaderboard' ? ['credits', 'desc'] : ['name', 'asc']}
-      featureName="teacher_statistics"
-      title="Teacher statistics"
-    />
+    <>
+      <OodiTableExcelExport data={excelData} exportColumnKeys={accessorKeys} />
+      <OodiTable columns={columns} data={data} options={tableOptions} />
+    </>
   )
 }
