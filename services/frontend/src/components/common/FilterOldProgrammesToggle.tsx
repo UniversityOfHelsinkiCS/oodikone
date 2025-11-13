ece089b5b6149e8ef6a8febd8a5da3c299814053
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-11-13 03:27:55 +0200

    [Frontend] Move FilterOldProgrammesToggle component from components/material to components/common

--- services/frontend/src/components/common/FilterOldProgrammesToggle.tsx
+++ services/frontend/src/components/common/FilterOldProgrammesToggle.tsx
@@ -1,9 +1,12 @@
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Switch from '@mui/material/Switch'
 
-export const FilterOldProgrammesToggle = ({ checked, onChange }: { checked: boolean; onChange: () => void }) => (
-  <FormControlLabel
-    control={<Switch checked={checked} onChange={onChange} />}
-    label="Filter out old and specialized programmes"
-  />
-)
+export const FilterOldProgrammesToggle = ({ checked, onChange }: { checked: boolean; onChange: () => void }) => {
+  return (
+    <FormControlLabel
+      control={<Switch checked={checked} onChange={onChange} />}
+      data-cy="filter-old-programmes-toggle"
+      label="Filter out old and specialized programmes"
+    />
+  )
+}
