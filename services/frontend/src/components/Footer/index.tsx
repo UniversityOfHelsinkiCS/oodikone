664d8fb83af771dbaebcbb2ed67f3528f2ee8e47
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-11-21 11:01:38 +0200

    [Footer] Make DOM shallower and adapt design to NavBar

--- services/frontend/src/components/Footer/index.tsx
+++ services/frontend/src/components/Footer/index.tsx
@@ -1,12 +1,10 @@
 import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import Divider from '@mui/material/Divider'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 import { isDefaultServiceProvider } from '@/common'
 import { ExternalLink } from '@/components/common/ExternalLink'
-import { Link } from '@/components/common/Link'
 import { ToskaLogo } from '@/components/Footer/ToskaLogo'
 import { builtAt, dataProtectionUrl, licenseUrl, sentryRelease, sourceCodeUrl } from '@/conf'
 import { DateFormat } from '@/constants/date'
@@ -24,34 +22,37 @@ export const Footer = () => {
       }}
     >
       <Divider sx={{ my: 1 }} />
-      <Container maxWidth="lg">
-        <Stack
-          alignItems={{ md: 'center' }}
-          direction={{ sm: 'column', md: 'row' }}
-          gap={3}
-          justifyContent="space-between"
-        >
-          <Stack direction={{ sm: 'column', md: 'row' }} gap={{ xs: 3, md: 4 }}>
-            <Stack>
-              <Typography variant="subtitle1">Oodikone</Typography>
-              {isDefaultServiceProvider() && <ExternalLink href={dataProtectionUrl} text="Data protection" />}
-              <ExternalLink href={sourceCodeUrl} text="Source code" />
-              <ExternalLink href={licenseUrl} text="License" />
-            </Stack>
-            <Stack>
-              <Typography variant="subtitle1">Build</Typography>
-              <Link to="/changelog">Changelog</Link>
-              <Typography color="text.secondary" variant="body2">
-                Updated {reformatDate(builtAt || new Date().toISOString(), DateFormat.DISPLAY_DATETIME)}
-              </Typography>
-              <Typography color="text.secondary" variant="body2">
-                Version {sentryRelease || 'dev'}
-              </Typography>
-            </Stack>
+      <Stack
+        direction={{ xs: 'column-reverse', sm: 'row' }}
+        sx={{
+          alignItems: 'center',
+          spacing: 3,
+          justifyContent: 'space-between',
+          maxWidth: 'xl',
+          px: 3,
+          mx: 'auto',
+        }}
+      >
+        <Stack direction="row" gap={3}>
+          <Stack>
+            <Typography variant="subtitle1">Oodikone</Typography>
+            {isDefaultServiceProvider() && <ExternalLink href={dataProtectionUrl} text="Data protection" />}
+            <ExternalLink href={sourceCodeUrl} text="Source code" />
+            <ExternalLink href={licenseUrl} text="License" />
           </Stack>
-          <ToskaLogo />
+          <Stack>
+            <Typography variant="subtitle1">Build</Typography>
+            <ExternalLink href="/changelog" text="Changelog" />
+            <Typography color="text.secondary" variant="body2">
+              Updated {reformatDate(builtAt || new Date().toISOString(), DateFormat.DISPLAY_DATETIME)}
+            </Typography>
+            <Typography color="text.secondary" variant="body2">
+              Version {sentryRelease || 'dev'}
+            </Typography>
+          </Stack>
         </Stack>
-      </Container>
+        <ToskaLogo />
+      </Stack>
     </Box>
   )
 }
