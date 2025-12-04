9a23799972271fcfe07552384d2e0e7da0bae394
joonas-a <joonas.ahovalli@gmail.com>
2025-12-04 20:15:18 +0200

    [Frontend] finish StudentAmountLimiter component

--- services/frontend/src/components/common/StudentAmountLimiter.tsx
+++ services/frontend/src/components/common/StudentAmountLimiter.tsx
@@ -2,9 +2,15 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { NumericTextField } from './NumericTextField'
 
-export const StudentAmountLimiter = ({ onStudentAmountLimitChange, studentAmountLimit }) => (
-  <Stack direction="row" spacing={1} sx={{ alignItems: 'center', mt: '0.5em' }}>
-    <Typography fontWeight={500}>Select all courses with at least</Typography>
+export const StudentAmountLimiter = ({
+  onStudentAmountLimitChange,
+  studentAmountLimit,
+}: {
+  onStudentAmountLimitChange: (value: string | number) => void
+  studentAmountLimit: number
+}) => (
+  <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
+    <Typography fontWeight={500}>Show all courses with at least</Typography>
     <NumericTextField
       initialValue={studentAmountLimit}
       onChange={({ target }) => onStudentAmountLimitChange(target.value)}
