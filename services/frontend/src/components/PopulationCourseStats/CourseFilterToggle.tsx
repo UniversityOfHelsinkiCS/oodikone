f3fd84598af090f319efe548392352514ad7cb06
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-12-10 19:30:14 +0200

    [Frontend] Fix course filter toggle texts

--- services/frontend/src/components/PopulationCourseStats/CourseFilterToggle.tsx
+++ services/frontend/src/components/PopulationCourseStats/CourseFilterToggle.tsx
@@ -3,24 +3,15 @@ import Tooltip from '@mui/material/Tooltip'
 import { FilterToggleIcon } from '@/components/common/FilterToggleIcon'
 import { isCourseSelected, toggleCourseSelection } from '@/components/FilterView/filters/courses'
 import { useFilters } from '@/components/FilterView/useFilters'
-import { useLanguage } from '@/components/LanguagePicker/useLanguage'
-import { Name } from '@oodikone/shared/types'
 
-interface CourseFilterToggleProps {
-  courseCode: string
-  courseName: Name
-}
-
-export const CourseFilterToggle = ({ courseCode, courseName }: CourseFilterToggleProps) => {
-  const { getTextIn } = useLanguage()
+export const CourseFilterToggle = ({ courseCode, courseName }: { courseCode: string; courseName: string }) => {
   const { useFilterSelector, filterDispatch } = useFilters()
 
   const isActive = useFilterSelector(isCourseSelected(courseCode))
-  const courseElement = <b>{getTextIn(courseName)}</b>
 
   const title = (
     <span>
-      {isActive ? 'Poista rajaus' : 'Rajaa opiskelijat'} kurssin {courseElement} perusteella
+      {isActive ? 'Poista rajaus' : 'Rajaa opiskelijat'} kurssin <b>{courseName}</b> perusteella
     </span>
   )
 
