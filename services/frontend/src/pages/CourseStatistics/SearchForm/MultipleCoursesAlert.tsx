ce08567bb46196e0af0d4ef7cccd5d8c5250edec
joonas-a <joonas.ahovalli@gmail.com>
2025-12-11 11:52:23 +0200

    [Course search] make multisearch notice fit in with rest of the app

--- services/frontend/src/pages/CourseStatistics/SearchForm/MultipleCoursesAlert.tsx
+++ services/frontend/src/pages/CourseStatistics/SearchForm/MultipleCoursesAlert.tsx
@@ -1,15 +1,12 @@
 import Alert from '@mui/material/Alert'
+import Typography from '@mui/material/Typography'
 
-export const MultipleCoursesAlert = ({ selectedCourses }: { selectedCourses: number }) => {
-  return (
-    <Alert severity="warning" variant="outlined">
-      <p>
-        <b>Notice:</b> Selecting multiple courses may fail if there are too many students in total. If the feature does
-        not work, try again with fewer courses.
-      </p>
-      <p>
-        <b>Currently selected: {selectedCourses}</b>
-      </p>
-    </Alert>
-  )
-}
+export const MultipleCoursesAlert = ({ selectedCourses }: { selectedCourses: number }) => (
+  <Alert severity="info" sx={{ p: 1.5 }}>
+    <Typography sx={{ mb: 1 }}>
+      <b>Notice:</b> Fetching statistics for multiple courses may fail if there are too many students in total. If the
+      query fails, try again with fewer courses.
+    </Typography>
+    <Typography fontWeight="bold">Currently selected: {selectedCourses}</Typography>
+  </Alert>
+)
