82a4d2a08dcb0c4180a74a22aea6a234b5613628
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-12-18 17:15:59 +0200

    [Updater] Add JSDocstrings to worker db index

--- updater/sis-updater-worker/src/db/index.js
+++ updater/sis-updater-worker/src/db/index.js
@@ -2,16 +2,57 @@ import { getLatestSnapshot, isActive, getActiveSnapshot, getLatestActiveSnapshot
 import logger from '../utils/logger.js'
 import { dbConnections } from './connection.js'
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string} id - Single id
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T>} Where T is the query result object
+ */
 export const selectOneById = async (table, id, col = 'id') => dbConnections.knex(table).where(col, id).first()
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string} id - Single id
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T>} Where T is the query result object
+ */
 export const selectLastById = async (table, id, col = 'id') => (await dbConnections.knex(table).where(col, id)).at(-1)
+
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string[]} ids - Array of ids
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectFromByIds = async (table, ids, col = 'id') => dbConnections.knex(table).whereIn(col, ids)
 
-export const selectFromByIdsOrderBy = async (table, ids, col = 'id', by, order = 'asc') =>
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string[]} ids - Array of ids
+ * @param {string} [col] - Id column in the table
+ * @param {string} [by]  - Column the result will be sorted by
+ * @param {string} [order] - The order the sort column will be sorted in
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
+export const selectFromByIdsOrderBy = async (table, ids, col = 'id', by = 'id', order = 'asc') =>
   dbConnections.knex(table).whereIn(col, ids).orderBy(by, order)
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectAllFrom = async table => dbConnections.knex(table)
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectAllFromSnapshots = async table =>
   (
     await dbConnections.knex
@@ -23,6 +64,13 @@ export const selectAllFromSnapshots = async table =>
     .filter(s => !!s)
     .filter(isActive)
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string[]} ids - Array of ids
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectFromSnapshotsByIds = async (table, ids, col = 'id') =>
   (
     await dbConnections.knex
@@ -35,6 +83,13 @@ export const selectFromSnapshotsByIds = async (table, ids, col = 'id') =>
     .filter(s => !!s)
     .filter(isActive)
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string[]} ids - Array of ids
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectLatestActiveFromSnapshotsByIds = async (table, ids, col = 'id') =>
   (
     await dbConnections.knex
@@ -46,6 +101,13 @@ export const selectLatestActiveFromSnapshotsByIds = async (table, ids, col = 'id
     .map(({ data }) => getLatestActiveSnapshot(data))
     .filter(s => !!s)
 
+/**
+ * @param {string} table - Table this function is trying to query against
+ * @param {string[]} ids - Array of ids
+ * @param {string} [col] - Id column in the table
+ *
+ * @returns {Promise<T[]>} Where T is the query result object
+ */
 export const selectFromActiveSnapshotsByIds = async (table, ids, col = 'id') =>
   (
     await dbConnections.knex
@@ -59,6 +121,14 @@ export const selectFromActiveSnapshotsByIds = async (table, ids, col = 'id') =>
 
 const getColumnsToUpdate = (model, keys) => Object.keys(model.rawAttributes).filter(a => !keys.includes(a))
 
+/**
+ * @param {import('sequelize').Model<T>} model - Model for the bulkCreate
+ * @param {import('sequelize').InferCreationAttributes<model>[]} entities - Model creation entity array
+ * @param {?import('sequelize').Transaction} [transaction] - Query transaction type
+ * @param {string[]} [properties] - Columns used for bulkCreate
+ * @param {boolean} [upsertStyle] - Use upsert for the creation
+ * @param {string} [findBy] - If upsertStyle is false, use findBy to findOrCreate
+ */
 export const bulkCreate = async (
   model,
   entities,
@@ -68,6 +138,7 @@ export const bulkCreate = async (
   findBy = 'id'
 ) => {
   try {
+    /** @type {import('sequelize').BulkCreateOptions<model>} */
     const options = upsertStyle
       ? { updateOnDuplicate: getColumnsToUpdate(model, properties), transaction }
       : { ignoreDuplicates: true, transaction }
