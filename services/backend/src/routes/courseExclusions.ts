bba918812c551b8da08557ffb1d68898f025c679
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-01-02 12:53:09 +0200

    [CourseExclusion] Add rights check to exclusion post and delete

--- services/backend/src/routes/courseExclusions.ts
+++ services/backend/src/routes/courseExclusions.ts
@@ -1,19 +1,20 @@
 import { Router } from 'express'
 
+import type { ExcludedCoursesResBody, ExcludedCoursesReqBody } from '@oodikone/shared/routes/courseExclusions'
+import { hasFullAccessToStudentData } from 'src/util'
 import { addExcludedCourses, removeExcludedCourses } from '../services/excludedCourses'
 
 const router = Router()
 
-export type ExcludedCoursesParams = { code: string }
-export type ExcludedCoursesResBody = void
-export type ExcludedCoursesReqBody = {
-  courseCodes: string[]
-  curriculumVersion: string
-}
+router.post<never, ExcludedCoursesResBody, ExcludedCoursesReqBody>('/', async (req, res) => {
+  const { programmeCode, courseCodes, curriculumVersion } = req.body
+  const { roles, programmeRights } = req.user
 
-router.post<ExcludedCoursesParams, ExcludedCoursesResBody, ExcludedCoursesReqBody>('/:code', async (req, res) => {
-  const { code: programmeCode } = req.params
-  const { courseCodes, curriculumVersion } = req.body
+  const hasFullAccess = hasFullAccessToStudentData(roles)
+  const hasAccessToProgramme = programmeRights.map(({ code }) => code).includes(programmeCode)
+
+  if (!hasFullAccess && !hasAccessToProgramme) return res.status(403).end()
+
   const result = await addExcludedCourses(courseCodes, curriculumVersion, programmeCode)
   if (!result) {
     res.status(400).end()
@@ -22,9 +23,15 @@ router.post<ExcludedCoursesParams, ExcludedCoursesResBody, ExcludedCoursesReqBod
   res.status(201).end()
 })
 
-router.delete<ExcludedCoursesParams, ExcludedCoursesResBody, ExcludedCoursesReqBody>('/:code', async (req, res) => {
-  const { code: programmeCode } = req.params
-  const { courseCodes, curriculumVersion } = req.body
+router.delete<never, ExcludedCoursesResBody, ExcludedCoursesReqBody>('/', async (req, res) => {
+  const { programmeCode, courseCodes, curriculumVersion } = req.body
+  const { roles, programmeRights } = req.user
+
+  const hasFullAccess = hasFullAccessToStudentData(roles)
+  const hasAccessToProgramme = programmeRights.map(({ code }) => code).includes(programmeCode)
+
+  if (!hasFullAccess && !hasAccessToProgramme) return res.status(403).end()
+
   const result = await removeExcludedCourses(courseCodes, curriculumVersion, programmeCode)
   if (!result) {
     res.status(400).end()
