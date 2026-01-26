63d8b122731b5bb383195d4887850332b3750b5f
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-01-26 17:56:43 +0200

    [Backend] Convert logger to typescript

--- services/backend/src/util/logger.ts
+++ services/backend/src/util/logger.ts
@@ -1,25 +1,18 @@
-const os = require('os')
-const winston = require('winston')
-const { WinstonGelfTransporter } = require('winston-gelf-transporter')
-const LokiTransport = require('winston-loki')
+import os from 'os'
+import * as winston from 'winston'
+import { WinstonGelfTransporter } from 'winston-gelf-transporter'
+import LokiTransport from 'winston-loki'
 
-const { isProduction, serviceProvider } = require('../config')
+import { isProduction, serviceProvider } from '../config'
 
-const { colorize, combine, timestamp, printf } = winston.format
-
-const transports = [new winston.transports.Console()]
+const transports: winston.transport[] = [new winston.transports.Console()]
 
 if (serviceProvider !== 'fd') {
   transports.push(
     new LokiTransport({
       host: 'http://loki-svc.toska-lokki.svc.cluster.local:3100',
       labels: { app: 'oodikone', environment: process.env.NODE_ENV ?? 'production' },
-    })
-  )
-}
-
-if (isProduction && serviceProvider !== 'fd') {
-  transports.push(
+    }),
     new WinstonGelfTransporter({
       handleExceptions: true,
       host: 'svm-116.cs.helsinki.fi',
@@ -30,10 +23,11 @@ if (isProduction && serviceProvider !== 'fd') {
         app: 'oodikone',
         environment: 'production',
       },
-    })
+    }) as winston.transport
   )
 }
 
+const { colorize, combine, timestamp, printf } = winston.format
 const logger = winston.createLogger({
   level: isProduction ? 'info' : 'debug',
   format: combine(
@@ -41,10 +35,10 @@ const logger = winston.createLogger({
     timestamp({ format: isProduction ? 'D.M.YYYY,HH.mm.ss' : 'HH.mm.ss' }),
     printf(({ level, message, timestamp, error, meta }) => {
       let log = `${timestamp} ${level}: ${message}`
-      if (error?.stack) {
+      if (error instanceof Error) {
         log = `${log}\n${error.stack}`
       }
-      if (Object.keys(meta || {}).length > 0) {
+      if (Object.keys(meta ?? {}).length > 0) {
         log = `${log}\n${JSON.stringify(meta, null, 2)}`
       }
       return log
@@ -55,4 +49,4 @@ const logger = winston.createLogger({
 
 logger.on('error', error => console.error('Logging failed! Reason: ', error)) // eslint-disable-line no-console
 
-module.exports = logger
+export default logger
