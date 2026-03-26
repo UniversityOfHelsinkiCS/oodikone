883129bee070668533ab21399885b7af344bd5b0
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-03-26 12:13:51 +0200

    [Frontend] Unify course page secondary chip with course search table chips

--- services/frontend/src/pages/CourseStatistics/CourseTab/CourseLabel.tsx
+++ services/frontend/src/pages/CourseStatistics/CourseTab/CourseLabel.tsx
@@ -1,11 +1,12 @@
 import Chip from '@mui/material/Chip'
 
-import { StarIcon, SwapHorizIcon } from '@/theme'
+import { GroupChip } from '@/components/common/EquivalenceGroupChip'
+import { StarIcon } from '@/theme'
 
 export const PrimaryCourseLabel = ({ code, name }: { code: string; name: string }) => (
   <Chip color={'primary'} icon={<StarIcon fontSize="small" />} label={`${code} • ${name}`} />
 )
 
 export const SecondaryCourseLabel = ({ code, name }: { code: string; name: string }) => (
-  <Chip icon={<SwapHorizIcon fontSize="small" />} label={`${code} • ${name}`} />
+  <GroupChip group={[`${code} • ${name}`]} />
 )
