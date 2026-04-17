b2b471dab5c98e5f09590ee1278982bfa32ff23a
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-04-17 12:32:52 +0300

    [ApiTests] Add api tests for /populationstatistics/studyprogrammes

---
+++ services/backend/tests/populationStudyProgrammes.test.ts
@@ -0,0 +1,106 @@
+import { Express } from 'express'
+import assert from 'node:assert/strict'
+import { describe, it, before } from 'node:test'
+import { Sequelize } from 'sequelize'
+import request, { Response } from 'supertest'
+
+import { PopulationstatisticsResBody } from '@oodikone/shared/routes/populations'
+import { dbConnections } from '../src/database/connection'
+import { initTests } from './utils'
+
+let app: Express
+let connection: Sequelize
+before(async () => {
+  app = await initTests()
+  connection = dbConnections.sequelize
+})
+
+void describe('Population statistics (study programme)', async () => {
+  await it('should return correctly formatted responses', async () => {
+    const res = await request(app)
+      .get('/populationstatistics/studyprogrammes')
+      .set('shib-session-id', 'test')
+      .set('uid', 'basic')
+      .set('hygroupcn', 'grp-oodikone-basic-users')
+
+    assert.strictEqual(res.status, 200)
+
+    assert.strictEqual(Object.keys(res.body).includes('allProgrammes'), true)
+    assert.strictEqual(Object.keys(res.body).includes('filteredProgrammes'), true)
+
+    assert.notStrictEqual(Object.keys(res.body.allProgrammes).length, 0)
+    assert.notStrictEqual(Object.keys(res.body.filteredProgrammes).length, 0)
+  })
+
+  await it('should return correct programmes to allProgrammes', async () => {
+    const res = await request(app)
+      .get('/populationstatistics/studyprogrammes')
+      .set('shib-session-id', 'test')
+      .set('uid', 'basic')
+      .set('hygroupcn', 'grp-oodikone-basic-users')
+
+    const testMathProgramme = {
+      code: 'KH50_001',
+      curriculumPeriodIds: [
+        'hy-lv-72',
+        'hy-lv-73',
+        'hy-lv-71',
+        'hy-lv-69',
+        'hy-lv-68',
+        'hy-lv-70',
+        'hy-lv-76',
+        'hy-lv-74',
+        'hy-lv-75',
+      ],
+      degreeProgrammeType: 'urn:code:degree-program-type:bachelors-degree',
+      name: {
+        en: "Bachelor's Programme in Mathematical Sciences",
+        fi: 'Matemaattisten tieteiden kandiohjelma',
+        sv: 'Kandidatsprogrammet i matematiska vetenskaper',
+      },
+      progId: 'MAT',
+    }
+
+    assert.strictEqual(Object.keys(res.body.allProgrammes).length, 972)
+    assert.deepStrictEqual(res.body.allProgrammes['KH50_001'], testMathProgramme)
+  })
+
+  await it('should return the programmes, that user has access to, to filteredProgrammes', async () => {
+    const res = await request(app)
+      .get('/populationstatistics/studyprogrammes')
+      .set('shib-session-id', 'test')
+      .set('uid', 'basic')
+      .set('hygroupcn', 'grp-oodikone-basic-users')
+
+    const testMathProgramme = {
+      code: 'KH50_001',
+      curriculumPeriodIds: [
+        'hy-lv-72',
+        'hy-lv-73',
+        'hy-lv-71',
+        'hy-lv-69',
+        'hy-lv-68',
+        'hy-lv-70',
+        'hy-lv-76',
+        'hy-lv-74',
+        'hy-lv-75',
+      ],
+      degreeProgrammeType: 'urn:code:degree-program-type:bachelors-degree',
+      name: {
+        en: "Bachelor's Programme in Mathematical Sciences",
+        fi: 'Matemaattisten tieteiden kandiohjelma',
+        sv: 'Kandidatsprogrammet i matematiska vetenskaper',
+      },
+      progId: 'MAT',
+    }
+
+    assert.strictEqual(Object.keys(res.body.filteredProgrammes).length, 2) // Basic has access to math bachelor + master
+
+    assert.deepStrictEqual(res.body.filteredProgrammes['KH50_001'], testMathProgramme)
+    assert.deepStrictEqual(res.body.allProgrammes['KH50_001'], testMathProgramme) // All programmes should still include filteredProgramme programmes
+  })
+
+  await it.todo(
+    'should return both degree levels of study programme when searching KH90_001 or MH90_001 (ELK, ELL) in filteredProgrammes'
+  )
+})
