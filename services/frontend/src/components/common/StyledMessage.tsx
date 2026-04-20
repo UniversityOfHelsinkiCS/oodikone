0f76e50e4b94ade8bdaf5f99c3b7b93585b0dddb
joonas-a <joonas.ahovalli@gmail.com>
2026-04-20 11:59:16 +0300

    [Frontend] Enhance StyledMessage

--- services/frontend/src/components/common/StyledMessage.tsx
+++ services/frontend/src/components/common/StyledMessage.tsx
@@ -1,8 +1,25 @@
 import Alert from '@mui/material/Alert'
+import AlertTitle from '@mui/material/AlertTitle'
 import type { SxProps } from '@mui/material/styles'
 
-export const StyledMessage = ({ children, sx: propSx }: { children: React.ReactNode; sx?: SxProps }) => (
-  <Alert icon={false} severity="info" sx={{ margin: 'auto', maxWidth: '800px', ...propSx }} variant="outlined">
+export const StyledMessage = ({
+  children,
+  sx: propSx,
+  showIcon,
+  title,
+}: {
+  children: React.ReactNode
+  sx?: SxProps
+  showIcon?: boolean
+  title?: string
+}) => (
+  <Alert
+    icon={showIcon ? null : false}
+    severity="info"
+    sx={{ margin: 'auto', maxWidth: '800px', ...propSx }}
+    variant="outlined"
+  >
+    {title?.length ? <AlertTitle>{title}</AlertTitle> : null}
     {children}
   </Alert>
 )
