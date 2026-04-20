6a9a49952b196a4b4ffdd62eff9a8f2c5f085e8d
joonas-a <joonas.ahovalli@gmail.com>
2026-04-20 12:02:49 +0300

    [Frontend] Further enhance StyledMessage

--- services/frontend/src/components/common/StyledMessage.tsx
+++ services/frontend/src/components/common/StyledMessage.tsx
@@ -4,20 +4,22 @@ import type { SxProps } from '@mui/material/styles'
 
 export const StyledMessage = ({
   children,
+  variant = 'outlined',
   sx: propSx,
   showIcon,
   title,
 }: {
   children: React.ReactNode
+  variant?: 'outlined' | 'standard' | 'filled'
   sx?: SxProps
   showIcon?: boolean
   title?: string
 }) => (
   <Alert
-    icon={showIcon ? null : false}
+    icon={showIcon ? null : false} // Null keeps icon enabled (default behaviour)
     severity="info"
     sx={{ margin: 'auto', maxWidth: '800px', ...propSx }}
-    variant="outlined"
+    variant={variant}
   >
     {title?.length ? <AlertTitle>{title}</AlertTitle> : null}
     {children}
