fe665cc8adad33993452150d98c8e71a80a8fd39
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-05-20 17:17:13 +0300

    [CourseStats] Fix enrollments

--- services/backend/src/services/courses/creditsAndEnrollmentsOfCourse.ts
+++ services/backend/src/services/courses/creditsAndEnrollmentsOfCourse.ts
@@ -1,6 +1,6 @@
 import { Op } from 'sequelize'
 
-import { Credit } from '@oodikone/shared/models'
+import { Credit, Enrollment } from '@oodikone/shared/models'
 import { EnrollmentState, Unification } from '@oodikone/shared/types'
 import {
   StudentModel,
@@ -78,7 +78,7 @@ export const getCreditsForCourses = async (codeGroups: string[][], unification: 
       else if (group.every(code => codesOfPassedCredits.includes(code))) {
         // The credit in question should always exist because we just checked that it does
         completedGroups.push(
-          group.map(code => studentCredits.find(credit => credit.course_code === code && CreditModel.passed(credit))!)
+          studentCredits.filter(credit => group.includes(credit.course_code) && CreditModel.passed(credit))
         )
       } else {
         // "Partially completed substitution, do not count towards anything"
@@ -130,27 +130,64 @@ export const getStudentNumberToSrElementsMap = async (studentNumbers: string[]) 
   }, {})
 }
 
-export const getEnrollmentsForCourses = async (codeGroup: string[][], unification: Unification) =>
-  EnrollmentModel.findAll({
-    attributes: ['studentnumber', 'enrollment_date_time', 'course_code'],
+export const getEnrollmentsForCourses = async (codeGroups: string[][], unification: Unification) => {
+  const allCourseCodes = codeGroups.flatMap(group => group)
+
+  const students = await StudentModel.findAll({
+    raw: true,
+    nest: true,
+    attributes: ['studentnumber'],
     include: [
       {
-        model: SemesterModel,
-        attributes: ['semestercode', 'name', 'yearcode', 'yearname'],
-        where: {
-          startdate: {
-            [Op.lte]: new Date(),
+        model: EnrollmentModel,
+        attributes: ['studentnumber', 'enrollment_date_time', 'course_code'],
+        include: [
+          {
+            model: SemesterModel,
+            attributes: ['semestercode', 'name', 'yearcode', 'yearname'],
+            where: {
+              startdate: {
+                [Op.lte]: new Date(),
+              },
+            },
           },
+        ],
+        where: {
+          course_code: {
+            [Op.in]: allCourseCodes,
+          },
+          enrollment_date_time: { [Op.gte]: new Date('2021-05-31') },
+          state: EnrollmentState.ENROLLED,
+          is_open: getIsOpen(unification),
         },
       },
     ],
-    where: {
-      // Include only enrollments from the original course and 1-to-1 substitutions
-      course_code: {
-        [Op.in]: codeGroup.filter(group => group.length === 1).flatMap(group => group),
-      },
-      enrollment_date_time: { [Op.gte]: new Date('2021-05-31') },
-      state: EnrollmentState.ENROLLED,
-      is_open: getIsOpen(unification),
-    },
   })
+
+  const studentNumberToEnrollments = students.reduce<Record<string, Enrollment[]>>((acc, student) => {
+    acc[student.studentnumber] ??= []
+    acc[student.studentnumber] = acc[student.studentnumber].concat(student.enrollments)
+
+    return acc
+  }, {})
+
+  // Calculate all code groups that have enrollments for all of the courses in the group
+  const completedEnrollments: Enrollment[][] = []
+  for (const studentEnrollments of Object.values(studentNumberToEnrollments)) {
+    const codesOfEnrollments = studentEnrollments.map(enrollment => enrollment.course_code)
+
+    for (const group of codeGroups) {
+      if (group.every(code => codesOfEnrollments.includes(code))) {
+        completedEnrollments.push(
+          studentEnrollments
+            .filter(enrollment => group.includes(enrollment.course_code))
+            .sort((a, b) => b.enrollment_date_time.getTime() - a.enrollment_date_time.getTime())
+        )
+      } else {
+        // Partially completed enrollments for a group, skipping...
+      }
+    }
+  }
+
+  return completedEnrollments
+}
