02935f7da871fd22cec41013cb03af3f4f00a237
Aaro Koivukangas <aaro@koivukangas.cc>
2026-07-03 15:03:32 +0300

    [ApiTests] Migrated populationStudyProgramme tests

--- services/backend/tests/population/populationStudyProgrammes.test.ts
+++ services/backend/tests/population/populationStudyProgrammes.test.ts
@@ -1,17 +1,16 @@
 import { Express } from 'express'
-import assert from 'node:assert/strict'
-import { describe, it, before } from 'node:test'
 import request from 'supertest'
+import { describe, it, beforeAll, assert } from 'vitest'
 
 import { initTests } from '../utils'
 
-let app: Express
-before(async () => {
-  app = await initTests()
-})
+void describe('Population statistics (study programme)', () => {
+  let app: Express
+  beforeAll(async () => {
+    app = await initTests()
+  })
 
-void describe('Population statistics (study programme)', async () => {
-  await it('should return correctly formatted responses', async () => {
+  it('should return correctly formatted responses', async () => {
     const res = await request(app)
       .get('/populationstatistics/studyprogrammes')
       .set('shib-session-id', 'test')
@@ -27,7 +26,7 @@ void describe('Population statistics (study programme)', async () => {
     assert.notStrictEqual(Object.keys(res.body.filteredProgrammes).length, 0)
   })
 
-  await it('should return correct programmes to allProgrammes', async () => {
+  it('should return correct programmes to allProgrammes', async () => {
     const res = await request(app)
       .get('/populationstatistics/studyprogrammes')
       .set('shib-session-id', 'test')
@@ -60,7 +59,7 @@ void describe('Population statistics (study programme)', async () => {
     assert.deepStrictEqual(res.body.allProgrammes.KH50_001, testMathProgramme)
   })
 
-  await it('should return the programmes, that user has access to, to filteredProgrammes', async () => {
+  it('should return the programmes, that user has access to, to filteredProgrammes', async () => {
     const res = await request(app)
       .get('/populationstatistics/studyprogrammes')
       .set('shib-session-id', 'test')
@@ -95,7 +94,7 @@ void describe('Population statistics (study programme)', async () => {
     assert.deepStrictEqual(res.body.allProgrammes.KH50_001, testMathProgramme) // All programmes should still include filteredProgramme programmes
   })
 
-  await it.todo(
+  it.todo(
     'should return both degree levels of study programme when searching KH90_001 or MH90_001 (ELK, ELL) in filteredProgrammes'
   )
 })
