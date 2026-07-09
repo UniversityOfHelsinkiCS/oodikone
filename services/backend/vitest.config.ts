06dc27e4fb0d688bb4d7b928cac26fad18132e85
Aaro Koivukangas <aaro@koivukangas.cc>
2026-07-09 14:49:16 +0300

    [Vitest] Improve api test performance

--- services/backend/vitest.config.ts
+++ services/backend/vitest.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
     reporters: ['tree'],
     exclude: ['./dist', 'node_modules'],
     setupFiles: ['./tests/setup.ts'],
+    pool: 'threads',
+    testTimeout: 10_000, // 10s timeout
+    sequence: {
+      concurrent: true, // Enable parallel tests by default
+    },
   },
 })
