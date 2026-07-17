e73153a834467937b52515f2a16dda9b0d6880ca
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2026-07-17 12:07:13 +0300

    [Frontend][CloseToGraduation] MRT to OT

--- services/frontend/src/pages/CloseToGraduation/index.tsx
+++ services/frontend/src/pages/CloseToGraduation/index.tsx
@@ -4,12 +4,10 @@ import Tabs from '@mui/material/Tabs'
 import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 
-import { MaterialReactTable, MRT_ColumnDef, useMaterialReactTable } from 'material-react-table'
 import { useEffect, useMemo, useState } from 'react'
 
 import { getEnrollmentTypeTextForExcel, isFall } from '@/common'
 import { closeToGraduationToolTips } from '@/common/InfoToolTips'
-import { ExportToExcelDialog } from '@/components/common/MRTExcelExport'
 import { PageLayout } from '@/components/common/PageLayout'
 import { PageTitle } from '@/components/common/PageTitle'
 import { StudentInfoItem } from '@/components/common/StudentInfoItem'
@@ -23,10 +21,12 @@ import { useTitle } from '@/hooks/title'
 import { useSemesters } from '@/hooks/useSemesters'
 import { useGetStudentsCloseToGraduationQuery } from '@/redux/closeToGraduation'
 import { CheckIcon } from '@/theme'
-import { getDefaultMRTOptions } from '@/util/getDefaultMRTOptions'
 import { reformatDate } from '@/util/timeAndDate'
-import { getFullLanguage } from '@oodikone/shared/language'
+import { getFullLanguage, Language } from '@oodikone/shared/language'
 import { range } from '@oodikone/shared/util'
+import { OodiTable } from '@/components/OodiTable'
+import { CloseToGraduationData } from '@oodikone/shared/routes/populations'
+import { createColumnHelper, TableOptions } from '@tanstack/react-table'
 
 const NUMBER_OF_DISPLAYED_SEMESTERS = 6
 
@@ -36,6 +36,8 @@ const CheckIconWithTitle = ({ visible, title }: { visible: boolean; title?: stri
   </Box>
 )
 
+const columnHelper = createColumnHelper<CloseToGraduationData>()
+
 export const CloseToGraduation = () => {
   useTitle('Students close to graduation')
   const { data: students, isFetching } = useGetStudentsCloseToGraduationQuery()
@@ -43,9 +45,7 @@ export const CloseToGraduation = () => {
   const { semesters, currentSemester } = semesterData
 
   const [selectedTab, setSelectedTab] = useState(0)
-  const [exportModalOpen, setExportModalOpen] = useState(false)
-  const [exportData, setExportData] = useState<Record<string, unknown>[]>([])
-  const { getTextIn, language } = useLanguage()
+  const { getTextIn } = useLanguage()
   const { getSemesterEnrollmentsContent, getSemesterEnrollmentsVal } = useMemo(
     () =>
       getSemestersPresentFunctions({
@@ -71,124 +71,109 @@ export const CloseToGraduation = () => {
     [currentSemesterCode]
   )
 
-  const columns = useMemo<MRT_ColumnDef<any>[]>(
+  const ooditableColumns = useMemo(
     () => [
-      {
-        accessorKey: 'student.studentNumber',
+      columnHelper.accessor('student.studentNumber', {
         header: 'Student number',
-        Cell: ({ cell }) => (
+        cell: cell => (
           <StudentInfoItem
             sisPersonId={cell.row.original.student.sis_person_id}
             studentNumber={cell.getValue<string>()}
           />
         ),
-        filterFn: 'startsWith',
-      },
-      {
-        accessorKey: 'student.name',
+        // filterFn: 'startsWith',
+      }),
+      columnHelper.accessor('student.name', {
         header: 'Name',
-        filterFn: 'startsWith',
-      },
-      {
-        accessorKey: 'student.phoneNumber',
+        // filterFn: 'startsWith',
+      }),
+      columnHelper.accessor('student.phoneNumber', {
         header: 'Phone number',
-      },
-      {
-        accessorKey: 'student.email',
+      }),
+      columnHelper.accessor('student.email', {
         header: 'Email',
-      },
-      {
-        accessorKey: 'student.secondaryEmail',
+      }),
+      columnHelper.accessor('student.secondaryEmail', {
         header: 'Secondary email',
-      },
-      {
-        accessorFn: row => getFullLanguage(row.student.preferredLanguage),
+      }),
+      columnHelper.accessor(row => getFullLanguage(row.student.preferredLanguage as Language), {
         id: 'preferredLanguage',
         header: 'Preferred language',
-        filterVariant: 'multi-select',
-      },
-      {
-        accessorFn: row => getTextIn(row.faculty),
+        // filterVariant: 'multi-select',
+      }),
+      columnHelper.accessor(row => getTextIn(row.faculty), {
         header: 'Faculty',
         id: 'faculty',
-        filterVariant: 'multi-select',
-      },
-      {
-        accessorFn: row => getTextIn(row.programme.name),
+        // filterVariant: 'multi-select',
+      }),
+      columnHelper.accessor(row => getTextIn(row.programme.name), {
         header: 'Programme',
         id: 'programme',
-        filterVariant: 'multi-select',
-      },
-      {
-        accessorFn: row => getTextIn(row.programme.studyTrack),
+        // filterVariant: 'multi-select',
+      }),
+      columnHelper.accessor(row => getTextIn(row.programme.studyTrack), {
         id: 'studyTrack',
         header: 'Study track',
-        filterVariant: 'multi-select',
-      },
-      {
-        accessorFn: row => new Date(row.studyright.startDate),
+        // filterVariant: 'multi-select',
+      }),
+      columnHelper.accessor(row => new Date(row.studyright.startDate), {
         id: 'startOfStudyRight',
-        Cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
+        cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
         header: 'Start of study right',
-        filterVariant: 'date-range',
-      },
-      {
-        accessorFn: row => new Date(row.programme.startedAt),
+        // filterVariant: 'date-range',
+      }),
+      columnHelper.accessor(row => new Date(row.programme.startedAt), {
         id: 'startedInProgramme',
-        Cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
-        header: 'Started in programme',
-        Header: (
+        cell: cell => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
+        header: () => (
           <TableHeaderWithTooltip
             header="Started in programme"
             tooltipText="For students with only a study right in the master’s programme, this date is the same as 'Start of study right'. For students with study rights in both the bachelor’s and master’s programmes, this date represents when they started in the master’s programme (i.e. one day after graduating from the bachelor’s programme)."
           />
         ),
-        filterVariant: 'date-range',
-      },
-      {
+        // filterVariant: 'date-range',
+      }),
+      columnHelper.accessor('credits.hops', {
         header: 'Completed credits – HOPS',
-        accessorKey: 'credits.hops',
-        filterVariant: 'range',
-        muiTableBodyCellProps: {
-          align: 'right',
-        },
-      },
-      {
+        // accessorKey: 'credits.hops',
+        // filterVariant: 'range',
+        // muiTableBodyCellProps: {
+        //   align: 'right',
+        // },
+      }),
+      columnHelper.accessor('credits.all', {
         header: 'Completed credits – Total',
-        accessorKey: 'credits.all',
-        filterVariant: 'range',
-        muiTableBodyCellProps: {
-          align: 'right',
-        },
-      },
-      {
-        header: 'BSc & MSc study right',
-        accessorKey: 'studyright.isBaMa',
-        filterVariant: 'checkbox',
-        Cell: ({ cell }) => <CheckIconWithTitle visible={cell.getValue<boolean>()} />,
-        Header: (
+        // accessorKey: 'credits.all',
+        // filterVariant: 'range',
+        // muiTableBodyCellProps: {
+        //   align: 'right',
+        // },
+      }),
+      columnHelper.accessor('studyright.isBaMa', {
+        // accessorKey: 'studyright.isBaMa',
+        // filterVariant: 'checkbox',
+        cell: cell => <CheckIconWithTitle visible={cell.getValue<boolean>()} />,
+        header: () => (
           <TableHeaderWithTooltip
             header="BSc & MSc study right"
             tooltipText="Indicates whether the student has been granted the study right to complete both a bachelor's and a master's degree."
           />
         ),
-      },
-      {
-        header: 'Curriculum period',
-        accessorKey: 'curriculumPeriod',
-        filterVariant: 'multi-select',
-        Header: (
+      }),
+      columnHelper.accessor('curriculumPeriod', {
+        // accessorKey: 'curriculumPeriod',
+        // filterVariant: 'multi-select',
+        header: () => (
           <TableHeaderWithTooltip
             header="Curriculum period"
             tooltipText="The curriculum period the student has chosen for their primary study plan"
           />
         ),
-      },
-      {
+      }),
+      columnHelper.accessor(row => getSemesterEnrollmentsVal(row.studyright), {
         header: 'Semester enrollments',
-        accessorFn: row => getSemesterEnrollmentsVal(row.studyright),
-        Cell: ({ row }) => {
-          const content = getSemesterEnrollmentsContent(row.original.student, row.original.studyright)
+        cell: cell => {
+          const content = getSemesterEnrollmentsContent(cell.row.original.student, cell.row.original.studyright)
 
           return (
             <Box sx={{ display: 'flex', m: 0.5 }}>
@@ -201,111 +186,99 @@ export const CloseToGraduation = () => {
           )
         },
         id: 'semesterEnrollments',
-      },
-      {
-        header: 'Semesters absent',
-        accessorKey: 'numberOfAbsentSemesters',
-        filterVariant: 'range',
-        Header: (
+      }),
+      columnHelper.accessor('numberOfAbsentSemesters', {
+        // accessorKey: 'numberOfAbsentSemesters',
+        // filterVariant: 'range',
+        header: () => (
           <TableHeaderWithTooltip
             header="Semesters absent"
             tooltipText="The number of semesters the student has been absent (both statutory (*lakiperusteinen*) and non-statutory absences) during their study right. The current semester is included."
           />
         ),
-        muiTableBodyCellProps: {
-          align: 'right',
-        },
-      },
-      {
-        header: 'Semesters used',
-        accessorKey: 'numberOfUsedSemesters',
-        filterVariant: 'range',
-        Header: (
+        // muiTableBodyCellProps: {
+        //   align: 'right',
+        // },
+      }),
+      columnHelper.accessor('numberOfUsedSemesters', {
+        // accessorKey: 'numberOfUsedSemesters',
+        // filterVariant: 'range',
+        header: () => (
           <TableHeaderWithTooltip
             header="Semesters used"
             tooltipText="The number of semesters the student has either been enrolled as present (*läsnäoleva*) or the enrollment was neglected (*laiminlyöty*) during their study right. The current semester is included."
           />
         ),
-        muiTableBodyCellProps: {
-          align: 'right',
-        },
-      },
-      {
-        header: 'Thesis completed',
-        accessorFn: row => row.thesisInfo != null,
+        // muiTableBodyCellProps: {
+        //   align: 'right',
+        // },
+      }),
+      columnHelper.accessor(row => row.thesisInfo != null, {
         id: 'thesisCompleted',
-        filterVariant: 'checkbox',
-        Cell: ({ cell, row }) => (
+        // filterVariant: 'checkbox',
+        cell: cell => (
           <CheckIconWithTitle
             title={
               cell.getValue()
                 ? [
-                    `Attainment date: ${reformatDate(row.original.thesisInfo.attainmentDate, DateFormat.ISO_DATE)}`,
-                    `Course code: ${row.original.thesisInfo.courseCode}`,
-                    `Grade: ${row.original.thesisInfo.grade}`,
+                    `Attainment date: ${reformatDate(cell.row.original.thesisInfo?.attainmentDate, DateFormat.ISO_DATE)}`,
+                    `Course code: ${cell.row.original.thesisInfo?.courseCode}`,
+                    `Grade: ${cell.row.original.thesisInfo?.grade}`,
                   ].join('\n')
                 : undefined
             }
             visible={cell.getValue<boolean>()}
           />
         ),
-        Header: (
+        header: () => (
           <TableHeaderWithTooltip
             header="Thesis completed"
             tooltipText="The thesis attainment must be linked to the correct study right. You can see the attainment date, course code, and grade by hovering over the check mark."
           />
         ),
-      },
-      {
-        header: 'Latest attainment date – HOPS',
-        accessorFn: row => new Date(row.attainmentDates.latestHops),
+      }),
+      columnHelper.accessor(row => new Date(row.attainmentDates.latestHops), {
         id: 'latestAttainmentDateHops',
-        Cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
-        Header: (
+        cell: cell => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
+        header: () => (
           <TableHeaderWithTooltip
             header="Latest attainment date – HOPS"
             tooltipText="The date when the student last completed a course in their primary study plan"
           />
         ),
-        filterVariant: 'date-range',
-      },
-      {
-        header: 'Latest attainment date – Total',
-        accessorFn: row => new Date(row.attainmentDates.latestTotal),
+        // filterVariant: 'date-range',
+      }),
+      columnHelper.accessor(row => new Date(row.attainmentDates.latestTotal), {
         id: 'latestAttainmentDateTotal',
-        Cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
-        Header: (
+        cell: cell => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
+        header: () => (
           <TableHeaderWithTooltip
             header="Latest attainment date – Total"
             tooltipText="The date when the student last completed any course at the university"
           />
         ),
-        filterVariant: 'date-range',
-      },
-      {
-        header: 'Earliest attainment date – HOPS',
-        accessorFn: row => new Date(row.attainmentDates.earliestHops),
+        // filterVariant: 'date-range',
+      }),
+      columnHelper.accessor(row => new Date(row.attainmentDates.earliestHops), {
         id: 'earlistAttainmentDateHops',
-        Cell: ({ cell }) => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
-        Header: (
+        cell: cell => reformatDate(cell.getValue<Date>(), DateFormat.ISO_DATE),
+        header: () => (
           <TableHeaderWithTooltip
             header="Earliest attainment date – HOPS"
             tooltipText="The date when the student first completed a course in their primary study plan"
           />
         ),
-        filterVariant: 'date-range',
-      },
-      ...semestersToInclude.map(semester => ({
-        id: getTextIn(semesters[`${semester}`]?.name)!,
-        header: `Enrollment status – ${getTextIn(semesters[`${semester}`]?.name)}`,
-        accessorFn: row => {
+        // filterVariant: 'date-range',
+      }),
+      ...semestersToInclude.map(semester => columnHelper.accessor(row => {
           if (!row.studyright.semesterEnrollments) {
             return 'Not enrolled'
           }
           const enrollment = row.studyright.semesterEnrollments.find(enrollment => enrollment.semester === semester)
           return getEnrollmentTypeTextForExcel(enrollment?.type, enrollment?.statutoryAbsence)
-        },
-        visibleInShowHideMenu: false,
+        }, {
+        id: `enrollmentFor${getTextIn(semesters[`${semester}`]?.name)!}`,
+        header: `Enrollment status – ${getTextIn(semesters[`${semester}`]?.name)}`,
       })),
     ],
     [semesters, semestersToInclude]
@@ -321,70 +294,28 @@ export const CloseToGraduation = () => {
   })
 
   useEffect(() => {
-    const hiddenColumns: Record<string, boolean> = {}
-    for (const column of columns) {
-      if (column.visibleInShowHideMenu === false) {
-        hiddenColumns[column.id ?? column.header] = false
-      }
+    const hiddenColumns: string[] = []
+    for (const column of ooditableColumns) {
+      if (column.id?.startsWith('enrollmentFor')) hiddenColumns.push(column.id ?? column.header)
     }
-    setColumnVisibility(prev => ({ ...prev, ...hiddenColumns }))
-  }, [columns])
+
+    setColumnVisibility(prev => ({ ...prev, ...Object.fromEntries(hiddenColumns.map(key => [key, false])) }))
+  }, [ooditableColumns])
 
   const displayedData = (selectedTab === 0 ? students?.bachelor : students?.masterAndLicentiate) ?? []
 
-  const muiFilterTextFieldProps = ({ column }) => {
-    if (column.columnDef.filterVariant && ['multi-select', 'select', 'text'].includes(column.columnDef.filterVariant)) {
-      let placeholder = ''
-      switch (column.columnDef.header) {
-        case 'Faculty':
-          placeholder = 'Matemaattis-luonnontieteellinen tiedekunta'
-          break
-        case 'Programme':
-          placeholder = 'Matemaattisten tieteiden kandiohjelma'
-          break
-        case 'Study track':
-          placeholder = 'Matematiikka'
-          break
-        case 'Curriculum period':
-          placeholder = '2023-2026'
-          break
-        case 'Student number':
-          placeholder = '012345678'
-          break
-      }
-      return { placeholder }
-    }
-    return {}
-  }
-
-  const defaultOptions = getDefaultMRTOptions(setExportData, setExportModalOpen, language)
-  defaultOptions.muiFilterTextFieldProps = muiFilterTextFieldProps // Add "patched" filter-placeholders to MRT options
-
-  const table = useMaterialReactTable({
-    ...defaultOptions,
-    columns,
-    data: displayedData,
+  const ooditable: Partial<TableOptions<CloseToGraduationData>> = {
     initialState: {
-      ...defaultOptions.initialState,
       sorting: [{ id: 'programme', desc: false }],
     },
-    state: { columnVisibility },
-    onColumnVisibilityChange: setColumnVisibility,
-  })
+    state: {
+      columnVisibility,
+    },
+  }
 
   return (
     <PageLayout maxWidth="lg">
       <PageTitle title="Students close to graduation" />
-      {!isFetching && (
-        <ExportToExcelDialog
-          exportColumns={columns}
-          exportData={exportData}
-          featureName="students_close_to_graduation"
-          onClose={() => setExportModalOpen(false)}
-          open={exportModalOpen}
-        />
-      )}
-
       <Box sx={{ my: 3, textAlign: 'center' }}>
         <InfoBox content={closeToGraduationToolTips} />
       </Box>
@@ -400,7 +331,7 @@ export const CloseToGraduation = () => {
         ) : null}
       </Box>
 
-      {isFetching ? <LoadingSection /> : <MaterialReactTable key={selectedTab} table={table} />}
+      {isFetching ? <LoadingSection /> : <OodiTable options={ooditable} data={displayedData} columns={ooditableColumns} />}
     </PageLayout>
   )
 }
