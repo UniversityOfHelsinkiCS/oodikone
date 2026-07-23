8091d2140521175d5ae36a6a90cb569599b4bb8f
joonas-a <joonas.ahovalli@gmail.com>
2026-07-23 14:11:05 +0300

    [Filter toggle] fix issue with tooltip on disabled state component

--- services/frontend/src/components/FilterView/FilterToggle.tsx
+++ services/frontend/src/components/FilterView/FilterToggle.tsx
@@ -3,7 +3,9 @@ import Tooltip from '@mui/material/Tooltip'
 import { FilterToggleIcon } from '@/components/common/FilterToggleIcon'
 
 export const FilterToggle = ({ active, applyFilter, clearFilter, popupContent, disabled }) => (
-  <Tooltip hidden={disabled} title={popupContent}>
-    <FilterToggleIcon disabled={disabled} isActive={active} onClick={active ? clearFilter : applyFilter} />
+  <Tooltip title={disabled ? null : popupContent}>
+    <span style={{ display: 'flex', flex: '1' }}>
+      <FilterToggleIcon disabled={disabled} isActive={active} onClick={active ? clearFilter : applyFilter} />
+    </span>
   </Tooltip>
 )
