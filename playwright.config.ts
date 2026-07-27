17cdcc0800462b5a06d866c6e08e2759c9fd1156
Aaro Koivukangas <aaro@koivukangas.cc>
2026-07-27 19:23:47 +0300

    [Playwright] Change js to ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,18 @@
-const { defineConfig, devices } = require('@playwright/test')
+import { defineConfig, devices } from '@playwright/test'
 
-module.exports = defineConfig({
-  testDir: './playwright',
-  testMatch: ['**/*.js'],
+export default defineConfig({
+  testDir: './e2e',
+  testMatch: ['**/*.spec.ts'],
   testIgnore: ['**/support/**', '**/plugins/**'],
-  timeout: 30_000,
+  timeout: 60_000,
+  expect: { timeout: 30_000 },
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
     viewport: { width: 1920, height: 1080 },
     trace: 'on-first-retry',
+    testIdAttribute: 'data-cy',
   },
   projects: [
     {
