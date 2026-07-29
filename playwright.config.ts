90aaf3686fe13204b0fbc7e7992ee2c5e3f8db53
Aaro Koivukangas <aaro@koivukangas.cc>
2026-07-29 14:50:17 +0300

    [Playwright][Config] Change better values for ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,10 +4,18 @@ export default defineConfig({
   testDir: './e2e',
   testMatch: ['**/*.spec.ts'],
   testIgnore: ['**/support/**', '**/plugins/**'],
-  timeout: 15_000,
+
+  // Fail the build on CI if you accidentally left test.only in the source code.
+  forbidOnly: !!process.env.CI,
+
+  timeout: 30_000,
   expect: { timeout: 10_000 },
+
+  reporter: [['list', { printFailuresInline: true }]],
+
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
+
   use: {
     baseURL: process.env.CI ? 'http://localhost:1337' : process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:3000',
     viewport: { width: 1920, height: 1080 },
