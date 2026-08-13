a3244ca95c538933e6b969ba957a2582b5e9996b
Aaro Koivukangas <aaro@koivukangas.cc>
2026-08-13 09:33:16 +0300

    [Precommit] Format lintstaged file

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -9,25 +9,24 @@ export default {
   '{services,updater}/**/*.{js,jsx,ts,tsx}': files =>
     `oxlint --fix --type-aware --type-check --no-error-on-unmatched-pattern --quiet ${files.join(' ')}`,
 
-  '*.{js,jsx,ts,tsx,json,md,yml,yaml,html,css}': files =>
-    `oxfmt --no-error-on-unmatched-pattern ${files.join(' ')} `,
+  '*.{js,jsx,ts,tsx,json,md,yml,yaml,html,css}': files => `oxfmt --no-error-on-unmatched-pattern ${files.join(' ')} `,
 
   '*.css': files => `stylelint --fix ${files.join(' ')}`,
 
   Dockerfile: files =>
     `${dockerCmdBase} hadolint/hadolint:v2.14.0 hadolint --ignore DL3006 ${relativeFilePaths(files)}`,
-    '*.sh': files => `${dockerCmdBase} koalaman/shellcheck:v0.11.0 ${relativeFilePaths(files)} -x`,
-    'docker-compose*': files => {
-      const composeFiles = file => {
-        if (['docker-compose.ci.yml', 'docker-compose.test.yml'].some(f => file.includes(f))) {
-          return `--file ${file}`
-        }
-        if (file.includes('docker-compose.real.yml')) {
-          return `--file ${cwd}/docker-compose.yml --file ${file}`
-        }
-        return ''
+  '*.sh': files => `${dockerCmdBase} koalaman/shellcheck:v0.11.0 ${relativeFilePaths(files)} -x`,
+  'docker-compose*': files => {
+    const composeFiles = file => {
+      if (['docker-compose.ci.yml', 'docker-compose.test.yml'].some(f => file.includes(f))) {
+        return `--file ${file}`
       }
+      if (file.includes('docker-compose.real.yml')) {
+        return `--file ${cwd}/docker-compose.yml --file ${file}`
+      }
+      return ''
+    }
 
-      return files.map(file => `docker compose ${composeFiles(file)} config --quiet`)
-    },
+    return files.map(file => `docker compose ${composeFiles(file)} config --quiet`)
+  },
 }
