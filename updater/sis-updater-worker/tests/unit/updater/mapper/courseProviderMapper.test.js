9f2b37858690ac7d927a808b9e2f3fc5f91551de
Aaro Koivukangas <aaro@koivukangas.cc>
2026-09-15 11:33:56 +0300

    [UnitTests][Updater] Change courseProvider mapper share to be a reasonable value

--- updater/sis-updater-worker/tests/unit/updater/mapper/courseProviderMapper.test.js
+++ updater/sis-updater-worker/tests/unit/updater/mapper/courseProviderMapper.test.js
@@ -7,10 +7,10 @@ import { courseProviderMapper } from '@/updater/mapper.js'
 describe('courseProviderMapper', () => {
   it('maps organisation shares to a course provider row', () => {
     const mapProvider = courseProviderMapper('CG1')
-    expect(mapProvider({ organisationId: 'org1', shares: 50 })).toEqual({
+    expect(mapProvider({ organisationId: 'org1', shares: 1 })).toEqual({
       coursecode: 'CG1',
       organizationcode: 'org1',
-      shares: 50,
+      shares: 1,
     })
   })
 })
