7a8e258b7c35b9b476b0d9f108079acb936688f4
joonas-a <joonas.ahovalli@gmail.com>
2026-09-22 13:31:30 +0300

    [Progress] Make infobox viewport-width-efficient
    and tsx

---
+++ services/frontend/src/components/PopulationComponents/Students/Table/ProgressTab/info.tsx
@@ -0,0 +1,50 @@
+import Alert from '@mui/material/Alert'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+
+import { CheckIcon, CloseIcon, RemoveIcon, SwapHorizIcon } from '@/theme'
+
+export const TableInfo = () => (
+  <Alert severity="info" sx={{ my: 2 }} variant="outlined">
+    <Box sx={{ display: 'grid', columnGap: 4, gridTemplateColumns: { xs: '1fr', md: '1fr 1fr' } }}>
+      <Box>
+        <Typography variant="h6">Criteria:</Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <CheckIcon color="success" />: Student has passed the course in the academic year.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <CheckIcon color="disabled" />: Student has passed the course outside of the corresponding academic year.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <SwapHorizIcon color="success" />: Student has credit transfer for the course.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <CloseIcon color="error" />: Student has failed the course.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <RemoveIcon color="disabled" />: Student has enrolled, but has not received any grade from the course.
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography variant="h6">Semester enrollments:</Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <span className="enrollment-label label-present" style={{ margin: 'auto 0' }} />: Student has an active
+          semester enrollment.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <span className="enrollment-label label-absent" style={{ margin: 'auto 0' }} />: Student has enrolled as
+          absent.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <span className="enrollment-label label-passive" style={{ margin: 'auto 0' }} />: Passive: Student did not
+          enroll at all.
+        </Typography>
+        <Typography sx={{ display: 'flex' }}>
+          <span className="enrollment-label label-none" style={{ margin: 'auto 0' }} />: Student has no enrollment, but
+          also no study right for the semester.
+        </Typography>
+      </Box>
+    </Box>
+  </Alert>
+)
