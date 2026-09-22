9e98fc1a6a598ac845cd11583c839d8de670305c
outisa <savolainen.outi@gmail.com>
2026-09-22 10:03:03 +0300

    [Updater meta]  Update relevant phd programmes

--- services/backend/src/services/studyProgramme/studyProgrammeHelpers.ts
+++ services/backend/src/services/studyProgramme/studyProgrammeHelpers.ts
@@ -312,7 +312,7 @@ export const getGoal = async (programme?: string) => {
   return ((minimumCredits / 60) * 12) / 6 // 60 Credits per year divided to semesters
 }
 
-export const isRelevantProgramme = (code: string) => /^(KH|MH)\d{2}_\d{3}$/.test(code) || /^T\d{6}$/.test(code)
+export const isRelevantProgramme = (code: string) => /^(KH|MH)\d{2}_\d{3}$/.test(code) || /^DP\d{3}$/.test(code)
 
 export const getStudyRightElementsWithPhase = (studyRight: Pick<SISStudyRight, 'studyRightElements'>, phase: Phase) => {
   return orderBy(
